41146d1caaccfbe86b1d348ed07de6b76da321fb
Kai Tietz <ktietz70@googlemail.com>
2007-08-20 14:30:24 +0000

    Add testcase for _sys_errlist.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@34 4407c894-4637-0410-b4f5-ada5f102cad1

---
+++ mingw-w64-crt/testcases/tstmain_sys_xxx.c
@@ -0,0 +1,16 @@
+/**
+ * This file has no copyright assigned and is placed in the Public Domain.
+ * This file is part of the w64 mingw-runtime package.
+ * No warranty is given; refer to the file DISCLAIMER within this package.
+ */
+#include <stdio.h>
+
+_CRTIMP char *_sys_errlist[];
+
+int main(int argc, char **argv)
+{
+  char **s = (char **) _sys_errlist;
+  if (s) printf ("Msg 0: %s\n", s[0]);
+  else printf ("Error list empty.\n");
+  return 0;
+}
