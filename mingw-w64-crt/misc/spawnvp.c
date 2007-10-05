01b4135b09ae2d52fd8a2b58f86dae816dc9eca4
Kai Tietz <ktietz70@googlemail.com>
2007-10-05 15:06:24 +0000

    Removed unwanted semicolon.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@103 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-crt/misc/spawnvp.c
+++ mingw-w64-crt/misc/spawnvp.c
@@ -1,6 +1,6 @@
 #include <process.h>
 
-intptr_t __cdecl spawnvp(int mode,const char *_Filename,char *const _ArgList[]);
+intptr_t __cdecl spawnvp(int mode,const char *_Filename,char *const _ArgList[])
 {
   return _spawnvp(mode, _Filename,(const char *const *)_ArgList);
 }
