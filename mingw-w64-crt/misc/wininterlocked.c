3fef2cef26736c4f2cd8a5793011dc9df9b8d9b4
Kai Tietz <ktietz70@googlemail.com>
2007-10-31 09:01:17 +0000

    Support 32-bit locks. 64 bit version in 32-bit mode currently disabled.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@166 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-crt/misc/wininterlocked.c
+++ mingw-w64-crt/misc/wininterlocked.c
@@ -63,6 +63,7 @@ BOOLEAN _interlockedbittestandreset(LONG *Base,LONG Offset)
   return (BOOLEAN) (old!=0);
 }
 
+#ifdef _WIN64
 BOOLEAN _bittest64(LONG64 const *Base,LONG64 Offset)
 {
   int old = 0;
@@ -116,7 +117,7 @@ BOOLEAN _interlockedbittestandreset64(LONG64 *Base,LONG64 Offset)
     :"Ir" (Offset));
   return (BOOLEAN) (old!=0);
 }
-
+#endif
 
 BOOLEAN _BitScanForward(DWORD *Index,DWORD Mask)
 {
@@ -130,6 +131,7 @@ BOOLEAN _BitScanReverse(DWORD *Index,DWORD Mask)
   return Mask!=0;
 }
 
+#ifdef _WIN64
 BOOLEAN _BitScanForward64(DWORD *Index,DWORD64 Mask)
 {
   __asm__ __volatile__("bsfq %1,%0" : "=r" (Mask),"=m" ((*(volatile long long *)Index)));
@@ -141,6 +143,7 @@ BOOLEAN _BitScanReverse64(DWORD *Index,DWORD64 Mask)
   __asm__ __volatile__("bsrq %1,%0" : "=r" (Mask),"=m" ((*(volatile long long *)Index)));
   return Mask!=0;
 }
+#endif
 
 SHORT InterlockedIncrement16(SHORT volatile *Addend)
 {
@@ -195,7 +198,8 @@ LONG InterlockedXor(LONG volatile *Destination,LONG Value)
     : : "r"(Value),"m"(*Destination) : "memory");
   return *Destination;
 }
-    //		$$$$
+
+#ifdef _WIN64
 LONG64 InterlockedAnd64(LONG64 volatile *Destination,LONG64 Value)
 {
   __asm__ __volatile__("lock ; andq %0,%1"
@@ -292,7 +296,9 @@ LONG64 InterlockedCompareExchange64(LONG64 volatile *Destination,LONG64 ExChange
   __asm__ __volatile__("lock ; cmpxchgq %1,%2" : "=a" (prev) : "q" (ExChange),"m" (*Destination), "0" (Comperand) : "memory");
   return prev;
 }
+#endif
 
+#ifdef _WIN64
 PVOID InterlockedCompareExchangePointer(PVOID volatile *Destination,PVOID ExChange,PVOID Comperand)
 {
   PVOID prev;
@@ -308,3 +314,4 @@ PVOID InterlockedExchangePointer(PVOID volatile *Target,PVOID Value)
     : "memory");
   return Value;
 }
+#endif
