4e1a6aa80526d78ad193d2eeeb0a8f3d7ee1085d
Kai Tietz <ktietz70@googlemail.com>
2007-10-31 09:03:20 +0000

    Add inline assembler for 32-bit mode. Additionally corrected subtract in 64-bit mode, too.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@167 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-crt/stdio/vfwscanf.c
+++ mingw-w64-crt/stdio/vfwscanf.c
@@ -14,11 +14,12 @@ int vfwscanf(FILE * __restrict__ stream, const wchar_t * __restrict__ format,
 
   int ret;
 
+#ifdef _WIN64
   __asm__(
 
     // allocate stack (esp += frame - arg3 - (8[arg1,2] + 12))
     "movq	%%rsp, %%r10\n\t"
-    "lea	0xFFFFFFD8(%%rsp, %6), %%rsp\n\t"
+    "lea	0xFFFFFFFFFFFFFFD8(%%rsp, %6), %%rsp\n\t"
     "subq	%5, %%rsp\n\t"
 
     // set up stack
@@ -42,6 +43,36 @@ int vfwscanf(FILE * __restrict__ stream, const wchar_t * __restrict__ format,
     : "1"(stream), "2"(format), "S"(arg),
       "a"(&ret)
     : "r10", "rdi");
+#else
+  __asm__(
+
+    // allocate stack (esp += frame - arg3 - (8[arg1,2] + 12))
+    "movl	%%esp, %%ebx\n\t"
+    "lea	0xFFFFFFEC(%%esp, %6), %%esp\n\t"
+    "subl	%5, %%esp\n\t"
+
+    // set up stack
+    "movl	%1, 0xC(%%esp)\n\t"  // stream
+    "movl	%2, 0x10(%%esp)\n\t"  // format
+    "lea	0x14(%%esp), %%edi\n\t"
+    "movl	%%edi, (%%esp)\n\t"  // memcpy dest
+    "movl	%5, 0x4(%%esp)\n\t"  // memcpy src
+    "movl	%5, 0x8(%%esp)\n\t"
+    "subl	%6, 0x8(%%esp)\n\t"  // memcpy len
+    "call	_memcpy\n\t"
+    "addl	$12, %%esp\n\t"
+
+    // call fscanf
+    "call	_fwscanf\n\t"
+
+    // restore stack
+    "movl	%%ebx, %%esp\n\t"
+
+    : "=a"(ret), "=c"(stream), "=d"(format)
+    : "1"(stream), "2"(format), "S"(arg),
+      "a"(&ret)
+    : "ebx", "edi");
+#endif
 
   return ret;
 }
