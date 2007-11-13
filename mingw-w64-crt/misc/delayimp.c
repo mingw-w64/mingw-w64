e4cdff7592ec00425df55238308b1a3934ebb5e8
Kai Tietz <ktietz70@googlemail.com>
2007-11-13 10:39:44 +0000

    Add note about derived work source.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@195 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-crt/misc/delayimp.c
+++ mingw-w64-crt/misc/delayimp.c
@@ -2,6 +2,9 @@
  * This file has no copyright assigned and is placed in the Public Domain.
  * This file is part of the w64 mingw-runtime package.
  * No warranty is given; refer to the file DISCLAIMER within this package.
+ *
+ * This file is derived from Microsoft implementation file delayhlp.cpp, which
+ * is free for users to modify and derive.
  */
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
