8c16df267e9ca00fb8b1afccb8789d02091fe80e
Kai Tietz <ktietz70@googlemail.com>
2007-11-19 15:10:12 +0000

    Add wide char variants of _findnext/_findfirst ...
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@201 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-crt/stdio/_findfirst64i32.c
+++ mingw-w64-crt/stdio/_findfirst64i32.c
@@ -13,3 +13,16 @@ intptr_t __cdecl _findfirst64i32(const char *_Filename,struct _finddata64i32_t *
   strncpy(_FindData->name,fd.name,260);
   return ret;
 }
+
+intptr_t __cdecl _wfindfirst64i32(const wchar_t *_Filename,struct _wfinddata64i32_t *_FindData)
+{
+  struct _wfinddata64_t fd;
+  intptr_t ret = _findfirst64(_Filename,&fd);
+  _FindData->attrib=fd.attrib;
+  _FindData->time_create=fd.time_create;
+  _FindData->time_access=fd.time_access;
+  _FindData->time_write=fd.time_write;
+  _FindData->size=(_fsize_t) fd.size;
+  memcpy(_FindData->name,fd.name,260*sizeof(wchar_t));
+  return ret;
+}
--- mingw-w64-crt/stdio/_findnext64i32.c
+++ mingw-w64-crt/stdio/_findnext64i32.c
@@ -13,3 +13,16 @@ int __cdecl _findnext64i32(intptr_t _FindHandle,struct _finddata64i32_t *_FindDa
   strncpy(_FindData->name,fd.name,260);
   return ret;
 }
+
+int __cdecl _wfindnext64i32(intptr_t _FindHandle,struct _wfinddata64i32_t *_FindData)
+{
+  struct _wfinddata64_t fd;
+  int ret = _wfindnext64(_FindHandle,&fd);
+  _FindData->attrib=fd.attrib;
+  _FindData->time_create=fd.time_create;
+  _FindData->time_access=fd.time_access;
+  _FindData->time_write=fd.time_write;
+  _FindData->size=(_fsize_t) fd.size;
+  memcpy(_FindData->name,fd.name,260*sizeof(wchar_t));
+  return ret;
+}
