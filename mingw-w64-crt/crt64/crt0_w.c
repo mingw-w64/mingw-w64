f4ab9937846891cc5d0ca8a1a614458a13c1b098
Kai Tietz <ktietz70@googlemail.com>
2007-12-17 09:57:08 +0000

    Remove warning about wrong definition of 3rd parameter of main.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@225 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-crt/crt64/crt0_w.c
+++ mingw-w64-crt/crt64/crt0_w.c
@@ -1,6 +1,6 @@
 #include <windows.h>
 
-int wmain (int flags, wchar_t **cmdline, wchar_t *inst)
+int wmain (int flags, wchar_t **cmdline, wchar_t **inst)
 {
   return (int) wWinMain ((HINSTANCE) inst, NULL, (LPWSTR) cmdline,(DWORD) flags);
 }
