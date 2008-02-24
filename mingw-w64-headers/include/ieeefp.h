ca4e20843fc338a637334b04a05131d6cc0544ef
NightStrike <nightstrike@gmail.com>
2008-02-24 09:56:11 +0000

    Clean up file
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@288 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/ieeefp.h
+++ mingw-w64-headers/include/ieeefp.h
@@ -1,8 +1,9 @@
-#ifndef _IEEE_FP_H_
-#define _IEEE_FP_H_
+#ifndef _IEEEFP_H_
+#define _IEEEFP_H_
 
-#include "_mingw.h"
-#include "math.h"
-#include "ansidecl.h"
+#include <_mingw.h>
+#include <math.h>
+#include <ansidecl.h>
 
 #endif /* _IEEE_FP_H_ */
+
