23ebf79fbc7753f920936feb908612d1c4b5d6b6
Kai Tietz <ktietz70@googlemail.com>
2008-03-21 20:29:46 +0000

    Correct typing of __cdecl declaration of the vsn*printf family.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@309 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-crt/stdio/snprintf.c
+++ mingw-w64-crt/stdio/snprintf.c
@@ -6,13 +6,13 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-int snprintf(char* buffer, size_t n, const char* format, ...)
+int __cdecl snprintf(char* buffer, size_t n, const char *format, ...)
 {
   int retval;
   va_list argptr;
          
-  va_start( argptr, format );
-  retval = _vsnprintf( buffer, n, format, argptr );
-  va_end( argptr );
+  va_start(argptr, format);
+  retval = _vsnprintf (buffer, n, format, argptr);
+  va_end(argptr);
   return retval;
 }
--- mingw-w64-crt/stdio/snwprintf.c
+++ mingw-w64-crt/stdio/snwprintf.c
@@ -6,13 +6,13 @@
 #include <stdarg.h>
 #include <wchar.h>
 
-int  snwprintf(wchar_t* buffer, size_t n, const wchar_t* format, ...)
+int __cdecl snwprintf(wchar_t *buffer, size_t n, const wchar_t *format, ...)
 {
     int retval;
     va_list argptr;
           
-    va_start( argptr, format );
-    retval = _vsnwprintf( buffer, n, format, argptr );
-    va_end( argptr );
+    va_start(argptr, format);
+    retval = _vsnwprintf(buffer, n, format, argptr);
+    va_end(argptr);
     return retval;
 }
