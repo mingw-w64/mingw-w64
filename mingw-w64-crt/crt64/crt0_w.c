c24294c62fba2600b773342b29f7957d60f4014f
Kai Tietz <ktietz70@googlemail.com>
2008-04-29 19:25:57 +0000

    Fix for wWinMain.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@389 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-crt/crt64/crt0_w.c
+++ mingw-w64-crt/crt64/crt0_w.c
@@ -1,5 +1,7 @@
 #include <windows.h>
 
+int WINAPI wWinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPWSTR lpCmdLine,int nShowCmd);
+
 int wmain (int flags, wchar_t **cmdline, wchar_t **inst)
 {
   return (int) wWinMain ((HINSTANCE) inst, NULL, (LPWSTR) cmdline,(DWORD) flags);
