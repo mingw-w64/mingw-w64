86c40e7b8cede1cb37202ad22b5825fdb1f9b5c8
Kai Tietz <ktietz70@googlemail.com>
2008-07-26 16:14:10 +0000

    2008-07-26  Kai Tietz  <kai.tietz@onevision.com>
            * process.h (exec...): Adjust prototypes to return type
            int if used in gcc.
    
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@447 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/process.h
+++ mingw-w64-headers/include/process.h
@@ -141,10 +141,21 @@ extern "C" {
   intptr_t __cdecl spawnlp(int,const char *_Filename,const char *_ArgList,...);
   intptr_t __cdecl spawnlpe(int,const char *_Filename,const char *_ArgList,...);
   int __cdecl getpid(void);
+#ifdef __GNUC__
+  /* Those methods are predefined by gcc builtins to return int. So to prevent
+     stupid warnings, define them in POSIX way.  This is save, because those
+     methods do not return in success case, so that the return value is not
+     really dependent to its scalar width.  */
+  int __cdecl execv(const char *_Filename,char *const _ArgList[]);
+  int __cdecl execve(const char *_Filename,char *const _ArgList[],char *const _Env[]);
+  int __cdecl execvp(const char *_Filename,char *const _ArgList[]);
+  int __cdecl execvpe(const char *_Filename,char *const _ArgList[],char *const _Env[]);
+#else
   intptr_t __cdecl execv(const char *_Filename,char *const _ArgList[]);
   intptr_t __cdecl execve(const char *_Filename,char *const _ArgList[],char *const _Env[]);
   intptr_t __cdecl execvp(const char *_Filename,char *const _ArgList[]);
   intptr_t __cdecl execvpe(const char *_Filename,char *const _ArgList[],char *const _Env[]);
+#endif
   intptr_t __cdecl spawnv(int,const char *_Filename,char *const _ArgList[]);
   intptr_t __cdecl spawnve(int,const char *_Filename,char *const _ArgList[],char *const _Env[]);
   intptr_t __cdecl spawnvp(int,const char *_Filename,char *const _ArgList[]);
