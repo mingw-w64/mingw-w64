48dc62bef4f3bce52f618a82096545e3825139a1
Kai Tietz <ktietz70@googlemail.com>
2008-07-30 20:57:56 +0000

    Correct 32-bit sys/stat.h.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@453 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/sys/stat.h
+++ mingw-w64-headers/include/sys/stat.h
@@ -63,6 +63,28 @@ extern "C" {
 
 #ifndef _STAT_DEFINED
 
+#ifdef _USE_32BIT_TIME_T
+#ifndef _WIN64
+#define _fstat32 _fstat
+#define _stat32 _stat
+#define _wstat32 _wstat
+#else
+#define _fstat _fstat32
+#define _stat _stat32
+#define _wstat _wstat32
+#endif
+#define _fstati64 _fstat32i64
+#define _stati64 _stat32i64
+#define _wstati64 _wstat32i64
+#else
+#define _fstat _fstat64i32
+#define _fstati64 _fstat64
+#define _stat _stat64i32
+#define _stati64 _stat64
+#define _wstat _wstat64i32
+#define _wstati64 _wstat64
+#endif
+
   struct _stat32 {
     _dev_t st_dev;
     _ino_t st_ino;
@@ -138,22 +160,6 @@ extern "C" {
 #endif
 
 #define __stat64 _stat64
-
-#ifdef _USE_32BIT_TIME_T
-#define _fstat _fstat32
-#define _fstati64 _fstat32i64
-#define _stat _stat32
-#define _stati64 _stat32i64
-#define _wstat _wstat32
-#define _wstati64 _wstat32i64
-#else
-#define _fstat _fstat64i32
-#define _fstati64 _fstat64
-#define _stat _stat64i32
-#define _stati64 _stat64
-#define _wstat _wstat64i32
-#define _wstati64 _wstat64
-#endif
 
 #define _STAT_DEFINED
 #endif
