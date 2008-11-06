a339cd1aca6b43bd0f324cdb7cc7b2d0d6649328
Kai Tietz <ktietz70@googlemail.com>
2008-11-06 09:09:56 +0000

    Adjust struct _PEB accordingly to msdn documentation. Add new structures PEB_LDR_DATA, LDR_DATA_TABLE_ENTRY, and RTL_USER_PROCESS_PARAMETERS.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@488 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/winternl.h
+++ mingw-w64-headers/include/winternl.h
@@ -12,11 +12,57 @@
 extern "C" {
 #endif
 
+  typedef struct _UNICODE_STRING {
+    USHORT Length;
+    USHORT MaximumLength;
+    PWSTR Buffer;
+  } UNICODE_STRING;
+
+  typedef struct _PEB_LDR_DATA {
+    BYTE Reserved1[8];
+    PVOID Reserved2[3];
+    LIST_ENTRY InMemoryOrderModuleList;
+  } PEB_LDR_DATA,*PPEB_LDR_DATA;
+ 
+  typedef struct _LDR_DATA_TABLE_ENTRY {
+    BYTE Reserved1[2];
+    LIST_ENTRY InMemoryOrderLinks;
+    PVOID Reserved2[2];
+    PVOID DllBase;
+    PVOID Reserved3[2];
+    UNICODE_STRING FullDllName;
+    BYTE Reserved4[8];
+    PVOID Reserved5[3];
+    union {
+      ULONG CheckSum;
+      PVOID Reserved6;
+    };
+    ULONG TimeDateStamp;
+  } LDR_DATA_TABLE_ENTRY,*PLDR_DATA_TABLE_ENTRY;
+
+typedef struct _RTL_USER_PROCESS_PARAMETERS {
+    BYTE Reserved1[16];
+    PVOID Reserved2[10];
+    UNICODE_STRING ImagePathName;
+    UNICODE_STRING CommandLine;
+  } RTL_USER_PROCESS_PARAMETERS,*PRTL_USER_PROCESS_PARAMETERS;
+  
+  /* This function pointer is undocumented and just valid for windows 2000.
+     Therefore I guess.  */
+  typedef VOID (WINAPI *PPS_POST_PROCESS_INIT_ROUTINE)(VOID);
+  
   typedef struct _PEB {
     BYTE Reserved1[2];
     BYTE BeingDebugged;
-    BYTE Reserved2[229];
-    PVOID Reserved3[59];
+    BYTE Reserved2[1];
+    PVOID Reserved3[2];
+    PPEB_LDR_DATA Ldr;
+    PRTL_USER_PROCESS_PARAMETERS ProcessParameters;
+    BYTE Reserved4[104];
+    PVOID Reserved5[52];
+    PPS_POST_PROCESS_INIT_ROUTINE PostProcessInitRoutine;
+    BYTE Reserved6[128];
+    PVOID Reserved7[1];
     ULONG SessionId;
   } PEB,*PPEB;
 
@@ -48,12 +94,6 @@ extern "C" {
   typedef STRING OEM_STRING;
   typedef PSTRING POEM_STRING;
   typedef CONST STRING *PCOEM_STRING;
-
-  typedef struct _UNICODE_STRING {
-    USHORT Length;
-    USHORT MaximumLength;
-    PWSTR Buffer;
-  } UNICODE_STRING;
 
   typedef UNICODE_STRING *PUNICODE_STRING;
   typedef const UNICODE_STRING *PCUNICODE_STRING;
