9412947c44288817cb4867e785c414ae4b287250
Kai Tietz <ktietz70@googlemail.com>
2008-11-17 08:37:50 +0000

    Add for wmain the gcc call to __nain();
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@501 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-crt/crt64/crt0_w.c
+++ mingw-w64-crt/crt64/crt0_w.c
@@ -1,8 +1,15 @@
 #include <windows.h>
 
+extern void __main(void);
+
 int WINAPI wWinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPWSTR lpCmdLine,int nShowCmd);
 
 int wmain (int flags, wchar_t **cmdline, wchar_t **inst)
 {
+#ifdef __GNUC__
+  /* C++ initialization.
+     gcc inserts this call automatically for a function called main , but not for wmain.  */
+  __main();
+#endif
   return (int) wWinMain ((HINSTANCE) inst, NULL, (LPWSTR) cmdline,(DWORD) flags);
 }
