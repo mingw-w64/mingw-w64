78303e9ec5bc28001c10fdb0ecd7ce7f3ad41ceb
Kai Tietz <ktietz70@googlemail.com>
2009-01-27 10:57:50 +0000

    Add initial comments.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@571 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-crt/include/sect_attribs.h
+++ mingw-w64-crt/include/sect_attribs.h
@@ -1,3 +1,9 @@
+/**
+ * This file has no copyright assigned and is placed in the Public Domain.
+ * This file is part of the w64 mingw-runtime package.
+ * No warranty is given; refer to the file DISCLAIMER within this package.
+ */
+
 #if defined (__ia64__) || defined (__x86_64)
 #define _ATTRIBUTES
 #else
