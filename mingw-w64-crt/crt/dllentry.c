7e6b1380c87f5badd89da8c5a65ded87479ee251
Kai Tietz <ktietz70@googlemail.com>
2009-01-27 10:55:56 +0000

    Add comment.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@570 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-crt/crt/dllentry.c
+++ mingw-w64-crt/crt/dllentry.c
@@ -1,3 +1,9 @@
+/**
+ * This file has no copyright assigned and is placed in the Public Domain.
+ * This file is part of the w64 mingw-runtime package.
+ * No warranty is given; refer to the file DISCLAIMER within this package.
+ */
+
 #include <oscalls.h>
 #define _DECL_DLLMAIN
 #include <process.h>
