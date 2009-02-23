58edc3013a7300b54f1f4b331747719cba7ff208
Kai Tietz <ktietz70@googlemail.com>
2009-02-23 13:33:10 +0000

    Add __mingw variants of _set/get_output_format.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@645 4407c894-4637-0410-b4f5-ada5f102cad1

---
+++ mingw-w64-crt/misc/mingw_output_format.c
@@ -0,0 +1,60 @@
+#include <windows.h>
+#include <stdio.h>
+
+static unsigned int last_value = 0;
+typedef unsigned int (*f_get_output_format)(void);
+typedef unsigned int (*f_set_output_format)(unsigned int);
+
+static int call_set_output_format(unsigned int _Format)
+{
+  f_set_output_format sof;
+  HMODULE lib = LoadLibrary ("msvcrt.dll");
+  if (!lib)
+    return 0;
+  sof = (f_set_output_format) GetProcAddress (lib, "_set_output_format");
+  if (sof)
+    {
+      last_value = (*sof)(_Format);
+    }
+  FreeLibrary(lib);
+  if (!sof) return 0;
+  return 1;
+}
+
+static int call_get_output_format(void)
+{
+  f_get_output_format sof;
+  HMODULE lib = LoadLibrary ("msvcrt.dll");
+  if (!lib)
+    return 0;
+  sof = (f_get_output_format) GetProcAddress (lib, "_get_output_format");
+  if (sof)
+    {
+      last_value = (*sof)();
+    }
+  FreeLibrary(lib);
+  if (!sof) return 0;
+  return 1;
+}  
+
+unsigned int __cdecl __mingw_set_output_format(unsigned int _Format)
+{
+  static int probe = 1;
+  unsigned int l = last_value;
+  if (probe)
+    probe = call_set_output_format (_Format);
+  if (!probe)
+    {
+      last_value = _Format;
+    }
+  return l;
+}
+
+unsigned int __cdecl __mingw_get_output_format(void)
+{
+  static probe = 1;
+  if (probe)
+    probe = call_get_output_format ();
+  return last_value;
+}
+
