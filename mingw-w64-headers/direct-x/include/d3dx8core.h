1bfcc3e956b546bcb20ebb86d4f33a9ffa223b68
Ozkan Sezer <sezeroz@gmail.com>
2009-04-04 16:11:41 +0000

    2009-04-04  Ozkan Sezer <sezeroz@gmail.com>
    	* d3dx8core.h: Make C++ compatible (wine git 
    		   91d3a794e8b756dbcb7047c77acf1a7f862627dd),
    	remove some extraneous #include directives (wine git 
    		   7e34c708c1b3bdbb7d4921c1153403a8d8840b29),
    	and fix D3DXAssembleShaderFromFileA/W declarations 
    	 (wine git 07cb7c6626292ca508dcd06541110ebb450e0a36).
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@715 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/direct-x/include/d3dx8core.h
+++ mingw-w64-headers/direct-x/include/d3dx8core.h
@@ -19,11 +19,7 @@
 #ifndef __WINE_D3DX8CORE_H
 #define __WINE_D3DX8CORE_H
 
-#include <objbase.h>
-
 #include <d3d8.h>
-#include <d3d8types.h>
-#include <d3d8caps.h>
 
 /*****************************************************************************
  * #defines and error codes
@@ -109,6 +105,10 @@ DECLARE_INTERFACE_(ID3DXFont,IUnknown)
 /*************************************************************************************
  * Define entrypoints 
  */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 HRESULT WINAPI D3DXCreateBuffer(DWORD NumBytes, LPD3DXBUFFER* ppBuffer);
 HRESULT WINAPI D3DXCreateFont(LPDIRECT3DDEVICE8 pDevice, HFONT hFont, LPD3DXFONT* ppFont);
 UINT WINAPI D3DXGetFVFVertexSize(DWORD FVF);
@@ -116,13 +116,17 @@ HRESULT WINAPI D3DXAssembleShader(LPCVOID pSrcData, UINT SrcDataLen, DWORD Flags
 			   LPD3DXBUFFER* ppConstants, 
 			   LPD3DXBUFFER* ppCompiledShader,
 			   LPD3DXBUFFER* ppCompilationErrors);
-HRESULT WINAPI D3DXAssembleShaderFromFileA(LPSTR pSrcFile, DWORD Flags,
+HRESULT WINAPI D3DXAssembleShaderFromFileA(LPCSTR pSrcFile, DWORD Flags,
 				    LPD3DXBUFFER* ppConstants,
 				    LPD3DXBUFFER* ppCompiledShader,
 				    LPD3DXBUFFER* ppCompilationErrors);
-HRESULT WINAPI D3DXAssembleShaderFromFileW(LPSTR pSrcFile, DWORD Flags,
+HRESULT WINAPI D3DXAssembleShaderFromFileW(LPWSTR pSrcFile, DWORD Flags,
 				    LPD3DXBUFFER* ppConstants,
 				    LPD3DXBUFFER* ppCompiledShader,
 				    LPD3DXBUFFER* ppCompilationErrors);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* __WINE_D3DX8CORE_H */
