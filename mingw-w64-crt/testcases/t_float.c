197812fa835f8e7b0c50468cccd8195fc756ebaf
Kai Tietz <ktietz70@googlemail.com>
2009-04-11 07:05:58 +0000

    Update float testcase.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@749 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-crt/testcases/t_float.c
+++ mingw-w64-crt/testcases/t_float.c
@@ -1,6 +1,9 @@
 #include <math.h>
 #include <stdio.h>
 
+volatile long double in1 = -1.0L / 0.0L;
+volatile long double in2 = 1.0L / 0.0L;
+
 volatile long double ld1 = 3.51L;
 volatile double ld2 = 3.51;
 volatile float ld3 = 3.51F;
@@ -17,6 +20,28 @@ volatile float ld3 = 3.51F;
     (double) NAME##f (ld3, VAL##f)); \
 } while (0)
 
+#define ARG3_FCT(NAME,VAL,VAL2) \
+  do {printf (#NAME ": %g %g %g\n", (double) NAME##l (ld1, VAL##L, VAL2##L), \
+    (double) NAME (ld2, VAL, VAL2), \
+    (double) NAME##f (ld3, VAL##f, VAL2##f)); \
+} while (0)
+
+#define ARG2_FCTINT(NAME) \
+  do {printf (#NAME ": %g %g %g\n", (double) NAME##l (ld1, 2), \
+    (double) NAME (ld2, 2), \
+    (double) NAME##f (ld3, 2)); \
+} while (0)
+
+#define ARG2_FCTPINT(NAME) \
+  do { \
+   int i = 0; \
+   printf (#NAME ": %g", (double) NAME##l (ld1, &i)); \
+   printf ("=%d", i); \
+   printf (" %g", (double) NAME (ld2, &i)); \
+   printf ("=%d", i); \
+   printf (" %g", (double) NAME##f (ld3, &i)); \
+   printf ("=%d\n", i); \
+  } while(0)
 
 int main()
 {
@@ -34,6 +59,7 @@ int main()
   ARG1_FCT (atan);
   ARG2_FCT (atan2, 0.5);
   ARG1_FCT (exp);
+  ARG1_FCT (exp2);
   ARG2_FCT (pow, 2.0);
   ARG1_FCT (sqrt);
   ARG1_FCT (ceil);
@@ -64,10 +90,20 @@ int main()
   ARG2_FCT (remainder, 2.0);
   ARG2_FCT (copysign, -1.0);
   ARG2_FCT (nexttoward, 0.5);
+  ARG2_FCT (nexttoward, 0.5);
   ARG2_FCT (fdim, 0.5);
   ARG2_FCT (fmax, 0.5);
   ARG2_FCT (fmin, 0.5);
   ARG2_FCT (hypot, 1.5);
+  ARG1_FCT (cbrt);
+  ARG2_FCTPINT (frexp);
+  ARG2_FCTINT (ldexp);
+  ARG1_FCT (ilogb);
+  ARG2_FCTINT (scalbn);
+  ARG2_FCTINT (scalbln);
+  ARG3_FCT (fma, 2.0, 1.5);
+  printf ("%g is%s nan\n", (double) in1, isnan (in1) ? "" :" not");
+  printf ("%g is%s inf\n", (double) in1, isinf (in1) ? "" :" not");
   __mingw_printf ("Ld:%lg d:%g f:%g\n", ld1, ld2, (double) ld3);
 
   return 0;
