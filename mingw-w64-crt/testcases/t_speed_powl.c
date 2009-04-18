fccf253cdf577a8d2f93d0da30c67354822da07f
Kai Tietz <ktietz70@googlemail.com>
2009-04-18 11:33:27 +0000

    Fix typo in print and optimize speed measurement.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@806 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-crt/testcases/t_speed_powl.c
+++ mingw-w64-crt/testcases/t_speed_powl.c
@@ -4,17 +4,16 @@
 
 int main()
 {
-  DWORD start_ticks = GetTickCount();
+  DWORD start_ticks;
   int i;
   long double ld, sum;
   sum = 0.0L;
   ld = 1.0L;
-
+  start_ticks = GetTickCount();
   for (i = 0;i < 0x1fffff ; i++) { sum += powl (ld, 0.5L); ld+=0.03; }
-
-  __mingw_printf ("Loop %d times with result %Lg\n", i, ld);
   start_ticks = GetTickCount() - start_ticks;
-  printf ("Used aabout %u ms\n", start_ticks);
+  __mingw_printf ("Loop %d times with result %Lg\n", i, ld);
+  printf ("Used about %u ms\n", start_ticks);
   return 0;
 }
 
