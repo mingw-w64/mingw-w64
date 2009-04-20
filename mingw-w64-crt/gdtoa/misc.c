4562a24fee50e0933da8ca23f65cb13c05868058
Ozkan Sezer <sezeroz@gmail.com>
2009-04-20 13:15:20 +0000

    2009-04-20  Ozkan Sezer  <sezeroz@gmail.com>
    	* gdtoa/misc.c: Sync'ed with the netlib.org sources from April 20, 2009.
    	The author's changelog states: "do not attempt to allocate large memory
    	blocks from the private memory pool (which was an unlikely event, but a
    	bug)."
    	* gdtoa/strtopx.c: Sync'ed with the netlib.org sources from April 20, 2009.
    	The author's changelog states: supply explicit bit for Infinity. "Note
    	that the Q routines (which do not supply this bit) are appropriate for
    	Sparc quad precision (probably known as long double with most current
    	compilers)."
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@810 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-crt/gdtoa/misc.c
+++ mingw-w64-crt/gdtoa/misc.c
@@ -138,7 +138,7 @@ Bigint *Balloc (int k)
 #else
 		len = (sizeof(Bigint) + (x-1)*sizeof(ULong) + sizeof(double) - 1)
 			/sizeof(double);
-		if (pmem_next - private_mem + len <= PRIVATE_mem) {
+		if (k <= Kmax && pmem_next - private_mem + len <= PRIVATE_mem) {
 			rv = (Bigint*)pmem_next;
 			pmem_next += len;
 		}
