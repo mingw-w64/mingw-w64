1ba364a6397e65c70622c14da5d6944c7593f669
Jonathan Yong <10walls@gmail.com>
2009-05-15 05:38:53 +0000

    Remove non-working testsuite.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@854 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-testsuite/common.mk
+++
@@ -1,4 +0,0 @@
-CC = i686-pc-mingw32-gcc
-X_OBJS = t_float t_matherr t_setjmp tstmain tstwassert
-TEST_ROOT = ../mingw-w64-crt/testcases
-C_OBJS = $(TEST_ROOT)/t_float.c $(TEST_ROOT)/t_matherr.c $(TEST_ROOT)/t_setjmp.c $(TEST_ROOT)/tstmain.c $(TEST_ROOT)/tstwassert.c
--- mingw-w64-testsuite/lc_test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh 
-C_OBJS=$1
-
-for i in ${C_OBJS}; do $i>./mingw.org/$i; done
-for i in ${C_OBJS}; do diff -u expected/$i ./mingw.org/$i; done
