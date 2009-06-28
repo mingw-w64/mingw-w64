fcabe2f31f34b6beb4a196932d9138e460215361
Ozkan Sezer <sezeroz@gmail.com>
2009-06-28 10:44:26 +0000

    much smaller TLS testcase (t_tls1.c). removed the useless second one (t_tls2.c).
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@986 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-crt/testcases/t_tls1.c
+++ mingw-w64-crt/testcases/t_tls1.c
@@ -1,110 +1,43 @@
+/*
+ need gcc-4.3.4 (gcc-4_3-branch r149015)
+   or gcc-4.4.1 (gcc-4_4-branch r149016)
+   or gcc-4.5.x (trunk r148061) or newer
+ which have a properly fixed gcc/emutls.c,
+ along with mingw-w64-headers r960 and
+ mingw-w64-crt r973.
+*/
+
 #include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
 
-typedef double	vec_t;
-typedef vec_t	vec3_t[3];
-#undef true
-#undef false
-#if defined(__cplusplus)
-typedef int	qboolean;
+#if defined(_MSC_VER) /* MS Visual Studio */
+#define __threadlocal__ __declspec(thread)
+#elif defined(__GNUC__) && ((__GNUC__ > 4) \
+  || (__GNUC__ == 4 && __GNUC_MINOR__ >= 3))
+/* gcc >= 4.3, also needs binutils >= 2.19 */
+#define __threadlocal__ __thread
 #else
-typedef enum {
-	false = 0,
-	true  = 1
-} qboolean;
+#error No keyword for TLS vars is defined.
 #endif
-typedef struct
+
+__threadlocal__ int tvar = 0;
+
+int main (int argc, char **argv)
 {
-	qboolean	original;       // don't free, it's part of the portal
-	qboolean	fixedsize;      // allocated to 16;
-	int	numpoints;
-	vec3_t	points[8];              // variable sized
-} winding_t;
-
-#define MAX_POINTS_ON_WINDING	64
-#define MAX_FREE_WINDINGS	(10000)
-#define MAX_FAST_ALLOC_WINDINGS	(16)
-
-int GilMode = 1;
-
-static winding_t *OldNewWinding (int points)
-{
-	winding_t	*w;
-	size_t			size;
-
-//	if (points > MAX_POINTS_ON_WINDING)
-//		Error ("%s: %i points", __thisfunc__, points);
-
-	size = (size_t)((winding_t *)0)->points[points];
-	w = (winding_t *) malloc (size);
-//	memset (w, 0, size);
-	w->fixedsize = 0;
-	w->original = 0;
-	w->numpoints = 0;
-
-	return w;
-}
-
-static void OldFreeWinding (winding_t *w)
-{
-	if (!w->original)
-		free (w);
-}
-
-static __thread int nFreeWindings = 0;
-static __thread winding_t *FreeWindings[MAX_FREE_WINDINGS];
-
-winding_t *NewWinding (int points)
-{
-	winding_t	*ret;
-
-	if (points > MAX_FAST_ALLOC_WINDINGS || !GilMode)
-	{
-		ret = OldNewWinding(points);
-		ret->fixedsize = 0;
-	}
-	else if (nFreeWindings)
-		ret = FreeWindings[--nFreeWindings];
-	else
-	{
-		ret = OldNewWinding(MAX_FAST_ALLOC_WINDINGS);
-		ret->fixedsize = 1;
-	}
-	ret->original = 0;
-	ret->numpoints = 0;
-	return ret;
-}
-
-void FreeWinding (winding_t *w)
-{
-	if (!w->original)
-	{
-		if (nFreeWindings >= MAX_FREE_WINDINGS || !w->fixedsize)
-			OldFreeWinding(w);
-		else
-			FreeWindings[nFreeWindings++] = w;
-	}
-}
-
-int main ()
-{
-	int i;
-	winding_t	*ww;
-	puts("Alloc for 5 points..");
-	ww = NewWinding(5);
-	puts("   .. done.");
-	puts("Alloc for 8 points..");
-	ww = NewWinding(8);
-	puts("   .. done.");
-	puts("Alloc for 32 points..");
-	ww = NewWinding(32);
-	puts("   .. done.");
-	puts("Alloc for 2 points..");
-	ww = NewWinding(2);
-	puts("   .. done.");
-	puts("Exiting....");
-	exit (0);
+  (void)argv;
+  if (argc != 1)
+    {
+      printf("not referencing tvar\n");
+    }
+  else
+    {
+      printf("incrementing tvar..\n");
+      ++tvar;
+      printf("   .. done (%i)\n", tvar);
+    }
+  printf("exiting....\n");
+  exit (0);
 }
 
