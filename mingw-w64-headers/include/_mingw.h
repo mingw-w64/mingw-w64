64ef4fb42b1cfd544b85a45bbae0900ffb70add4
Kai Tietz <ktietz70@googlemail.com>
2009-07-17 17:00:14 +0000

    2009-07-17  Kai Tietz  <kai.tietz@onevision.com>
            * _mingw_mac.h: New file. It defines just macros also useful in
            assembly file. Moved some part from _mingw.h into it.
            (__MINGW_USYMBOL): New macro for assembly to append underscore if necessary.
    
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@1031 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/_mingw.h
+++ mingw-w64-headers/include/_mingw.h
@@ -7,20 +7,7 @@
 #ifndef _INC_CRTDEFS
 #define _INC_CRTDEFS
 
-#ifndef __MINGW64
-#define __MINGW64
-#define __MINGW64_VERSION	"1.0"
-#define __MINGW64_VERSION_MAJOR	1
-#define __MINGW64_VERSION_MINOR	0
-#define __MINGW64_VERSION_STATE	"beta"
-
-/* mingw.org's version macros: these make gcc to define
-   MINGW32_SUPPORTS_MT_EH and to use the _CRT_MT global
-   and the __mingwthr_key_dtor() function from the MinGW
-   CRT in its private gthr-win32.h header. */
-#define __MINGW32_MAJOR_VERSION 3
-#define __MINGW32_MINOR_VERSION 11
-#endif
+#include "_mingw_mac.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -30,57 +17,7 @@ const char *__mingw_get_crt_info (void);
 }
 #endif
 
-#ifdef _WIN64
-/* MS does not prefix symbols by underscores for 64-bit.  */
-#ifndef __MINGW_USE_UNDERSCORE_PREFIX
-#define __MINGW_USE_UNDERSCORE_PREFIX 1
-#endif
-#else
-/* For 32-bits we have always to prefix by underscore.  */
-#undef __MINGW_USE_UNDERSCORE_PREFIX
-#define __MINGW_USE_UNDERSCORE_PREFIX 1
-#endif
-
-#if __MINGW_USE_UNDERSCORE_PREFIX == 0
-#define __MINGW_IMP_SYMBOL(sym)	_imp_##sym
-#else
-#define __MINGW_IMP_SYMBOL(sym)	_imp__##sym
-#endif
-
-/* Use alias for msvcr80 export of get/set_output_format.  */
-#ifndef __USE_MINGW_OUTPUT_FORMAT_EMU
-#define __USE_MINGW_OUTPUT_FORMAT_EMU 1
-#endif
-
-/* Set VC specific compiler target macros.  */
-#if defined(__x86_64) && defined(_X86_)
-#undef _X86_	/* _X86_ is not for __x86_64 */
-#endif
-
-#if defined(_X86_) && !defined(_M_IX86) && !defined(_M_IA64) \
-   && !defined(_M_AMD64) && !defined(__x86_64)
-#if defined(__i486__)
-#define _M_IX86 400
-#elif defined(__i586__)
-#define _M_IX86 500
-#else
-/* This gives wrong (600 instead of 300) value if -march=i386 is specified
-   but we cannot check for__i386__ as it is defined for all 32-bit CPUs. */
-#define _M_IX86 600
-#endif
-#endif
-
-#if defined(__x86_64) && !defined(_M_IX86) && !defined(_M_IA64) \
-   && !defined(_M_AMD64)
-#define _M_AMD64 100
-#define _M_X64 100
-#endif
-
-#if defined(__ia64__) && !defined(_M_IX86) && !defined(_M_IA64) \
-   && !defined(_M_AMD64) && !defined(_X86_) && !defined(__x86_64)
-#define _M_IA64 100
-#endif
-
+/* C/C++ specific language defines.  */
 #ifdef _WIN64
 #ifdef __stdcall
 #undef __stdcall
@@ -584,14 +521,6 @@ extern "C" {
  */
 #undef  _CRT_glob
 #define _CRT_glob _dowildcard
-
-#ifdef UNICODE
-# define __MINGW_NAME_AW(func) func##W
-#else
-# define __MINGW_NAME_AW(func) func##A
-#endif
-#define __MINGW_TYPEDEF_AW(type) \
-    typedef __MINGW_NAME_AW(type) type;
 
 #ifndef DUMMYUNIONNAME
 # ifdef NONAMELESSUNION
---
+++ mingw-w64-headers/include/_mingw_mac.h
@@ -0,0 +1,86 @@
+/**
+ * This file has no copyright assigned and is placed in the Public Domain.
+ * This file is part of the w64 mingw-runtime package.
+ * No warranty is given; refer to the file DISCLAIMER within this package.
+ */
+
+#ifndef _INC_CRTDEFS_MACRO
+#define _INC_CRTDEFS_MACRO
+
+#ifndef __MINGW64
+#define __MINGW64
+#define __MINGW64_VERSION	"1.0"
+#define __MINGW64_VERSION_MAJOR	1
+#define __MINGW64_VERSION_MINOR	0
+#define __MINGW64_VERSION_STATE	"beta"
+
+/* mingw.org's version macros: these make gcc to define
+   MINGW32_SUPPORTS_MT_EH and to use the _CRT_MT global
+   and the __mingwthr_key_dtor() function from the MinGW
+   CRT in its private gthr-win32.h header. */
+#define __MINGW32_MAJOR_VERSION 3
+#define __MINGW32_MINOR_VERSION 11
+#endif
+
+#ifdef _WIN64
+/* MS does not prefix symbols by underscores for 64-bit.  */
+#ifndef __MINGW_USE_UNDERSCORE_PREFIX
+#define __MINGW_USE_UNDERSCORE_PREFIX 1
+#endif
+#else
+/* For 32-bits we have always to prefix by underscore.  */
+#undef __MINGW_USE_UNDERSCORE_PREFIX
+#define __MINGW_USE_UNDERSCORE_PREFIX 1
+#endif
+
+#if __MINGW_USE_UNDERSCORE_PREFIX == 0
+#define __MINGW_IMP_SYMBOL(sym)	_imp_##sym
+#define __MINGW_USYMBOL(sym) sym
+#else
+#define __MINGW_IMP_SYMBOL(sym)	_imp__##sym
+#define __MINGW_USYMBOL(sym) _##sym
+#endif
+
+/* Use alias for msvcr80 export of get/set_output_format.  */
+#ifndef __USE_MINGW_OUTPUT_FORMAT_EMU
+#define __USE_MINGW_OUTPUT_FORMAT_EMU 1
+#endif
+
+/* Set VC specific compiler target macros.  */
+#if defined(__x86_64) && defined(_X86_)
+#undef _X86_	/* _X86_ is not for __x86_64 */
+#endif
+
+#if defined(_X86_) && !defined(_M_IX86) && !defined(_M_IA64) \
+   && !defined(_M_AMD64) && !defined(__x86_64)
+#if defined(__i486__)
+#define _M_IX86 400
+#elif defined(__i586__)
+#define _M_IX86 500
+#else
+/* This gives wrong (600 instead of 300) value if -march=i386 is specified
+   but we cannot check for__i386__ as it is defined for all 32-bit CPUs. */
+#define _M_IX86 600
+#endif
+#endif
+
+#if defined(__x86_64) && !defined(_M_IX86) && !defined(_M_IA64) \
+   && !defined(_M_AMD64)
+#define _M_AMD64 100
+#define _M_X64 100
+#endif
+
+#if defined(__ia64__) && !defined(_M_IX86) && !defined(_M_IA64) \
+   && !defined(_M_AMD64) && !defined(_X86_) && !defined(__x86_64)
+#define _M_IA64 100
+#endif
+
+#ifdef UNICODE
+# define __MINGW_NAME_AW(func) func##W
+#else
+# define __MINGW_NAME_AW(func) func##A
+#endif
+#define __MINGW_TYPEDEF_AW(type) \
+    typedef __MINGW_NAME_AW(type) type;
+
+#endif
