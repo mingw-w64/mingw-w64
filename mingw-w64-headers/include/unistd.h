f8738f46f1eeeaadc9f67294830069c5df8629db
Kai Tietz <ktietz70@googlemail.com>
2009-07-27 08:44:44 +0000

    Guard _CRT_INLINE by __CRT__NO_INLINE
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@1057 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/unistd.h
+++ mingw-w64-headers/include/unistd.h
@@ -34,16 +34,20 @@ int __cdecl __MINGW_NOTHROW usleep(useconds_t useconds);
       to verify its existence. */
 #if !defined(NO_OLDNAMES) || defined(_POSIX)
   int ftruncate(int, off_t);
+#ifndef __CRT__NO_INLINE
   __CRT_INLINE int ftruncate(int __fd, off_t __length)
   {
     return _chsize (__fd, __length);
   }
+#endif /* !__CRT__NO_INLINE */
 #else
   int ftruncate(int, _off_t);
+#ifndef __CRT__NO_INLINE
   __CRT_INLINE int ftruncate(int __fd, _off_t __length)
   {
     return _chsize (__fd, __length);
   }
+#endif /* !__CRT__NO_INLINE */
 #endif
 #endif
 
