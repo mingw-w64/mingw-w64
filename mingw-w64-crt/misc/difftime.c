c4e9b8fdfa37e898d8bdb462feba38db18c96f40
Ozkan Sezer <sezeroz@gmail.com>
2009-07-27 09:12:30 +0000

    2009-07-27  Ozkan Sezer  <sezeroz@gmail.com>
    	* misc/_mkgmtime.c, misc/ctime.c, misc/difftime.c, misc/gmtime.c,
    	misc/localtime.c, misc/mktime.c, misc/time.c: Define __CRT__NO_INLINE
    	before the includes to prevent declaration mismatches. Also added fixme
    	notes about the broken _USE_32BIT_TIME_T condition.
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@1064 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-crt/misc/difftime.c
+++ mingw-w64-crt/misc/difftime.c
@@ -1,5 +1,11 @@
+#define __CRT__NO_INLINE
 #include <time.h>
 #include <memory.h>
+
+/* FIXME: Relying on _USE_32BIT_TIME_T, which is a user-macro,
+during CRT compilation is plainly broken.  Need an appropriate
+implementation to provide users the ability of compiling the
+CRT only with 32-bit time_t behavior. */
 
 double __cdecl difftime(time_t _Time1,time_t _Time2)
 {
--- mingw-w64-crt/misc/localtime.c
+++ mingw-w64-crt/misc/localtime.c
@@ -1,4 +1,10 @@
+#define __CRT__NO_INLINE
 #include <time.h>
+
+/* FIXME: Relying on _USE_32BIT_TIME_T, which is a user-macro,
+during CRT compilation is plainly broken.  Need an appropriate
+implementation to provide users the ability of compiling the
+CRT only with 32-bit time_t behavior. */
 
 struct tm *__cdecl localtime(const time_t *_Time)
 {
