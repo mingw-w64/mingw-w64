40b225453eeea5c8dd8d4b10d00e0f9efcecb4c9
Ozkan Sezer <sezeroz@gmail.com>
2009-07-29 12:32:37 +0000

    2009-07-29  Ozkan Sezer  <sezeroz@gmail.com>
    	* intrincs/dbgassert.c, membarrier.c: New. For cases where inlining
    	is disabled.
    	* Makefile.am: Added the new files to build for w64.
    	* Makefile.in: Regenerated.
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@1086 4407c894-4637-0410-b4f5-ada5f102cad1

---
+++ mingw-w64-crt/intrincs/dbgassert.c
@@ -0,0 +1,13 @@
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#define __CRT__NO_INLINE
+#include <windows.h>
+
+#if(defined(_X86_) && !defined(__x86_64))
+VOID DbgRaiseAssertionFailure(void)
+{
+    __asm__ __volatile__("int 0x2c ");
+}
+#endif
+
---
+++ mingw-w64-crt/intrincs/membarrier.c
@@ -0,0 +1,15 @@
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#define __CRT__NO_INLINE
+#include <windows.h>
+
+#if(defined(_X86_) && !defined(__x86_64))
+VOID MemoryBarrier(VOID)
+{
+    LONG Barrier;
+    __asm__ __volatile__("xchgl %eax,%0 "
+      :"=r" (Barrier));
+}
+#endif
+
