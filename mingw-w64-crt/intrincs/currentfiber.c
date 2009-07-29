c13f547737d8f73cb6a9ab5d776350ed62b12e1f
Kai Tietz <ktietz70@googlemail.com>
2009-07-29 09:58:08 +0000

    Add 32-bit compatible code for currentfiber.c and use inline assembler for 32-bit code.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@1084 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-crt/intrincs/currentfiber.c
+++ mingw-w64-crt/intrincs/currentfiber.c
@@ -6,6 +6,13 @@
 
  PVOID GetCurrentFiber(VOID)
  {
+#ifdef _WIN64
    return(PVOID)__readgsqword(FIELD_OFFSET(NT_TIB,FiberData));
+#else
+    void *ret;
+    __asm__ volatile ("movl     %%fs:0x10,%0"
+        : "=r" (ret));
+    return ret;
+#endif
  }
 
--- mingw-w64-crt/intrincs/fiberdata.c
+++ mingw-w64-crt/intrincs/fiberdata.c
@@ -6,6 +6,14 @@
 
  PVOID GetFiberData(VOID)
  {
+#ifdef _WIN64
    return *(PVOID *)GetCurrentFiber();
+#else
+  void *ret;
+  __asm__ volatile ("movl     %%fs:0x10,%0\n"
+      "movl   (%0),%0"
+      : "=r" (ret));
+  return ret;
+#endif
  }
 
