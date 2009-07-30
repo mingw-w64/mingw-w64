0cc0a97c3f2d5adebea7b5274766d1c147620946
Kai Tietz <ktietz70@googlemail.com>
2009-07-30 18:54:14 +0000

    Work-a-round strict-aliasing issue in stat/fstat inlines. Maybe this can be solved by __attribute__ ((may_alias)), but my first tests weren't successful by this attribute. I'll continue on this after holiday.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@1103 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/sys/stat.h
+++ mingw-w64-headers/include/sys/stat.h
@@ -262,6 +262,8 @@ int __cdecl stat(const char *_Filename,struct stat *_Stat);
 int __cdecl fstat(int _Desc,struct stat *_Stat);
 int __cdecl wstat(const wchar_t *_Filename,struct stat *_Stat);
 #ifndef __CRT__NO_INLINE
+#if 0
+/* Disabled due to strict-aliasing issues to be solved here.  */
 #ifdef _USE_32BIT_TIME_T
 __CRT_INLINE int __cdecl fstat(int _Desc,struct stat *_Stat) {
   return _fstat32(_Desc,(struct _stat32 *)_Stat);
@@ -277,6 +279,7 @@ __CRT_INLINE int __cdecl stat(const char *_Filename,struct stat *_Stat) {
   return _stat64i32(_Filename,(struct _stat64i32 *)_Stat);
 }
 #endif /* _USE_32BIT_TIME_T */
+#endif /* 0 */
 #endif /* __CRT__NO_INLINE */
 #endif /* !RC_INVOKED && !NO_OLDNAMES */
 
