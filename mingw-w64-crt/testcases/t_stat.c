c81e7e4de63e2c1a9ed00c7b93b66a3881f497f3
Kai Tietz <ktietz70@googlemail.com>
2009-07-31 06:30:05 +0000

    Add two new testcases.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@1104 4407c894-4637-0410-b4f5-ada5f102cad1

---
+++ mingw-w64-crt/testcases/t_stat.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int main(int argc, char *argv[])
+{
+    struct stat st;
+    if (0 == stat(argv[0], &st))
+        printf("mode = %x\n", st.st_mode);
+    return 0;
+}
+
