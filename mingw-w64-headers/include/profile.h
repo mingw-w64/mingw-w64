15d3590e591057554b86d019d03eedc03d3a8e94
Kai Tietz <ktietz70@googlemail.com>
2009-08-30 10:58:43 +0000

    Try to fix profiling for x64.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@1284 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/profile.h
+++ mingw-w64-headers/include/profile.h
@@ -48,7 +48,7 @@
    Older versions of GCC (pre-4.1) will still fail with regparm since the
    compiler used %edx to store an unneeded counter variable.  */
 
-#ifndef _WIN64
+ize_t) __builtin_return_address (0)ifndef _WIN64
 #define	MCOUNT \
 void									\
 mcount(void)								\
@@ -87,32 +87,41 @@ mcount(void)								\
 void									\
 mcount(void)								\
 {									\
-	size_t selfpc, frompcindex;					\
-	/*								\
-	 * Save registers, since this may be called from		\
-	 * the prologue of a regparm function.				\
-	 */								\
 	__asm __volatile__ ("pushq %rax\n\t"				\
 			    "pushq %rcx\n\t"				\
-		 	    "pushq %rdx");				\
-	/*								\
-	 * find the return address for mcount,				\
-	 * and the return address for mcount's caller.			\
-	 *								\
-	 * selfpc = pc pushed by mcount call				\
-	 */								\
-	/* __asm  ("movl 4(%%ebp),%0" : "=r" (selfpc));	*/		\
-	selfpc = (size_t) __builtin_return_address (0);			\
-	/*								\
-	 * frompcindex = pc pushed by call into self.			\
-	 */								\
-	/*  __asm ("movl (%%ebp),%0;movl 4(%0),%0" : "=r" (frompcindex)); */  \
-	frompcindex = (size_t) __builtin_return_address (1);		\
-	_mcount(frompcindex, selfpc);					\
+		 	    "pushq %rdx"				\
+                            "pushq %rbx\n\t"                            \
+                            "pushq %rbp\n\t"                            \
+                            "pushq %rsi\n\t"                            \
+                            "pushq %rdi\n\t"                            \
+                            "pushq %r8\n\t"                             \
+                            "pushq %r9\n\t"                             \
+                            "pushq %r10\n\t"                            \
+                            "pushq %r11\n\t"                            \
+                            "pushq %r12\n\t"                            \
+                            "pushq %r13\n\t"                            \
+                            "pushq %r14\n\t"                            \
+                            "pushq %r15\n\t"                            \
+			    "subq $32, %rsp\n\t");			\
+	_mcount((size_t) __builtin_return_address (1),			\
+			 (size_t) __builtin_return_address (0));	\
 	/*								\
 	 * Restore registers.						\
 	 */								\
-	__asm __volatile__ ("popq %rdx\n\t"				\
+	__asm __volatile__ ("addq $32, %rsp\n\t"				\
+                            "popq %r15\n\t"                             \
+                            "popq %r14\n\t"                             \
+                            "popq %r13\n\t"                             \
+                            "popq %r12\n\t"                             \
+                            "popq %r11\n\t"                             \
+                            "popq %r10\n\t"                             \
+                            "popq %r9\n\t"                              \
+                            "popq %r8\n\t"                              \
+                            "popq %rdi\n\t"                             \
+                            "popq %rsi\n\t"                             \
+                            "popq %rbp\n\t"                             \
+                            "popq %rbx\n\t"                             \
+                            "popq %rdx\n\t"                             \
 			    "popq %rcx\n\t"				\
 			    "popq %rax");				\
 }
