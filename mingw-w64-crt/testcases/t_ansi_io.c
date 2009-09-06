c61743c0f84c60a05f1d60d527048c57c52a2d57
Kai Tietz <ktietz70@googlemail.com>
2009-09-06 07:59:21 +0000

    Extend testcase.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@1318 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-crt/testcases/t_ansi_io.c
+++ mingw-w64-crt/testcases/t_ansi_io.c
@@ -1,15 +1,16 @@
 #define __USE_MINGW_ANSI_STDIO
 #include <stdio.h>
 #include <stdint.h>
+#include <inttypes.h>
 
 int main (int argc, char **argv)
 {
   printf ("%zd %p\n", (intptr_t) argc, argv[0]);
-  fprintf (stdout, "%zd %p\n", (intptr_t) argc, argv[0]);
+  fprintf (stdout, "%zd %p %" PRIuPTR "\n", (intptr_t) argc, argv[0], (intptr_t) argv[0]);
 
 #include <_mingw_print_push.h>
   printf ("%Id %p\n", (intptr_t) argc, argv[0]);
-  fprintf (stdout, "%Id %p\n", (intptr_t) argc, argv[0]);
+  fprintf (stdout, "%Id %p %" PRIuPTR "\n", (intptr_t) argc, argv[0], (intptr_t) argv[0]);
 #include <_mingw_print_pop.h>
   return 0;
 }
