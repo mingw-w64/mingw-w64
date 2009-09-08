49ecddb95d1538d583802452d86e3151e27a67e7
Kai Tietz <ktietz70@googlemail.com>
2009-09-08 08:11:54 +0000

    Remove extern before __MINGW_EXTENSION.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@1324 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/math.h
+++ mingw-w64-headers/include/math.h
@@ -624,9 +624,9 @@ extern long __cdecl lrint (double);
 extern long __cdecl lrintf (float);
 extern long __cdecl lrintl (long double);
 
-extern __MINGW_EXTENSION long long __cdecl llrint (double);
-extern __MINGW_EXTENSION long long __cdecl llrintf (float);
-extern __MINGW_EXTENSION long long __cdecl llrintl (long double);
+__MINGW_EXTENSION long long __cdecl llrint (double);
+__MINGW_EXTENSION long long __cdecl llrintf (float);
+__MINGW_EXTENSION long long __cdecl llrintl (long double);
 
 /* Inline versions of above. 
    GCC 4.0+ can do a better fast-math job with __builtins. */
@@ -714,9 +714,9 @@ extern __MINGW_EXTENSION long long __cdecl llrintl (long double);
   extern long __cdecl lround (double);
   extern long __cdecl lroundf (float);
   extern long __cdecl lroundl (long double);
-  extern __MINGW_EXTENSION long long __cdecl llround (double);
-  extern __MINGW_EXTENSION long long __cdecl llroundf (float);
-  extern __MINGW_EXTENSION long long __cdecl llroundl (long double);
+  __MINGW_EXTENSION long long __cdecl llround (double);
+  __MINGW_EXTENSION long long __cdecl llroundf (float);
+  __MINGW_EXTENSION long long __cdecl llroundl (long double);
   
 /* 7.12.9.8 */
 /* round towards zero, regardless of fpu control word settings */
