512dfe20dc13a3f10ed769b3144ade7b4fa76f0f
Kai Tietz <ktietz70@googlemail.com>
2009-09-15 14:57:15 +0000

    Fix _lc_codepage symbol.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@1366 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-crt/misc/mb_wc_common.h
+++ mingw-w64-crt/misc/mb_wc_common.h
@@ -4,10 +4,10 @@
  * No warranty is given; refer to the file DISCLAIMER within this package.
  */
 
-__declspec(dllimport) unsigned int __lc_codepage;
- 
+__declspec(dllimport) unsigned int _lc_codepage;
+
 static inline
 unsigned int get_codepage (void)
 {
-	return __lc_codepage;
+	return _lc_codepage;
 }
