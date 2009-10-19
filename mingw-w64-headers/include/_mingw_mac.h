877cf44185ff933b3a40578aa3d7a43afc703058
Kai Tietz <ktietz70@googlemail.com>
2009-10-19 12:45:42 +0000

    Adjust none-underscore-prefixed import symbol name. Add new macro for linker provided symbols as __MINGW_LSYMBOL.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@1481 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/_mingw_mac.h
+++ mingw-w64-headers/include/_mingw_mac.h
@@ -34,11 +34,13 @@
 #endif
 
 #if __MINGW_USE_UNDERSCORE_PREFIX == 0
-#define __MINGW_IMP_SYMBOL(sym)	_imp_##sym
+#define __MINGW_IMP_SYMBOL(sym)	__imp_##sym
 #define __MINGW_USYMBOL(sym) sym
+#define __MINGW_LSYMBOL(sym) _##sym
 #else
 #define __MINGW_IMP_SYMBOL(sym)	_imp__##sym
 #define __MINGW_USYMBOL(sym) _##sym
+#define __MINGW_LSYMBOL(sym) sym
 #endif
 
 /* Use alias for msvcr80 export of get/set_output_format.  */
