eca102689e3bfd6aa614bfad627562ba4f89ffd1
Ozkan Sezer <sezeroz@gmail.com>
2009-10-20 19:52:15 +0000

    2009-10-20  Ozkan Sezer  <sezeroz@gmail.com>
    	* crt/pseudo-reloc.c:  Merged documentation updates and most of the
    	whitespace changes from cygwin.  Additional whitespace changes.
    	Shortened some of the error messages.  Removed assert.h #include
    	which isn't necessary any more.
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@1483 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-crt/crt/pseudo-reloc.c
+++ mingw-w64-crt/crt/pseudo-reloc.c
@@ -1,19 +1,25 @@
-/**
- * This file has no copyright assigned and is placed in the Public Domain.
- * This file is part of the w64 mingw-runtime package.
- * No warranty is given; refer to the file DISCLAIMER within this package.
- *
- * Contributed by Egor Duda  <deo@logos-m.ru>
- * Modified by addition of runtime_pseudo_reloc version 2
- * by Kai Tietz  <kai.tietz@onevision.com>
- */
+/* pseudo-reloc.c
+
+   Contributed by Egor Duda  <deo@logos-m.ru>
+   Modified by addition of runtime_pseudo_reloc version 2
+   by Kai Tietz  <kai.tietz@onevision.com>
+	
+   THIS SOFTWARE IS NOT COPYRIGHTED
+
+   This source code is offered for use in the public domain. You may
+   use, modify or distribute it freely.
+
+   This code is distributed in the hope that it will be useful but
+   WITHOUT ANY WARRANTY. ALL WARRENTIES, EXPRESS OR IMPLIED ARE HEREBY
+   DISCLAMED. This includes but is not limited to warrenties of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
 
 #include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <memory.h>
-#include <assert.h>
 
 #ifndef __MINGW_LSYMBOL
 #define __MINGW_LSYMBOL(sym) sym
@@ -23,11 +29,22 @@ extern char __RUNTIME_PSEUDO_RELOC_LIST__;
 extern char __RUNTIME_PSEUDO_RELOC_LIST_END__;
 extern char __MINGW_LSYMBOL(_image_base__);
 
+/* v1 relocation is basically:
+ *   *(base + .target) += .addend
+ * where (base + .target) is always assumed to point
+ * to a DWORD (4 bytes).
+ */
 typedef struct {
   DWORD addend;
   DWORD target;
 } runtime_pseudo_reloc_item_v1;
 
+/* v2 relocation is more complex. In effect, it is
+ *    *(base + .target) += *(base + .sym) - (base + .sym)
+ * with care taken in both reading, sign extension, and writing
+ * because .flags may indicate that (base + .target) may point
+ * to a BYTE, WORD, DWORD, or QWORD (w64).
+ */
 typedef struct {
   DWORD sym;
   DWORD target;
@@ -35,9 +52,9 @@ typedef struct {
 } runtime_pseudo_reloc_item_v2;
 
 typedef struct {
-	DWORD magic1;
-	DWORD magic2;
-	DWORD version;
+  DWORD magic1;
+  DWORD magic2;
+  DWORD version;
 } runtime_pseudo_reloc_v2;
 
 static void
@@ -54,24 +71,41 @@ __report_error (const char *msg, ...)
   abort ();
 }
 
+/* This function temporarily marks the page containing addr
+ * writable, before copying len bytes from *src to *addr, and
+ * then restores the original protection settings to the page.
+ *
+ * Using this function eliminates the requirement with older
+ * pseudo-reloc implementations, that sections containing
+ * pseudo-relocs (such as .text and .rdata) be permanently
+ * marked writable. This older behavior sabotaged any memory
+ * savings achieved by shared libraries on win32 -- and was
+ * slower, too.  However, on cygwin as of binutils 2.20 the
+ * .text section is still marked writable, and the .rdata section
+ * is folded into the (writable) .data when --enable-auto-import.
+ */
 static void
 __write_memory (void *addr, const void *src, size_t len)
 {
   MEMORY_BASIC_INFORMATION b;
   DWORD oldprot;
-  
+
   if (!len)
     return;
+
   if (!VirtualQuery (addr, &b, sizeof(b)))
     {
-      __report_error ("  VirtualQuery failed for %d bytes at address %p", (int) sizeof(b),
-		      addr);
+      __report_error ("  VirtualQuery failed for %d bytes at address %p",
+		      (int) sizeof(b), addr);
     }
+
   /* Temporarily allow write access to read-only protected memory.  */
   if (b.Protect != PAGE_EXECUTE_READWRITE && b.Protect != PAGE_READWRITE)
     VirtualProtect (b.BaseAddress, b.RegionSize, PAGE_EXECUTE_READWRITE,
 		  &oldprot);
+  /* write the data. */
   memcpy (addr, src, len);
+  /* Restore original protection. */
   if (b.Protect != PAGE_EXECUTE_READWRITE && b.Protect != PAGE_READWRITE)
     VirtualProtect (b.BaseAddress, b.RegionSize, oldprot, &oldprot);
 }
@@ -80,64 +114,122 @@ __write_memory (void *addr, const void *src, size_t len)
 #define RP_VERSION_V2 1
 
 static void
-do_pseudo_reloc (void *start, void *end, void *base)
+do_pseudo_reloc (void * start, void * end, void * base)
 {
   ptrdiff_t addr_imp, reldata;
   ptrdiff_t reloc_target = (ptrdiff_t) ((char *)end - (char*)start);
   runtime_pseudo_reloc_v2 *v2_hdr = (runtime_pseudo_reloc_v2 *) start;
   runtime_pseudo_reloc_item_v2 *r;
 
+  /* A valid relocation list will contain at least one entry, and
+   * one v1 data structure (the smallest one) requires two DWORDs.
+   * So, if the relocation list is smaller than 8 bytes, bail.
+   */
   if (reloc_target < 8)
     return;
-  /* Check if this is old version pseudo relocation version.  */
+
+  /* Check if this is the old pseudo relocation version.  */
+  /* There are two kinds of v1 relocation lists:
+   *   1) With a (v2-style) version header. In this case, the
+   *      first entry in the list is a 3-DWORD structure, with
+   *      value:
+   *         { 0, 0, RP_VERSION_V1 }
+   *      In this case, we skip to the next entry in the list,
+   *      knowing that all elements after the head item can
+   *      be cast to runtime_pseudo_reloc_item_v1.
+   *   2) Without a (v2-style) version header. In this case, the
+   *      first element in the list IS an actual v1 relocation
+   *      record, which is two DWORDs.  Because there will never
+   *      be a case where a v1 relocation record has both
+   *      addend == 0 and target == 0, this case will not be
+   *      confused with the prior one.
+   * All current binutils, when generating a v1 relocation list,
+   * use the second (e.g. original) form -- that is, without the
+   * v2-style version header.
+   */
   if (reloc_target >= 12
       && v2_hdr->magic1 == 0 && v2_hdr->magic2 == 0
       && v2_hdr->version == RP_VERSION_V1)
+    {
+      /* We have a list header item indicating that the rest
+       * of the list contains v1 entries.  Move the pointer to
+       * the first true v1 relocation record.  By definition,
+       * that v1 element will not have both addend == 0 and
+       * target == 0 (and thus, when interpreted as a
+       * runtime_pseudo_reloc_v2, it will not have both
+       * magic1 == 0 and magic2 == 0).
+       */
       v2_hdr++;
+    }
+
   if (v2_hdr->magic1 != 0 || v2_hdr->magic2 != 0)
     {
-      runtime_pseudo_reloc_item_v1 *o;
-      for (o = (runtime_pseudo_reloc_item_v1 *) v2_hdr; o < (runtime_pseudo_reloc_item_v1 *)end; o++)
-        {
-          DWORD newval;
+      /*************************
+       * Handle v1 relocations *
+       *************************/
+      runtime_pseudo_reloc_item_v1 * o;
+      for (o = (runtime_pseudo_reloc_item_v1 *) v2_hdr;
+	   o < (runtime_pseudo_reloc_item_v1 *)end;
+           o++)
+	{
+	  DWORD newval;
 	  reloc_target = (ptrdiff_t) base + o->target;
 	  newval = (*((DWORD*) reloc_target)) + o->addend;
 	  __write_memory ((void *) reloc_target, &newval, sizeof(DWORD));
-        }
+	}
       return;
     }
+
+  /* If we got this far, then we have relocations of version 2 or newer */
+
   /* Check if this is a known version.  */
   if (v2_hdr->version != RP_VERSION_V2)
     {
-      __report_error ("  Pseudo relocation protocol version %d is unknown to this runtime.\n",
+      __report_error ("  Unknown pseudo relocation protocol version %d.\n",
 		      (int) v2_hdr->version);
       return;
     }
-  /* Walk over header.  */
+
+  /*************************
+   * Handle v2 relocations *
+   *************************/
+
+  /* Walk over header. */
   r = (runtime_pseudo_reloc_item_v2 *) &v2_hdr[1];
 
   for (; r < (runtime_pseudo_reloc_item_v2 *) end; r++)
     {
+      /* location where new address will be written */
       reloc_target = (ptrdiff_t) base + r->target;
+
+      /* get sym pointer. It points either to the iat entry
+       * of the referenced element, or to the stub function.
+       */
       addr_imp = (ptrdiff_t) base + r->sym;
       addr_imp = *((ptrdiff_t *) addr_imp);
 
-      switch ((r->flags&0xff))
+      /* read existing relocation value from image, casting to the
+       * bitsize indicated by the 8 LSBs of flags. If the value is
+       * negative, manually sign-extend to ptrdiff_t width. Raise an
+       * error if the bitsize indicated by the 8 LSBs of flags is not
+       * supported.
+       */
+      switch ((r->flags & 0xff))
         {
           case 8:
 	    reldata = (ptrdiff_t) (*((unsigned char *)reloc_target));
-	    if ((reldata&0x80) != 0)
+	    if ((reldata & 0x80) != 0)
 	      reldata |= ~((ptrdiff_t) 0xff);
 	    break;
 	  case 16:
 	    reldata = (ptrdiff_t) (*((unsigned short *)reloc_target));
-	    if ((reldata&0x8000) != 0)
+	    if ((reldata & 0x8000) != 0)
 	      reldata |= ~((ptrdiff_t) 0xffff);
 	    break;
 	  case 32:
 	    reldata = (ptrdiff_t) (*((unsigned int *)reloc_target));
 #ifdef _WIN64
-	    if ((reldata&0x80000000) != 0)
+	    if ((reldata & 0x80000000) != 0)
 	      reldata |= ~((ptrdiff_t) 0xffffffff);
 #endif
 	    break;
@@ -148,14 +240,18 @@ do_pseudo_reloc (void *start, void *end, void *base)
 #endif
 	  default:
 	    reldata=0;
-	    __report_error ("Unknown pseudo relocation bit size %d\n",
-			    (int) (r->flags & 0xff));
+	    __report_error ("  Unknown pseudo relocation bit size %d.\n",
+		    (int) (r->flags & 0xff));
 	    break;
         }
+
+      /* Adjust the relocation value */
       reldata -= ((ptrdiff_t) base + r->sym);
       reldata += addr_imp;
+
+      /* Write the new relocation value back to *reloc_target */
       switch ((r->flags & 0xff))
-        {
+	{
          case 8:
            __write_memory ((void *) reloc_target, &reldata, 1);
 	   break;
@@ -170,8 +266,8 @@ do_pseudo_reloc (void *start, void *end, void *base)
            __write_memory ((void *) reloc_target, &reldata, 8);
 	   break;
 #endif
-        }
-    }
+	}
+     }
 }
 
 void _pei386_runtime_relocator (void);
@@ -183,6 +279,7 @@ _pei386_runtime_relocator (void)
   if (was_init)
     return;
   ++was_init;
-  do_pseudo_reloc (&__RUNTIME_PSEUDO_RELOC_LIST__,&__RUNTIME_PSEUDO_RELOC_LIST_END__,
+  do_pseudo_reloc (&__RUNTIME_PSEUDO_RELOC_LIST__,
+		   &__RUNTIME_PSEUDO_RELOC_LIST_END__,
 		   &__MINGW_LSYMBOL(_image_base__));
 }
