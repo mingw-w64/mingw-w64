15191e7dbc9a1c2b549e87e78318bbc1a85cd1d1
Kai Tietz <ktietz70@googlemail.com>
2009-12-04 18:32:50 +0000

    Add global.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@1601 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-crt/crt/tlsmthread.c
+++ mingw-w64-crt/crt/tlsmthread.c
@@ -6,7 +6,7 @@
 
 /* We support TLS cleanup code in any case. If shared version of libgcc is used _CRT_MT has value 1, otherwise
    we do tls cleanup in runtime and _CRT_MT has value 2.  */
-int _CRT_MT = 2;
+extern int _CRT_MT;
 
 int __mingwthr_key_dtor (DWORD key, void (*dtor)(void *));
 int __mingwthr_remove_key_dtor (DWORD key);
