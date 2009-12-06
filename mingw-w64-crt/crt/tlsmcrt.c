9996593c8eee462cd0c89c9459e430cdf96be6e2
Kai Tietz <ktietz70@googlemail.com>
2009-12-06 12:24:12 +0000

    Don't define this _CRT_MT weak.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@1609 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-crt/crt/tlsmcrt.c
+++ mingw-w64-crt/crt/tlsmcrt.c
@@ -1,5 +1,5 @@
 /* We support TLS cleanup code in any case. If shared version of libgcc is used _CRT_MT has value 1,
  otherwise
    we do tls cleanup in runtime and _CRT_MT has value 2.  */
-int _CRT_MT __attribute__((weak)) = 2;
+int _CRT_MT = 2;
 
