429cdefd83fd0d26b402f150d6449b6131ef7179
Kai Tietz <ktietz70@googlemail.com>
2009-12-12 12:41:34 +0000

    Add HAVE_CONFIG_H and include of config.h
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@1633 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-tools/gendef/src/gendef.c
+++ mingw-w64-tools/gendef/src/gendef.c
@@ -21,7 +21,9 @@
    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
    DEALINGS IN THE SOFTWARE.
 */
-
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
 #include <stdio.h>
 #include <stdlib.h>
 #include <memory.h>
