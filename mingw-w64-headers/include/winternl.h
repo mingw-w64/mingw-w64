afd1465722e7c116d9f55718551fe544cf57edd6
Kai Tietz <ktietz70@googlemail.com>
2010-01-18 09:04:10 +0000

    2010-01-18  Roland Schwingel  <roland.schwingel@onevision.com>
    	* include/winternl.h (NtQueryInformationFile): Declare
    	function.
    	(NTQueryObject): Likewise.
    	(FILE_INFORMATION_CLASS): Filled with all known definitions.
    	(FILE_ALL_INFORMATION): Define struct.
    	(FILE_PIPE_LOCAL_INFORMATION): Likewise.
    	(FILE_MAILSLOT_SET_INFORMATION): Likewise.
    	(FILE_MAILSLOT_QUERY_INFORMATION): Likewise.
    	(FILE_ATTRIBUTE_TAG_INFORMATION): Likewise.
    	(FILE_STREAM_INFORMATION): Likewise.
    	(FILE_MODE_INFORMATION): Likewise.
    	(FILE_FULL_EA_INFORMATION): Likewise.
    	(FILE_END_OF_FILE_INFORMATION): Likewise.
    	(FILE_ALLOCATION_INFORMATION): Likewise.
    	(FILE_ALIGNMENT_INFORMATION): Likewise.
    	(FILE_POSITION_INFORMATION): Likewise.
    	(FILE_DISPOSITION_INFORMATION): Likewise.
    	(FILE_NAMES_INFORMATION): Likewise.
    	(FILE_RENAME_INFORMATION): Likewise.
    	(FILE_NAME_INFORMATION): Likewise.
    	(FILE_POSITION_INFORMATION): Likewise.
    	(FILE_ACCESS_INFORMATION): Likewise.
    	(FILE_EA_INFORMATION): Likewise.
    	(FILE_INTERNAL_INFORMATION): Likewise.
    	(FILE_ACCESS_INFORMATION): Likewise.
    	(FILE_STANDARD_INFORMATION): Likewise.
    	(FILE_BASIC_INFORMATION): Likewise.
    	(FILE_ID_BOTH_DIRECTORY_INFORMATION): Likewise.
    	(FILE_BOTH_DIRECTORY_INFORMATION): Likewise.
    	(FILE_ID_FULL_DIRECTORY_INFORMATION): Likewise.
    	(FILE_FULL_DIRECTORY_INFORMATION): Likewise.
    	(FILE_NETWORK_OPEN_INFORMATION): Likewise.
    	(FILE_DIRECTORY_INFORMATION): Likewise.
    	(OBJECT_DATA_INFORMATION): Likewise.
    	(OBJECT_BASIC_INFORMATION): Likewise.
    	(OBJECT_NAME_INFORMATION): Likewise.
    	(OBJECT_TYPE_INFORMATION): Likewise.
    	(OBJECT_ALL_INFORMATION): Likewise.
    	(OBJECT_INFORMATION_CLASS): Define enum.
    	(OBJECT_ATTRIBUTES): Reformat.
    
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@1754 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/winternl.h
+++ mingw-w64-headers/include/winternl.h
@@ -105,10 +105,329 @@ typedef struct _RTL_USER_PROCESS_PARAMETERS {
     ULONG Attributes;
     PVOID SecurityDescriptor;
     PVOID SecurityQualityOfService;
-  } OBJECT_ATTRIBUTES;
+  } OBJECT_ATTRIBUTES, *POBJECT_ATTRIBUTES;
 
-  typedef OBJECT_ATTRIBUTES *POBJECT_ATTRIBUTES;
+  typedef struct _OBJECT_DATA_INFORMATION {
+    BOOLEAN InheritHandle;
+    BOOLEAN ProtectFromClose;
+  } OBJECT_DATA_INFORMATION, *POBJECT_DATA_INFORMATION;
 
+  typedef struct _OBJECT_BASIC_INFORMATION {
+    ULONG  Attributes;
+    ACCESS_MASK  GrantedAccess;
+    ULONG  HandleCount;
+    ULONG  PointerCount;
+    ULONG  PagedPoolUsage;
+    ULONG  NonPagedPoolUsage;
+    ULONG  Reserved[3];
+    ULONG  NameInformationLength;
+    ULONG  TypeInformationLength;
+    ULONG  SecurityDescriptorLength;
+    LARGE_INTEGER  CreateTime;
+  } OBJECT_BASIC_INFORMATION, *POBJECT_BASIC_INFORMATION;
+
+  typedef struct _OBJECT_NAME_INFORMATION {
+    UNICODE_STRING Name;
+  } OBJECT_NAME_INFORMATION, *POBJECT_NAME_INFORMATION;
+
+  typedef struct _OBJECT_TYPE_INFORMATION {
+    UNICODE_STRING TypeName;
+    ULONG TotalNumberOfObjects;
+    ULONG TotalNumberOfHandles;
+    ULONG TotalPagedPoolUsage;
+    ULONG TotalNonPagedPoolUsage;
+    ULONG TotalNamePoolUsage;
+    ULONG TotalHandleTableUsage;
+    ULONG HighWaterNumberOfObjects;
+    ULONG HighWaterNumberOfHandles;
+    ULONG HighWaterPagedPoolUsage;
+    ULONG HighWaterNonPagedPoolUsage;
+    ULONG HighWaterNamePoolUsage;
+    ULONG HighWaterHandleTableUsage;
+    ULONG InvalidAttributes;
+    GENERIC_MAPPING GenericMapping;
+    ULONG ValidAccessMask;
+    BOOLEAN SecurityRequired;
+    BOOLEAN MaintainHandleCount;
+    ULONG PoolType;
+    ULONG DefaultPagedPoolCharge;
+    ULONG DefaultNonPagedPoolCharge;
+  } OBJECT_TYPE_INFORMATION, *POBJECT_TYPE_INFORMATION;
+
+  typedef struct _OBJECT_ALL_INFORMATION { 
+    ULONG NumberOfObjects; 
+    OBJECT_TYPE_INFORMATION ObjectTypeInformation[1]; 
+  }OBJECT_ALL_INFORMATION, *POBJECT_ALL_INFORMATION;
+
+  typedef enum _FILE_INFORMATION_CLASS {
+    FileDirectoryInformation = 1,
+    FileFullDirectoryInformation,
+    FileBothDirectoryInformation,
+    FileBasicInformation,
+    FileStandardInformation,
+    FileInternalInformation,
+    FileEaInformation,
+    FileAccessInformation,
+    FileNameInformation,
+    FileRenameInformation,
+    FileLinkInformation,
+    FileNamesInformation,
+    FileDispositionInformation,
+    FilePositionInformation,
+    FileFullEaInformation,
+    FileModeInformation,
+    FileAlignmentInformation,
+    FileAllInformation,
+    FileAllocationInformation,
+    FileEndOfFileInformation,
+    FileAlternateNameInformation,
+    FileStreamInformation,
+    FilePipeInformation,
+    FilePipeLocalInformation,
+    FilePipeRemoteInformation,
+    FileMailslotQueryInformation,
+    FileMailslotSetInformation,
+    FileCompressionInformation,
+    FileObjectIdInformation,
+    FileCompletionInformation,
+    FileMoveClusterInformation,
+    FileQuotaInformation,
+    FileReparsePointInformation,
+    FileNetworkOpenInformation,
+    FileAttributeTagInformation,
+    FileTrackingInformation,
+    FileIdBothDirectoryInformation,
+    FileIdFullDirectoryInformation,
+    FileValidDataLengthInformation,
+    FileShortNameInformation = 40,
+    FileSfioReserveInformation = 44,
+    FileSfioVolumeInformation = 45,
+    FileHardLinkInformation = 46,
+    FileNormalizedNameInformation = 48,
+    FileIdGlobalTxDirectoryInformation = 50,
+    FileStandardLinkInformation = 54,
+    FileMaximumInformation
+  } FILE_INFORMATION_CLASS, *PFILE_INFORMATION_CLASS;
+
+  typedef struct _FILE_DIRECTORY_INFORMATION {
+    ULONG NextEntryOffset;
+    ULONG FileIndex;
+    LARGE_INTEGER CreationTime;
+    LARGE_INTEGER LastAccessTime;
+    LARGE_INTEGER LastWriteTime;
+    LARGE_INTEGER ChangeTime;
+    LARGE_INTEGER EndOfFile;
+    LARGE_INTEGER AllocationSize;
+    ULONG FileAttributes;
+    ULONG FileNameLength;
+    WCHAR FileName[ANYSIZE_ARRAY];
+  } FILE_DIRECTORY_INFORMATION, *PFILE_DIRECTORY_INFORMATION;
+
+  typedef struct _FILE_FULL_DIRECTORY_INFORMATION {
+    ULONG NextEntryOffset;
+    ULONG FileIndex;
+    LARGE_INTEGER CreationTime;
+    LARGE_INTEGER LastAccessTime;
+    LARGE_INTEGER LastWriteTime;
+    LARGE_INTEGER ChangeTime;
+    LARGE_INTEGER EndOfFile;
+    LARGE_INTEGER AllocationSize;
+    ULONG FileAttributes;
+    ULONG FileNameLength;
+    ULONG EaSize;
+    WCHAR FileName[ANYSIZE_ARRAY];
+  } FILE_FULL_DIRECTORY_INFORMATION, *PFILE_FULL_DIRECTORY_INFORMATION,FILE_FULL_DIR_INFORMATION, *PFILE_FULL_DIR_INFORMATION;
+
+  typedef struct _FILE_ID_FULL_DIRECTORY_INFORMATION {
+    ULONG NextEntryOffset;
+    ULONG FileIndex;
+    LARGE_INTEGER CreationTime;
+    LARGE_INTEGER LastAccessTime;
+    LARGE_INTEGER LastWriteTime;
+    LARGE_INTEGER ChangeTime;
+    LARGE_INTEGER EndOfFile;
+    LARGE_INTEGER AllocationSize;
+    ULONG FileAttributes;
+    ULONG FileNameLength;
+    ULONG EaSize;
+    LARGE_INTEGER FileId;
+    WCHAR FileName[ANYSIZE_ARRAY];
+  } FILE_ID_FULL_DIRECTORY_INFORMATION, *PFILE_ID_FULL_DIRECTORY_INFORMATION;
+
+  typedef struct _FILE_BOTH_DIRECTORY_INFORMATION {
+    ULONG NextEntryOffset;
+	 ULONG FileIndex;
+    LARGE_INTEGER CreationTime;
+    LARGE_INTEGER LastAccessTime;
+    LARGE_INTEGER LastWriteTime;
+    LARGE_INTEGER ChangeTime;
+    LARGE_INTEGER EndOfFile;
+    LARGE_INTEGER AllocationSize;
+    ULONG FileAttributes;
+    ULONG FileNameLength;
+    ULONG EaSize;
+    CHAR ShortNameLength;
+    WCHAR ShortName[12];
+    WCHAR FileName[ANYSIZE_ARRAY];
+  } FILE_BOTH_DIRECTORY_INFORMATION, *PFILE_BOTH_DIRECTORY_INFORMATION,FILE_BOTH_DIR_INFORMATION, *PFILE_BOTH_DIR_INFORMATION;
+
+  typedef struct _FILE_ID_BOTH_DIRECTORY_INFORMATION {
+    ULONG NextEntryOffset;
+    ULONG FileIndex;
+    LARGE_INTEGER CreationTime;
+    LARGE_INTEGER LastAccessTime;
+    LARGE_INTEGER LastWriteTime;
+    LARGE_INTEGER ChangeTime;
+    LARGE_INTEGER EndOfFile;
+    LARGE_INTEGER AllocationSize;
+    ULONG FileAttributes;
+    ULONG FileNameLength;
+    ULONG EaSize;
+    CHAR ShortNameLength;
+    WCHAR ShortName[12];
+    LARGE_INTEGER FileId;
+    WCHAR FileName[ANYSIZE_ARRAY];
+  } FILE_ID_BOTH_DIRECTORY_INFORMATION, *PFILE_ID_BOTH_DIRECTORY_INFORMATION;
+
+  typedef struct _FILE_BASIC_INFORMATION {
+    LARGE_INTEGER CreationTime;
+    LARGE_INTEGER LastAccessTime;
+    LARGE_INTEGER LastWriteTime;
+    LARGE_INTEGER ChangeTime;
+    ULONG FileAttributes;
+  } FILE_BASIC_INFORMATION, *PFILE_BASIC_INFORMATION;
+
+  typedef struct _FILE_STANDARD_INFORMATION {
+    LARGE_INTEGER AllocationSize;
+    LARGE_INTEGER EndOfFile;
+    ULONG NumberOfLinks;
+    BOOLEAN DeletePending;
+    BOOLEAN Directory;
+  } FILE_STANDARD_INFORMATION, *PFILE_STANDARD_INFORMATION;
+
+  typedef struct _FILE_INTERNAL_INFORMATION {
+    LARGE_INTEGER IndexNumber;
+  } FILE_INTERNAL_INFORMATION, *PFILE_INTERNAL_INFORMATION;
+
+  typedef struct _FILE_EA_INFORMATION {
+    ULONG EaSize;
+  } FILE_EA_INFORMATION, *PFILE_EA_INFORMATION;
+
+  typedef struct _FILE_ACCESS_INFORMATION {
+    ACCESS_MASK AccessFlags;
+  } FILE_ACCESS_INFORMATION, *PFILE_ACCESS_INFORMATION;
+
+  typedef struct _FILE_NAME_INFORMATION {
+    ULONG FileNameLength;
+    WCHAR FileName[1];
+  } FILE_NAME_INFORMATION, *PFILE_NAME_INFORMATION;
+
+  typedef struct _FILE_RENAME_INFORMATION {
+    BOOLEAN Replace;
+    HANDLE RootDir;
+    ULONG FileNameLength;
+    WCHAR FileName[1];
+  } FILE_RENAME_INFORMATION, *PFILE_RENAME_INFORMATION;
+
+  typedef struct _FILE_NAMES_INFORMATION {
+    ULONG NextEntryOffset;
+    ULONG FileIndex;
+    ULONG FileNameLength;
+    WCHAR FileName[1];
+  } FILE_NAMES_INFORMATION, *PFILE_NAMES_INFORMATION;
+
+  typedef struct _FILE_DISPOSITION_INFORMATION {
+    BOOLEAN DoDeleteFile;
+  } FILE_DISPOSITION_INFORMATION, *PFILE_DISPOSITION_INFORMATION;
+
+  typedef struct _FILE_POSITION_INFORMATION {
+    LARGE_INTEGER CurrentByteOffset;
+  } FILE_POSITION_INFORMATION, *PFILE_POSITION_INFORMATION;
+
+  typedef struct _FILE_ALIGNMENT_INFORMATION {
+    ULONG AlignmentRequirement;
+  } FILE_ALIGNMENT_INFORMATION, *PFILE_ALIGNMENT_INFORMATION;
+
+  typedef struct _FILE_ALLOCATION_INFORMATION {
+    LARGE_INTEGER AllocationSize;
+  } FILE_ALLOCATION_INFORMATION, *PFILE_ALLOCATION_INFORMATION;
+
+  typedef struct _FILE_END_OF_FILE_INFORMATION {
+    LARGE_INTEGER EndOfFile;
+  } FILE_END_OF_FILE_INFORMATION, *PFILE_END_OF_FILE_INFORMATION;
+
+  typedef struct _FILE_NETWORK_OPEN_INFORMATION {
+    LARGE_INTEGER CreationTime;
+    LARGE_INTEGER LastAccessTime;
+    LARGE_INTEGER LastWriteTime;
+    LARGE_INTEGER ChangeTime;
+    LARGE_INTEGER AllocationSize;
+    LARGE_INTEGER EndOfFile;
+    ULONG FileAttributes;
+  } FILE_NETWORK_OPEN_INFORMATION, *PFILE_NETWORK_OPEN_INFORMATION;
+
+  typedef struct _FILE_FULL_EA_INFORMATION {
+    ULONG NextEntryOffset;
+    UCHAR Flags;
+    UCHAR EaNameLength;
+    USHORT EaValueLength;
+    CHAR EaName[1];
+  } FILE_FULL_EA_INFORMATION, *PFILE_FULL_EA_INFORMATION;
+
+  typedef struct _FILE_MODE_INFORMATION {
+    ULONG Mode;
+  } FILE_MODE_INFORMATION, *PFILE_MODE_INFORMATION;
+
+  typedef struct _FILE_STREAM_INFORMATION {
+    ULONG NextEntryOffset;
+    ULONG StreamNameLength;
+    LARGE_INTEGER StreamSize;
+    LARGE_INTEGER StreamAllocationSize;
+    WCHAR StreamName[1];
+  } FILE_STREAM_INFORMATION, *PFILE_STREAM_INFORMATION;
+
+  typedef struct _FILE_ATTRIBUTE_TAG_INFORMATION {
+    ULONG FileAttributes;
+    ULONG ReparseTag;
+  } FILE_ATTRIBUTE_TAG_INFORMATION, *PFILE_ATTRIBUTE_TAG_INFORMATION;
+
+  typedef struct _FILE_MAILSLOT_QUERY_INFORMATION {
+    ULONG MaximumMessageSize;
+    ULONG MailslotQuota;
+    ULONG NextMessageSize;
+    ULONG MessagesAvailable;
+    LARGE_INTEGER ReadTimeout;
+  } FILE_MAILSLOT_QUERY_INFORMATION, *PFILE_MAILSLOT_QUERY_INFORMATION;
+
+  typedef struct _FILE_MAILSLOT_SET_INFORMATION {
+    LARGE_INTEGER ReadTimeout;
+  } FILE_MAILSLOT_SET_INFORMATION, *PFILE_MAILSLOT_SET_INFORMATION;
+
+  typedef struct _FILE_PIPE_LOCAL_INFORMATION {
+    ULONG NamedPipeType;
+    ULONG NamedPipeConfiguration;
+    ULONG MaximumInstances;
+    ULONG CurrentInstances;
+    ULONG InboundQuota;
+    ULONG ReadDataAvailable;
+    ULONG OutboundQuota;
+    ULONG WriteQuotaAvailable;
+    ULONG NamedPipeState;
+    ULONG NamedPipeEnd;
+  } FILE_PIPE_LOCAL_INFORMATION, *PFILE_PIPE_LOCAL_INFORMATION;
+
+  typedef struct _FILE_ALL_INFORMATION {
+    FILE_BASIC_INFORMATION     BasicInformation;
+    FILE_STANDARD_INFORMATION  StandardInformation;
+    FILE_INTERNAL_INFORMATION  InternalInformation;
+    FILE_EA_INFORMATION        EaInformation;
+    FILE_ACCESS_INFORMATION    AccessInformation;
+    FILE_POSITION_INFORMATION  PositionInformation;
+    FILE_MODE_INFORMATION      ModeInformation;
+    FILE_ALIGNMENT_INFORMATION AlignmentInformation;
+    FILE_NAME_INFORMATION      NameInformation;
+  } FILE_ALL_INFORMATION, *PFILE_ALL_INFORMATION;
+ 
   typedef struct _IO_STATUS_BLOCK {
     __MINGW_EXTENSION union {
       NTSTATUS Status;
@@ -384,10 +703,6 @@ typedef struct _RTL_USER_PROCESS_PARAMETERS {
     SYSTEM_HANDLE_ENTRY Handle[1];
   } SYSTEM_HANDLE_INFORMATION, *PSYSTEM_HANDLE_INFORMATION;
 
-  typedef enum _FILE_INFORMATION_CLASS {
-    FileDirectoryInformation = 1
-  } FILE_INFORMATION_CLASS;
-
   typedef enum _PROCESSINFOCLASS {
     ProcessBasicInformation = 0,ProcessQuotaLimits = 1,ProcessIoCounters = 2,ProcessVmCounters = 3,ProcessTimes = 4,ProcessBasePriority = 5,
     ProcessRaisePriority = 6,ProcessDebugPort = 7,ProcessExceptionPort = 8,ProcessAccessToken = 9,ProcessWow64Information = 26,ProcessImageFileName = 27
@@ -402,6 +717,14 @@ typedef struct _RTL_USER_PROCESS_PARAMETERS {
     SystemProcessorPerformanceInformation = 8,SystemHandleInformation = 16,SystemInterruptInformation = 23,SystemExceptionInformation = 33,SystemRegistryQuotaInformation = 37,
     SystemLookasideInformation = 45
   } SYSTEM_INFORMATION_CLASS;
+
+  typedef enum _OBJECT_INFORMATION_CLASS {
+    ObjectBasicInformation,
+    ObjectNameInformation,
+    ObjectTypeInformation,
+    ObjectAllInformation,
+    ObjectDataInformation
+ } OBJECT_INFORMATION_CLASS, *POBJECT_INFORMATION_CLASS;
 
 #define INTERNAL_TS_ACTIVE_CONSOLE_ID (*((volatile ULONG*)(0x7ffe02d8)))
 
@@ -418,6 +741,8 @@ typedef struct _RTL_USER_PROCESS_PARAMETERS {
   ULONG WINAPI RtlNtStatusToDosError (NTSTATUS Status);
   NTSTATUS WINAPI NtQueryInformationProcess(HANDLE ProcessHandle,PROCESSINFOCLASS ProcessInformationClass,PVOID ProcessInformation,ULONG ProcessInformationLength,PULONG ReturnLength);
   NTSTATUS WINAPI NtQueryInformationThread(HANDLE ThreadHandle,THREADINFOCLASS ThreadInformationClass,PVOID ThreadInformation,ULONG ThreadInformationLength,PULONG ReturnLength);
+  NTSTATUS WINAPI NtQueryInformationFile(HANDLE hFile,PIO_STATUS_BLOCK io,PVOID ptr,LONG len,FILE_INFORMATION_CLASS FileInformationClass);
+  NTSTATUS WINAPI NtQueryObject(HANDLE Handle,OBJECT_INFORMATION_CLASS ObjectInformationClass,PVOID ObjectInformation,ULONG ObjectInformationLength,PULONG ReturnLength);
   NTSTATUS WINAPI NtQuerySystemInformation(SYSTEM_INFORMATION_CLASS SystemInformationClass,PVOID SystemInformation,ULONG SystemInformationLength,PULONG ReturnLength);
   NTSTATUS WINAPI NtQuerySystemTime(PLARGE_INTEGER SystemTime);
   NTSTATUS WINAPI RtlLocalTimeToSystemTime(PLARGE_INTEGER LocalTime,PLARGE_INTEGER SystemTime);
