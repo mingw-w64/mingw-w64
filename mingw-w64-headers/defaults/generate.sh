023a2f3a4c2a81eff957ea8dffe8f4e1f137eccd
Jonathan Yong <10walls@gmail.com>
2010-01-23 12:56:54 +0000

    Make _mingw_ddk.h auto-generated by configure.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@1778 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/defaults/generate.sh
+++ mingw-w64-headers/defaults/generate.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 sed s/@MINGW_HAS_DX@/0/ crt/sdks/_mingw_directx.h.in > defaults/include/sdks/_mingw_directx.h
+sed s/@MINGW_HAS_DDK@/0/ crt/sdks/_mingw_ddk.h.in > defaults/include/sdks/_mingw_ddk.h
