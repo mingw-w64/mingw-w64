ed668d8740247c975e035c987d4bfbb04a0c0497
Ozkan Sezer <sezeroz@gmail.com>
2010-03-04 22:06:28 +0000

    2010-03-04  Ozkan Sezer  <sezeroz@gmail.com>
    	Back-merges from the trunk:
    
    	NightStrike, 2010-01-25, revs. 1790, 1793, 1799:
    	* ntsecpkg.h (SECPKG_EXTENDED_INFORMATION_CLASS): Add line breaks.
    	(SECPKG_NAME_TYPE): Likewise.
    	* shlwapi.h (ASSOCF_*): Likewise.
    	* wtsapi32.h (WTS_INFO_CLASS): Reformat likewise.
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/branches/releases/v1.0@2010 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/wtsapi32.h
+++ mingw-w64-headers/include/wtsapi32.h
@@ -98,10 +98,31 @@ extern "C" {
 #define WTS_PROTOCOL_TYPE_RDP 2
 
   typedef enum _WTS_INFO_CLASS {
-    WTSInitialProgram,WTSApplicationName,WTSWorkingDirectory,WTSOEMId,WTSSessionId,WTSUserName,WTSWinStationName,WTSDomainName,WTSConnectState,
-    WTSClientBuildNumber,WTSClientName,WTSClientDirectory,WTSClientProductId,WTSClientHardwareId,WTSClientAddress,WTSClientDisplay,
-    WTSClientProtocolType,
-    WTSIdleTime,WTSLogonTime,WTSIncomingBytes,WTSOutgoingBytes,WTSIncomingFrames,WTSOutgoingFrames,WTSClientInfo,WTSSessionInfo
+    WTSInitialProgram       = 0,
+    WTSApplicationName      = 1,
+    WTSWorkingDirectory     = 2,
+    WTSOEMId                = 3,
+    WTSSessionId            = 4,
+    WTSUserName             = 5,
+    WTSWinStationName       = 6,
+    WTSDomainName           = 7,
+    WTSConnectState         = 8,
+    WTSClientBuildNumber    = 9,
+    WTSClientName           = 10,
+    WTSClientDirectory      = 11,
+    WTSClientProductId      = 12,
+    WTSClientHardwareId     = 13,
+    WTSClientAddress        = 14,
+    WTSClientDisplay        = 15,
+    WTSClientProtocolType   = 16,
+    WTSIdleTime             = 17,
+    WTSLogonTime            = 18,
+    WTSIncomingBytes        = 19,
+    WTSOutgoingBytes        = 20,
+    WTSIncomingFrames       = 21,
+    WTSOutgoingFrames       = 22,
+    WTSClientInfo           = 23,
+    WTSSessionInfo          = 24
   } WTS_INFO_CLASS;
 
   typedef struct _WTS_CLIENT_ADDRESS {
