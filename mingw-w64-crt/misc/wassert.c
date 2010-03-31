a2938d99824402de2fa3f914aaa88ab99a264e0b
Kai Tietz <ktietz70@googlemail.com>
2010-03-31 12:32:48 +0000

    Add default ASCII convert function _assert.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@2128 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-crt/misc/wassert.c
+++ mingw-w64-crt/misc/wassert.c
@@ -26,6 +26,25 @@ static void cpy_wstr (char *buf, const wchar_t *src, size_t max)
 }
 
 void __cdecl _wassert (const wchar_t *, const wchar_t *,unsigned);
+void __cdecl _assert (const char *, const char *, unsigned);
+
+void __cdecl
+_assert (const char *_Message, const char *_File, unsigned _Line)
+{
+  wchar_t *m, *f;
+  int i;
+  m = (wchar_t *) malloc ((strlen (_Message) + 1) * sizeof (wchar_t));
+  f = (wchar_t *) malloc ((strlen (_File) + 1) * sizeof (wchar_t));
+  for (i = 0; _Message[i] != 0; i++)
+    m[i] = ((wchar_t) _Message[i]) & 0xff;
+  m[i] = 0;
+  for (i = 0; _File[i] != 0; i++)
+    f[i] = ((wchar_t) _File[i]) & 0xff;
+  f[i] = 0;
+  _wassert (m, f, _Line);
+  free (m);
+  free (f);
+}
 
 void __cdecl
 _wassert (const wchar_t *_Message, const wchar_t *_File, unsigned _Line)
