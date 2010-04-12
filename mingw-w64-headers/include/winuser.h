519cd18685916c8c9d04426d93ee186346524ac7
NightStrike <nightstrike@gmail.com>
2010-04-12 17:08:27 +0000

    Update argument for UnregisterPowerSettingNotification according to http://msdn.microsoft.com/en-us/library/aa373237%28v=VS.85%29.aspx
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@2173 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/winuser.h
+++ mingw-w64-headers/include/winuser.h
@@ -1973,7 +1973,7 @@ extern "C" {
   WINUSERAPI HDEVNOTIFY WINAPI RegisterDeviceNotificationW(HANDLE hRecipient,LPVOID NotificationFilter,DWORD Flags);
 #if (_WIN32_WINNT >= 0x0600)
   WINUSERAPI HPOWERNOTIFY WINAPI RegisterPowerSettingNotification(HANDLE hRecipient,LPCGUID PowerSettingGuid,DWORD Flags);
-  WINUSERAPI WINBOOL WINAPI UnregisterPowerSettingNotification(HPOWERNOTIFY);
+  WINUSERAPI WINBOOL WINAPI UnregisterPowerSettingNotification(HPOWERNOTIFY Handle);
 #endif
   WINUSERAPI WINBOOL WINAPI UnregisterDeviceNotification(HDEVNOTIFY Handle);
   WINUSERAPI WINBOOL WINAPI PostMessageA(HWND hWnd,UINT Msg,WPARAM wParam,LPARAM lParam);
