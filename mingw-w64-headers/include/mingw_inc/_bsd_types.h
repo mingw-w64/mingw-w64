06a7a8fb80e8dd8e286a2fa35a05dbc3637b3736
Ozkan Sezer <sezeroz@gmail.com>
2010-05-26 17:21:09 +0000

    2010-05-26  Ozkan Sezer  <sezeroz@gmail.com>
    	In case winsock2.h is included after winsock.h, probably by way of
    	windows.h, perform a "winsock2 upgrade" on top of winsock1 by splitting
    	the headers into some helpers holding shared data types and undefining
    	macros when necessary:
    
    	* mingw_inc/_bsd_types.h: New.
    	* mingw_inc/_fd_types.h: New.
    	* mingw_inc/_ip_mreq1.h: New.
    	* mingw_inc/_ip_types.h: New.
    	* mingw_inc/_socket_types.h: New.
    	* mingw_inc/_ws1_undef.h: New.
    	* mingw_inc/_wsa_errnos.h: New.
    	* mingw_inc/_wsadata.h: New.
    	* mingw_inc/_xmitfile.h: New.
    	* _timeval.h: New.
    	* inaddr.h: New.
    	* in6addr.h: New.
    	* time.h: Include _timeval.h. Move struct timeval, timerisset,
    	timercmp and timerclear to _timeval.h.
    	* sys/time.h: Likewise.
    	* ipexport.h: Include inaddr.h and in6addr.h. Move struct in6_addr
    	and its related macros to in6addr.h. Move struct in_addr and its
    	related macros to inaddr.h.
    	* mswsock.h: Include mingw_inc/_xmitfile.h.
    	Move struct _TRANSMIT_FILE_BUFFERS to mingw_inc/_xmitfile.h.
    	Guard WSARecvEx, TransmitFile, AcceptEx and GetAcceptExSockaddrs
    	prototypes by __MSWSOCK_WS1_SHARED ifdefs.
    	* winsock.h: Include _timeval.h, inaddr.h, mingw_inc/_bsd_types.h,
    	mingw_inc/_socket_types.h, mingw_inc/_fd_types.h, mingw_inc/_ip_types.h,
    	mingw_inc/_ip_mreq1.h, mingw_inc/_wsadata.h and mingw_inc/_xmitfile.h.
    	Define WINSOCK_API_LINKAGE and WSAAPI at the top and undefine them at
    	the end of the file.
    	Move struct timeval, timerisset,timercmp and timerclear to _timeval.h.
    	Move struct in_addr and its related macros to inaddr.h.
    	Move u_char, u_short, u_int, u_long and u_int64 to mingw_inc/_bsd_types.h.
    	Move SOCKET, INVALID_SOCKET and SOCKET_ERROR to mingw_inc/_socket_types.h.
    	Move struct fd_set and FD_SETSIZE to mingw_inc/_fd_types.h.
    	Move struct ip_mreq to mingw_inc/_ip_mreq1.h.
    	Move structs hostent, netent, servent, protoent, sockaddr_in, sockaddr,
    	sockproto and linger to mingw_inc/_ip_types.h. Moved struct WSAData to
    	mingw_inc/_wsadata.h.
    	Move WSA error macros to mingw_inc/_wsa_errnos.h.
    	Move struct _TRANSMIT_FILE_BUFFERS to mingw_inc/_xmitfile.h.
    	Define __WINSOCK_WS1_SHARED and __MSWSOCK_WS1_SHARED.
    	* winsock2.h: Do not re-define _WINSOCKAPI_. If it is already defined,
    	do a #warning that winsock2.h must be included before windows.h.
    	Include mingw_inc/_ws1_undef.h to make sure that macros from winsock.h
    	are undefined. Include _timeval.h, inaddr.h, mingw_inc/_bsd_types.h,
    	mingw_inc/_socket_types.h, mingw_inc/_fd_types.h, mingw_inc/_ip_types.h
    	and mingw_inc/_wsadata.h.
    	Move struct timeval, timerisset,timercmp and timerclear to _timeval.h.
    	Move struct in_addr and its related macros to inaddr.h.
    	Move u_char, u_short, u_int, u_long and u_int64 to mingw_inc/_bsd_types.h.
    	Move SOCKET, INVALID_SOCKET and SOCKET_ERROR to mingw_inc/_socket_types.h.
    	Move struct fd_set and FD_SETSIZE to mingw_inc/_fd_types.h.
    	Move structs hostent, netent, servent, protoent, sockaddr_in, sockaddr,
    	sockproto and linger to mingw_inc/_ip_types.h. Moved struct WSAData to
    	mingw_inc/_wsadata.h.
    	Move WSA error macros to mingw_inc/_wsa_errnos.h.
    	Guard the 46 functions shared by 1.1 api using __WINSOCK_WS1_SHARED ifdefs.
    	* ws2tcpip.h: Include mingw_inc/_ip_mreq1.h.
    	Move struct ip_mreq to mingw_inc/_ip_mreq1.h.
    	Move struct in6_addr, IN6_ADDR, PIN6_ADDR, LPIN6_ADDR to in6addr.h.
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/branches/releases/v1.0@2444 4407c894-4637-0410-b4f5-ada5f102cad1

---
+++ mingw-w64-headers/include/mingw_inc/_bsd_types.h
@@ -0,0 +1,18 @@
+/**
+ * This file has no copyright assigned and is placed in the Public Domain.
+ * This file is part of the w64 mingw-runtime package.
+ * No warranty is given; refer to the file DISCLAIMER.PD within this package.
+ */
+
+#ifndef ___WSA_BSD_TYPES_H
+#define ___WSA_BSD_TYPES_H
+
+typedef unsigned char	u_char;
+typedef unsigned short	u_short;
+typedef unsigned int	u_int;
+typedef unsigned long	u_long;
+__MINGW_EXTENSION
+typedef unsigned __int64 u_int64;
+
+#endif /* ___WSA_BSD_TYPES_H */
+
