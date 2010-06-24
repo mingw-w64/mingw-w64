58b66ec9591e6ec04cd7c20edfc57f382f7bcb9d
Doug Semler <dougsemler@gmail.com>
2010-06-24 12:54:25 +0000

    Use APIENTRY instead of WINAPI for OpenGL headers.
    glew, amongst other things, expects GL functions to use APIENTRY
    and not WINAPI for its function type declarations, which is how
    it is defined in the Platform SDK.
    
    mingw-w64-headers/include/ChangeLog:
    
    2010-06-23  Doug Semler  <dougsemler@gmail.com>
    
            * GL/gl.h: Use APIENTRY instead of WINAPI.
            * GL/glaux.h: Likewise.
            * GL/glu.h: Likewise.
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@2622 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/GL/glu.h
+++ mingw-w64-headers/include/GL/glu.h
@@ -21,18 +21,18 @@ extern "C" {
 #define gluErrorStringWIN(errCode) ((LPCWSTR) gluErrorString(errCode))
 #endif
 
-const GLubyte *WINAPI gluErrorString(GLenum errCode);
-const wchar_t *WINAPI gluErrorUnicodeStringEXT(GLenum errCode);
-const GLubyte *WINAPI gluGetString(GLenum name);
-void WINAPI gluOrtho2D(GLdouble left,GLdouble right,GLdouble bottom,GLdouble top);
-void WINAPI gluPerspective(GLdouble fovy,GLdouble aspect,GLdouble zNear,GLdouble zFar);
-void WINAPI gluPickMatrix(GLdouble x,GLdouble y,GLdouble width,GLdouble height,GLint viewport[4]);
-void WINAPI gluLookAt(GLdouble eyex,GLdouble eyey,GLdouble eyez,GLdouble centerx,GLdouble centery,GLdouble centerz,GLdouble upx,GLdouble upy,GLdouble upz);
-int WINAPI gluProject(GLdouble objx,GLdouble objy,GLdouble objz,const GLdouble modelMatrix[16],const GLdouble projMatrix[16],const GLint viewport[4],GLdouble *winx,GLdouble *winy,GLdouble *winz);
-int WINAPI gluUnProject(GLdouble winx,GLdouble winy,GLdouble winz,const GLdouble modelMatrix[16],const GLdouble projMatrix[16],const GLint viewport[4],GLdouble *objx,GLdouble *objy,GLdouble *objz);
-int WINAPI gluScaleImage(GLenum format,GLint widthin,GLint heightin,GLenum typein,const void *datain,GLint widthout,GLint heightout,GLenum typeout,void *dataout);
-int WINAPI gluBuild1DMipmaps(GLenum target,GLint components,GLint width,GLenum format,GLenum type,const void *data);
-int WINAPI gluBuild2DMipmaps(GLenum target,GLint components,GLint width,GLint height,GLenum format,GLenum type,const void *data);
+const GLubyte *APIENTRY gluErrorString(GLenum errCode);
+const wchar_t *APIENTRY gluErrorUnicodeStringEXT(GLenum errCode);
+const GLubyte *APIENTRY gluGetString(GLenum name);
+void APIENTRY gluOrtho2D(GLdouble left,GLdouble right,GLdouble bottom,GLdouble top);
+void APIENTRY gluPerspective(GLdouble fovy,GLdouble aspect,GLdouble zNear,GLdouble zFar);
+void APIENTRY gluPickMatrix(GLdouble x,GLdouble y,GLdouble width,GLdouble height,GLint viewport[4]);
+void APIENTRY gluLookAt(GLdouble eyex,GLdouble eyey,GLdouble eyez,GLdouble centerx,GLdouble centery,GLdouble centerz,GLdouble upx,GLdouble upy,GLdouble upz);
+int APIENTRY gluProject(GLdouble objx,GLdouble objy,GLdouble objz,const GLdouble modelMatrix[16],const GLdouble projMatrix[16],const GLint viewport[4],GLdouble *winx,GLdouble *winy,GLdouble *winz);
+int APIENTRY gluUnProject(GLdouble winx,GLdouble winy,GLdouble winz,const GLdouble modelMatrix[16],const GLdouble projMatrix[16],const GLint viewport[4],GLdouble *objx,GLdouble *objy,GLdouble *objz);
+int APIENTRY gluScaleImage(GLenum format,GLint widthin,GLint heightin,GLenum typein,const void *datain,GLint widthout,GLint heightout,GLenum typeout,void *dataout);
+int APIENTRY gluBuild1DMipmaps(GLenum target,GLint components,GLint width,GLenum format,GLenum type,const void *data);
+int APIENTRY gluBuild2DMipmaps(GLenum target,GLint components,GLint width,GLint height,GLenum format,GLenum type,const void *data);
 
 #ifdef __cplusplus
 
@@ -55,43 +55,43 @@ typedef struct GLUtesselator GLUtesselatorObj;
 typedef struct GLUtesselator GLUtriangulatorObj;
 #endif
 
-GLUquadric *WINAPI gluNewQuadric(void);
-void WINAPI gluDeleteQuadric(GLUquadric *state);
-void WINAPI gluQuadricNormals(GLUquadric *quadObject,GLenum normals);
-void WINAPI gluQuadricTexture(GLUquadric *quadObject,GLboolean textureCoords);
-void WINAPI gluQuadricOrientation(GLUquadric *quadObject,GLenum orientation);
-void WINAPI gluQuadricDrawStyle(GLUquadric *quadObject,GLenum drawStyle);
-void WINAPI gluCylinder(GLUquadric *qobj,GLdouble baseRadius,GLdouble topRadius,GLdouble height,GLint slices,GLint stacks);
-void WINAPI gluDisk(GLUquadric *qobj,GLdouble innerRadius,GLdouble outerRadius,GLint slices,GLint loops);
-void WINAPI gluPartialDisk(GLUquadric *qobj,GLdouble innerRadius,GLdouble outerRadius,GLint slices,GLint loops,GLdouble startAngle,GLdouble sweepAngle);
-void WINAPI gluSphere(GLUquadric *qobj,GLdouble radius,GLint slices,GLint stacks);
-void WINAPI gluQuadricCallback(GLUquadric *qobj,GLenum which,void (CALLBACK *fn)());
-GLUtesselator *WINAPI gluNewTess(void);
-void WINAPI gluDeleteTess(GLUtesselator *tess);
-void WINAPI gluTessBeginPolygon(GLUtesselator *tess,void *polygon_data);
-void WINAPI gluTessBeginContour(GLUtesselator *tess);
-void WINAPI gluTessVertex(GLUtesselator *tess,GLdouble coords[3],void *data);
-void WINAPI gluTessEndContour(GLUtesselator *tess);
-void WINAPI gluTessEndPolygon(GLUtesselator *tess);
-void WINAPI gluTessProperty(GLUtesselator *tess,GLenum which,GLdouble value);
-void WINAPI gluTessNormal(GLUtesselator *tess,GLdouble x,GLdouble y,GLdouble z);
-void WINAPI gluTessCallback(GLUtesselator *tess,GLenum which,void (CALLBACK *fn)());
-void WINAPI gluGetTessProperty(GLUtesselator *tess,GLenum which,GLdouble *value);
-GLUnurbs *WINAPI gluNewNurbsRenderer(void);
-void WINAPI gluDeleteNurbsRenderer(GLUnurbs *nobj);
-void WINAPI gluBeginSurface(GLUnurbs *nobj);
-void WINAPI gluBeginCurve(GLUnurbs *nobj);
-void WINAPI gluEndCurve(GLUnurbs *nobj);
-void WINAPI gluEndSurface(GLUnurbs *nobj);
-void WINAPI gluBeginTrim(GLUnurbs *nobj);
-void WINAPI gluEndTrim(GLUnurbs *nobj);
-void WINAPI gluPwlCurve(GLUnurbs *nobj,GLint count,GLfloat *array,GLint stride,GLenum type);
-void WINAPI gluNurbsCurve(GLUnurbs *nobj,GLint nknots,GLfloat *knot,GLint stride,GLfloat *ctlarray,GLint order,GLenum type);
-void WINAPI gluNurbsSurface(GLUnurbs *nobj,GLint sknot_count,float *sknot,GLint tknot_count,GLfloat *tknot,GLint s_stride,GLint t_stride,GLfloat *ctlarray,GLint sorder,GLint torder,GLenum type);
-void WINAPI gluLoadSamplingMatrices(GLUnurbs *nobj,const GLfloat modelMatrix[16],const GLfloat projMatrix[16],const GLint viewport[4]);
-void WINAPI gluNurbsProperty(GLUnurbs *nobj,GLenum property,GLfloat value);
-void WINAPI gluGetNurbsProperty(GLUnurbs *nobj,GLenum property,GLfloat *value);
-void WINAPI gluNurbsCallback(GLUnurbs *nobj,GLenum which,void (CALLBACK *fn)());
+GLUquadric *APIENTRY gluNewQuadric(void);
+void APIENTRY gluDeleteQuadric(GLUquadric *state);
+void APIENTRY gluQuadricNormals(GLUquadric *quadObject,GLenum normals);
+void APIENTRY gluQuadricTexture(GLUquadric *quadObject,GLboolean textureCoords);
+void APIENTRY gluQuadricOrientation(GLUquadric *quadObject,GLenum orientation);
+void APIENTRY gluQuadricDrawStyle(GLUquadric *quadObject,GLenum drawStyle);
+void APIENTRY gluCylinder(GLUquadric *qobj,GLdouble baseRadius,GLdouble topRadius,GLdouble height,GLint slices,GLint stacks);
+void APIENTRY gluDisk(GLUquadric *qobj,GLdouble innerRadius,GLdouble outerRadius,GLint slices,GLint loops);
+void APIENTRY gluPartialDisk(GLUquadric *qobj,GLdouble innerRadius,GLdouble outerRadius,GLint slices,GLint loops,GLdouble startAngle,GLdouble sweepAngle);
+void APIENTRY gluSphere(GLUquadric *qobj,GLdouble radius,GLint slices,GLint stacks);
+void APIENTRY gluQuadricCallback(GLUquadric *qobj,GLenum which,void (CALLBACK *fn)());
+GLUtesselator *APIENTRY gluNewTess(void);
+void APIENTRY gluDeleteTess(GLUtesselator *tess);
+void APIENTRY gluTessBeginPolygon(GLUtesselator *tess,void *polygon_data);
+void APIENTRY gluTessBeginContour(GLUtesselator *tess);
+void APIENTRY gluTessVertex(GLUtesselator *tess,GLdouble coords[3],void *data);
+void APIENTRY gluTessEndContour(GLUtesselator *tess);
+void APIENTRY gluTessEndPolygon(GLUtesselator *tess);
+void APIENTRY gluTessProperty(GLUtesselator *tess,GLenum which,GLdouble value);
+void APIENTRY gluTessNormal(GLUtesselator *tess,GLdouble x,GLdouble y,GLdouble z);
+void APIENTRY gluTessCallback(GLUtesselator *tess,GLenum which,void (CALLBACK *fn)());
+void APIENTRY gluGetTessProperty(GLUtesselator *tess,GLenum which,GLdouble *value);
+GLUnurbs *APIENTRY gluNewNurbsRenderer(void);
+void APIENTRY gluDeleteNurbsRenderer(GLUnurbs *nobj);
+void APIENTRY gluBeginSurface(GLUnurbs *nobj);
+void APIENTRY gluBeginCurve(GLUnurbs *nobj);
+void APIENTRY gluEndCurve(GLUnurbs *nobj);
+void APIENTRY gluEndSurface(GLUnurbs *nobj);
+void APIENTRY gluBeginTrim(GLUnurbs *nobj);
+void APIENTRY gluEndTrim(GLUnurbs *nobj);
+void APIENTRY gluPwlCurve(GLUnurbs *nobj,GLint count,GLfloat *array,GLint stride,GLenum type);
+void APIENTRY gluNurbsCurve(GLUnurbs *nobj,GLint nknots,GLfloat *knot,GLint stride,GLfloat *ctlarray,GLint order,GLenum type);
+void APIENTRY gluNurbsSurface(GLUnurbs *nobj,GLint sknot_count,float *sknot,GLint tknot_count,GLfloat *tknot,GLint s_stride,GLint t_stride,GLfloat *ctlarray,GLint sorder,GLint torder,GLenum type);
+void APIENTRY gluLoadSamplingMatrices(GLUnurbs *nobj,const GLfloat modelMatrix[16],const GLfloat projMatrix[16],const GLint viewport[4]);
+void APIENTRY gluNurbsProperty(GLUnurbs *nobj,GLenum property,GLfloat value);
+void APIENTRY gluGetNurbsProperty(GLUnurbs *nobj,GLenum property,GLfloat *value);
+void APIENTRY gluNurbsCallback(GLUnurbs *nobj,GLenum which,void (CALLBACK *fn)());
 
 typedef void (CALLBACK *GLUquadricErrorProc)(GLenum);
 typedef void (CALLBACK *GLUtessBeginProc)(GLenum);
@@ -232,9 +232,9 @@ typedef void (CALLBACK *GLUnurbsErrorProc)(GLenum);
 #define GLU_NURBS_ERROR36 100286
 #define GLU_NURBS_ERROR37 100287
 
-void WINAPI gluBeginPolygon(GLUtesselator *tess);
-void WINAPI gluNextContour(GLUtesselator *tess,GLenum type);
-void WINAPI gluEndPolygon(GLUtesselator *tess);
+void APIENTRY gluBeginPolygon(GLUtesselator *tess);
+void APIENTRY gluNextContour(GLUtesselator *tess,GLenum type);
+void APIENTRY gluEndPolygon(GLUtesselator *tess);
 
 #define GLU_CW 100120
 #define GLU_CCW 100121
