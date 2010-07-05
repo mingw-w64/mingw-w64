ecc8fb668970d1ec56e9a7ea410dc8e8a5d188df
Doug Semler <dougsemler@gmail.com>
2010-07-05 18:29:55 +0000

    Fix cproj to project z back to z.
    The C99 standard specifies that z projects to z, except for complex
    infinities.  This fixes the implementation to project to z directly
    back to z, rather than stereographically.
    
    mingw-w64-crt/ChangeLog:
    2010-07-05  Doug Semler  <dougsemler@gmail.com>
    
    	* complex/cproj.def.h (cproj): Project z back to z, rather than
    	  stereographically.
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@2781 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-crt/complex/cproj.def.h
+++ mingw-w64-crt/complex/cproj.def.h
@@ -46,22 +46,13 @@ __FLT_TYPE __complex__ __cdecl
 __FLT_ABI(cproj) (__FLT_TYPE __complex__ z)
 {
   __complex__ __FLT_TYPE ret;
-  __FLT_TYPE d;
 
-  if (isnan (__real__ z) && isnan (__imag__ z))
-    return z;
-
-  if (!isfinite (__real__ z) || !isfinite (__imag__ z))
+  if (isinf (__real__ z) || isinf (__imag__ z))
   {
     __real__ ret = INFINITY;
     __imag__ ret = __FLT_ABI(copysign) (__FLT_CST(0.0), __imag__ z);
     return ret;
   }
 
-  d = __real__ z * __real__ z + __imag__ z * __imag__ z + __FLT_CST(1.0);
-
-  __real__ ret = (__FLT_CST(2.0) * __real__ z) / d;
-  __imag__ ret = (__FLT_CST(2.0) * __imag__ z) / d;
-
-  return ret;
+  return z;
 }
