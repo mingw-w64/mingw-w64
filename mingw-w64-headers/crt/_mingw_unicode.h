8b791834b8004e931f174698ab79a66beebfce01
Ozkan Sezer <sezeroz@gmail.com>
2010-07-17 13:10:33 +0000

    * _mingw_unicode.h (__MINGW_STRING_AW): Added comment that it is the same as TEXT() from winnt.h.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@2859 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/crt/_mingw_unicode.h
+++ mingw-w64-headers/crt/_mingw_unicode.h
@@ -13,7 +13,7 @@
 # define __MINGW_NAME_AW_EXT(func,ext) func##W##ext
 # define __MINGW_NAME_UAW(func) func##_W
 # define __MINGW_NAME_UAW_EXT(func,ext) func##_W_##ext
-# define __MINGW_STRING_AW(str) L##str
+# define __MINGW_STRING_AW(str) L##str	/* same as TEXT() from winnt.h */
 # define __MINGW_PROCNAMEEXT_AW "W"
 #else
 # define _INC_CRT_UNICODE_MACROS 2
@@ -21,7 +21,7 @@
 # define __MINGW_NAME_AW_EXT(func,ext) func##A##ext
 # define __MINGW_NAME_UAW(func) func##_A
 # define __MINGW_NAME_UAW_EXT(func,ext) func##_A_##ext
-# define __MINGW_STRING_AW(str) str
+# define __MINGW_STRING_AW(str) str	/* same as TEXT() from winnt.h */
 # define __MINGW_PROCNAMEEXT_AW "A"
 #endif
 
