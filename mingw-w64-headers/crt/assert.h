bcb43f9a36789237a03a2e5e4960dd0ed72db4f6
Ozkan Sezer <sezeroz@gmail.com>
2010-07-18 14:47:17 +0000

    * assert.h (assert): Check against both _UNICODE and UNICODE. 	Reword and reformat a few places.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@2895 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/crt/assert.h
+++ mingw-w64-headers/crt/assert.h
@@ -4,11 +4,12 @@
  * No warranty is given; refer to the file DISCLAIMER.PD within this package.
  */
 
-/* According to C99 standard (section 7.2) the assert macro shall be refined for each time
-   assert.h gets included dependent to NDEBUG define.  */
+/* According to C99 standard (section 7.2) the assert
+   macro shall be redefined each time assert.h gets
+   included depending on the status of NDEBUG macro.  */
 #if defined (__STDC_VERSION__) && __STDC_VERSION__ >= 199901L
 #undef assert
-#endif
+#endif /* C99 */
 
 #ifndef __ASSERT_H_
 #define __ASSERT_H_
@@ -58,17 +59,17 @@ _assert (const char *_Message, const char *_File, unsigned _Line);
 #ifdef NDEBUG
 #define assert(_Expression) ((void)0)
 #else /* !defined (NDEBUG) */
-#ifdef _UNICODE
+#if defined(_UNICODE) || defined(UNICODE)
 #define assert(_Expression) \
  (void) \
  ((!!(_Expression)) || \
   (_wassert(_CRT_WIDE(#_Expression),_CRT_WIDE(__FILE__),__LINE__),0))
-#else
+#else /* not unicode */
 #define assert(_Expression) \
  (void) \
  ((!!(_Expression)) || \
   (_assert(#_Expression,__FILE__,__LINE__),0))
-#endif
-
+#endif /* _UNICODE||UNICODE */
 #endif /* !defined (NDEBUG) */
 #endif /* !defined assert */
+
