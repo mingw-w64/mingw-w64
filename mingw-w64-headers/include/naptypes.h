87f7b9cc0809e21a912f21f348b7c70460e259ee
Jonathan Yong <10walls@gmail.com>
2010-08-11 03:27:02 +0000

    Move incorrectly placed structs and enums in naptypes.h to nsemail.h
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@3232 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/naptypes.h
+++ mingw-w64-headers/include/naptypes.h
@@ -88,17 +88,6 @@ typedef enum tagFailureCategory {
     napNotifyTypeQuarState      = 2
   } NapNotifyType;
   
-  typedef enum _NAPI_PROVIDER_LEVEL {
-    ProviderLevel_None        = 0,
-    ProviderLevel_Secondary,
-    ProviderLevel_Primary
-  } NAPI_PROVIDER_LEVEL;
-
-  typedef enum _NAPI_PROVIDER_TYPE {
-    ProviderType_Application   = 1,
-    ProviderType_Service
-  } NAPI_PROVIDER_TYPE;
-
   typedef struct tagResultCodes {
     UINT16  count;
     HRESULT *results;
@@ -188,21 +177,6 @@ typedef enum tagFailureCategory {
     FixupInfo            fixupInfo;
   } SystemHealthAgentState;
   
-typedef struct _NAPI_DOMAIN_DESCRIPTION_BLOB {
-  DWORD AuthLevel;
-  DWORD cchDomainName;
-  DWORD OffsetNextDomainDescription;
-  DWORD OffsetThisDomainName;
-} NAPI_DOMAIN_DESCRIPTION_BLOB, *PNAPI_DOMAIN_DESCRIPTION_BLOB;
-
-typedef struct _NAPI_PROVIDER_INSTALLATION_BLOB {
-  DWORD dwVersion;
-  DWORD dwProviderType;
-  DWORD fSupportsWildCard;
-  DWORD cDomains;
-  DWORD OffsetFirstDomain;
-} NAPI_PROVIDER_INSTALLATION_BLOB, *PNAPI_PROVIDER_INSTALLATION_BLOB;
-
 #ifdef __cplusplus
 }
 #endif
