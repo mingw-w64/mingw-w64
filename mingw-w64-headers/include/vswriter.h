785daba74a9303139fa20157c3b9a647c3e29cc1
Kai Tietz <ktietz70@googlemail.com>
2010-08-12 15:18:16 +0000

    Add one missing type VSS_USAGE_TYPE. There are more, classes for sure too ...
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@3286 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/vswriter.h
+++ mingw-w64-headers/include/vswriter.h
@@ -6,10 +6,19 @@
 #ifndef _INC_VSWRITER
 #define _INC_VSWRITER
 
-typedef enum VSS_COMPONENT_FLAGS {
-  VSS_CF_BACKUP_RECOVERY         = 0x00000001,
-  VSS_CF_APP_ROLLBACK_RECOVERY   = 0x00000002,
-  VSS_CF_NOT_SYSTEM_STATE        = 0x00000004 
-} VSS_COMPONENT_FLAGS;
+  typedef enum VSS_COMPONENT_FLAGS {
+    VSS_CF_BACKUP_RECOVERY         = 0x00000001,
+    VSS_CF_APP_ROLLBACK_RECOVERY   = 0x00000002,
+    VSS_CF_NOT_SYSTEM_STATE        = 0x00000004 
+  } VSS_COMPONENT_FLAGS;
+
+  /* http://msdn.microsoft.com/en-us/library/aa384976%28v=VS.85%29.aspx */
+  typedef enum VSS_USAGE_TYPE {
+    VSS_UT_UNDEFINED             = 0,
+    VSS_UT_BOOTABLESYSTEMSTATE   = 1,
+    VSS_UT_SYSTEMSERVICE         = 2,
+    VSS_UT_USERDATA              = 3,
+    VSS_UT_OTHER                 = 4 
+  } VSS_USAGE_TYPE;
 
 #endif /*_INC_VSWRITER*/
