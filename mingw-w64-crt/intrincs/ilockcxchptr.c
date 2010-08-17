e1f24e015fbfb69a0f68b7bdf7c551f0d7a6dcff
Kai Tietz <ktietz70@googlemail.com>
2010-08-17 19:59:09 +0000

    Fix some part for x86 and intrinsic ...
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@3390 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-crt/intrincs/ilockcxchptr.c
+++ mingw-w64-crt/intrincs/ilockcxchptr.c
@@ -1,11 +1,29 @@
 #include <intrin.h>
 
+#ifdef _WIN64
 void *_InterlockedCompareExchangePointer(void * volatile *Destination, void *ExChange, void *Comperand)
 {
   void *prev;
   __asm__ __volatile__("lock ; cmpxchgq %1,%2" : "=a" (prev) : "q" (ExChange),"m" (*Destination), "0" (Comperand) : "memory");
   return prev;
 }
+#else
+void *_InterlockedCompareExchangePointer(void * volatile *Destination, void *ExChange, void *Comperand);
 
+void *_InterlockedCompareExchangePointer(void * volatile *Destination, void *ExChange, void *Comperand)
+{
+  void *prev;
+  __asm__ __volatile__("lock ; cmpxchgl %1,%2" : "=a" (prev) : "q" (ExChange),"m" (*Destination), "0" (Comperand) : "memory");
+  return prev;
+}
+#endif
+
+#ifdef _WIN64
 void *InterlockedCompareExchangePointer(void * volatile *, void *, void *) __attribute__((alias("_InterlockedCompareExchangePointer")));
+#else
+void * __stdcall InterlockedCompareExchangePointer(void * volatile *Destination, void *ExChange, void *Comperand)
+{
+  return _InterlockedCompareExchangePointer(Destination, ExChange, Comperand);
+}
+#endif
 
