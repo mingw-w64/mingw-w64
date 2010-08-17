09149e418fbd28dd7b309fc67fd1cf8b606a3c19
Jonathan Yong <10walls@gmail.com>
2010-08-17 00:23:54 +0000

    Uncomment functions and add __cplusplus extern "C"
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@3378 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/msctfmonitorapi.h
+++ mingw-w64-headers/include/msctfmonitorapi.h
@@ -8,12 +8,19 @@
 #define _INC_MSCTFMONITORAPI
 
 #if (_WIN32_WINNT >= 0x0600)
-/* CALLBACKS
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 HRESULT CALLBACK UninitLocalMsCtfMonitor(void);
 HRESULT CALLBACK InitLocalMsCtfMonitor(
   DWORD dwFlags
 );
-*/
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif /*(_WIN32_WINNT >= 0x0600)*/
 
 #endif _INC_MSCTFMONITORAPI
