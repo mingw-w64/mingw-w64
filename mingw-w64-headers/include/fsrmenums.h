551ada103a4ebbb7859b3b0a93c0526ed2cc1230
Jonathan Yong <10walls@gmail.com>
2010-08-24 07:10:29 +0000

    Add missing FSRM interfaces.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@3434 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/fsrmenums.h
+++ mingw-w64-headers/include/fsrmenums.h
@@ -28,6 +28,14 @@ typedef enum _FsrmActionType {
   FsrmActionType_Report     = 4 
 } FsrmActionType;
 
+typedef enum _FsrmClassificationLoggingFlags {
+  FsrmClassificationLoggingFlags_None                         = 0x00000000,
+  FsrmClassificationLoggingFlags_ClassificationsInLogFile     = 0x00000001,
+  FsrmClassificationLoggingFlags_ErrorsInLogFile              = 0x00000002,
+  FsrmClassificationLoggingFlags_ClassificationsInSystemLog   = 0x00000004,
+  FsrmClassificationLoggingFlags_ErrorsInSystemLog            = 0x00000008 
+} FsrmClassificationLoggingFlags;
+
 typedef enum _FsrmCollectionState {
   FsrmCollectionState_Fetching     = 1,
   FsrmCollectionState_Committing   = 2,
@@ -54,9 +62,95 @@ typedef enum _FsrmEventType {
   FsrmEventType_Error         = 3 
 } FsrmEventType;
 
+typedef enum _FsrmExecutionOption {
+  FsrmExecutionOption_Unknown                            = 0,
+  FsrmExecutionOption_EvaluateUnset                      = 1,
+  FsrmExecutionOption_ReEvaluate_ConsiderExistingValue   = 2,
+  FsrmExecutionOption_ReEvaluate_IgnoreExistingValue     = 3 
+} FsrmExecutionOption;
+
+typedef enum _FsrmFileManagementLoggingFlags {
+  FsrmFileManagementLoggingFlags_None          = 0x00000000,
+  FsrmFileManagementLoggingFlags_Error         = 0x00000001,
+  FsrmFileManagementLoggingFlags_Information   = 0x00000002,
+  FsrmFileManagementLoggingFlags_Audit         = 0x00000004 
+} FsrmFileManagementLoggingFlags;
+
+typedef enum _FsrmFileManagementType {
+  FsrmFileManagementType_Unknown      = 0,
+  FsrmFileManagementType_Expiration   = 1,
+  FsrmFileManagementType_Custom       = 2 
+} FsrmFileManagementType;
+
+typedef enum _FsrmFileStreamingInterfaceType {
+  FsrmFileStreamingInterfaceType_Unknown      = 0x00000000,
+  FsrmFileStreamingInterfaceType_ILockBytes   = 0x00000001,
+  FsrmFileStreamingInterfaceType_IStream      = 0x00000002 
+} FsrmFileStreamingInterfaceType;
+
+typedef enum _FsrmFileStreamingMode {
+  FsrmFileStreamingMode_Unknown   = 0x00000000,
+  FsrmFileStreamingMode_Read      = 0x00000001,
+  FsrmFileStreamingMode_Write     = 0x00000002 
+} FsrmFileStreamingMode;
+
 typedef enum _FsrmFileScreenFlags {
   FsrmFileScreenFlags_Enforce   = 0x00000001 
 } FsrmFileScreenFlags;
+
+typedef enum _FsrmPipelineModuleType {
+  FsrmPipelineModuleType_Unknown      = 0,
+  FsrmPipelineModuleType_Storage      = 1,
+  FsrmPipelineModuleType_Classifier   = 2 
+} FsrmPipelineModuleType;
+
+typedef enum _FsrmPropertyBagFlags {
+  FsrmPropertyBagFlags_UpdatedByClassifier           = 0x00000001,
+  FsrmPropertyBagFlags_FailedLoadingProperties       = 0x00000002,
+  FsrmPropertyBagFlags_FailedSavingProperties        = 0x00000004,
+  FsrmPropertyBagFlags_FailedClassifyingProperties   = 0x00000008 
+} FsrmPropertyBagFlags;
+
+typedef enum _FsrmPropertyConditionType {
+  FsrmPropertyConditionType_Unknown       = 0,
+  FsrmPropertyConditionType_Equal         = 1,
+  FsrmPropertyConditionType_NotEqual      = 2,
+  FsrmPropertyConditionType_GreaterThan   = 3,
+  FsrmPropertyConditionType_LessThan      = 4,
+  FsrmPropertyConditionType_Contain       = 5,
+  FsrmPropertyConditionType_Exist         = 6,
+  FsrmPropertyConditionType_NotExist      = 7,
+  FsrmPropertyConditionType_StartWith     = 8,
+  FsrmPropertyConditionType_EndWith       = 9,
+  FsrmPropertyConditionType_ContainedIn   = 10,
+  FsrmPropertyConditionType_PrefixOf      = 11,
+  FsrmPropertyConditionType_SuffixOf      = 12 
+} FsrmPropertyConditionType;
+
+typedef enum _FsrmPropertyDefinitionType {
+  FsrmPropertyDefinitionType_Unknown           = 0,
+  FsrmPropertyDefinitionType_OrderedList       = 1,
+  FsrmPropertyDefinitionType_MultiChoiceList   = 2,
+  FsrmPropertyDefinitionType_String            = 4,
+  FsrmPropertyDefinitionType_MultiString       = 5,
+  FsrmPropertyDefinitionType_Int               = 6,
+  FsrmPropertyDefinitionType_Bool              = 7,
+  FsrmPropertyDefinitionType_Date              = 8 
+} FsrmPropertyDefinitionType;
+
+typedef enum _FsrmPropertyFlags {
+  FsrmPropertyFlags_Orphaned                      = 0x00000001,
+  FsrmPropertyFlags_RetrievedFromCache            = 0x00000002,
+  FsrmPropertyFlags_RetrievedFromStorage          = 0x00000004,
+  FsrmPropertyFlags_SetByClassifier               = 0x00000008,
+  FsrmPropertyFlags_Deleted                       = 0x00000010,
+  FsrmPropertyFlags_Reclassified                  = 0x00000020,
+  FsrmPropertyFlags_AggregationFailed             = 0x00000040,
+  FsrmPropertyFlags_Existing                      = 0x00000080,
+  FsrmPropertyFlags_FailedLoadingProperties       = 0x00000100,
+  FsrmPropertyFlags_FailedClassifyingProperties   = 0x00000200,
+  FsrmPropertyFlags_PropertySourceMask            = 0x0000000E 
+} FsrmPropertyFlags;
 
 typedef enum _FsrmQuotaFlags {
   FsrmQuotaFlags_Enforce            = 0x00000100,
@@ -128,6 +222,30 @@ typedef enum _FsrmReportType {
   FsrmReportType_AutomaticClassification   = 11,
   FsrmReportType_Expiration                = 12 
 } FsrmReportType;
+
+typedef enum _FsrmRuleFlags {
+  FsrmRuleFlags_Disabled   = 0x00000100,
+  FsrmRuleFlags_Invalid    = 0x00001000 
+} FsrmRuleFlags;
+
+typedef enum _FsrmRuleType {
+  FsrmRuleType_Unknown          = 0,
+  FsrmRuleType_Classification   = 1,
+  FsrmRuleType_Generic          = 2 
+} FsrmRuleType;
+
+typedef enum _FsrmStorageModuleCaps {
+  FsrmStorageModuleCaps_Unknown   = 0x00000000,
+  FsrmStorageModuleCaps_CanGet    = 0x00000001,
+  FsrmStorageModuleCaps_CanSet    = 0x00000002 
+} FsrmStorageModuleCaps;
+
+typedef enum _FsrmStorageModuleType {
+  FsrmStorageModuleType_Unknown    = 0,
+  FsrmStorageModuleType_Cache      = 1,
+  FsrmStorageModuleType_InFile     = 2,
+  FsrmStorageModuleType_Database   = 3 
+} FsrmStorageModuleType;
 
 typedef enum _FsrmTemplateApplyOptions {
   FsrmTemplateApplyOptions_ApplyToDerivedMatching   = 1,
