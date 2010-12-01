91f302eabee426d9d852e444827d2aefcb8460e3
Kai Tietz <ktietz70@googlemail.com>
2010-12-01 10:31:27 +0000

    2010-12-01  Pascal Obry  <pascal@obry.net>
            * stdio/_stat.c: Handle trailing slash for path.
            * stdio/_wstat.c: Likewise
    
    Subject: [PATCH] (stat): Remove trailing slash in stat routine as this fails when calling
      Windows _stat() routine. For some obscure reasons:
    
        stat("c:\windows")     works as expected
        stst("c:\windows\")    fails and returns -1
    
    (wstat): Likewise.
    
    This patch is to be consistent with the UNIX behavior. It fixes a
    problem when calling gcc with a directory with ending slash as in:
    
       $ gcc -I/mingw32/include/ main.c
    
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@3832 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-crt/stdio/_wstat.c
+++ mingw-w64-crt/stdio/_wstat.c
@@ -1,6 +1,63 @@
 #define __CRT__NO_INLINE
 #include <sys/stat.h>
 
+/**
+ * Returns _path without trailing slash if any
+ *
+ * - if _path has no trailing slash, the function returns it
+ * - if _path has a trailing slash, but is of the form C:/, then it returns it
+ * - otherwise, the function creates a new string, which is a copy of _path
+ *   without the trailing slash. It is then the responsibility of the caller
+ *   to free it.
+ */
+
+static wchar_t*
+_mingw_no_trailing_slash (const wchar_t* _path)
+{
+  int len;
+  wchar_t *p;
+  int start;
+
+  p = (wchar_t*)_path;
+
+  if (_path && *_path) {
+    len = wcslen (_path);
+
+    /* Ignore X:\ */
+
+    if (len <= 1 || ((len == 2 || len == 3) && _path[1] == L':'))
+      return p;
+
+    /* Check UNC \\abc\<name>\ */
+    if ((_path[0] == L'\\' || _path[0] == L'/')
+	&& (_path[1] == L'\\' || _path[1] == L'/'))
+      {
+	const wchar_t *r = &_path[2];
+	while (*r != 0 && *r != L'\\' && *r != L'/')
+	  ++r;
+	if (*r != 0)
+	  ++r;
+	if (*r == 0)
+	  return p;
+	while (*r != 0 && *r != L'\\' && *r != L'/')
+	  ++r;
+	if (*r != 0)
+	  ++r;
+	if (*r == 0)
+	  return p;
+      }
+
+    if (_path[len - 1] == L'/' || _path[len - 1] == L'\\')
+      {
+	p = (wchar_t*)malloc (len * sizeof(wchar_t));
+	memcpy (p, _path, (len - 1) * sizeof(wchar_t));
+	p[len - 1] = L'\0';
+      }
+  }
+
+  return p;
+}
+
 /* FIXME: Relying on _USE_32BIT_TIME_T, which is a user-macro,
 during CRT compilation is plainly broken.  Need an appropriate
 implementation to provide users the ability of compiling the
@@ -10,7 +67,13 @@ int __cdecl
 wstat(const wchar_t *_Filename,struct stat *_Stat)
 {
   struct _stat32 st;
-  int ret=_wstat32(_Filename,&st);
+  wchar_t *_path = _mingw_no_trailing_slash(_Filename);
+
+  int ret=_wstat32(_path,&st);
+
+  if (_path != _Filename)
+    free (_path);
+
   if (ret == -1) {
     memset(_Stat,0,sizeof(struct stat));
     return -1;
@@ -25,7 +88,13 @@ int __cdecl
 wstat(const wchar_t *_Filename,struct stat *_Stat)
 {
   struct _stat64 st;
-  int ret=_wstat64(_Filename,&st);
+  wchar_t *_path = _mingw_no_trailing_slash(_Filename);
+
+  int ret=_wstat64(_path,&st);
+
+  if (_path != _Filename)
+    free (_path);
+
   if (ret == -1) {
     memset(_Stat,0,sizeof(struct stat));
     return -1;
