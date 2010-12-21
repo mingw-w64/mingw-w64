0893abcb678d6bebdc76562a6e3fb1127b09c6a5
Kai Tietz <ktietz70@googlemail.com>
2010-12-21 09:18:54 +0000

    Sorry need to use linker symbol here
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@3909 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-crt/crt/tlssup.c
+++ mingw-w64-crt/crt/tlssup.c
@@ -50,7 +50,7 @@ _CRTALLOC(".tls$ZZZ") char _tls_end = 0;
 _CRTALLOC(".CRT$XLA") PIMAGE_TLS_CALLBACK __xl_a = 0;
 _CRTALLOC(".CRT$XLZ") PIMAGE_TLS_CALLBACK __xl_z = 0;
 
-_CRTALLOC(".tls") const IMAGE_TLS_DIRECTORY __MINGW_USYMBOL(_tls_used) = {
+_CRTALLOC(".tls") const IMAGE_TLS_DIRECTORY __MINGW_LSYMBOL(_tls_used) = {
   (ULONG_PTR) &_tls_start+1, (ULONG_PTR) &_tls_end,
   (ULONG_PTR) &_tls_index, (ULONG_PTR) (&__xl_a+1),
   (ULONG) 0, (ULONG) 0
