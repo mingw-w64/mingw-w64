82aeae2e8a1b05b32f3eaa1931355fab98deb5d9
Jacek Caban <cjacek@gmail.com>
2011-03-25 14:56:59 +0000

    Added missing prototypes to vsnprintf_s.c
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@4095 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-crt/stdio/vsnprintf_s.c
+++ mingw-w64-crt/stdio/vsnprintf_s.c
@@ -6,7 +6,9 @@
 #define __CRT__NO_INLINE
 #include <stdarg.h>
 #include <stdio.h>
-#include <sec_api/stdio_s.h>
+
+int __cdecl vsnprintf_s(char *_DstBuf,size_t _DstSize,size_t _MaxCount,const char *_Format,va_list _ArgList);
+_CRTIMP int __cdecl _vsnprintf_s(char *_DstBuf,size_t _DstSize,size_t _MaxCount,const char *_Format,va_list _ArgList);
 
 int __cdecl vsnprintf_s (char *s, size_t _DstSize, size_t _MaxCount, const char *format, va_list arg)
 {
