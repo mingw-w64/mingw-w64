e102d5d7cd390e8c3de642fbb277d53e99de6662
Jacek Caban <cjacek@gmail.com>
2011-07-08 12:38:03 +0000

    identitystore.h: Removed IPropertyStore declaration that is declared in propsys.idl
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@4260 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/identitystore.h
+++ mingw-w64-headers/include/identitystore.h
@@ -6,50 +6,7 @@
 #ifndef __IDENTITYSTORE_H__
 #define __IDENTITYSTORE_H__
 
-#if (_WIN32_WINNT >= 0x0600)
 #include <objbase.h>
-
-#ifndef __IPropertyStore_FWD_DEFINED__
-#define __IPropertyStore_FWD_DEFINED__
-typedef struct IPropertyStore IPropertyStore;
-#endif
-
-#undef  INTERFACE
-#define INTERFACE IPropertyStore
-#ifdef __GNUC__
-#warning COM interfaces layout in this header has not been verified.
-#warning COM interfaces with incorrect layout may not work at all.
-__MINGW_BROKEN_INTERFACE(INTERFACE)
-#endif
-DECLARE_INTERFACE_(IPropertyStore,IUnknown)
-{
-    BEGIN_INTERFACE
-
-    /* IUnknown methods */
-    STDMETHOD(QueryInterface)(THIS_ REFIID riid, void **ppvObject) PURE;
-    STDMETHOD_(ULONG, AddRef)(THIS) PURE;
-    STDMETHOD_(ULONG, Release)(THIS) PURE;
-
-    /* IPropertyStore methods */
-    STDMETHOD_(HRESULT,Commit)(THIS) PURE;
-    STDMETHOD_(HRESULT,GetAt)(THIS_ DWORD iProp,PROPERTYKEY *pkey) PURE;
-    STDMETHOD_(HRESULT,GetCount)(THIS_ DWORD *cProps) PURE;
-    STDMETHOD_(HRESULT,GetValue)(THIS_ REFPROPERTYKEY key,PROPVARIANT *pv) PURE;
-    STDMETHOD_(HRESULT,SetValue)(THIS_ REFPROPERTYKEY key,REFPROPVARIANT propvar) PURE;
-
-    END_INTERFACE
-};
-#ifdef COBJMACROS
-#define IPropertyStore_QueryInterface(This,riid,ppvObject) (This)->pVtbl->QueryInterface(This,riid,ppvObject)
-#define IPropertyStore_AddRef(This) (This)->pVtbl->AddRef(This)
-#define IPropertyStore_Release(This) (This)->pVtbl->Release(This)
-#define IPropertyStore_Commit() (This)->lpVtbl->Commit(This)
-#define IPropertyStore_GetAt(This,iProp,pkey) (This)->lpVtbl->GetAt(This,iProp,pkey)
-#define IPropertyStore_GetCount(This,cProps) (This)->lpVtbl->GetCount(This,cProps)
-#define IPropertyStore_GetValue(This,key,pv) (This)->lpVtbl->GetValue(This,key,pv)
-#define IPropertyStore_SetValue(This,key,propvar) (This)->lpVtbl->SetValue(This,key,propvar)
-#endif /*COBJMACROS*/
-#endif /*(_WIN32_WINNT >= 0x0600)*/
 
 #if (_WIN32_WINNT >= 0x0601)
 
