6617ed6488510221ab1133f63407313f48e30569
Ozkan Sezer <sezeroz@gmail.com>
2011-09-21 09:51:13 +0000

    _timeval.h (timercmp): Added missing parens around the AND condition.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/stable/v1.x@4486 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/_timeval.h
+++ mingw-w64-headers/include/_timeval.h
@@ -16,7 +16,7 @@ struct timeval
 #define timerisset(tvp)		((tvp)->tv_sec || (tvp)->tv_usec)
 #define timercmp(tvp,uvp,cmp)					\
 		((tvp)->tv_sec cmp (uvp)->tv_sec ||		\
-		 (tvp)->tv_sec == (uvp)->tv_sec && (tvp)->tv_usec cmp (uvp)->tv_usec)
+		 ((tvp)->tv_sec == (uvp)->tv_sec && (tvp)->tv_usec cmp (uvp)->tv_usec))
 #define timerclear(tvp)		(tvp)->tv_sec = (tvp)->tv_usec = 0
 
 #endif /* _TIMEVAL_DEFINED */
