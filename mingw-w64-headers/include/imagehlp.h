1362007b2a2567d696a0117512460959ac33330a
Ozkan Sezer <sezeroz@gmail.com>
2011-10-19 07:11:36 +0000

    imagehlp.h: Fixed misplaced extern "C" {} braces which used to break C_ASSERTs. (bug #3425478.)
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@4549 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/imagehlp.h
+++ mingw-w64-headers/include/imagehlp.h
@@ -16,10 +16,6 @@
 
 #include <wintrust.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 #include <psdk_inc/_dbg_LOAD_IMAGE.h>
 
   typedef enum _IMAGEHLP_STATUS_REASON {
@@ -44,6 +40,10 @@ extern "C" {
     BindForwarderNOT32,
     BindForwarderNOT64
   } IMAGEHLP_STATUS_REASON;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
   typedef WINBOOL (WINAPI *PIMAGEHLP_STATUS_ROUTINE)(IMAGEHLP_STATUS_REASON Reason,PCSTR ImageName,PCSTR DllName,ULONG_PTR Va,ULONG_PTR Parameter);
   typedef WINBOOL (WINAPI *PIMAGEHLP_STATUS_ROUTINE32)(IMAGEHLP_STATUS_REASON Reason,PCSTR ImageName,PCSTR DllName,ULONG Va,ULONG_PTR Parameter);
@@ -103,9 +103,10 @@ extern "C" {
   WINBOOL IMAGEAPI UpdateDebugInfoFile(PCSTR ImageFileName,PCSTR SymbolPath,PSTR DebugFilePath,PIMAGE_NT_HEADERS32 NtHeaders);
   WINBOOL IMAGEAPI UpdateDebugInfoFileEx(PCSTR ImageFileName,PCSTR SymbolPath,PSTR DebugFilePath,PIMAGE_NT_HEADERS32 NtHeaders,DWORD OldChecksum);
 
-#include <psdk_inc/_dbg_common.h>
-
 #ifdef __cplusplus
 }
 #endif
+
+#include <psdk_inc/_dbg_common.h>
+
 #endif
