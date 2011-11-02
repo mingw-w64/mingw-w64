92542830859b8a9e8d0439578e39ba78f0ebab22
Ozkan Sezer <sezeroz@gmail.com>
2011-11-02 08:20:46 +0000

    Break stdio/asprintf.c and misc/uchar.c into pieces, one function per compilation unit: 	* stdio/vasprintf.c: New. 	* misc/uchar_c16rtomb.c: New. 	* misc/uchar_c32rtomb.c: New. 	* misc/uchar_mbrtoc16.c: New. 	* misc/uchar_mbrtoc32.c: New. 	* Makefile.am: Adjust. 	* Makefile.in: Regenerate.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@4567 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-crt/stdio/vasprintf.c
+++ mingw-w64-crt/stdio/vasprintf.c
@@ -4,28 +4,21 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-int __mingw_asprintf(char ** __restrict__ ret,
-		     const char * __restrict__ format,
-		     ...) {
-  va_list ap;
+int __mingw_vasprintf(char ** __restrict__ ret,
+		      const char * __restrict__ format,
+		      va_list ap) {
   int len;
-  va_start(ap,format);
   /* Get Length */
   len = __mingw_vsnprintf(NULL,0,format,ap);
-  if (len < 0) goto _end;
+  if (len < 0) return -1;
   /* +1 for \0 terminator. */
   *ret = malloc(len + 1);
   /* Check malloc fail*/
-  if (!*ret) {
-    len = -1;
-    goto _end;
-  }
+  if (!*ret) return -1;
   /* Write String */
   __mingw_vsnprintf(*ret,len,format,ap);
   /* Terminate explicitly */
   *ret[len] = '\0';
-  _end:
-  va_end(ap);
   return len;
 }
 
