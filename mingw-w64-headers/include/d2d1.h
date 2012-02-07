5fc9875f67fc107fce82d371e8753e64667d2788
Jacek Caban <cjacek@gmail.com>
2012-02-07 12:28:27 +0000

    d2d1.h: Fixed ID2D1HwndRenderTarget declaration
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@4804 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/d2d1.h
+++ mingw-w64-headers/include/d2d1.h
@@ -1714,142 +1714,39 @@ interface ID2D1GradientStopCollection {
 
 #endif
 
-#define INTERFACE ID2D1HwndRenderTarget
-DECLARE_INTERFACE_(ID2D1HwndRenderTarget, ID2D1RenderTarget)
-{
-  BEGIN_INTERFACE
+#ifndef D2D_USE_C_DEFINITIONS
 
-  /* IUnknown methods */
-  STDMETHOD(QueryInterface)(THIS_ REFIID riid, void **ppvObject) PURE;
-  STDMETHOD_(ULONG, AddRef)(THIS) PURE;
-  STDMETHOD_(ULONG, Release)(THIS) PURE;
+interface ID2D1HwndRenderTarget : public ID2D1RenderTarget {
+    STDMETHOD_(D2D1_WINDOW_STATE, CheckWindowState)() PURE;
+    STDMETHOD(Resize)(const D2D1_SIZE_U *pixelSize) PURE;
+    STDMETHOD_(HWND, GetHwnd)() const PURE;
 
-  /* ID2D1Resource methods */
-  STDMETHOD_(void, GetFactory)(THIS_ ID2D1Factory **factory) PURE;
-
-  /* ID2D1RenderTarget methods */
-  STDMETHOD(CreateBitmap)(ID2D1HwndRenderTarget *This, D2D1_SIZE_U size, const void *srcData, UINT32 pitch, const D2D1_BITMAP_PROPERTIES *bitmapProperties, ID2D1Bitmap **bitmap) PURE;
-  STDMETHOD(CreateBitmapFromWicBitmap)(ID2D1HwndRenderTarget *This, IWICBitmapSource *wicBitmapSource, const D2D1_BITMAP_PROPERTIES *bitmapProperties, ID2D1Bitmap **bitmap) PURE;
-  STDMETHOD(CreateSharedBitmap)(ID2D1HwndRenderTarget *This, REFIID riid, void *data, const D2D1_BITMAP_PROPERTIES *bitmapProperties, ID2D1Bitmap **bitmap) PURE;
-  STDMETHOD(CreateBitmapBrush)(ID2D1HwndRenderTarget *This, ID2D1Bitmap *bitmap, const D2D1_BITMAP_BRUSH_PROPERTIES *bitmapBrushProperties, const D2D1_BRUSH_PROPERTIES *brushProperties, ID2D1BitmapBrush **bitmapBrush) PURE;
-  STDMETHOD(CreateSolidColorBrush)(ID2D1HwndRenderTarget *This, const D2D1_COLOR_F *color, const D2D1_BRUSH_PROPERTIES *brushProperties, ID2D1SolidColorBrush **solidColorBrush) PURE;
-  STDMETHOD(CreateGradientStopCollection)(ID2D1HwndRenderTarget *This, const D2D1_GRADIENT_STOP *gradientStops, UINT gradientStopsCount, D2D1_GAMMA colorInterpolationGamma, D2D1_EXTEND_MODE extendMode, ID2D1GradientStopCollection **gradientStopCollection) PURE;
-  STDMETHOD(CreateLinearGradientBrush)(ID2D1HwndRenderTarget *This, const D2D1_LINEAR_GRADIENT_BRUSH_PROPERTIES *linearGradientBrushProperties, const D2D1_BRUSH_PROPERTIES *brushProperties, ID2D1GradientStopCollection *gradientStopCollection, ID2D1LinearGradientBrush **linearGradientBrush) PURE;
-  STDMETHOD(CreateRadialGradientBrush)(ID2D1HwndRenderTarget *This, const D2D1_RADIAL_GRADIENT_BRUSH_PROPERTIES *radialGradientBrushProperties, const D2D1_BRUSH_PROPERTIES *brushProperties, ID2D1GradientStopCollection *gradientStopCollection, ID2D1RadialGradientBrush **radialGradientBrush) PURE;
-  STDMETHOD(CreateCompatibleRenderTarget)(ID2D1HwndRenderTarget *This, const D2D1_SIZE_F *desiredSize, const D2D1_SIZE_U *desiredPixelSize, const D2D1_PIXEL_FORMAT *desiredFormat, D2D1_COMPATIBLE_RENDER_TARGET_OPTIONS options, ID2D1BitmapRenderTarget **bitmapRenderTarget) PURE;
-  STDMETHOD(CreateLayer)(ID2D1HwndRenderTarget *This, const D2D1_SIZE_F *size, ID2D1Layer **layer) PURE;
-  STDMETHOD(CreateMesh)(ID2D1HwndRenderTarget *This, ID2D1Mesh **mesh) PURE;
-  STDMETHOD_(void, DrawLine)(ID2D1HwndRenderTarget *This, D2D1_POINT_2F point0, D2D1_POINT_2F point1, ID2D1Brush *brush, FLOAT strokeWidth, ID2D1StrokeStyle *strokeStyle) PURE;
-  STDMETHOD_(void, DrawRectangle)(ID2D1HwndRenderTarget *This, const D2D1_RECT_F *rect, ID2D1Brush *brush, FLOAT strokeWidth, ID2D1StrokeStyle *strokeStyle) PURE;
-  STDMETHOD_(void, FillRectangle)(ID2D1HwndRenderTarget *This, const D2D1_RECT_F *rect, ID2D1Brush *brush) PURE;
-  STDMETHOD_(void, DrawRoundedRectangle)(ID2D1HwndRenderTarget *This, const D2D1_ROUNDED_RECT *roundedRect, ID2D1Brush *brush, FLOAT strokeWidth, ID2D1StrokeStyle *strokeStyle) PURE;
-  STDMETHOD_(void, FillRoundedRectangle)(ID2D1HwndRenderTarget *This, const D2D1_ROUNDED_RECT *roundedRect, ID2D1Brush *brush) PURE;
-  STDMETHOD_(void, DrawEllipse)(ID2D1HwndRenderTarget *This, const D2D1_ELLIPSE *ellipse, ID2D1Brush *brush, FLOAT strokeWidth, ID2D1StrokeStyle *strokeStyle) PURE;
-  STDMETHOD_(void, FillEllipse)(ID2D1HwndRenderTarget *This, const D2D1_ELLIPSE *ellipse, ID2D1Brush *brush) PURE;
-  STDMETHOD_(void, DrawGeometry)(ID2D1HwndRenderTarget *This, ID2D1Geometry *geometry, ID2D1Brush *brush, FLOAT strokeWidth, ID2D1StrokeStyle *strokeStyle) PURE;
-  STDMETHOD_(void, FillGeometry)(ID2D1HwndRenderTarget *This, ID2D1Geometry *geometry, ID2D1Brush *brush, ID2D1Brush *opacityBrush) PURE;
-  STDMETHOD_(void, FillMesh)(ID2D1HwndRenderTarget *This, ID2D1Mesh *mesh, ID2D1Brush *brush) PURE;
-  STDMETHOD_(void, FillOpacityMask)(ID2D1HwndRenderTarget *This, ID2D1Bitmap *opacityMask, ID2D1Brush *brush, D2D1_OPACITY_MASK_CONTENT content, const D2D1_RECT_F *destinationRectangle, const D2D1_RECT_F *sourceRectangle) PURE;
-  STDMETHOD_(void, DrawBitmap)(ID2D1HwndRenderTarget *This, ID2D1Bitmap *bitmap, const D2D1_RECT_F *destinationRectangle, FLOAT opacity, D2D1_BITMAP_INTERPOLATION_MODE interpolationMode, const D2D1_RECT_F *sourceRectangle) PURE;
-  STDMETHOD_(void, DrawText)(ID2D1HwndRenderTarget *This, const WCHAR *string, UINT stringLength, IDWriteTextFormat *textFormat, const D2D1_RECT_F *layoutRect, ID2D1Brush *defaultForegroundBrush, D2D1_DRAW_TEXT_OPTIONS options, DWRITE_MEASURING_MODE measuringMode) PURE;
-  STDMETHOD_(void, DrawTextLayout)(ID2D1HwndRenderTarget *This, D2D1_POINT_2F origin, IDWriteTextLayout *textLayout, ID2D1Brush *defaultForegroundBrush, D2D1_DRAW_TEXT_OPTIONS options) PURE;
-  STDMETHOD_(void, DrawGlyphRun)(ID2D1HwndRenderTarget *This, D2D1_POINT_2F baselineOrigin, const DWRITE_GLYPH_RUN *glyphRun, ID2D1Brush *foregroundBrush, DWRITE_MEASURING_MODE measuringMode) PURE;
-  STDMETHOD_(void, SetTransform)(ID2D1HwndRenderTarget *This, const D2D1_MATRIX_3X2_F *transform) PURE;
-  STDMETHOD_(void, GetTransform)(ID2D1HwndRenderTarget *This, D2D1_MATRIX_3X2_F *transform) PURE;
-  STDMETHOD_(void, SetAntialiasMode)(ID2D1HwndRenderTarget *This, D2D1_ANTIALIAS_MODE antialiasMode) PURE;
-  STDMETHOD_(D2D1_ANTIALIAS_MODE, GetAntialiasMode)(ID2D1HwndRenderTarget *This) PURE;
-  STDMETHOD_(void, SetTextAntialiasMode)(ID2D1HwndRenderTarget *This, D2D1_TEXT_ANTIALIAS_MODE textAntialiasMode) PURE;
-  STDMETHOD_(D2D1_TEXT_ANTIALIAS_MODE, GetTextAntialiasMode)(ID2D1HwndRenderTarget *This) PURE;
-  STDMETHOD_(void, SetTextRenderingParams)(ID2D1HwndRenderTarget *This, IDWriteRenderingParams *textRenderingParams) PURE;
-  STDMETHOD_(void, GetTextRenderingParams)(ID2D1HwndRenderTarget *This, IDWriteRenderingParams **textRenderingParams) PURE;
-  STDMETHOD_(void, SetTags)(ID2D1HwndRenderTarget *This, D2D1_TAG tag1, D2D1_TAG tag2) PURE;
-  STDMETHOD_(void, GetTags)(ID2D1HwndRenderTarget *This, D2D1_TAG *tag1, D2D1_TAG *tag2) PURE;
-  STDMETHOD_(void, PushLayer)(ID2D1HwndRenderTarget *This, const D2D1_LAYER_PARAMETERS *layerParameters, ID2D1Layer *layer) PURE;
-  STDMETHOD_(void, PopLayer)(ID2D1HwndRenderTarget *This) PURE;
-  STDMETHOD(Flush)(ID2D1HwndRenderTarget *This, D2D1_TAG *tag1, D2D1_TAG *tag2) PURE;
-  STDMETHOD_(void, SaveDrawingState)(ID2D1HwndRenderTarget *This, ID2D1DrawingStateBlock *drawingStateBlock) PURE;
-  STDMETHOD_(void, RestoreDrawingState)(ID2D1HwndRenderTarget *This, ID2D1DrawingStateBlock *drawingStateBlock) PURE;
-  STDMETHOD_(void, PushAxisAlignedClip)(ID2D1HwndRenderTarget *This, const D2D1_RECT_F *clipRect, D2D1_ANTIALIAS_MODE antialiasMode) PURE;
-  STDMETHOD_(void, PopAxisAlignedClip)(ID2D1HwndRenderTarget *This) PURE;
-  STDMETHOD_(void, Clear)(ID2D1HwndRenderTarget *This, const D2D1_COLOR_F *clearColor) PURE;
-  STDMETHOD_(void, BeginDraw)(ID2D1HwndRenderTarget *This) PURE;
-  STDMETHOD(EndDraw)(ID2D1HwndRenderTarget *This, D2D1_TAG *tag1, D2D1_TAG *tag2) PURE;
-  STDMETHOD_(D2D1_PIXEL_FORMAT, GetPixelFormat)(ID2D1HwndRenderTarget *This) PURE;
-  STDMETHOD_(void, SetDpi)(ID2D1HwndRenderTarget *This, FLOAT dpiX, FLOAT dpiY) PURE;
-  STDMETHOD_(void, GetDpi)(ID2D1HwndRenderTarget *This, FLOAT *dpiX, FLOAT *dpiY) PURE;
-  STDMETHOD_(D2D1_SIZE_F, GetSize)(ID2D1HwndRenderTarget *This) PURE;
-  STDMETHOD_(D2D1_SIZE_U, GetPixelSize)(ID2D1HwndRenderTarget *This) PURE;
-  STDMETHOD_(UINT32, GetMaximumBitmapSize)(ID2D1HwndRenderTarget *This) PURE;
-  STDMETHOD_(BOOL, IsSupported)(ID2D1HwndRenderTarget *This, const D2D1_RENDER_TARGET_PROPERTIES *renderTargetProperties) PURE;
-
-  /* ID2D1HwndRenderTarget methods */
-  STDMETHOD_(D2D1_WINDOW_STATE, CheckWindowState)(THIS) PURE;
-  STDMETHOD(Resize)(THIS_ D2D1_SIZE_U *pixelSize) PURE;
-  STDMETHOD_(HWND, GetHwnd)(THIS) PURE;
-
-  END_INTERFACE
+    HRESULT Resize(const D2D1_SIZE_U &pixelSize) {
+        return Resize(&pixelSize);
+    }
 };
-#undef INTERFACE
 
-#define ID2D1HwndRenderTarget_QueryInterface(this,A,B) (this)->lpVtbl->QueryInterface(this,A,B)
-#define ID2D1HwndRenderTarget_AddRef(this) (this)->lpVtbl->AddRef(this)
-#define ID2D1HwndRenderTarget_Release(this) (this)->lpVtbl->Release(this)
-#define ID2D1HwndRenderTarget_BeginDraw(this) (this)->lpVtbl->BeginDraw(this)
-#define ID2D1HwndRenderTarget_Clear(this,A) (this)->lpVtbl->Clear(this,A)
-#define ID2D1HwndRenderTarget_CreateBitmap(this,A,B,C,D,E) (this)->lpVtbl->CreateBitmap(this,A,B,C,D,E)
-#define ID2D1HwndRenderTarget_CreateBitmapBrush(this,A,B) (this)->lpVtbl->CreateBitmapBrush(this,A,B)
-#define ID2D1HwndRenderTarget_CreateBitmapFromWicBitmap(this,A,B) (this)->lpVtbl->CreateBitmapFromWicBitmap(this,A,B)
-#define ID2D1HwndRenderTarget_CreateCompatibleRenderTarget(this,A) (this)->lpVtbl->CreateCompatibleRenderTarget(this,A)
-#define ID2D1HwndRenderTarget_CreateGradientStopCollection(this,A,B,C) (this)->lpVtbl->CreateGradientStopCollection(this,A,B,C)
-#define ID2D1HwndRenderTarget_CreateLayer(this,A) (this)->lpVtbl->CreateLayer(this,A)
-#define ID2D1HwndRenderTarget_CreateLinearGradientBrush(this,A,B,C,D) (this)->lpVtbl->CreateLinearGradientBrush(this,A,B,C,D)
-#define ID2D1HwndRenderTarget_CreateMesh(this,A) (this)->lpVtbl->CreateMesh(this,A)
-#define ID2D1HwndRenderTarget_CreateRadialGradientBrush(this,A,B,C,D) (this)->lpVtbl->CreateRadialGradientBrush(this,A,B,C,D)
-#define ID2D1HwndRenderTarget_CreateSharedBitmap(this,A,B,C,D) (this)->lpVtbl->CreateSharedBitmap(this,A,B,C,D)
-#define ID2D1HwndRenderTarget_CreateSolidColorBrush(this,A,B,C) (this)->lpVtbl->CreateSolidColorBrush(this,A,B,C)
-#define ID2D1HwndRenderTarget_DrawBitmap(this,A,B,C,D,E) (this)->lpVtbl->DrawBitmap(this,A,B,C,D,E)
-#define ID2D1HwndRenderTarget_DrawEllipse(this,A,B,C,D) (this)->lpVtbl->DrawEllipse(this,A,B,C,D)
-#define ID2D1HwndRenderTarget_DrawGeometry(this,A,B,C,D) (this)->lpVtbl->DrawGeometry(this,A,B,C,D)
-#define ID2D1HwndRenderTarget_DrawGlyphRun(this,A,B,C,D) (this)->lpVtbl->DrawGlyphRun(this,A,B,C,D)
-#define ID2D1HwndRenderTarget_DrawLine(this,A,B,C,D,E) (this)->lpVtbl->DrawLine(this,A,B,C,D,E)
-#define ID2D1HwndRenderTarget_DrawRectangle(this,A,B,C,D) (this)->lpVtbl->DrawRectangle(this,A,B,C,D)
-#define ID2D1HwndRenderTarget_DrawRoundedRectangle(this,A,B,C,D) (this)->lpVtbl->DrawRoundedRectangle(this,A,B,C,D)
-#define ID2D1HwndRenderTarget_DrawText(this,A,B,C,D,E,F,G) (this)->lpVtbl->DrawText(this,A,B,C,D,E,F,G)
-#define ID2D1HwndRenderTarget_DrawTextLayout(this,A,B,C,D) (this)->lpVtbl->DrawTextLayout(this,A,B,C,D)
-#define ID2D1HwndRenderTarget_EndDraw(this,A,B) (this)->lpVtbl->EndDraw(this,A,B)
-#define ID2D1HwndRenderTarget_FillEllipse(this,A,B) (this)->lpVtbl->FillEllipse(this,A,B)
-#define ID2D1HwndRenderTarget_FillGeometry(this,A,B,C) (this)->lpVtbl->FillGeometry(this,A,B,C)
-#define ID2D1HwndRenderTarget_FillMesh(this,A,B) (this)->lpVtbl->FillMesh(this,A,B)
-#define ID2D1HwndRenderTarget_FillOpacityMask(this,A,B,C,D,E) (this)->lpVtbl->FillOpacityMask(this,A,B,C,D,E)
-#define ID2D1HwndRenderTarget_FillRectangle(this,A,B) (this)->lpVtbl->FillRectangle(this,A,B)
-#define ID2D1HwndRenderTarget_FillRoundedRectangle(this,A,B) (this)->lpVtbl->FillRoundedRectangle(this,A,B)
-#define ID2D1HwndRenderTarget_Flush(this,A,B) (this)->lpVtbl->Flush(this,A,B)
-#define ID2D1HwndRenderTarget_GetAntialiasMode(this) (this)->lpVtbl->GetAntialiasMode(this)
-#define ID2D1HwndRenderTarget_GetDpi(this,A,B) (this)->lpVtbl->GetDpi(this,A,B)
-#define ID2D1HwndRenderTarget_GetMaximumBitmapSize(this) (this)->lpVtbl->GetMaximumBitmapSize(this)
-#define ID2D1HwndRenderTarget_GetPixelFormat(this) (this)->lpVtbl->GetPixelFormat(this)
-#define ID2D1HwndRenderTarget_GetPixelSize(this) (this)->lpVtbl->GetPixelSize(this)
-#define ID2D1HwndRenderTarget_GetSize(this) (this)->lpVtbl->GetSize(this)
-#define ID2D1HwndRenderTarget_GetTags(this,A,B) (this)->lpVtbl->GetTags(this,A,B)
-#define ID2D1HwndRenderTarget_GetTextAntialiasMode(this) (this)->lpVtbl->GetTextAntialiasMode(this)
-#define ID2D1HwndRenderTarget_GetTextRenderingParams(this,A) (this)->lpVtbl->GetTextRenderingParams(this,A)
-#define ID2D1HwndRenderTarget_GetTransform(this,A) (this)->lpVtbl->GetTransform(this,A)
-#define ID2D1HwndRenderTarget_IsSupported(this,A) (this)->lpVtbl->IsSupported(this,A)
-#define ID2D1HwndRenderTarget_PopAxisAlignedClip(this) (this)->lpVtbl->PopAxisAlignedClip(this)
-#define ID2D1HwndRenderTarget_PopLayer(this) (this)->lpVtbl->PopLayer(this)
-#define ID2D1HwndRenderTarget_PushAxisAlignedClip(this,A,B) (this)->lpVtbl->PushAxisAlignedClip(this,A,B)
-#define ID2D1HwndRenderTarget_PushLayer(this,A,B) (this)->lpVtbl->PushLayer(this,A,B)
-#define ID2D1HwndRenderTarget_RestoreDrawingState(this,A) (this)->lpVtbl->RestoreDrawingState(this,A)
-#define ID2D1HwndRenderTarget_SaveDrawingState(this,A) (this)->lpVtbl->SaveDrawingState(this,A)
-#define ID2D1HwndRenderTarget_SetAntialiasMode(this,A) (this)->lpVtbl->SetAntialiasMode(this,A)
-#define ID2D1HwndRenderTarget_SetDpi(this,A,B) (this)->lpVtbl->SetDpi(this,A,B)
-#define ID2D1HwndRenderTarget_SetTags(this,A,B) (this)->lpVtbl->SetTags(this,A,B)
-#define ID2D1HwndRenderTarget_SetTextAntialiasMode(this,A) (this)->lpVtbl->SetTextAntialiasMode(this,A)
-#define ID2D1HwndRenderTarget_SetTextRenderingParams(this,A) (this)->lpVtbl->SetTextRenderingParams(this,A)
-#define ID2D1HwndRenderTarget_SetTransform(this,A) (this)->lpVtbl->SetTransform(this,A)
+#else
+
+typedef interface ID2D1HwndRenderTarget ID2D1HwndRenderTarget;
+
+typedef struct ID2D1HwndRenderTargetVtbl {
+    ID2D1RenderTargetVtbl Base;
+
+    STDMETHOD_(D2D1_WINDOW_STATE, CheckWindowState)(ID2D1HwndRenderTarget *This) PURE;
+    STDMETHOD(Resize)(ID2D1HwndRenderTarget *This, const D2D1_SIZE_U *pixelSize) PURE;
+    STDMETHOD_(HWND, GetHwnd)(ID2D1HwndRenderTarget *This) PURE;
+} ID2D1HwndRenderTargetVtbl;
+
+interface ID2D1HwndRenderTarget {
+    const struct ID2D1HwndRenderTargetVtbl *lpVtbl;
+};
+
 #define ID2D1HwndRenderTarget_CheckWindowState(this) (this)->lpVtbl->CheckWindowState(this)
 #define ID2D1HwndRenderTarget_GetHwnd(this) (this)->lpVtbl->GetHwnd(this)
 #define ID2D1HwndRenderTarget_Resize(this,A) (this)->lpVtbl->Resize(this,A)
+
+#endif
 
 DEFINE_GUID(IID_ID2D1Layer, 0x2cd9069b,0x12e2,0x11dc,0x9f,0xed,0x00,0x11,0x43,0xa0,0x55,0xf9);
 
