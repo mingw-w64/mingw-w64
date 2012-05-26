0ed3c56cb3fd9fbc94b25bb90d4fadea222d9c50
Jacek Caban <cjacek@gmail.com>
2012-05-26 18:45:27 +0000

    fusion.idl: Updated to current Wine version
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@5068 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/fusion.h
+++ mingw-w64-headers/include/fusion.h
@@ -79,7 +79,8 @@ typedef enum __WIDL_fusion_generated_name_00000000 {
     ASM_CACHE_ZAP = 0x1,
     ASM_CACHE_GAC = 0x2,
     ASM_CACHE_DOWNLOAD = 0x4,
-    ASM_CACHE_ROOT = 0x8
+    ASM_CACHE_ROOT = 0x8,
+    ASM_CACHE_ROOT_EX = 0x80
 } ASM_CACHE_FLAGS;
 typedef enum __WIDL_fusion_generated_name_00000001 {
     peNone = 0x0,
