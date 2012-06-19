e1a4ca1974d61852b0760f157fad763dcf8bae5a
Kai Tietz <ktietz70@googlemail.com>
2012-06-19 20:18:16 +0000

    * math/fdim.c: Adjust for being C99-posix compatible.         * math/fdimf.c: Likewise.         * math/fdiml.c: Likewise.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@5129 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-crt/math/fdim.c
+++ mingw-w64-crt/math/fdim.c
@@ -4,9 +4,21 @@
  * No warranty is given; refer to the file DISCLAIMER.PD within this package.
  */
 #include <math.h>
+#include <errno.h>
 
 double
 fdim (double x, double y)
 {
-  return  (isgreater(x, y) ? (x - y) : 0.0);
+  int cx = fpclassify (x), cy = fpclassify (y);
+  double r;
+
+  if (cx == FP_NAN || cy == FP_NAN
+      || (y < 0 && cx == FP_INFINITE && cy == FP_INFINITE))
+    return x - y;  /* Take care invalid flag is raised.  */
+  if (x <= y)
+    return 0.0;
+  r = x - y;
+  if (fplclassify (r) == FP_INFINITE)
+    errno = ERANGE;
+  return r;
 }
--- mingw-w64-crt/math/fdimf.c
+++ mingw-w64-crt/math/fdimf.c
@@ -3,10 +3,22 @@
  * This file is part of the w64 mingw-runtime package.
  * No warranty is given; refer to the file DISCLAIMER.PD within this package.
  */
+#include <errno.h>
 #include <math.h>
 
 float
 fdimf (float x, float y)
 {
-  return  (isgreater(x, y) ? (x - y) : 0.0F);
+  int cx = fpclassify (x), cy = fpclassify (y);
+  float r;
+
+  if (cx == FP_NAN || cy == FP_NAN
+      || (y < 0 && cx == FP_INFINITE && cy == FP_INFINITE))
+    return x - y;  /* Take care invalid flag is raised.  */
+  if (x <= y)
+    return 0.0f;
+  r = x - y;
+  if (fplclassify (r) == FP_INFINITE)
+    errno = ERANGE;
+  return r;
 }
