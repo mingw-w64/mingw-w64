ab2e0a84f89637b03db60591d578a72cdc004876
Kai Tietz <ktietz70@googlemail.com>
2012-06-19 14:12:32 +0000

    * math/isnan.c: Special-case for x64.         * math/isnanf.c: Likewise.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@5119 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-crt/math/isnan.c
+++ mingw-w64-crt/math/isnan.c
@@ -8,11 +8,23 @@
 int
 __isnan (double _x)
 {
+#ifdef __x86_64__
+    __mingw_dbl_type_t hlp;
+    int l, h;
+
+    hlp.x = _x;
+    l = hlp.lh.low;
+    h = hlp.lh.high & 0x7fffffff;
+    h |= (unsigned int) (l | -l) >> 31;
+    h = 0x7ff00000 - h;
+    return (int) ((unsigned int) h) >> 31;
+#else
   unsigned short _sw;
   __asm__ __volatile__ ("fxam;"
 	   "fstsw %%ax": "=a" (_sw) : "t" (_x));
   return (_sw & (FP_NAN | FP_NORMAL | FP_INFINITE | FP_ZERO | FP_SUBNORMAL))
     == FP_NAN;
+#endif
 }
 
 #undef isnan
--- mingw-w64-crt/math/isnanf.c
+++ mingw-w64-crt/math/isnanf.c
@@ -7,11 +7,21 @@
 int
 __isnanf (float _x)
 {
+#ifdef __x86_64__
+    __mingw_flt_type_t hlp;
+    int i;
+    
+    hlp.x = _x;
+    i = hlp.val & 0x7fffffff;
+    i = 0x7f800000 - i;
+    return (int) (((unsigned int) i) >> 31);
+#else
   unsigned short _sw;
   __asm__ __volatile__ ("fxam;"
 	   "fstsw %%ax": "=a" (_sw) : "t" (_x) );
   return (_sw & (FP_NAN | FP_NORMAL | FP_INFINITE | FP_ZERO | FP_SUBNORMAL))
     == FP_NAN;
+#endif
 }
 
 int __attribute__ ((alias ("__isnanf"))) isnanf (float);
