8a1da775883a62e55be928d5da8b06d29e34fe2a
Kai Tietz <ktietz70@googlemail.com>
2012-06-27 15:03:03 +0000

    Add new header sal.h for sake of _In_ and _Out_ definition.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@5135 4407c894-4637-0410-b4f5-ada5f102cad1

---
+++ mingw-w64-headers/include/sal.h
@@ -0,0 +1,17 @@
+/**
+ * This file has no copyright assigned and is placed in the Public Domain.
+ * This file is part of the w64 mingw-runtime package.
+ * No warranty is given; refer to the file DISCLAIMER.PD within this package.
+ */
+#include <specstrings.h>
+
+#ifndef SAL_HXX
+#define SAL_HXX
+
+#include <specstrings.h>
+
+#define _In_  SAL__in
+#define _Out_  SAL__out
+
+#endif
+
