ac5c22ecfb5f458a7d67bb009b5ab6dd6d1ca77d
Ozkan Sezer <sezeroz@gmail.com>
2012-06-28 16:27:27 +0000

    merge from trunk rev.5136: 	2012-06-27  Corinna Vinschen  <vinschen@redhat.com> 	* include/stralign.h: Fix and add #endif comments.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/stable/v2.x@5150 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/stralign.h
+++ mingw-w64-headers/include/stralign.h
@@ -54,7 +54,7 @@ extern "C" {
     return uaw_CharUpperW(String);
   }
 #endif /* !__CRT__NO_INLINE */
-#endif /* _X86_ */
+#endif /* CharUpper */
 
 #ifdef lstrcmp
   int ua_lstrcmpW(LPCUWSTR String1,LPCUWSTR String2);
@@ -137,7 +137,7 @@ extern "C" {
     return uaw_wcslen(String);
   }
 #endif /* !__CRT__NO_INLINE */
-#endif
+#endif /* _X86_ */
   int ua_wcsicmp(LPCUWSTR String1,LPCUWSTR String2);
 
 #ifndef __CRT__NO_INLINE
@@ -147,7 +147,7 @@ extern "C" {
     return uaw_wcsicmp(String1,String2);
   }
 #endif /* !__CRT__NO_INLINE */
-#endif
+#endif /* _WSTRING_DEFINED */
 
 #ifndef __UA_WCSLEN
 #define __UA_WCSLEN ua_wcslen
