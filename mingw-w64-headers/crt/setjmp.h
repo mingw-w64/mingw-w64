31c76a346cf3bf4a8f3d5a6398788907beae47fa
Kai Tietz <ktietz70@googlemail.com>
2012-07-04 13:13:33 +0000

    * setjmp.h (setjmp): Use frame-address builtin.         Reformat #if/#else block.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@5162 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/crt/setjmp.h
+++ mingw-w64-headers/crt/setjmp.h
@@ -148,34 +148,34 @@ extern "C" {
 #define _JMP_BUF_DEFINED
 #endif
 
-  void * __cdecl __attribute__ ((__nothrow__)) mingw_getsp(void);
+void * __cdecl __attribute__ ((__nothrow__)) mingw_getsp (void);
 
 #ifndef USE_NO_MINGW_SETJMP_TWO_ARGS
-#ifndef _INC_SETJMPEX
-#ifdef _WIN64
-#define setjmp(BUF) _setjmp((BUF), mingw_getsp())
-#else
-#define setjmp(BUF) _setjmp3((BUF), NULL)
-#endif
+#  ifndef _INC_SETJMPEX
+#    ifdef _WIN64
+#      define setjmp(BUF) _setjmp((BUF), __builtin_frame_address (0))
+#    else
+#      define setjmp(BUF) _setjmp3((BUF), NULL)
+#    endif
   int __cdecl __attribute__ ((__nothrow__,__returns_twice__)) _setjmp(jmp_buf _Buf, void *_Ctx);
   int __cdecl __attribute__ ((__nothrow__,__returns_twice__)) _setjmp3(jmp_buf _Buf, void *_Ctx);
-#else
-#undef setjmp
-#ifdef _WIN64
-#define setjmp(BUF) _setjmpex((BUF), mingw_getsp())
-#define setjmpex(BUF) _setjmpex((BUF), mingw_getsp())
-#else
-#define setjmp(BUF) _setjmpex((BUF), NULL)
-#define setjmpex(BUF) _setjmpex((BUF), NULL)
-#endif
+#  else
+#    undef setjmp
+#    ifdef _WIN64
+#      define setjmp(BUF) _setjmpex((BUF), __builtin_frame_address (0))
+#      define setjmpex(BUF) _setjmpex((BUF), __builtin_frame_address (0))
+#    else
+#      define setjmp(BUF) _setjmpex((BUF), NULL)
+#      define setjmpex(BUF) _setjmpex((BUF), NULL)
+#    endif
   int __cdecl __attribute__ ((__nothrow__,__returns_twice__)) _setjmpex(jmp_buf _Buf,void *_Ctx);
-#endif
+#  endif
 
 #else
 
-#ifndef _INC_SETJMPEX
-#define setjmp _setjmp
-#endif
+#  ifndef _INC_SETJMPEX
+#    define setjmp _setjmp
+#  endif
 
   int __cdecl __attribute__ ((__nothrow__,__returns_twice__)) setjmp(jmp_buf _Buf);
 #endif
