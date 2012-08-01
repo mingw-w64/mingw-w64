9910a91fb845ebac5d21dbe4be77a2900200b4e7
Kai Tietz <ktietz70@googlemail.com>
2012-08-01 16:59:07 +0000

    * secapi/_chsize_s.c: New file.         * secapi/_mktemp_s.c: New file.         * secapi/_wmktemp_s.c: New file.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@5313 4407c894-4637-0410-b4f5-ada5f102cad1

---
+++ mingw-w64-crt/secapi/_wmktemp_s.c
@@ -0,0 +1,56 @@
+#include <windows.h>
+#include <malloc.h>
+#include <errno.h>
+
+HMODULE __mingw_get_msvcrt_handle (void);
+size_t __cdecl wcsnlen (const wchar_t *, size_t);
+char * __cdecl _wmktemp (wchar_t *);
+errno_t __cdecl _wmktemp_s (wchar_t *, size_t);
+static errno_t __cdecl _int_wmktemp_s (wchar_t *, size_t);
+static errno_t __cdecl _stub (wchar_t *, size_t);
+
+errno_t __cdecl (*__MINGW_IMP_SYMBOL(_wmktemp_s))(wchar_t *, size_t) = 
+ _stub;
+
+static errno_t __cdecl
+_stub (wchar_t *d, size_t dn)
+{
+  errno_t __cdecl (*f)(wchar_t *, size_t) = __MINGW_IMP_SYMBOL(_wmktemp_s);
+
+  if (f == _stub)
+    {
+	f = (errno_t __cdecl (*)(wchar_t *, size_t))
+	    GetProcAddress (__mingw_get_msvcrt_handle (), "_wmktemp_s");
+	if (!f)
+	  f = _int_wmktemp_s;
+	__MINGW_IMP_SYMBOL(_wmktemp_s) = f;
+    }
+  return (*f)(d, dn);
+}
+
+errno_t __cdecl
+_wmktemp_s (wchar_t *d, size_t dn)
+{
+  return _stub (d, dn);
+}
+
+static errno_t __cdecl
+_int_wmktemp_s (wchar_t *d, size_t dn)
+{
+  size_t sz;
+  if (!d || !dn)
+    {
+      _wmktemp (NULL);
+      return EINVAL;
+    }
+  sz = wcsnlen (d, dn);
+  if (sz >= dn || sz < 6)
+    {
+      d[0] = 0;
+      _wmktemp (NULL);
+      return EINVAL;
+    }
+  if (_wmktemp (d) != NULL)
+    return 0;
+  return errno;
+}
