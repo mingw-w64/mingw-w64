ee2722e6f9253a649b1a0c4546b6643f93a467a2
Jacek Caban <cjacek@gmail.com>
2012-12-20 14:22:55 +0000

    comutil.h: Use s[w]printf_s for _COM_PRINTF_S_1 to avoid conflict with strsafe.h
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@5514 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/comutil.h
+++ mingw-w64-headers/include/comutil.h
@@ -13,7 +13,15 @@
 #endif
 
 #define _COM_MEMCPY_S(dest,destsize,src,count) memcpy(dest,src,count)
+
+/* Use of wsprintf might be impossible, if strsafe.h is included. */
+#ifndef __STDC_SECURE_LIB__
 #define _COM_PRINTF_S_1(dest,destsize,format,arg1) wsprintf(dest,format,arg1)
+#elif defined(UNICODE)
+#define _COM_PRINTF_S_1(dest,destsize,format,arg1) swprintf_s(dest,destsize,format,arg1)
+#else
+#define _COM_PRINTF_S_1(dest,destsize,format,arg1) sprintf_s(dest,destsize,format,arg1)
+#endif
 
 #ifdef __cplusplus
 
