875137bf397fd55e70d7bbd1afbf16d1c20d8cbf
Rafaël Carré <funman@videolan.org>
2013-01-21 20:28:43 +0000

    Add replacement for InitializeCriticalSection using InitializeCriticalSectionEx
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@5550 4407c894-4637-0410-b4f5-ada5f102cad1

---
+++ mingw-w64-libraries/winstorecompat/src/InitializeCriticalSection.c
@@ -0,0 +1,39 @@
+/*
+    Copyright (c) 2013 mingw-w64 project
+
+    Contributing authors: Jean-Baptiste Kempf
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
+*/
+
+#define _WIN32_WINNT 0x600 /* InitializeCriticalSectionEx is Vista+ */
+
+#define InitializeCriticalSection __InitializeCriticalSection
+#include <windef.h>
+#include <winbase.h>
+#undef InitializeCriticalSection
+
+HANDLE WINAPI InitializeCriticalSection(LPCRITICAL_SECTION lpCriticalSection)
+{
+    int flags = 0;
+    /* flags = CRITICAL_SECTION_NO_DEBUG_INFO */
+    return InitializeCriticalSectionEx(lpCriticalSection, 0, flags);
+}
+
+HANDLE WINAPI (*__MINGW_IMP_SYMBOL(InitializeCriticalSection))(LPCRITICAL_SECTION lpCriticalSection) asm("__imp__InitializeCriticalSection@4") = InitializeCriticalSection;
