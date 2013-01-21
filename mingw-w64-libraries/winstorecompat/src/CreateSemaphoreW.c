688ed344a45f2aafebd9a4df7ca16ed28fd64419
Rafaël Carré <funman@videolan.org>
2013-01-21 20:28:14 +0000

    Add replacement for CreateSemaphoreW using CreateSemaphoreExW
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@5549 4407c894-4637-0410-b4f5-ada5f102cad1

---
+++ mingw-w64-libraries/winstorecompat/src/CreateSemaphoreW.c
@@ -0,0 +1,40 @@
+/*
+    Copyright (c) 2013 mingw-w64 project
+
+    Contributing authors: Jean-Baptiste Kempf
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
+*/
+
+#define _WIN32_WINNT 0x600 /* CreateSemaphoreExW is Vista+ */
+
+#define CreateSemaphoreW __CreateSemaphoreW
+#include <windef.h>
+#include <winbase.h>
+#undef CreateSemaphoreW
+
+HANDLE WINAPI CreateSemaphoreW(LPSECURITY_ATTRIBUTES lpSemaphoreAttributes,
+                               LONG lInitialCount,
+                               LONG lMaximumCount,
+                               LPCWSTR lpName)
+{
+    return CreateSemaphoreExW(lpSemaphoreAttributes, lInitialCount, lMaximumCount, lpName, 0, EVENT_ALL_ACCESS);
+}
+
+HANDLE WINAPI (*__MINGW_IMP_SYMBOL(CreateSemaphoreW))(LPSECURITY_ATTRIBUTES lpSemaphoreAttributes, LONG lInitialCount, LONG lMaximumCount, LPCWSTR lpName) asm("__imp__CreateSemaphoreW@16") = CreateSemaphoreW;
