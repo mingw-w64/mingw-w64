740036f387bec79f67ed7dfbc5213730d4f4e316
Rafaël Carré <funman@videolan.org>
2013-01-23 16:26:33 +0000

    Add replacement for GetFileAttributesA/W
    Using GetFileAttributesExA/W
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@5554 4407c894-4637-0410-b4f5-ada5f102cad1

---
+++ mingw-w64-libraries/winstorecompat/src/GetFileAttributes.c
@@ -0,0 +1,55 @@
+/*
+    Copyright (c) 2013 mingw-w64 project
+
+    Contributing authors: Jean-Baptiste Kempf
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
+*/
+
+#define _WIN32_WINNT 0x502 /* GetFileAttributesEx is XP+ */
+
+#define GetFileAttributesW __GetFileAttributesW
+#define GetFileAttributesA __GetFileAttributesA
+#include <windef.h>
+#include <winbase.h>
+#undef GetFileAttributesA
+#undef GetFileAttributesW
+
+DWORD WINAPI GetFileAttributesW(LPCWSTR lpFileName)
+{
+    WIN32_FILE_ATTRIBUTE_DATA fileInformation;
+    if( GetFileAttributesExW(lpFileName, GetFileExInfoStandard, &fileInformation) != 0)
+        return fileInformation.dwFileAttributes;
+    else
+        return INVALID_FILE_ATTRIBUTES;
+}
+
+DWORD WINAPI (*__MINGW_IMP_SYMBOL(GetFileAttributesW))(LPCWSTR lpFileName) asm("__imp__GetFileAttributesW@4") = GetFileAttributesW;
+
+DWORD WINAPI GetFileAttributesA(LPCSTR lpFileName)
+{
+    WIN32_FILE_ATTRIBUTE_DATA fileInformation;
+    if( GetFileAttributesExA(lpFileName, GetFileExInfoStandard, &fileInformation) != 0)
+        return fileInformation.dwFileAttributes;
+    else
+        return INVALID_FILE_ATTRIBUTES;
+}
+
+DWORD WINAPI (*__MINGW_IMP_SYMBOL(GetFileAttributesA))(LPCWSTR lpFileName) asm("__imp__GetFileAttributesA@4") = GetFileAttributesW;
+
