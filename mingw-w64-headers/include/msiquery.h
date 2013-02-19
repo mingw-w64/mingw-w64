72be6f9a9c6f2cda4e689f9e22e29abdee6cfe62
Kai Tietz <ktietz70@googlemail.com>
2013-02-19 11:27:33 +0000

    Resorting of API for checking if something needs adjustemt for _WIN32_MSI >= 400.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@5598 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/msiquery.h
+++ mingw-w64-headers/include/msiquery.h
@@ -79,12 +79,10 @@ extern "C" {
 
   UINT WINAPI MsiDatabaseOpenViewA(MSIHANDLE hDatabase,LPCSTR szQuery,MSIHANDLE *phView);
   UINT WINAPI MsiDatabaseOpenViewW(MSIHANDLE hDatabase,LPCWSTR szQuery,MSIHANDLE *phView);
-
 #define MsiDatabaseOpenView __MINGW_NAME_AW(MsiDatabaseOpenView)
 
   MSIDBERROR WINAPI MsiViewGetErrorA(MSIHANDLE hView,LPSTR szColumnNameBuffer,DWORD *pcchBuf);
   MSIDBERROR WINAPI MsiViewGetErrorW(MSIHANDLE hView,LPWSTR szColumnNameBuffer,DWORD *pcchBuf);
-
 #define MsiViewGetError __MINGW_NAME_AW(MsiViewGetError)
 
   UINT WINAPI MsiViewExecute(MSIHANDLE hView,MSIHANDLE hRecord);
@@ -94,64 +92,52 @@ extern "C" {
   UINT WINAPI MsiViewClose(MSIHANDLE hView);
   UINT WINAPI MsiDatabaseGetPrimaryKeysA(MSIHANDLE hDatabase,LPCSTR szTableName,MSIHANDLE *phRecord);
   UINT WINAPI MsiDatabaseGetPrimaryKeysW(MSIHANDLE hDatabase,LPCWSTR szTableName,MSIHANDLE *phRecord);
-
 #define MsiDatabaseGetPrimaryKeys __MINGW_NAME_AW(MsiDatabaseGetPrimaryKeys)
 
   MSICONDITION WINAPI MsiDatabaseIsTablePersistentA(MSIHANDLE hDatabase,LPCSTR szTableName);
   MSICONDITION WINAPI MsiDatabaseIsTablePersistentW(MSIHANDLE hDatabase,LPCWSTR szTableName);
-
 #define MsiDatabaseIsTablePersistent __MINGW_NAME_AW(MsiDatabaseIsTablePersistent)
 
   UINT WINAPI MsiGetSummaryInformationA(MSIHANDLE hDatabase,LPCSTR szDatabasePath,UINT uiUpdateCount,MSIHANDLE *phSummaryInfo);
   UINT WINAPI MsiGetSummaryInformationW(MSIHANDLE hDatabase,LPCWSTR szDatabasePath,UINT uiUpdateCount,MSIHANDLE *phSummaryInfo);
-
 #define MsiGetSummaryInformation __MINGW_NAME_AW(MsiGetSummaryInformation)
 
   UINT WINAPI MsiSummaryInfoGetPropertyCount(MSIHANDLE hSummaryInfo,UINT *puiPropertyCount);
   UINT WINAPI MsiSummaryInfoSetPropertyA(MSIHANDLE hSummaryInfo,UINT uiProperty,UINT uiDataType,INT iValue,FILETIME *pftValue,LPCSTR szValue);
   UINT WINAPI MsiSummaryInfoSetPropertyW(MSIHANDLE hSummaryInfo,UINT uiProperty,UINT uiDataType,INT iValue,FILETIME *pftValue,LPCWSTR szValue);
-
 #define MsiSummaryInfoSetProperty __MINGW_NAME_AW(MsiSummaryInfoSetProperty)
 
   UINT WINAPI MsiSummaryInfoGetPropertyA(MSIHANDLE hSummaryInfo,UINT uiProperty,UINT *puiDataType,INT *piValue,FILETIME *pftValue,LPSTR szValueBuf,DWORD *pcchValueBuf);
   UINT WINAPI MsiSummaryInfoGetPropertyW(MSIHANDLE hSummaryInfo,UINT uiProperty,UINT *puiDataType,INT *piValue,FILETIME *pftValue,LPWSTR szValueBuf,DWORD *pcchValueBuf);
-
 #define MsiSummaryInfoGetProperty __MINGW_NAME_AW(MsiSummaryInfoGetProperty)
 
   UINT WINAPI MsiSummaryInfoPersist(MSIHANDLE hSummaryInfo);
   UINT WINAPI MsiOpenDatabaseA(LPCSTR szDatabasePath,LPCSTR szPersist,MSIHANDLE *phDatabase);
   UINT WINAPI MsiOpenDatabaseW(LPCWSTR szDatabasePath,LPCWSTR szPersist,MSIHANDLE *phDatabase);
-
 #define MsiOpenDatabase __MINGW_NAME_AW(MsiOpenDatabase)
 
   UINT WINAPI MsiDatabaseImportA(MSIHANDLE hDatabase,LPCSTR szFolderPath,LPCSTR szFileName);
   UINT WINAPI MsiDatabaseImportW(MSIHANDLE hDatabase,LPCWSTR szFolderPath,LPCWSTR szFileName);
-
 #define MsiDatabaseImport __MINGW_NAME_AW(MsiDatabaseImport)
 
   UINT WINAPI MsiDatabaseExportA(MSIHANDLE hDatabase,LPCSTR szTableName,LPCSTR szFolderPath,LPCSTR szFileName);
   UINT WINAPI MsiDatabaseExportW(MSIHANDLE hDatabase,LPCWSTR szTableName,LPCWSTR szFolderPath,LPCWSTR szFileName);
-
 #define MsiDatabaseExport __MINGW_NAME_AW(MsiDatabaseExport)
 
   UINT WINAPI MsiDatabaseMergeA(MSIHANDLE hDatabase,MSIHANDLE hDatabaseMerge,LPCSTR szTableName);
   UINT WINAPI MsiDatabaseMergeW(MSIHANDLE hDatabase,MSIHANDLE hDatabaseMerge,LPCWSTR szTableName);
-
 #define MsiDatabaseMerge __MINGW_NAME_AW(MsiDatabaseMerge)
 
   UINT WINAPI MsiDatabaseGenerateTransformA(MSIHANDLE hDatabase,MSIHANDLE hDatabaseReference,LPCSTR szTransformFile,int iReserved1,int iReserved2);
   UINT WINAPI MsiDatabaseGenerateTransformW(MSIHANDLE hDatabase,MSIHANDLE hDatabaseReference,LPCWSTR szTransformFile,int iReserved1,int iReserved2);
-
 #define MsiDatabaseGenerateTransform __MINGW_NAME_AW(MsiDatabaseGenerateTransform)
 
   UINT WINAPI MsiDatabaseApplyTransformA(MSIHANDLE hDatabase,LPCSTR szTransformFile,int iErrorConditions);
   UINT WINAPI MsiDatabaseApplyTransformW(MSIHANDLE hDatabase,LPCWSTR szTransformFile,int iErrorConditions);
-
 #define MsiDatabaseApplyTransform __MINGW_NAME_AW(MsiDatabaseApplyTransform)
 
   UINT WINAPI MsiCreateTransformSummaryInfoA(MSIHANDLE hDatabase,MSIHANDLE hDatabaseReference,LPCSTR szTransformFile,int iErrorConditions,int iValidation);
   UINT WINAPI MsiCreateTransformSummaryInfoW(MSIHANDLE hDatabase,MSIHANDLE hDatabaseReference,LPCWSTR szTransformFile,int iErrorConditions,int iValidation);
-
 #define MsiCreateTransformSummaryInfo __MINGW_NAME_AW(MsiCreateTransformSummaryInfo)
 
   UINT WINAPI MsiDatabaseCommit(MSIHANDLE hDatabase);
@@ -160,122 +146,112 @@ extern "C" {
   WINBOOL WINAPI MsiRecordIsNull(MSIHANDLE hRecord,UINT iField);
   UINT WINAPI MsiRecordDataSize(MSIHANDLE hRecord,UINT iField);
   UINT WINAPI MsiRecordSetInteger(MSIHANDLE hRecord,UINT iField,int iValue);
-  UINT WINAPI MsiRecordSetStringA(MSIHANDLE hRecord,UINT iField,LPCSTR szValue);
-  UINT WINAPI MsiRecordSetStringW(MSIHANDLE hRecord,UINT iField,LPCWSTR szValue);
-
-#define MsiRecordSetString __MINGW_NAME_AW(MsiRecordSetString)
-
   int WINAPI MsiRecordGetInteger(MSIHANDLE hRecord,UINT iField);
-
-  UINT WINAPI MsiRecordGetStringA(MSIHANDLE hRecord,UINT iField,LPSTR szValueBuf,DWORD *pcchValueBuf);
-  UINT WINAPI MsiRecordGetStringW(MSIHANDLE hRecord,UINT iField,LPWSTR szValueBuf,DWORD *pcchValueBuf);
-
-#define MsiRecordGetString __MINGW_NAME_AW(MsiRecordGetString)
-
   UINT WINAPI MsiRecordGetFieldCount(MSIHANDLE hRecord);
-  UINT WINAPI MsiRecordSetStreamA(MSIHANDLE hRecord,UINT iField,LPCSTR szFilePath);
-  UINT WINAPI MsiRecordSetStreamW(MSIHANDLE hRecord,UINT iField,LPCWSTR szFilePath);
-
-#define MsiRecordSetStream __MINGW_NAME_AW(MsiRecordSetStream)
-
   UINT WINAPI MsiRecordReadStream(MSIHANDLE hRecord,UINT iField,char *szDataBuf,DWORD *pcbDataBuf);
   UINT WINAPI MsiRecordClearData(MSIHANDLE hRecord);
   MSIHANDLE WINAPI MsiGetActiveDatabase(MSIHANDLE hInstall);
+  LANGID WINAPI MsiGetLanguage(MSIHANDLE hInstall);
+  WINBOOL WINAPI MsiGetMode(MSIHANDLE hInstall,MSIRUNMODE eRunMode);
+  UINT WINAPI MsiSetMode(MSIHANDLE hInstall,MSIRUNMODE eRunMode,WINBOOL fState);
+  int WINAPI MsiProcessMessage(MSIHANDLE hInstall,INSTALLMESSAGE eMessageType,MSIHANDLE hRecord);
+
+  UINT WINAPI MsiRecordSetStringA(MSIHANDLE hRecord,UINT iField,LPCSTR szValue);
+  UINT WINAPI MsiRecordSetStringW(MSIHANDLE hRecord,UINT iField,LPCWSTR szValue);
+#define MsiRecordSetString __MINGW_NAME_AW(MsiRecordSetString)
+
+  UINT WINAPI MsiRecordGetStringA(MSIHANDLE hRecord,UINT iField,LPSTR szValueBuf,DWORD *pcchValueBuf);
+  UINT WINAPI MsiRecordGetStringW(MSIHANDLE hRecord,UINT iField,LPWSTR szValueBuf,DWORD *pcchValueBuf);
+#define MsiRecordGetString __MINGW_NAME_AW(MsiRecordGetString)
+
+  UINT WINAPI MsiRecordSetStreamA(MSIHANDLE hRecord,UINT iField,LPCSTR szFilePath);
+  UINT WINAPI MsiRecordSetStreamW(MSIHANDLE hRecord,UINT iField,LPCWSTR szFilePath);
+#define MsiRecordSetStream __MINGW_NAME_AW(MsiRecordSetStream)
+
   UINT WINAPI MsiSetPropertyA(MSIHANDLE hInstall,LPCSTR szName,LPCSTR szValue);
   UINT WINAPI MsiSetPropertyW(MSIHANDLE hInstall,LPCWSTR szName,LPCWSTR szValue);
-
 #define MsiSetProperty __MINGW_NAME_AW(MsiSetProperty)
 
   UINT WINAPI MsiGetPropertyA(MSIHANDLE hInstall,LPCSTR szName,LPSTR szValueBuf,DWORD *pcchValueBuf);
   UINT WINAPI MsiGetPropertyW(MSIHANDLE hInstall,LPCWSTR szName,LPWSTR szValueBuf,DWORD *pcchValueBuf);
-
 #define MsiGetProperty __MINGW_NAME_AW(MsiGetProperty)
 
-  LANGID WINAPI MsiGetLanguage(MSIHANDLE hInstall);
-  WINBOOL WINAPI MsiGetMode(MSIHANDLE hInstall,MSIRUNMODE eRunMode);
-  UINT WINAPI MsiSetMode(MSIHANDLE hInstall,MSIRUNMODE eRunMode,WINBOOL fState);
   UINT WINAPI MsiFormatRecordA(MSIHANDLE hInstall,MSIHANDLE hRecord,LPSTR szResultBuf,DWORD *pcchResultBuf);
   UINT WINAPI MsiFormatRecordW(MSIHANDLE hInstall,MSIHANDLE hRecord,LPWSTR szResultBuf,DWORD *pcchResultBuf);
-
 #define MsiFormatRecord __MINGW_NAME_AW(MsiFormatRecord)
 
   UINT WINAPI MsiDoActionA(MSIHANDLE hInstall,LPCSTR szAction);
   UINT WINAPI MsiDoActionW(MSIHANDLE hInstall,LPCWSTR szAction);
-
 #define MsiDoAction __MINGW_NAME_AW(MsiDoAction)
 
   UINT WINAPI MsiSequenceA(MSIHANDLE hInstall,LPCSTR szTable,INT iSequenceMode);
   UINT WINAPI MsiSequenceW(MSIHANDLE hInstall,LPCWSTR szTable,INT iSequenceMode);
-
 #define MsiSequence __MINGW_NAME_AW(MsiSequence)
 
-  int WINAPI MsiProcessMessage(MSIHANDLE hInstall,INSTALLMESSAGE eMessageType,MSIHANDLE hRecord);
   MSICONDITION WINAPI MsiEvaluateConditionA(MSIHANDLE hInstall,LPCSTR szCondition);
   MSICONDITION WINAPI MsiEvaluateConditionW(MSIHANDLE hInstall,LPCWSTR szCondition);
-
 #define MsiEvaluateCondition __MINGW_NAME_AW(MsiEvaluateCondition)
 
   UINT WINAPI MsiGetFeatureStateA(MSIHANDLE hInstall,LPCSTR szFeature,INSTALLSTATE *piInstalled,INSTALLSTATE *piAction);
   UINT WINAPI MsiGetFeatureStateW(MSIHANDLE hInstall,LPCWSTR szFeature,INSTALLSTATE *piInstalled,INSTALLSTATE *piAction);
-
 #define MsiGetFeatureState __MINGW_NAME_AW(MsiGetFeatureState)
 
   UINT WINAPI MsiSetFeatureStateA(MSIHANDLE hInstall,LPCSTR szFeature,INSTALLSTATE iState);
   UINT WINAPI MsiSetFeatureStateW(MSIHANDLE hInstall,LPCWSTR szFeature,INSTALLSTATE iState);
-
 #define MsiSetFeatureState __MINGW_NAME_AW(MsiSetFeatureState)
 
 #if (_WIN32_MSI >= 110)
   UINT WINAPI MsiSetFeatureAttributesA(MSIHANDLE hInstall,LPCSTR szFeature,DWORD dwAttributes);
   UINT WINAPI MsiSetFeatureAttributesW(MSIHANDLE hInstall,LPCWSTR szFeature,DWORD dwAttributes);
-
 #define MsiSetFeatureAttributes __MINGW_NAME_AW(MsiSetFeatureAttributes)
 #endif
 
   UINT WINAPI MsiGetComponentStateA(MSIHANDLE hInstall,LPCSTR szComponent,INSTALLSTATE *piInstalled,INSTALLSTATE *piAction);
   UINT WINAPI MsiGetComponentStateW(MSIHANDLE hInstall,LPCWSTR szComponent,INSTALLSTATE *piInstalled,INSTALLSTATE *piAction);
-
 #define MsiGetComponentState __MINGW_NAME_AW(MsiGetComponentState)
 
   UINT WINAPI MsiSetComponentStateA(MSIHANDLE hInstall,LPCSTR szComponent,INSTALLSTATE iState);
   UINT WINAPI MsiSetComponentStateW(MSIHANDLE hInstall,LPCWSTR szComponent,INSTALLSTATE iState);
-
 #define MsiSetComponentState __MINGW_NAME_AW(MsiSetComponentState)
 
   UINT WINAPI MsiGetFeatureCostA(MSIHANDLE hInstall,LPCSTR szFeature,MSICOSTTREE iCostTree,INSTALLSTATE iState,INT *piCost);
   UINT WINAPI MsiGetFeatureCostW(MSIHANDLE hInstall,LPCWSTR szFeature,MSICOSTTREE iCostTree,INSTALLSTATE iState,INT *piCost);
-
 #define MsiGetFeatureCost __MINGW_NAME_AW(MsiGetFeatureCost)
 
 #if (_WIN32_MSI >= 150)
-#define MsiEnumComponentCosts __MINGW_NAME_AW(MsiEnumComponentCosts)
   UINT WINAPI MsiEnumComponentCostsA(MSIHANDLE hInstall,LPCSTR szComponent,DWORD dwIndex,INSTALLSTATE iState,LPSTR szDriveBuf,DWORD *pcchDriveBuf,INT *piCost,INT *piTempCost);
   UINT WINAPI MsiEnumComponentCostsW(MSIHANDLE hInstall,LPCWSTR szComponent,DWORD dwIndex,INSTALLSTATE iState,LPWSTR szDriveBuf,DWORD *pcchDriveBuf,INT *piCost,INT *piTempCost);
+#define MsiEnumComponentCosts __MINGW_NAME_AW(MsiEnumComponentCosts)
 #endif
 
-#define MsiGetFeatureValidStates __MINGW_NAME_AW(MsiGetFeatureValidStates)
-#define MsiGetSourcePath __MINGW_NAME_AW(MsiGetSourcePath)
-#define MsiGetTargetPath __MINGW_NAME_AW(MsiGetTargetPath)
-#define MsiSetTargetPath __MINGW_NAME_AW(MsiSetTargetPath)
-#define MsiPreviewDialog __MINGW_NAME_AW(MsiPreviewDialog)
-#define MsiPreviewBillboard __MINGW_NAME_AW(MsiPreviewBillboard)
-
   UINT WINAPI MsiSetInstallLevel(MSIHANDLE hInstall,int iInstallLevel);
-  UINT WINAPI MsiGetFeatureValidStatesA(MSIHANDLE hInstall,LPCSTR szFeature,DWORD *dwInstallStates);
-  UINT WINAPI MsiGetFeatureValidStatesW(MSIHANDLE hInstall,LPCWSTR szFeature,DWORD *dwInstallStates);
-  UINT WINAPI MsiGetSourcePathA(MSIHANDLE hInstall,LPCSTR szFolder,LPSTR szPathBuf,DWORD *pcchPathBuf);
-  UINT WINAPI MsiGetSourcePathW(MSIHANDLE hInstall,LPCWSTR szFolder,LPWSTR szPathBuf,DWORD *pcchPathBuf);
-  UINT WINAPI MsiGetTargetPathA(MSIHANDLE hInstall,LPCSTR szFolder,LPSTR szPathBuf,DWORD *pcchPathBuf);
-  UINT WINAPI MsiGetTargetPathW(MSIHANDLE hInstall,LPCWSTR szFolder,LPWSTR szPathBuf,DWORD *pcchPathBuf);
-  UINT WINAPI MsiSetTargetPathA(MSIHANDLE hInstall,LPCSTR szFolder,LPCSTR szFolderPath);
-  UINT WINAPI MsiSetTargetPathW(MSIHANDLE hInstall,LPCWSTR szFolder,LPCWSTR szFolderPath);
   UINT WINAPI MsiVerifyDiskSpace(MSIHANDLE hInstall);
   UINT WINAPI MsiEnableUIPreview(MSIHANDLE hDatabase,MSIHANDLE *phPreview);
+  MSIHANDLE WINAPI MsiGetLastErrorRecord();
+
+  UINT WINAPI MsiGetFeatureValidStatesA(MSIHANDLE hInstall,LPCSTR szFeature,DWORD *dwInstallStates);
+  UINT WINAPI MsiGetFeatureValidStatesW(MSIHANDLE hInstall,LPCWSTR szFeature,DWORD *dwInstallStates);
+#define MsiGetFeatureValidStates __MINGW_NAME_AW(MsiGetFeatureValidStates)
+
+  UINT WINAPI MsiGetSourcePathA(MSIHANDLE hInstall,LPCSTR szFolder,LPSTR szPathBuf,DWORD *pcchPathBuf);
+  UINT WINAPI MsiGetSourcePathW(MSIHANDLE hInstall,LPCWSTR szFolder,LPWSTR szPathBuf,DWORD *pcchPathBuf);
+#define MsiGetSourcePath __MINGW_NAME_AW(MsiGetSourcePath)
+
+  UINT WINAPI MsiGetTargetPathA(MSIHANDLE hInstall,LPCSTR szFolder,LPSTR szPathBuf,DWORD *pcchPathBuf);
+  UINT WINAPI MsiGetTargetPathW(MSIHANDLE hInstall,LPCWSTR szFolder,LPWSTR szPathBuf,DWORD *pcchPathBuf);
+#define MsiGetTargetPath __MINGW_NAME_AW(MsiGetTargetPath)
+
+  UINT WINAPI MsiSetTargetPathA(MSIHANDLE hInstall,LPCSTR szFolder,LPCSTR szFolderPath);
+  UINT WINAPI MsiSetTargetPathW(MSIHANDLE hInstall,LPCWSTR szFolder,LPCWSTR szFolderPath);
+#define MsiSetTargetPath __MINGW_NAME_AW(MsiSetTargetPath)
+
   UINT WINAPI MsiPreviewDialogA(MSIHANDLE hPreview,LPCSTR szDialogName);
   UINT WINAPI MsiPreviewDialogW(MSIHANDLE hPreview,LPCWSTR szDialogName);
+#define MsiPreviewDialog __MINGW_NAME_AW(MsiPreviewDialog)
+
   UINT WINAPI MsiPreviewBillboardA(MSIHANDLE hPreview,LPCSTR szControlName,LPCSTR szBillboard);
   UINT WINAPI MsiPreviewBillboardW(MSIHANDLE hPreview,LPCWSTR szControlName,LPCWSTR szBillboard);
-  MSIHANDLE WINAPI MsiGetLastErrorRecord();
+#define MsiPreviewBillboard __MINGW_NAME_AW(MsiPreviewBillboard)
 
 #ifdef __cplusplus
 }
