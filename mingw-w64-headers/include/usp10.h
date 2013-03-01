08271fb27bdf7998d2a0f2a6ef0e3a065777ff6a
Jacek Caban <cjacek@gmail.com>
2013-03-01 16:51:03 +0000

    usp10.h: Updated SCRIPT_CONTROL struct.
    Patch author: Jan Holesovsky <kendy@suse.cz>
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@5626 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/usp10.h
+++ mingw-w64-headers/include/usp10.h
@@ -33,7 +33,9 @@ extern "C" {
     DWORD fNeutralOverride :1;
     DWORD fNumericOverride :1;
     DWORD fLegacyBidiClass :1;
-    DWORD fReserved :8;
+    DWORD fMergeNeutralItems :1;
+    DWORD fUseStandardBidi :1;
+    DWORD fReserved :6;
   } SCRIPT_CONTROL;
 
   typedef struct tag_SCRIPT_STATE {
