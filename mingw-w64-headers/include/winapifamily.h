5167f8651ddbec56e6ff663f1b97e325496fd5e7
Kai Tietz <ktietz70@googlemail.com>
2013-03-10 15:00:18 +0000

    Add platform-header to make difference between desktop and app mode
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@5631 4407c894-4637-0410-b4f5-ada5f102cad1

---
+++ mingw-w64-headers/include/winapifamily.h
@@ -0,0 +1,23 @@
+/**
+ * This file is part of the mingw-w64 runtime package.
+ * No warranty is given; refer to the file DISCLAIMER within this package.
+ */
+
+#ifndef _INC_WINAPIFAMILY
+#define _INC_WINAPIFAMILY
+
+#define WINAPI_PARTITION_DESKTOP 0x1
+#define WINAPI_PARTITION_APP     0x2    
+
+#define WINAPI_FAMILY_APP WINAPI_PARTITION_APP
+#define WINAPI_FAMILY_DESKTOP_APP (WINAPI_PARTITION_DESKTOP \
+				   | WINAPI_PARTITION_APP)    
+
+#ifndef WINAPI_FAMILY
+#define WINAPI_FAMILY WINAPI_FAMILY_DESKTOP_APP
+#endif
+
+#define WINAPI_FAMILY_PARTITION(v) ((WINAPI_FAMILY & v) == v)
+#define WINAPI_FAMILY_ONE_PARTITION(vset, v) ((WINAPI_FAMILY & vset) == v)
+
+#endif 
