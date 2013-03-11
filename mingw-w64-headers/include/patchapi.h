ed4b4b52c4dbdc05db790ac0ae89fb46739dbbef
Kai Tietz <ktietz70@googlemail.com>
2013-03-11 13:11:15 +0000

    Apply missed commit
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@5638 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/patchapi.h
+++ mingw-w64-headers/include/patchapi.h
@@ -1,7 +1,6 @@
 /**
- * This file has no copyright assigned and is placed in the Public Domain.
  * This file is part of the mingw-w64 runtime package.
- * No warranty is given; refer to the file DISCLAIMER.PD within this package.
+ * No warranty is given; refer to the file DISCLAIMER within this package.
  */
 #ifndef _PATCHAPI_H_
 #define _PATCHAPI_H_
@@ -12,12 +11,17 @@
 extern "C" {
 #endif
 
-#define PATCH_OPTION_USE_BEST 0x00000000
+#ifdef IMPORTING_PATCHAPI_DLL
+#define PATCHAPI WINAPI __declspec(dllimport)
+#else
+#define PATCHAPI WINAPI
+#endif
 
-#define PATCH_OPTION_USE_LZX_BEST 0x00000003
-#define PATCH_OPTION_USE_LZX_A 0x00000001
-#define PATCH_OPTION_USE_LZX_B 0x00000002
-#define PATCH_OPTION_USE_LZX_LARGE 0x00000004
+#define PATCH_OPTION_USE_BEST               0x0
+#define PATCH_OPTION_USE_LZX_A              0x1
+#define PATCH_OPTION_USE_LZX_B              0x2
+#define PATCH_OPTION_USE_LZX_BEST           0x3
+#define PATCH_OPTION_USE_LZX_LARGE          0x4
 
 #define PATCH_OPTION_NO_BINDFIX 0x00010000
 #define PATCH_OPTION_NO_LOCKFIX 0x00020000
@@ -28,9 +32,10 @@ extern "C" {
 #define PATCH_OPTION_NO_RESTIMEFIX 0x00400000
 #define PATCH_OPTION_NO_TIMESTAMP 0x00800000
 #define PATCH_OPTION_SIGNATURE_MD5 0x01000000
+#define PATCH_OPTION_INTERLEAVE_FILES 0x40000000
 #define PATCH_OPTION_RESERVED1 0x80000000
 
-#define PATCH_OPTION_VALID_FLAGS 0x80FF0007
+#define PATCH_OPTION_VALID_FLAGS 0xc0ff0007
 
 #define PATCH_SYMBOL_NO_IMAGEHLP 0x00000001
 #define PATCH_SYMBOL_NO_FAILURES 0x00000002
@@ -112,6 +117,15 @@ extern "C" {
     PPATCH_RETAIN_RANGE RetainRangeArray;
   } PATCH_OLD_FILE_INFO,*PPATCH_OLD_FILE_INFO;
 
+  typedef struct _PATCH_INTERLEAVE_MAP {
+    ULONG CountRanges;
+    struct {
+      ULONG OldOffset;
+      ULONG OldLength;
+      ULONG NewLength;
+    } Range[1];
+  } PATCH_INTERLEAVE_MAP, *PPATCH_INTERLEAVE_MAP;
+
   typedef struct _PATCH_OPTION_DATA {
     ULONG SizeOfThisStruct;
     ULONG SymbolOptionFlags;
@@ -120,43 +134,49 @@ extern "C" {
     ULONG ExtendedOptionFlags;
     PPATCH_SYMLOAD_CALLBACK SymLoadCallback;
     PVOID SymLoadContext;
+    PPATCH_INTERLEAVE_MAP *InterleaveMapArray;
+    ULONG MaxLzxWindowSize;
   } PATCH_OPTION_DATA,*PPATCH_OPTION_DATA;
-
-#ifdef IMPORTING_PATCHAPI_DLL
-#define PATCHAPI WINAPI __declspec(dllimport)
-#else
-#define PATCHAPI WINAPI
-#endif
 
   WINBOOL PATCHAPI CreatePatchFileA(LPCSTR OldFileName,LPCSTR NewFileName,LPCSTR PatchFileName,ULONG OptionFlags,PPATCH_OPTION_DATA OptionData);
   WINBOOL PATCHAPI CreatePatchFileW(LPCWSTR OldFileName,LPCWSTR NewFileName,LPCWSTR PatchFileName,ULONG OptionFlags,PPATCH_OPTION_DATA OptionData);
-  WINBOOL PATCHAPI CreatePatchFileByHandles(HANDLE OldFileHandle,HANDLE NewFileHandle,HANDLE PatchFileHandle,ULONG OptionFlags,PPATCH_OPTION_DATA OptionData);
+#define CreatePatchFile __MINGW_NAME_AW(CreatePatchFile)
+
   WINBOOL PATCHAPI CreatePatchFileExA(ULONG OldFileCount,PPATCH_OLD_FILE_INFO_A OldFileInfoArray,LPCSTR NewFileName,LPCSTR PatchFileName,ULONG OptionFlags,PPATCH_OPTION_DATA OptionData,PPATCH_PROGRESS_CALLBACK ProgressCallback,PVOID CallbackContext);
   WINBOOL PATCHAPI CreatePatchFileExW(ULONG OldFileCount,PPATCH_OLD_FILE_INFO_W OldFileInfoArray,LPCWSTR NewFileName,LPCWSTR PatchFileName,ULONG OptionFlags,PPATCH_OPTION_DATA OptionData,PPATCH_PROGRESS_CALLBACK ProgressCallback,PVOID CallbackContext);
-  WINBOOL PATCHAPI CreatePatchFileByHandlesEx(ULONG OldFileCount,PPATCH_OLD_FILE_INFO_H OldFileInfoArray,HANDLE NewFileHandle,HANDLE PatchFileHandle,ULONG OptionFlags,PPATCH_OPTION_DATA OptionData,PPATCH_PROGRESS_CALLBACK ProgressCallback,PVOID CallbackContext);
+#define CreatePatchFileEx __MINGW_NAME_AW(CreatePatchFileEx)
+
   WINBOOL PATCHAPI ExtractPatchHeaderToFileA(LPCSTR PatchFileName,LPCSTR PatchHeaderFileName);
   WINBOOL PATCHAPI ExtractPatchHeaderToFileW(LPCWSTR PatchFileName,LPCWSTR PatchHeaderFileName);
-  WINBOOL PATCHAPI ExtractPatchHeaderToFileByHandles(HANDLE PatchFileHandle,HANDLE PatchHeaderFileHandle);
+#define ExtractPatchHeaderToFile __MINGW_NAME_AW(ExtractPatchHeaderToFile)
+
   WINBOOL PATCHAPI TestApplyPatchToFileA(LPCSTR PatchFileName,LPCSTR OldFileName,ULONG ApplyOptionFlags);
   WINBOOL PATCHAPI TestApplyPatchToFileW(LPCWSTR PatchFileName,LPCWSTR OldFileName,ULONG ApplyOptionFlags);
-  WINBOOL PATCHAPI TestApplyPatchToFileByHandles(HANDLE PatchFileHandle,HANDLE OldFileHandle,ULONG ApplyOptionFlags);
+#define TestApplyPatchToFile __MINGW_NAME_AW(TestApplyPatchToFile)
+
   WINBOOL PATCHAPI ApplyPatchToFileA(LPCSTR PatchFileName,LPCSTR OldFileName,LPCSTR NewFileName,ULONG ApplyOptionFlags);
   WINBOOL PATCHAPI ApplyPatchToFileW(LPCWSTR PatchFileName,LPCWSTR OldFileName,LPCWSTR NewFileName,ULONG ApplyOptionFlags);
-  WINBOOL PATCHAPI ApplyPatchToFileByHandles(HANDLE PatchFileHandle,HANDLE OldFileHandle,HANDLE NewFileHandle,ULONG ApplyOptionFlags);
+#define ApplyPatchToFile __MINGW_NAME_AW(ApplyPatchToFile)
+
   WINBOOL PATCHAPI ApplyPatchToFileExA(LPCSTR PatchFileName,LPCSTR OldFileName,LPCSTR NewFileName,ULONG ApplyOptionFlags,PPATCH_PROGRESS_CALLBACK ProgressCallback,PVOID CallbackContext);
   WINBOOL PATCHAPI ApplyPatchToFileExW(LPCWSTR PatchFileName,LPCWSTR OldFileName,LPCWSTR NewFileName,ULONG ApplyOptionFlags,PPATCH_PROGRESS_CALLBACK ProgressCallback,PVOID CallbackContext);
-  WINBOOL PATCHAPI ApplyPatchToFileByHandlesEx(HANDLE PatchFileHandle,HANDLE OldFileHandle,HANDLE NewFileHandle,ULONG ApplyOptionFlags,PPATCH_PROGRESS_CALLBACK ProgressCallback,PVOID CallbackContext);
+#define ApplyPatchToFileEx __MINGW_NAME_AW(ApplyPatchToFileEx)
+
   WINBOOL PATCHAPI GetFilePatchSignatureA(LPCSTR FileName,ULONG OptionFlags,PVOID OptionData,ULONG IgnoreRangeCount,PPATCH_IGNORE_RANGE IgnoreRangeArray,ULONG RetainRangeCount,PPATCH_RETAIN_RANGE RetainRangeArray,ULONG SignatureBufferSize,PVOID SignatureBuffer);
   WINBOOL PATCHAPI GetFilePatchSignatureW(LPCWSTR FileName,ULONG OptionFlags,PVOID OptionData,ULONG IgnoreRangeCount,PPATCH_IGNORE_RANGE IgnoreRangeArray,ULONG RetainRangeCount,PPATCH_RETAIN_RANGE RetainRangeArray,ULONG SignatureBufferSizeInBytes,PVOID SignatureBuffer);
-  WINBOOL PATCHAPI GetFilePatchSignatureByHandle(HANDLE FileHandle,ULONG OptionFlags,PVOID OptionData,ULONG IgnoreRangeCount,PPATCH_IGNORE_RANGE IgnoreRangeArray,ULONG RetainRangeCount,PPATCH_RETAIN_RANGE RetainRangeArray,ULONG SignatureBufferSize,PVOID SignatureBuffer);
-
-#define CreatePatchFile __MINGW_NAME_AW(CreatePatchFile)
-#define CreatePatchFileEx __MINGW_NAME_AW(CreatePatchFileEx)
-#define TestApplyPatchToFile __MINGW_NAME_AW(TestApplyPatchToFile)
-#define ApplyPatchToFile __MINGW_NAME_AW(ApplyPatchToFile)
-#define ApplyPatchToFileEx __MINGW_NAME_AW(ApplyPatchToFileEx)
-#define ExtractPatchHeaderToFile __MINGW_NAME_AW(ExtractPatchHeaderToFile)
 #define GetFilePatchSignature __MINGW_NAME_AW(GetFilePatchSignature)
+
+  WINBOOL PATCHAPI GetFilePatchSignatureByHandle(HANDLE FileHandle,ULONG OptionFlags,PVOID OptionData,ULONG IgnoreRangeCount,PPATCH_IGNORE_RANGE IgnoreRangeArray,ULONG RetainRangeCount,PPATCH_RETAIN_RANGE RetainRangeArray,ULONG SignatureBufferSize,PVOID SignatureBuffer);
+  WINBOOL PATCHAPI CreatePatchFileByHandles(HANDLE OldFileHandle,HANDLE NewFileHandle,HANDLE PatchFileHandle,ULONG OptionFlags,PPATCH_OPTION_DATA OptionData);
+  WINBOOL PATCHAPI CreatePatchFileByHandlesEx(ULONG OldFileCount,PPATCH_OLD_FILE_INFO_H OldFileInfoArray,HANDLE NewFileHandle,HANDLE PatchFileHandle,ULONG OptionFlags,PPATCH_OPTION_DATA OptionData,PPATCH_PROGRESS_CALLBACK ProgressCallback,PVOID CallbackContext);
+  WINBOOL PATCHAPI ExtractPatchHeaderToFileByHandles(HANDLE PatchFileHandle,HANDLE PatchHeaderFileHandle);
+  WINBOOL PATCHAPI TestApplyPatchToFileByHandles(HANDLE PatchFileHandle,HANDLE OldFileHandle,ULONG ApplyOptionFlags);
+  WINBOOL PATCHAPI ApplyPatchToFileByHandles(HANDLE PatchFileHandle,HANDLE OldFileHandle,HANDLE NewFileHandle,ULONG ApplyOptionFlags);
+  WINBOOL PATCHAPI ApplyPatchToFileByHandlesEx(HANDLE PatchFileHandle,HANDLE OldFileHandle,HANDLE NewFileHandle,ULONG ApplyOptionFlags,PPATCH_PROGRESS_CALLBACK ProgressCallback,PVOID CallbackContext);
+
+WINBOOL PATCHAPI GetFilePatchSignatureByBuffer(PBYTE FileBufferWritable, ULONG FileSize, ULONG OptionFlags, PVOID OptionData, ULONG IgnoreRangeCount, PPATCH_IGNORE_RANGE IgnoreRangeArray, ULONG RetainRangeCount, PPATCH_RETAIN_RANGE RetainRangeArray, ULONG SignatureBufferSize, LPSTR SignatureBuffer);
+WINBOOL PATCHAPI ApplyPatchToFileByBuffers(PBYTE PatchFileMapped, ULONG PatchFileSize, PBYTE OldFileMapped, ULONG OldFileSize, PBYTE *NewFileBuffer, ULONG NewFileBufferSize, ULONG *NewFileActualSize, FILETIME *NewFileTime, ULONG ApplyOptionFlags, PPATCH_PROGRESS_CALLBACK ProgressCallback, PVOID CallbackContext);
+WINBOOL PATCHAPI TestApplyPatchToFileByBuffers(PBYTE PatchFileBuffer, ULONG PatchFileSize, PBYTE OldFileBuffer, ULONG OldFileSize, ULONG *NewFileSize, ULONG ApplyOptionFlags);
 
 #ifdef __cplusplus
 }
