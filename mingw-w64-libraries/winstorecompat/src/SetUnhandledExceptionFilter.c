f109cc5a399d8aa98231c77885c352e4f2ee343d
Jean-Baptiste Kempf <jb@videolan.org>
2013-03-18 23:07:13 +0000

    Add a dummy SetUnhandledExceptionFilter method
    It returns the previous filter
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@5667 4407c894-4637-0410-b4f5-ada5f102cad1

---
+++ mingw-w64-libraries/winstorecompat/src/SetUnhandledExceptionFilter.c
@@ -0,0 +1,41 @@
+/*
+    Copyright (c) 2013 mingw-w64 project
+
+    Contributing authors: Jean-Baptiste Kempf
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
+*/
+
+#define _WIN32_WINNT 0x501 /* SetUnhandledExceptionFilter is XP+ */
+
+#define SetUnhandledExceptionFilter __SetUnhandledExceptionFilter
+#include <windef.h>
+#include <winbase.h>
+#undef SetUnhandledExceptionFilter
+
+LPTOP_LEVEL_EXCEPTION_FILTER WINAPI SetUnhandledExceptionFilter(LPTOP_LEVEL_EXCEPTION_FILTER lpTopLevelExceptionFilter)
+{
+    static union {
+        PVOID *target;
+        LPTOP_LEVEL_EXCEPTION_FILTER prev_filter;
+    } u;
+    return (LPTOP_LEVEL_EXCEPTION_FILTER)(INT_PTR)InterlockedExchangePointer(u.target, lpTopLevelExceptionFilter);
+}
+
+LPTOP_LEVEL_EXCEPTION_FILTER WINAPI (*__MINGW_IMP_SYMBOL(SetUnhandledExceptionFilter))(LPTOP_LEVEL_EXCEPTION_FILTER) asm("__imp__SetUnhandledExceptionFilter@4") = SetUnhandledExceptionFilter;
