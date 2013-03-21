0ff24273a95ac7e0b65fb9c2699fc999d293d0d0
Ozkan Sezer <sezeroz@gmail.com>
2013-03-21 08:11:36 +0000

    Merge from trunk r5679 (Fix wrong code-bug.)
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/stable/v1.x@5680 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-crt/intrincs/bitscanfwd64.c
+++ mingw-w64-crt/intrincs/bitscanfwd64.c
@@ -2,7 +2,9 @@
 
 unsigned char _BitScanForward64(unsigned long *Index, unsigned __int64 Mask)
 {
-  __asm__ __volatile__("bsfq %1,%0" : "=r" (Mask),"=m" ((*(volatile long long *)Index)));
+  unsigned __int64 n;
+  __asm__ __volatile__("bsfq %0,%1" : "+r" (Mask),"=rm" (n) : : "memory");
+  *Index = (unsigned long) n;
   return (Mask != 0);
 }
 
