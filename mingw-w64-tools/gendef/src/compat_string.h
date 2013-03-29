c1fe019261b5b32f67dc10f2690a55a419f4081a
Jonathan Yong <10walls@gmail.com>
2013-03-29 15:04:11 +0000

    Use GPLv3 or later license
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@5706 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-tools/gendef/src/compat_string.h
+++ mingw-w64-tools/gendef/src/compat_string.h
@@ -1,3 +1,20 @@
+/*
+    gendef - Generate list of exported symbols from a Portable Executable.
+    Copyright (C) 2009, 2010, 2011, 2012, 2013  mingw-w64 project
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #ifndef __COMPAT_STRING_H
 #define __COMPAT_STRING_H
 
