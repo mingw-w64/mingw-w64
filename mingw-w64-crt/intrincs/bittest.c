cb3a1484bf810264136525c82312aaab54f3a6c5
Ozkan Sezer <sezeroz@gmail.com>
2013-04-09 19:32:48 +0000

    propagate r5735 changes from winnt.h to crt/intrincs static versions in bittest.c, bittest64.c, bittestc.c, bittestc64.c, bittestr.c, bittestr64.c, bittestri.c, bittestri64.c, bittests.c, bittests64.c, bittestsi.c, bittestsi64.c
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@5740 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-crt/intrincs/bittest.c
+++ mingw-w64-crt/intrincs/bittest.c
@@ -5,7 +5,7 @@ unsigned char _bittest(__LONG32 const *Base, __LONG32 Offset)
   int old = 0;
   __asm__ __volatile__("btl %2,%1\n\tsbbl %0,%0 "
    :"=r" (old),"=m" ((*(volatile __LONG32 *) Base))
-   :"Ir" (Offset));
+   :"Ir" (Offset) : "memory");
   return (old != 0);
 }
 
--- mingw-w64-crt/intrincs/bittestri.c
+++ mingw-w64-crt/intrincs/bittestri.c
@@ -5,7 +5,7 @@ unsigned char _interlockedbittestandreset(__LONG32 *Base, __LONG32 Offset)
   int old = 0;
   __asm__ __volatile__("lock ; btrl %2,%1\n\tsbbl %0,%0 "
     :"=r" (old),"=m" ((*(volatile __LONG32 *) Base))
-    :"Ir" (Offset));
+    :"Ir" (Offset) : "memory");
   return (old != 0);
 }
 
