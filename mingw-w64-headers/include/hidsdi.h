19d235e2744f57f6fc512573be7ee564c5fa2600
Kai Tietz <ktietz70@googlemail.com>
2013-05-20 17:54:19 +0000

    Add HidD_GetProductString, HidD_SetNumInputBuffers, and HidD_GetNumInputBuffers function prototypes to hidsid.h header.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@5868 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/hidsdi.h
+++ mingw-w64-headers/include/hidsdi.h
@@ -28,22 +28,28 @@ extern "C" {
 #endif
 
   /* http://msdn.microsoft.com/en-us/library/ff538900%28v=VS.85%29.aspx */
-HIDAPI BOOLEAN NTAPI
-HidD_GetAttributes(HANDLE HidDeviceObject, PHIDD_ATTRIBUTES Attributes);
+HIDAPI BOOLEAN NTAPI HidD_GetAttributes(HANDLE HidDeviceObject, PHIDD_ATTRIBUTES Attributes);
 
   /* http://msdn.microsoft.com/en-us/library/ff538910%28v=VS.85%29.aspx */
-HIDAPI BOOLEAN NTAPI
-HidD_GetFeature(HANDLE HidDeviceObject, PVOID ReportBuffer,
-		ULONG ReportBufferLength);
+HIDAPI BOOLEAN NTAPI HidD_GetFeature(HANDLE HidDeviceObject, PVOID ReportBuffer, ULONG ReportBufferLength);
 
   /* http://msdn.microsoft.com/en-us/library/ff539684%28v=VS.85%29.aspx */
-HIDAPI BOOLEAN NTAPI
-HidD_SetFeature(HANDLE HidDeviceObject, PVOID ReportBuffer,
-		ULONG ReportBufferLength);
+HIDAPI BOOLEAN NTAPI HidD_SetFeature(HANDLE HidDeviceObject, PVOID ReportBuffer, ULONG ReportBufferLength);
 
-  /* http://msdn.microsoft.com/en-us/library/ff538924(v=vs.85).aspx */
-HIDAPI VOID NTAPI
-HidD_GetHidGuid(LPGUID HidGuid);
+/* http://msdn.microsoft.com/en-us/library/ff538924(v=vs.85).aspx */
+HIDAPI VOID NTAPI HidD_GetHidGuid(LPGUID HidGuid);
+
+/* http://msdn.microsoft.com/en-us/library/windows/hardware/ff538959(v=vs.85).aspx */
+HIDAPI BOOLEAN NTAPI HidD_GetManufacturerString (HANDLE HidDeviceObject, PVOID Buffer, ULONG BufferLength);
+
+/* http://msdn.microsoft.com/en-us/library/windows/hardware/ff539681(v=vs.85).aspx */
+HIDAPI BOOLEAN NTAPI HidD_GetProductString (HANDLE HidDeviceObject, PVOID Buffer, ULONG BufferLength);
+
+/* http://msdn.microsoft.com/en-us/library/windows/hardware/ff539686(v=vs.85).aspx */
+HIDAPI BOOLEAN NTAPI HidD_SetNumInputBuffers (HANDLE HidDeviceObject, ULONG NumberBuffers);
+
+/* http://msdn.microsoft.com/en-us/library/windows/hardware/ff539675(v=vs.85).aspx */
+HIDAPI BOOLEAN NTAPI HidD_GetNumInputBuffers (HANDLE HidDeviceObject, PULONG NumberBuffers);
 
 #ifdef __cplusplus
 }
