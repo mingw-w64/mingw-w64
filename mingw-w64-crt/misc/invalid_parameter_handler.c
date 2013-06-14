532e1c596b7ab809346c7bc2db25eb8b4f00a524
Jacek Caban <cjacek@gmail.com>
2013-06-14 16:03:07 +0000

    Fixed *_invalid_parameter_handler symbold handling.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@5896 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-crt/misc/invalid_parameter_handler.c
+++ mingw-w64-crt/misc/invalid_parameter_handler.c
@@ -3,12 +3,17 @@
 
 static _invalid_parameter_handler handler;
 
-_invalid_parameter_handler __cdecl __MINGW_IMP_SYMBOL(_set_invalid_parameter_handler)(_invalid_parameter_handler new_handler)
+static _invalid_parameter_handler __cdecl mingw_set_invalid_parameter_handler(_invalid_parameter_handler new_handler)
 {
     return InterlockedExchangePointer(&handler, new_handler);
 }
 
-_invalid_parameter_handler __cdecl __MINGW_IMP_SYMBOL(_get_invalid_parameter_handler)(void)
+_invalid_parameter_handler (__cdecl *__MINGW_IMP_SYMBOL(_set_invalid_parameter_handler))(_invalid_parameter_handler) =
+    mingw_set_invalid_parameter_handler;
+
+static _invalid_parameter_handler __cdecl mingw_get_invalid_parameter_handler(void)
 {
     return handler;
 }
+
+_invalid_parameter_handler (__cdecl *__MINGW_IMP_SYMBOL(_get_invalid_parameter_handler))(void) = mingw_get_invalid_parameter_handler;
