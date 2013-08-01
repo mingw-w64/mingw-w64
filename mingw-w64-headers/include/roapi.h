2dd99229ad682933f6bdbaaa7045e3310b1be44f
Kai Tietz <ktietz70@googlemail.com>
2013-08-01 12:51:22 +0000

    Begin to add C++ namespace stuff for Ro-API.  Additional add mising APIs for Apartment-shutdown registration.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@5985 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/roapi.h
+++ mingw-w64-headers/include/roapi.h
@@ -6,6 +6,7 @@
 #ifndef __ROAPI_H__
 #define __ROAPI_H__
 
+#include <winapifamily.h>
 #include <windows.h>
 #include <sdkddkver.h>
 #include <hstring.h>
@@ -15,12 +16,15 @@
 #if _WIN32_WINNT >= _WIN32_WINNT_WIN8
 
 typedef enum RO_INIT_TYPE {
+#if WINAPI_FAMILY_PARTITION (WINAPI_PARTITION_DESKTOP)
+  RO_INIT_SINGLETHREADED = 0,
+#endif
   RO_INIT_MULTITHREADED  = 1
 } RO_INIT_TYPE;
 
-typedef struct {}* RO_REGISTRATION_COOKIE;
+typedef struct { } *RO_REGISTRATION_COOKIE;
 
-typedef HRESULT (WINAPI * PFNGETACTIVATIONFACTORY)(HSTRING, IActivationFactory **);
+typedef HRESULT (WINAPI *PFNGETACTIVATIONFACTORY)(HSTRING, IActivationFactory **);
 
 /* */
 
@@ -38,5 +42,46 @@ void WINAPI RoRevokeActivationFactories(RO_REGISTRATION_COOKIE cookie);
 
 void WINAPI RoUninitialize(void);
 
+typedef interface IApartmentShutdown IApartmentShutdown;
+DECLARE_HANDLE (APARTMENT_SHUTDOWN_REGISTRATION_COOKIE);
+
+HRESULT WINAPI RoRegisterForApartmentShutdown (IApartmentShutdown *callbackObj, UINT64 *apartmentId, APARTMENT_SHUTDOWN_REGISTRATION_COOKIE *regCookie);
+
+HRESULT WINAPI RoUnregisterForApartmentShutdown (APARTMENT_SHUTDOWN_REGISTRATION_COOKIE regCookie);
+
+HRESULT WINAPI RoGetApartmentIdentifier (UINT64 *apartmentId);
+
 #endif
+
+#ifdef __cplusplus
+
+namespace Windows {
+  namespace Foundation {
+    __inline HRESULT Initalize (RO_INIT_TYPE it
+#if WINAPI_FAMILY_PARTITION (WINAPI_PARTITION_DESKTOP)
+	 = 0
+#endif
+    ) { return RoInitialize (it); }
+    __inline void Uninitialize ()
+    { RoUninitialize (); }
+
+  }
+}
+
+namespace ABI {
+  namespace Windows {
+    namespace Foundation {
+      __inline HRESULT Initialze (RO_INIT_TYPE it
+#if WINAPI_FAMILY_PARTITION (WINAPI_PARTITION_DESKTOP)
+	= 0
+#endif
+      ) { return RoInitialize (it); }
+      __inline void Uninitialize ()
+      { RoUninitialize (); }
+    }
+  }
+}
+
+#endif
+
 #endif
