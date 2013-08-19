03c0ec6334dc6a50eeccdb0433214cfc8cc3ea71
Kai Tietz <ktietz70@googlemail.com>
2013-08-19 16:19:46 +0000

    Add winapi-family check
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@6104 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/ole2ver.h
+++ mingw-w64-headers/include/ole2ver.h
@@ -1,12 +1,15 @@
 /**
- * This file has no copyright assigned and is placed in the Public Domain.
  * This file is part of the mingw-w64 runtime package.
- * No warranty is given; refer to the file DISCLAIMER.PD within this package.
+ * No warranty is given; refer to the file DISCLAIMER within this package.
  */
 #ifndef _OLE2VER_H_
 #define _OLE2VER_H_
 
+#include <winapifamily.h>
+
+#if WINAPI_FAMILY_PARTITION (WINAPI_PARTITION_DESKTOP)
 #define rmm 23
 #define rup 639
+#endif
 
 #endif
