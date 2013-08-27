006f91dcbe55f25a159fee479be2ed4731eea730
Kai Tietz <ktietz70@googlemail.com>
2013-08-27 07:01:34 +0000

    Add winapi-family check and re-moved wrong winver >= 0x0600 check.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@6146 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/avrt.h
+++ mingw-w64-headers/include/avrt.h
@@ -1,99 +1,53 @@
-/**
- * This file has no copyright assigned and is placed in the Public Domain.
- * This file is part of the mingw-w64 runtime package.
- * No warranty is given; refer to the file DISCLAIMER.PD within this package.
- */
-#ifndef _INC_AVRT
-#define _INC_AVRT
+**
+* This file is part of the mingw-w64 runtime package.
+* No warranty is given; refer to the file DISCLAIMER within this package.
+*/
 
-#if (_WIN32_WINNT >= 0x0600)
-typedef enum _AVRT_PRIORITY {
-  AVRT_PRIORITY_LOW = -1,
-  AVRT_PRIORITY_NORMAL,		/* 0 */
-  AVRT_PRIORITY_HIGH,		/* 1 */
-  AVRT_PRIORITY_CRITICAL	/* 2 */
-} AVRT_PRIORITY, *PAVRT_PRIORITY;
+#include <winapifamily.h>
+
+#ifndef _AVRT_
+#define _AVRT_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if WINAPI_FAMILY_PARTITION (WINAPI_PARTITION_DESKTOP)
+  typedef enum _AVRT_PRIORITY {
+    AVRT_PRIORITY_VERYLOW = -2,
+    AVRT_PRIORITY_LOW,
+    AVRT_PRIORITY_NORMAL,
+    AVRT_PRIORITY_HIGH,
+    AVRT_PRIORITY_CRITICAL
+  } AVRT_PRIORITY,*PAVRT_PRIORITY;
+
+#define THREAD_ORDER_GROUP_INFINITE_TIMEOUT (-1LL)
+
+#define AVRTAPI
+
+  WINBOOL WINAPI AvQuerySystemResponsiveness (HANDLE AvrtHandle, PULONG SystemResponsivenessValue);
+  WINBOOL WINAPI AvRevertMmThreadCharacteristics (HANDLE AvrtHandle);
+  WINBOOL WINAPI AvRtCreateThreadOrderingGroup (PHANDLE Context, PLARGE_INTEGER Period, GUID *ThreadOrderingGuid, PLARGE_INTEGER Timeout);
+  WINBOOL WINAPI AvRtCreateThreadOrderingGroupExA (PHANDLE Context, PLARGE_INTEGER Period, GUID *ThreadOrderingGuid, PLARGE_INTEGER Timeout, LPCSTR TaskName);
+  WINBOOL WINAPI AvRtCreateThreadOrderingGroupExW (PHANDLE Context, PLARGE_INTEGER Period, GUID *ThreadOrderingGuid, PLARGE_INTEGER Timeout, LPCWSTR TaskName);
+  WINBOOL WINAPI AvRtDeleteThreadOrderingGroup (HANDLE Context);
+  WINBOOL WINAPI AvRtJoinThreadOrderingGroup (PHANDLE Context, GUID *ThreadOrderingGuid, WINBOOL Before);
+  WINBOOL WINAPI AvRtLeaveThreadOrderingGroup (HANDLE Context);
+  WINBOOL WINAPI AvRtWaitOnThreadOrderingGroup (HANDLE Context);
+  HANDLE WINAPI AvSetMmMaxThreadCharacteristicsA (LPCSTR FirstTask, LPCSTR SecondTask, LPDWORD TaskIndex);
+  HANDLE WINAPI AvSetMmMaxThreadCharacteristicsW (LPCWSTR FirstTask, LPCWSTR SecondTask, LPDWORD TaskIndex);
+  HANDLE WINAPI AvSetMmThreadCharacteristicsA (LPCSTR TaskName, LPDWORD TaskIndex);
+  HANDLE WINAPI AvSetMmThreadCharacteristicsW (LPCWSTR TaskName, LPDWORD TaskIndex);
+  WINBOOL WINAPI AvSetMmThreadPriority (HANDLE AvrtHandle, AVRT_PRIORITY Priority);
 
 #define AvSetMmThreadCharacteristics __MINGW_NAME_AW(AvSetMmThreadCharacteristics)
 #define AvSetMmMaxThreadCharacteristics __MINGW_NAME_AW(AvSetMmMaxThreadCharacteristics)
 #define AvRtCreateThreadOrderingGroupEx __MINGW_NAME_AW(AvRtCreateThreadOrderingGroupEx)
 
-WINBOOL WINAPI AvQuerySystemResponsiveness(
-  HANDLE AvrtHandle,
-  PULONG SystemResponsivenessValue
-);
+#endif
 
-HANDLE WINAPI AvSetMmThreadCharacteristicsA(
-  LPCSTR TaskName,
-  LPDWORD TaskIndex
-);
+#ifdef __cplusplus
+}
+#endif
 
-HANDLE WINAPI AvSetMmThreadCharacteristicsW(
-  LPCWSTR TaskName,
-  LPDWORD TaskIndex
-);
-
-WINBOOL WINAPI AvRevertMmThreadCharacteristics(
-  HANDLE AvrtHandle
-);
-
-HANDLE WINAPI AvSetMmMaxThreadCharacteristicsA(
-  LPCSTR FirstTask,
-  LPCSTR SecondTask,
-  LPDWORD TaskIndex
-);
-
-HANDLE WINAPI AvSetMmMaxThreadCharacteristicsW(
-  LPCWSTR FirstTask,
-  LPCWSTR SecondTask,
-  LPDWORD TaskIndex
-);
-
-WINBOOL WINAPI AvSetMmThreadPriority(
-  HANDLE AvrtHandle,
-  AVRT_PRIORITY Priority
-);
-
-WINBOOL WINAPI AvRtCreateThreadOrderingGroup(
-  PHANDLE Context,
-  PLARGE_INTEGER Period,
-  GUID *ThreadOrderingGuid,
-  PLARGE_INTEGER Timeout
-);
-
-WINBOOL WINAPI AvRtCreateThreadOrderingGroupExA(
-  PHANDLE Context,
-  PLARGE_INTEGER Period,
-  GUID *ThreadOrderingGuid,
-  PLARGE_INTEGER Timeout,
-  LPCSTR TaskName
-);
-
-WINBOOL WINAPI AvRtCreateThreadOrderingGroupExW(
-  PHANDLE Context,
-  PLARGE_INTEGER Period,
-  GUID *ThreadOrderingGuid,
-  PLARGE_INTEGER Timeout,
-  LPCWSTR TaskName
-);
-
-WINBOOL WINAPI AvRtDeleteThreadOrderingGroup(
-  HANDLE Context
-);
-
-WINBOOL WINAPI AvRtJoinThreadOrderingGroup(
-  PHANDLE Context,
-  GUID *ThreadOrderingGuid,
-  WINBOOL Before
-);
-
-WINBOOL WINAPI AvRtLeaveThreadOrderingGroup(
-  HANDLE Context
-);
-
-WINBOOL WINAPI AvRtWaitOnThreadOrderingGroup(
-  HANDLE Context
-);
-
-#endif /*(_WIN32_WINNT >= 0x0600)*/
-#endif /*_INC_AVRT*/
+#endif
