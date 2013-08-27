d82fd2604fb4ffd83aa3559a1b4aa089c80e8612
Kai Tietz <ktietz70@googlemail.com>
2013-08-27 07:21:59 +0000

    Add winapi-family support, add missing structure AUDIT_IP_ADDRESS, and annotate header so that it can be used within widl
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@6148 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/adtgen.h
+++ mingw-w64-headers/include/adtgen.h
@@ -1,17 +1,22 @@
-/**
- * This file has no copyright assigned and is placed in the Public Domain.
- * This file is part of the mingw-w64 runtime package.
- * No warranty is given; refer to the file DISCLAIMER.PD within this package.
- */
+**
+* This file is part of the mingw-w64 runtime package.
+* No warranty is given; refer to the file DISCLAIMER within this package.
+*/
+
+#include <winapifamily.h>
+
 #ifndef _ADTGEN_H
 #define _ADTGEN_H
+
+#if WINAPI_FAMILY_PARTITION (WINAPI_PARTITION_DESKTOP)
 
 #define AUDIT_TYPE_LEGACY 1
 #define AUDIT_TYPE_WMI 2
 
 typedef enum _AUDIT_PARAM_TYPE {
   APT_None = 1,
-  APT_String,APT_Ulong,
+  APT_String,
+  APT_Ulong,
   APT_Pointer,
   APT_Sid,
   APT_LogonId,
@@ -19,20 +24,34 @@ typedef enum _AUDIT_PARAM_TYPE {
   APT_Luid,
   APT_Guid,
   APT_Time,
-  APT_Int64
+  APT_Int64,
+  APT_IpAddress,
+  APT_LogonIdWithSid
 } AUDIT_PARAM_TYPE;
 
 #define AP_ParamTypeBits 8
-#define AP_ParamTypeMask __MSABI_LONG(0x000000ff)
+#define AP_ParamTypeMask __MSABI_LONG(0xff)
 
-#define AP_FormatHex (__MSABI_LONG(0x0001) << AP_ParamTypeBits)
-#define AP_AccessMask (__MSABI_LONG(0x0002) << AP_ParamTypeBits)
-#define AP_Filespec (__MSABI_LONG(0x0001) << AP_ParamTypeBits)
-#define AP_PrimaryLogonId (__MSABI_LONG(0x0001) << AP_ParamTypeBits)
-#define AP_ClientLogonId (__MSABI_LONG(0x0002) << AP_ParamTypeBits)
-
-#define ApExtractType(TypeFlags) ((AUDIT_PARAM_TYPE)(TypeFlags & AP_ParamTypeMask))
+#define AP_FormatHex (__MSABI_LONG(0x1) << AP_ParamTypeBits)
+#define AP_AccessMask (__MSABI_LONG(0x2) << AP_ParamTypeBits)
+#define AP_Filespec (__MSABI_LONG(0x1) << AP_ParamTypeBits)
+#define AP_SidAsLogonId (__MSABI_LONG(0x1) << AP_ParamTypeBits)
+#define AP_PrimaryLogonId (__MSABI_LONG(0x1) << AP_ParamTypeBits)
+#define AP_ClientLogonId (__MSABI_LONG(0x2) << AP_ParamTypeBits)
+#define ApExtractType(TypeFlags) ((AUDIT_PARAM_TYPE) (TypeFlags & AP_ParamTypeMask))
 #define ApExtractFlags(TypeFlags) ((TypeFlags & ~AP_ParamTypeMask))
+
+#define _AUTHZ_SS_MAXSIZE 128
+
+#define APF_AuditFailure 0x0
+#define APF_AuditSuccess 0x1
+
+#define APF_ValidFlags (APF_AuditSuccess)
+
+#define AUTHZ_ALLOW_MULTIPLE_SOURCE_INSTANCES 0x1
+#define AUTHZ_MIGRATED_LEGACY_PUBLISHER 0x2
+
+#define AUTHZ_AUDIT_INSTANCE_INFORMATION 0x2
 
 typedef struct _AUDIT_OBJECT_TYPE {
   GUID ObjectType;
@@ -44,63 +63,128 @@ typedef struct _AUDIT_OBJECT_TYPE {
 typedef struct _AUDIT_OBJECT_TYPES {
   USHORT Count;
   USHORT Flags;
+#ifdef __WIDL__
+  [size_is (Count)]
+#endif
   AUDIT_OBJECT_TYPE *pObjectTypes;
 } AUDIT_OBJECT_TYPES,*PAUDIT_OBJECT_TYPES;
+
+typedef struct _AUDIT_IP_ADDRESS {
+  BYTE pIpAddress[_AUTHZ_SS_MAXSIZE];
+} AUDIT_IP_ADDRESS,*PAUDIT_IP_ADDRESS;
 
 typedef struct _AUDIT_PARAM {
   AUDIT_PARAM_TYPE Type;
   ULONG Length;
   DWORD Flags;
-  __C89_NAMELESS union {
+#ifdef __WIDL__
+  [switch_type (AUDIT_PARAM_TYPE), switch_is (Type)]
+#else
+  __C89_NAMELESS
+#endif
+  union {
+#ifdef __WIDL__
+    [default]
+#endif
     ULONG_PTR Data0;
+#ifdef __WIDL__
+    [case (APT_String)]
+    [string]
+#endif
     PWSTR String;
+#ifdef __WIDL__
+    [case (APT_Ulong, APT_Pointer)]
+#endif
     ULONG_PTR u;
+#ifdef __WIDL__
+    [case (APT_Sid)]
+#endif
     SID *psid;
+#ifdef __WIDL__
+    [case (APT_Guid)]
+#endif
     GUID *pguid;
+#ifdef __WIDL__
+    [case (APT_LogonId)]
+#endif
     ULONG LogonId_LowPart;
+#ifdef __WIDL__
+    [case (APT_ObjectTypeList)]
+#endif
     AUDIT_OBJECT_TYPES *pObjectTypes;
+#ifdef __WIDL__
+    [case (APT_IpAddress)]
+#endif
+    AUDIT_IP_ADDRESS *pIpAddress;
   };
-  __C89_NAMELESS union {
+#ifdef __WIDL__
+  [switch_type (AUDIT_PARAM_TYPE), switch_is (Type)]
+#else
+  __C89_NAMELESS
+#endif
+  union {
+#ifdef __WIDL__
+    [default]
+#endif
     ULONG_PTR Data1;
+#ifdef __WIDL__
+    [case (APT_LogonId)]
+#endif
     LONG LogonId_HighPart;
   };
 } AUDIT_PARAM,*PAUDIT_PARAM;
-
-#define APF_AuditFailure 0x00000000
-#define APF_AuditSuccess 0x00000001
-
-#define APF_ValidFlags (APF_AuditSuccess)
 
 typedef struct _AUDIT_PARAMS {
   ULONG Length;
   DWORD Flags;
   USHORT Count;
+#ifdef __WIDL__
+  [size_is (Count)]
+#endif
   AUDIT_PARAM *Parameters;
 } AUDIT_PARAMS,*PAUDIT_PARAMS;
-
 typedef struct _AUTHZ_AUDIT_EVENT_TYPE_LEGACY {
   USHORT CategoryId;
   USHORT AuditId;
   USHORT ParameterCount;
 } AUTHZ_AUDIT_EVENT_TYPE_LEGACY,*PAUTHZ_AUDIT_EVENT_TYPE_LEGACY;
 
-typedef union _AUTHZ_AUDIT_EVENT_TYPE_UNION {
+typedef
+#ifdef __WIDL__
+[switch_type (BYTE)]
+#endif
+union _AUTHZ_AUDIT_EVENT_TYPE_UNION {
+#ifdef __WIDL__
+  [case (AUDIT_TYPE_LEGACY)]
+#endif
   AUTHZ_AUDIT_EVENT_TYPE_LEGACY Legacy;
 } AUTHZ_AUDIT_EVENT_TYPE_UNION,*PAUTHZ_AUDIT_EVENT_TYPE_UNION;
 
-typedef struct _AUTHZ_AUDIT_EVENT_TYPE_OLD {
+typedef
+struct _AUTHZ_AUDIT_EVENT_TYPE_OLD {
   ULONG Version;
   DWORD dwFlags;
   LONG RefCount;
   ULONG_PTR hAudit;
   LUID LinkId;
+#ifdef __WIDL__
+  [switch_is (Version)]
+#endif
   AUTHZ_AUDIT_EVENT_TYPE_UNION u;
 } AUTHZ_AUDIT_EVENT_TYPE_OLD;
 
-typedef AUTHZ_AUDIT_EVENT_TYPE_OLD *PAUTHZ_AUDIT_EVENT_TYPE_OLD;
-typedef PVOID AUDIT_HANDLE,*PAUDIT_HANDLE;
+typedef
+#ifdef __WIDL__
+[handle]
+#endif
+AUTHZ_AUDIT_EVENT_TYPE_OLD *PAUTHZ_AUDIT_EVENT_TYPE_OLD;
+#define AUTHZP_WPD_EVENT 0x10
 
-#define AUTHZ_ALLOW_MULTIPLE_SOURCE_INSTANCES 0x1
-#define AUTHZ_AUDIT_INSTANCE_INFORMATION 0x2
+typedef
+#ifdef __WIDL__
+[context_handle]
+#endif
+PVOID AUDIT_HANDLE,*PAUDIT_HANDLE;
 
+#endif
 #endif
