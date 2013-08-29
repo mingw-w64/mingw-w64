3dd00b40e2e9051b432c88c4e2d0861e7544f89c
Kai Tietz <ktietz70@googlemail.com>
2013-08-29 20:04:59 +0000

    Add winapi-family check, add global guids for IRowset & co.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@6163 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/adc.h
+++ mingw-w64-headers/include/adc.h
@@ -1,28 +1,25 @@
 /**
- * This file has no copyright assigned and is placed in the Public Domain.
  * This file is part of the mingw-w64 runtime package.
- * No warranty is given; refer to the file DISCLAIMER.PD within this package.
+ * No warranty is given; refer to the file DISCLAIMER within this package.
  */
+
+#include <winapifamily.h>
+
 #ifndef ADC_INCLUDED
 #define ADC_INCLUDED
 
-#if defined(DBINITCONSTANTS) || defined(ADCINITCONSTANTS)
-extern const CLSID CLSID_FoxRowset = { 0x3ff292b6,0xb204,0x11cf,{ 0x8d,0x23,0,0xaa,0,0x5f,0xfe,0x58 } };
-extern const GUID DBPROPSET_ADC = { 0xb68e3cc1,0x6deb,0x11d0,{ 0x8d,0xf6,0x00,0xaa,0x00,0x5f,0xfe,0x58 } };
-extern const GUID IID_IAsyncAllowed = {0xf5f2893a,0xba9e,0x11d0,{ 0xab,0xb9,0x0,0xc0,0x4f,0xc2,0x9f,0x8f } };
-extern const IID IID_IRowsetADCExtensions = { 0xF17324c4,0x68E0,0x11D0,{ 0xAD,0x45,0x00,0xC0,0x4F,0xC2,0x98,0x63 } };
-extern const IID IID_IUpdateInfo = { 0xa0385420,0x62b8,0x11d1,{ 0x9a,0x6,0x0,0xa0,0xc9,0x3,0xaa,0x45 } };
-extern const IID IID_IRowsetSynchronize = { 0x1be41e60,0x807a,0x11d1,{ 0x9a,0x14,0x0,0xa0,0xc9,0x3,0xaa,0x45 } };
-extern const IID IID_IRowsetProperties = { 0x1e837070,0xbcfc,0x11d1,{ 0x9a,0x2c,0x0,0xa0,0xc9,0x3,0xaa,0x45 } };
-#else
-extern const CLSID CLSID_FoxRowset;
-extern const GUID DBPROPSET_ADC;
-extern const GUID IID_IAsyncAllowed;
-extern const IID IID_IRowsetADCExtensions;
-extern const IID IID_IUpdateInfo;
-extern const IID IID_IRowsetSynchronize;
-extern const IID IID_IRowsetProperties;
-#endif
+#if WINAPI_FAMILY_PARTITION (WINAPI_PARTITION_DESKTOP)
+
+#undef ADCDECLSPEC
+#define ADCDECLSPEC __declspec (selectany)
+
+extern const ADCDECLSPEC CLSID CLSID_FoxRowset = { 0x3ff292b6, 0xb204, 0x11cf,{ 0x8d, 0x23, 0, 0xaa, 0, 0x5f, 0xfe, 0x58 } };
+extern const ADCDECLSPEC GUID DBPROPSET_ADC = { 0xb68e3cc1, 0x6deb, 0x11d0,{ 0x8d, 0xf6, 0x00, 0xaa, 0x00, 0x5f, 0xfe, 0x58 } };
+extern const ADCDECLSPEC GUID IID_IAsyncAllowed = { 0xf5f2893a, 0xba9e, 0x11d0,{ 0xab, 0xb9, 0x0, 0xc0, 0x4f, 0xc2, 0x9f, 0x8f } };
+extern const ADCDECLSPEC IID IID_IRowsetADCExtensions = { 0xf17324c4, 0x68e0, 0x11d0,{ 0xad, 0x45, 0x00, 0xc0, 0x4f, 0xc2, 0x98, 0x63 } };
+extern const ADCDECLSPEC IID IID_IUpdateInfo = { 0xa0385420, 0x62b8, 0x11d1,{ 0x9a, 0x6, 0x0, 0xa0, 0xc9, 0x3, 0xaa, 0x45 } };
+extern const ADCDECLSPEC IID IID_IRowsetSynchronize = { 0x1be41e60, 0x807a, 0x11d1,{ 0x9a, 0x14, 0x0, 0xa0, 0xc9, 0x3, 0xaa, 0x45 } };
+extern const ADCDECLSPEC IID IID_IRowsetProperties = { 0x1e837070, 0xbcfc, 0x11d1,{ 0x9a, 0x2c, 0x0, 0xa0, 0xc9, 0x3, 0xaa, 0x45 } };
 
 enum ADCPROPENUM {
   DBPROP_ADC_ASYNCHFETCHSIZE = 3,
@@ -46,6 +43,7 @@ enum ADCPROPENUM {
 
 #ifndef _COMMON_ADC_AND_ADO_PROPS_
 #define _COMMON_ADC_AND_ADO_PROPS_
+
 enum ADCPROP_UPDATECRITERIA_ENUM {
   adCriteriaKey = 0,
   adCriteriaAllCols = 1,
@@ -67,7 +65,7 @@ enum ADCPROP_UPDATERESYNC_ENUM {
   adResyncConflicts = 0x2,
   adResyncUpdates = 0x4,
   adResyncInserts = 0x8,
-  adResyncAll = 0xf
+  adResyncAll = 0x0f
 };
 
 enum ADCPROP_AUTORECALC_ENUM {
@@ -79,4 +77,6 @@ enum ADCPROP_AUTORECALC_ENUM {
 enum FOXROWSETPROPENUM {
   DBPROP_FOXTABLENAME = 0xeeff
 };
+#endif
+
 #endif
