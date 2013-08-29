8d5dd7181dc67cd0db709715fdadb9635b7e3d85
Kai Tietz <ktietz70@googlemail.com>
2013-08-29 12:46:40 +0000

    Use ADO?_MINOR/MAJOR version-identifiers for building ADO?_VERSION defines. Add ADO?_LIBRARYNAME macros. Guarded adoid.h header by winapi-family check.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@6159 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/adodef.h
+++ mingw-w64-headers/include/adodef.h
@@ -1,40 +1,36 @@
 /**
- * This file has no copyright assigned and is placed in the Public Domain.
  * This file is part of the mingw-w64 runtime package.
- * No warranty is given; refer to the file DISCLAIMER.PD within this package.
+ * No warranty is given; refer to the file DISCLAIMER within this package.
  */
+
 #ifndef _ADODEF_H_
 #define _ADODEF_H_
 
-#define ADO_MAJOR 2
-#define ADOR_MAJOR 2
-#define ADOX_MAJOR 2
-#define ADOMD_MAJOR 2
+#define ADO_MAJOR 6
+#define ADOR_MAJOR 6
+#define ADOX_MAJOR 6
+#define ADOMD_MAJOR 6
 #define JRO_MAJOR 2
 
-#define ADO_MINOR 8
-#define ADOR_MINOR 8
-#define ADOX_MINOR 8
-#define ADOMD_MINOR 8
+#define ADO_MINOR 1
+#define ADOR_MINOR 0
+#define ADOX_MINOR 0
+#define ADOMD_MINOR 0
 #define JRO_MINOR 6
 
-#define ADO_VERSION 2.8
-#define ADOR_VERSION 2.8
-#define ADOX_VERSION 2.8
-#define ADOMD_VERSION 2.8
-#define JRO_VERSION 2.6
+#define ADO_VERSION ADO_MAJOR##.##ADO_MINOR
+#define ADOR_VERSION ADOR_MAJOR##.##ADOR_MINOR
+#define ADOX_VERSION ADOX_MAJOR##.##ADOX_MINOR
+#define ADOMD_VERSION ADOMD_MAJOR##.##ADOMD_MINOR
+#define JRO_VERSION JRO_MAJOR##.##JRO_MINOR
 
-#define ADO_HELPFILE "ado270.chm"
-#define ADOX_HELPFILE "ado270.chm"
-#define ADOMD_HELPFILE "ado270.chm"
-#define JRO_HELPFILE "msjro.chm"
-
-#define ADO_LIBRARYNAME "Microsoft ActiveX Data Objects 2.8 Library"
-#define ADOR_LIBRARYNAME "Microsoft ActiveX Data Objects Recordset 2.8 Library"
-#define ADOX_LIBRARYNAME "Microsoft ADO Ext. 2.8 for DDL and Security"
-#define ADOMD_LIBRARYNAME "Microsoft ActiveX Data Objects (Multi-dimensional) 2.8 Library"
+#define ADO_LIBRARYNAME "Microsoft ActiveX Data Objects 6.1 Library"
+#define ADOR_LIBRARYNAME "Microsoft ActiveX Data Objects Recordset 6.0 Library"
+#define ADOX_LIBRARYNAME "Microsoft ADO Ext. 6.0 for DDL and Security"
+#define ADOMD_LIBRARYNAME "Microsoft ActiveX Data Objects (Multi-dimensional) 6.0 Library"
 #define JRO_LIBRARYNAME "Microsoft Jet and Replication Objects 2.6 Library"
 
-#define ADOMD_TYPELIB_UUID uuid(22813728-8BD3-11D0-B4EF-00A0C9138CA4)
-#define JRO_TYPELIB_UUID uuid(AC3B8B4C-B6CA-11d1-9F31-00C04FC29D52)
+#define ADOMD_TYPELIB_UUID uuid(22813728-8bd3-11d0-B4EF-00a0c9138ca4)
+#define JRO_TYPELIB_UUID uuid(AC3B8B4C-B6CA-11d1-9f31-00c04fc29d52)
+
 #endif
