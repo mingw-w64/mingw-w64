d5ddecfbb75dc4d32599c7bea253f41377a441de
Kai Tietz <ktietz70@googlemail.com>
2013-09-04 13:00:32 +0000

    Add winapi-family check, restruct code a bit.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@6189 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/winnls32.h
+++ mingw-w64-headers/include/winnls32.h
@@ -1,12 +1,15 @@
 /**
- * This file has no copyright assigned and is placed in the Public Domain.
  * This file is part of the mingw-w64 runtime package.
- * No warranty is given; refer to the file DISCLAIMER.PD within this package.
+ * No warranty is given; refer to the file DISCLAIMER within this package.
  */
+
 #ifndef _WINNLS32_
 #define _WINNLS32_
 
 #include <_mingw_unicode.h>
+#include <winapifamily.h>
+
+#if WINAPI_FAMILY_PARTITION (WINAPI_PARTITION_DESKTOP)
 
 #ifdef __cplusplus
 extern "C" {
@@ -28,7 +31,7 @@ extern "C" {
     BYTE szDescription[50];
     BYTE szName[80];
     BYTE szOptions[30];
-  } IMEPROA,*PIMEPROA,NEAR *NPIMEPROA,*LPIMEPROA;
+  } IMEPROA,*PIMEPROA,*NPIMEPROA,*LPIMEPROA;
 
   typedef struct _tagIMEPROW {
     HWND hWnd;
@@ -37,28 +40,31 @@ extern "C" {
     WCHAR szDescription[50];
     WCHAR szName[80];
     WCHAR szOptions[30];
-  } IMEPROW,*PIMEPROW,NEAR *NPIMEPROW,*LPIMEPROW;
+  } IMEPROW,*PIMEPROW,*NPIMEPROW,*LPIMEPROW;
 
   __MINGW_TYPEDEF_AW(IMEPRO)
   __MINGW_TYPEDEF_AW(PIMEPRO)
   __MINGW_TYPEDEF_AW(NPIMEPRO)
   __MINGW_TYPEDEF_AW(LPIMEPRO)
 
+  WINBOOL WINAPI IMPGetIMEA (HWND, LPIMEPROA);
+  WINBOOL WINAPI IMPGetIMEW (HWND, LPIMEPROW);
+  WINBOOL WINAPI IMPQueryIMEA (LPIMEPROA);
+  WINBOOL WINAPI IMPQueryIMEW (LPIMEPROW);
+  WINBOOL WINAPI IMPSetIMEA (HWND, LPIMEPROA);
+  WINBOOL WINAPI IMPSetIMEW (HWND, LPIMEPROW);
+  UINT WINAPI WINNLSGetIMEHotkey (HWND);
+  WINBOOL WINAPI WINNLSEnableIME (HWND, WINBOOL);
+  WINBOOL WINAPI WINNLSGetEnableStatus (HWND);
+
 #define IMPGetIME __MINGW_NAME_AW(IMPGetIME)
 #define IMPQueryIME __MINGW_NAME_AW(IMPQueryIME)
 #define IMPSetIME __MINGW_NAME_AW(IMPSetIME)
 
-  WINBOOL WINAPI IMPGetIMEA(HWND,LPIMEPROA);
-  WINBOOL WINAPI IMPGetIMEW(HWND,LPIMEPROW);
-  WINBOOL WINAPI IMPQueryIMEA(LPIMEPROA);
-  WINBOOL WINAPI IMPQueryIMEW(LPIMEPROW);
-  WINBOOL WINAPI IMPSetIMEA(HWND,LPIMEPROA);
-  WINBOOL WINAPI IMPSetIMEW(HWND,LPIMEPROW);
-  UINT WINAPI WINNLSGetIMEHotkey(HWND);
-  WINBOOL WINAPI WINNLSEnableIME(HWND,WINBOOL);
-  WINBOOL WINAPI WINNLSGetEnableStatus(HWND);
-
 #ifdef __cplusplus
 }
 #endif
+
+#endif
+
 #endif
