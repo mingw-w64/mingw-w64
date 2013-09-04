83c9feca572c34111bbf0d747e87c2f7873026a4
Kai Tietz <ktietz70@googlemail.com>
2013-09-04 13:03:41 +0000

    Add missing type ber_len_t, and add winapi-family check
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@6190 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/winber.h
+++ mingw-w64-headers/include/winber.h
@@ -1,19 +1,23 @@
 /**
- * This file has no copyright assigned and is placed in the Public Domain.
  * This file is part of the mingw-w64 runtime package.
- * No warranty is given; refer to the file DISCLAIMER.PD within this package.
+ * No warranty is given; refer to the file DISCLAIMER within this package.
  */
+
 #ifndef _WINBER_DEFINED_
 #define _WINBER_DEFINED_
 
-#include "winldap.h"
+#include <winapifamily.h>
+
+#if WINAPI_FAMILY_PARTITION (WINAPI_PARTITION_DESKTOP)
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#ifndef WINBERAPI
+#ifndef _WINBER_
 #define WINBERAPI DECLSPEC_IMPORT
+#else
+#define WINBERAPI
 #endif
 
 #ifndef BERAPI
@@ -26,23 +30,27 @@ extern "C" {
   typedef unsigned int ber_tag_t;
   typedef int ber_int_t;
   typedef unsigned int ber_uint_t;
+  typedef unsigned int ber_len_t;
   typedef int ber_slen_t;
 
-  WINBERAPI BerElement *BERAPI ber_init(BERVAL *pBerVal);
-  WINBERAPI VOID BERAPI ber_free(BerElement *pBerElement,INT fbuf);
-  WINBERAPI VOID BERAPI ber_bvfree(BERVAL *pBerVal);
-  WINBERAPI VOID BERAPI ber_bvecfree(PBERVAL *pBerVal);
-  WINBERAPI BERVAL *BERAPI ber_bvdup(BERVAL *pBerVal);
-  WINBERAPI BerElement *BERAPI ber_alloc_t(INT options);
-  WINBERAPI ULONG BERAPI ber_skip_tag(BerElement *pBerElement,ULONG *pLen);
-  WINBERAPI ULONG BERAPI ber_peek_tag(BerElement *pBerElement,ULONG *pLen);
-  WINBERAPI ULONG BERAPI ber_first_element(BerElement *pBerElement,ULONG *pLen,CHAR **ppOpaque);
-  WINBERAPI ULONG BERAPI ber_next_element(BerElement *pBerElement,ULONG *pLen,CHAR *opaque);
-  WINBERAPI INT BERAPI ber_flatten(BerElement *pBerElement,PBERVAL *pBerVal);
-  WINBERAPI INT BERAPI ber_printf(BerElement *pBerElement,PCHAR fmt,...);
-  WINBERAPI ULONG BERAPI ber_scanf(BerElement *pBerElement,PCHAR fmt,...);
+  WINBERAPI BerElement *BERAPI ber_init (BERVAL *pBerVal);
+  WINBERAPI VOID BERAPI ber_free (BerElement *pBerElement, INT fbuf);
+  WINBERAPI VOID BERAPI ber_bvfree (BERVAL *pBerVal);
+  WINBERAPI VOID BERAPI ber_bvecfree (PBERVAL *pBerVal);
+  WINBERAPI BERVAL *BERAPI ber_bvdup (BERVAL *pBerVal);
+  WINBERAPI BerElement *BERAPI ber_alloc_t (INT options);
+  WINBERAPI ULONG BERAPI ber_skip_tag (BerElement *pBerElement, ULONG *pLen);
+  WINBERAPI ULONG BERAPI ber_peek_tag (BerElement *pBerElement, ULONG *pLen);
+  WINBERAPI ULONG BERAPI ber_first_element (BerElement *pBerElement, ULONG *pLen, CHAR **ppOpaque);
+  WINBERAPI ULONG BERAPI ber_next_element (BerElement *pBerElement, ULONG *pLen, CHAR *opaque);
+  WINBERAPI INT BERAPI ber_flatten (BerElement *pBerElement, PBERVAL *pBerVal);
+  WINBERAPI INT BERAPI ber_printf (BerElement *pBerElement, PSTR fmt,...);
+  WINBERAPI ULONG BERAPI ber_scanf (BerElement *pBerElement, PSTR fmt,...);
 
 #ifdef __cplusplus
 }
 #endif
+
+#endif
+
 #endif
