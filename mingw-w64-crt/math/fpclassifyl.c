c6b98feb7c00f708c727633c4e6b0e0e3fafbd38
Kai Tietz <ktietz70@googlemail.com>
2013-09-22 19:16:11 +0000

    Avoid use of x87 instruction for x64 version of fpclassifyl
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@6311 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-crt/math/fpclassifyl.c
+++ mingw-w64-crt/math/fpclassifyl.c
@@ -1,12 +1,29 @@
 /**
- * This file has no copyright assigned and is placed in the Public Domain.
  * This file is part of the mingw-w64 runtime package.
- * No warranty is given; refer to the file DISCLAIMER.PD within this package.
+ * No warranty is given; refer to the file DISCLAIMER within this package.
  */
 #define __CRT__NO_INLINE
 #include <math.h>
 
 int __fpclassifyl (long double _x){
+#ifdef __x86_64__
+    __mingw_fp_types_t hlp;
+    unsigned int e;
+    hlp.ld = &_x;
+    e = hlp.ldt->lh.sign_exponent & 0x7fff;
+    if (!e)
+      {
+        unsigned int h = hlp.ldt->lh.high;
+        if (!(hlp.ldt->lh.low | h))
+          return FP_ZERO;
+        else if (!(h & 0x80000000))
+          return FP_SUBNORMAL;
+      }
+    else if (e == 0x7fff)
+      return (((hlp.ldt->lh.high & 0x7fffffff) | hlp.ldt->lh.low) == 0 ?
+	      FP_INFINITE : FP_NAN);
+    return FP_NORMAL;
+#else
   unsigned short sw;
   __asm__ __volatile__ (
 	"fxam; fstsw %%ax;"
@@ -14,4 +31,5 @@ int __fpclassifyl (long double _x){
 	: "t" (_x)
 	);
   return sw & (FP_NAN | FP_NORMAL | FP_ZERO );
+#endif
 }
