4a061ad9550357d18a4a208b0e271036629b729a
Kai Tietz <ktietz70@googlemail.com>
2013-09-22 19:14:21 +0000

    Avoid x87 instructions for x64 for fpclassifyl
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@6309 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/crt/math.h
+++ mingw-w64-headers/crt/math.h
@@ -371,9 +371,28 @@ typedef long double double_t;
 
 #ifndef __CRT__NO_INLINE
   __CRT_INLINE int __cdecl __fpclassifyl (long double x) {
+#ifdef __x86_64__
+    __mingw_fp_types_t hlp;
+    unsigned int e;
+    hlp.ld = &x;
+    e = hlp.ldt->lh.sign_exponent & 0x7fff;
+    if (!e)
+      {
+        unsigned int h = hlp.ldt->lh.high;
+        if (!(hlp.ldt->lh.low | h))
+          return FP_ZERO;
+        else if (!(h & 0x80000000))
+          return FP_SUBNORMAL;
+      }
+    else if (e == 0x7fff)
+      return (((hlp.ldt->lh.high & 0x7fffffff) | hlp.ldt->lh.low) == 0 ?
+              FP_INFINITE : FP_NAN);
+    return FP_NORMAL;
+#else
     unsigned short sw;
     __asm__ __volatile__ ("fxam; fstsw %%ax;" : "=a" (sw): "t" (x));
     return sw & (FP_NAN | FP_NORMAL | FP_ZERO );
+#endif
   }
   __CRT_INLINE int __cdecl __fpclassify (double x) {
 #ifdef __x86_64__
