5d79dd7293bcdd31fd6f27a7f9c50e16753fd810
Erik van Pienbroek <erik@vanpienbroek.nl>
2014-01-26 19:47:34 +0000

    Add secapi wrapper for sprintf_s
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@6469 4407c894-4637-0410-b4f5-ada5f102cad1

---
+++ mingw-w64-crt/secapi/sprintf_s.c
@@ -0,0 +1,21 @@
+#include <windows.h>
+#include <malloc.h>
+#include <errno.h>
+#include <msvcrt.h>
+
+int __cdecl sprintf_s (char *, size_t, const char *, ...);
+int __cdecl vsprintf_s (char *, size_t, const char *, va_list);
+
+int __cdecl (*__MINGW_IMP_SYMBOL(sprintf_s))(char *, size_t, const char *,...) = sprintf_s;
+
+int __cdecl
+sprintf_s (char *_DstBuf, size_t _Size, const char *_Format, ...)
+{
+  va_list argp;
+  int r;
+
+  va_start (argp, _Format);
+  r = vsprintf_s (_DstBuf, _Size, _Format, argp);
+  va_end (argp);
+  return r; 
+}
