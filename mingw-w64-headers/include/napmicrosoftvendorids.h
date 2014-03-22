7a18d25f1d7b0620c8cba4598b5d66992178b27c
Kai Tietz <ktietz70@googlemail.com>
2014-03-22 13:27:21 +0000

    Adjust header to fit more msdn-documentation.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@6535 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/napmicrosoftvendorids.h
+++ mingw-w64-headers/include/napmicrosoftvendorids.h
@@ -1,26 +1,36 @@
 /**
- * This file has no copyright assigned and is placed in the Public Domain.
  * This file is part of the mingw-w64 runtime package.
- * No warranty is given; refer to the file DISCLAIMER.PD within this package.
+ * No warranty is given; refer to the file DISCLAIMER within this package.
  */
-#ifndef _INC_NAPMICROSOFTVENDORIDS
-#define _INC_NAPMICROSOFTVENDORIDS
 
-#if (_WIN32_WINNT >= 0x0600)
-#define NapSystemId                     0x00013700
-#define NapDhcpEnforcementId            0x00013701
-#define NapRasEnforcementId             0x00013702
-#define NapIpsecEnforcementId           0x00013703
-#define Nap8021xEnforcementId           0x00013704
-#define NapAnywhereAccessEnforcementId  0x00013705
-#define NapIsaEnforcementId             0x00013706
-#define NapEapEnforcementId             0x00013707
-#define NapOutOfBoxSystemHealthId       0x00013780
-#define NapSmsSystemHealthId            0x00013781
-#define NapFCSv1SystemHealthId          0x00013782
-#define NapFCSv2SystemHealthId          0x00013783
-#define NapTpmSystemHealthId            0x00013784
-#define MicrosoftVendorId               0x0137
-#endif /*(_WIN32_WINNT >= 0x0600)*/
+#include <winapifamily.h>
 
-#endif /* _INC_NAPMICROSOFTVENDORIDS */
+#ifndef NAPMICROSOFTVENDORIDS_H
+#define NAPMICROSOFTVENDORIDS_H
+
+#if WINAPI_FAMILY_PARTITION (WINAPI_PARTITION_DESKTOP)
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  __MINGW_ATTRIB_UNUSED static UINT32 MicrosoftVendorId = 0x137;
+  __MINGW_ATTRIB_UNUSED static UINT32 NapSystemId = 0x00013700;
+  __MINGW_ATTRIB_UNUSED static UINT32 NapDhcpEnforcementId = 0x00013701;
+  __MINGW_ATTRIB_UNUSED static UINT32 NapRasEnforcementId = 0x00013702;
+  __MINGW_ATTRIB_UNUSED static UINT32 NapIpsecEnforcementId = 0x00013703;
+  __MINGW_ATTRIB_UNUSED static UINT32 Nap8021xEnforcementId = 0x00013704;
+  __MINGW_ATTRIB_UNUSED static UINT32 NapAnywhereAccessEnforcementId = 0x00013705;
+  __MINGW_ATTRIB_UNUSED static UINT32 NapIsaEnforcementId = 0x00013706;
+  __MINGW_ATTRIB_UNUSED static UINT32 NapEapEnforcementId = 0x00013707;
+  __MINGW_ATTRIB_UNUSED static UINT32 NapOutOfBoxSystemHealthId = 0x00013780;
+  __MINGW_ATTRIB_UNUSED static UINT32 NapSmsSystemHealthId = 0x00013781;
+  __MINGW_ATTRIB_UNUSED static UINT32 NapFCSv1SystemHealthId = 0x00013782;
+  __MINGW_ATTRIB_UNUSED static UINT32 NapFCSv2SystemHealthId = 0x00013783;
+  __MINGW_ATTRIB_UNUSED static UINT32 NapTpmSystemHealthId = 0x00013784;
+
+#ifdef __cplusplus
+}
+#endif
+#endif
+
+#endif
