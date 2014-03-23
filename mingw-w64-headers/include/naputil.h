79297f8a8596b24578aacf2d1a4fa80fbbd27331
Kai Tietz <ktietz70@googlemail.com>
2014-03-23 10:56:46 +0000

    Restructure header, add winapi-family checks
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@6537 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/include/naputil.h
+++ mingw-w64-headers/include/naputil.h
@@ -1,41 +1,49 @@
 /**
- * This file has no copyright assigned and is placed in the Public Domain.
  * This file is part of the mingw-w64 runtime package.
- * No warranty is given; refer to the file DISCLAIMER.PD within this package.
+ * No warranty is given; refer to the file DISCLAIMER within this package.
  */
-#ifndef _INC_NAPUTIL
-#define _INC_NAPUTIL
 
-#include <naptypes.h>
-#if (_WIN32_WINNT >= 0x0600)
+#ifndef NAPUTIL_H
+#define NAPUTIL_H
+
+#include <winapifamily.h>
+
+#if WINAPI_FAMILY_PARTITION (WINAPI_PARTITION_DESKTOP)
+
+#include "naptypes.h"
+#include "napmanagement.h"
+#include "napservermanagement.h"
+#include "napprotocol.h"
+#include "napenforcementclient.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+#ifndef NAPAPI
 #define NAPAPI DECLSPEC_IMPORT
+#endif
 
-  NAPAPI HRESULT WINAPI AllocConnections(Connections **connections,UINT16 connectionsCount);
-  NAPAPI VOID WINAPI FreeConnections(Connections *connections);
-  NAPAPI HRESULT WINAPI AllocFixupInfo(FixupInfo **fixupInfo,UINT16 countResultCodes);
-  NAPAPI VOID WINAPI FreeCountedString(CountedString *countedString);
+  NAPAPI HRESULT WINAPI AllocFixupInfo(FixupInfo **fixupInfo, UINT16 countResultCodes);
+  NAPAPI HRESULT WINAPI AllocConnections(Connections **connections, UINT16 connectionsCount);
+  NAPAPI HRESULT WINAPI AllocCountedString(CountedString **countedString, CONST WCHAR *string);
   NAPAPI VOID WINAPI FreeFixupInfo(FixupInfo *fixupInfo);
+  NAPAPI VOID WINAPI FreeConnections(Connections *connections);
   NAPAPI VOID WINAPI FreeIsolationInfo(IsolationInfo *isolationInfo);
   NAPAPI VOID WINAPI FreeIsolationInfoEx(IsolationInfoEx *isolationInfo);
-  NAPAPI VOID WINAPI FreeNapComponentRegistrationInfoArray(UINT16 count,NapComponentRegistrationInfo **info);
+  NAPAPI VOID WINAPI FreeCountedString(CountedString *countedString);
+  NAPAPI VOID WINAPI FreeSoH(SoH *soh);
   NAPAPI VOID WINAPI FreeNetworkSoH(NetworkSoH *networkSoh);
   NAPAPI VOID WINAPI FreePrivateData(PrivateData *privateData);
-  NAPAPI VOID WINAPI FreeSoH(SoH *soh);
-  NAPAPI VOID WINAPI FreeSoHAttributeValue(SoHAttributeType type,SoHAttributeValue *value);
+  NAPAPI VOID WINAPI FreeSoHAttributeValue(SoHAttributeType type, SoHAttributeValue *value);
+  NAPAPI VOID WINAPI FreeNapComponentRegistrationInfoArray(UINT16 count, NapComponentRegistrationInfo **info);
   NAPAPI VOID WINAPI FreeSystemHealthAgentState(SystemHealthAgentState *state);
-  NAPAPI HRESULT WINAPI InitializeNapAgentNotifier(NapNotifyType type,HANDLE hNotifyEvent);
+  NAPAPI HRESULT WINAPI InitializeNapAgentNotifier(NapNotifyType type, HANDLE hNotifyEvent);
   NAPAPI VOID WINAPI UninitializeNapAgentNotifier(NapNotifyType type);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /*(_WIN32_WINNT >= 0x0600)*/
-
-#endif /* _INC_NAPUTIL */
-
+#endif
+#endif
