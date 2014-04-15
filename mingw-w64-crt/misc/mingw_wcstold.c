4713dd1ef4aaad07bebb5c20984a3137713d3bb6
Ozkan Sezer <sezeroz@gmail.com>
2014-04-15 14:15:25 +0000

    fix from trunk r6487
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/stable/v3.x@6580 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-crt/misc/mingw_wcstold.c
+++ mingw-w64-crt/misc/mingw_wcstold.c
@@ -22,9 +22,10 @@ __mingw_wcstold (const wchar_t * __restrict__ _Str, wchar_t ** __restrict__ _End
     l2 = MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, n, -1, NULL, 0);
     if (l2 > 0)
       l2 -= 1; /* Remove zero terminator from length.  */
-    *_EndPtr = (wchar_t *) &_Str[l2];
+    if (_EndPtr)
+      *_EndPtr = (wchar_t *) &_Str[l2];
   }
-  else
+  else if (_EndPtr)
    *_EndPtr = NULL;
   return r;
 }
