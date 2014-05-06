c00f1a1f9538153da9baa1762a4501fa366eabc0
André Hentschel <nerv@dawncrow.de>
2014-05-06 21:55:51 +0000

    Define _ALLOCA_S_MARKER_SIZE on ARM
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/trunk@6613 4407c894-4637-0410-b4f5-ada5f102cad1

--- mingw-w64-headers/crt/malloc.h
+++ mingw-w64-headers/crt/malloc.h
@@ -134,7 +134,7 @@ void * __mingw_aligned_realloc (void *_Memory, size_t _Size, size_t _Offset);
 #define _ALLOCA_S_STACK_MARKER 0xCCCC
 #define _ALLOCA_S_HEAP_MARKER 0xDDDD
 
-#if(defined(_X86_) && !defined(__x86_64))
+#if defined(_ARM_) || (defined(_X86_) && !defined(__x86_64))
 #define _ALLOCA_S_MARKER_SIZE 8
 #elif defined(__ia64__) || defined(__x86_64)
 #define _ALLOCA_S_MARKER_SIZE 16
