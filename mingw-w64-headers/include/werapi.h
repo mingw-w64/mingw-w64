c96f15f579254cb79fdccf39d566e158aece00f7
Jonathan Yong <10walls@gmail.com>
2014-05-20 06:26:12 +0800

    Add declaration for WerRegisterRuntimeExceptionModule and WerUnregisterRuntimeExceptionModule

--- mingw-w64-headers/include/werapi.h
+++ mingw-w64-headers/include/werapi.h
@@ -124,6 +124,56 @@ HRESULT WINAPI WerSetFlags(DWORD dwFlags);
 HRESULT WINAPI WerUnregisterFile(PCWSTR pwzFilePath);
 HRESULT WINAPI WerUnregisterMemoryBlock(PVOID pvAddress);
 
+#if (_WIN32_WINNT >= 0x0601)
+typedef struct _WER_RUNTIME_EXCEPTION_INFORMATION {
+  DWORD            dwSize;
+  HANDLE           hProcess;
+  HANDLE           hThread;
+  EXCEPTION_RECORD exceptionRecord;
+  CONTEXT          context;
+  PCWSTR           pwszReportId;
+} WER_RUNTIME_EXCEPTION_INFORMATION, *PWER_RUNTIME_EXCEPTION_INFORMATION;
+
+typedef HRESULT (WINAPI *PFN_WER_RUNTIME_EXCEPTION_EVENT)(
+  PVOID pContext,
+  const PWER_RUNTIME_EXCEPTION_INFORMATION pExceptionInformation,
+  WINBOOL *pbOwnershipClaimed,
+  PWSTR pwszEventName,
+  PDWORD pchSize,
+  PDWORD pdwSignatureCount
+);
+
+typedef HRESULT (WINAPI *PFN_WER_RUNTIME_EXCEPTION_DEBUGGER_LAUNCH)(
+  PVOID pContext,
+  const PWER_RUNTIME_EXCEPTION_INFORMATION pExceptionInformation,
+  PBOOL pbIsCustomDebugger,
+  PWSTR pwszDebuggerLaunch,
+  PDWORD pchDebuggerLaunch,
+  PBOOL pbIsDebuggerAutolaunch
+);
+
+typedef HRESULT (WINAPI *PFN_WER_RUNTIME_EXCEPTION_EVENT_SIGNATURE)(
+  PVOID pContext,
+  const PWER_RUNTIME_EXCEPTION_INFORMATION pExceptionInformation,
+  DWORD dwIndex,
+  PWSTR pwszName,
+  PDWORD pchName,
+  PWSTR pwszValue,
+  PDWORD pchValue
+);
+
+HRESULT WINAPI WerRegisterRuntimeExceptionModule(
+  PCWSTR pwszOutOfProcessCallbackDll,
+  PVOID pContext
+);
+
+HRESULT WINAPI WerUnregisterRuntimeExceptionModule(
+  PCWSTR pwszOutOfProcessCallbackDll,
+  PVOID pContext
+);
+
+#endif /*(_WIN32_WINNT >= 0x0601)*/
+
 #ifdef __cplusplus
 }
 #endif
