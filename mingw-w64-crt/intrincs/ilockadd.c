82e51ff29211e097dfb7670d9013df8556e06a41
André Hentschel <nerv@dawncrow.de>
2014-05-30 19:25:33 +0200

    Add platform independent version of _InterlockedAdd

--- mingw-w64-crt/intrincs/ilockadd.c
+++ mingw-w64-crt/intrincs/ilockadd.c
@@ -4,6 +4,7 @@ __LONG32 _InterlockedAdd(__LONG32 volatile *, __LONG32);			/* not in intrin.h */
 __LONG32 _InterlockedAdd(__LONG32 volatile *Addend, __LONG32 Value)
 {
 /* return InterlockedExchangeAdd(Addend,Value) + Value; */
+#if defined(__x86_64__) || defined(_AMD64_) || defined(__i386__) || defined(_X86_)
   __LONG32 ret;
   __asm__ __volatile__ ("lock\n\t"
            "xaddl %0,(%1)"
@@ -11,6 +12,9 @@ __LONG32 _InterlockedAdd(__LONG32 volatile *Addend, __LONG32 Value)
            : "r" (Addend), "0" (Value)
            : "memory");
   return ret + Value;
+#else
+  return __sync_fetch_and_add(Addend, Value) + Value;
+#endif
 }
 
 #ifdef _WIN64
