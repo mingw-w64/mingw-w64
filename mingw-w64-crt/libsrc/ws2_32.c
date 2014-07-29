88ab6fbdd0a185702a1fce4db935e303030e082f
Erik van Pienbroek <epienbro@fedoraproject.org>
2014-07-29 23:51:43 +0200

    Use correct initializer for in6addr_loopback and in6addr_any
    In commit b8e8160d a fix was applied to prevent compiler
    warnings when using IN6ADDR_ANY_INIT or IN6ADDR_LOOPBACK_INIT:
    https://bugzilla.redhat.com/show_bug.cgi?id=1067426
    
    Apparently this change wasn't fully complete as it turned
    out to break the contents of the global variable in6addr_loopback:
    https://bugzilla.redhat.com/show_bug.cgi?id=1124368

--- mingw-w64-crt/libsrc/ws2_32.c
+++ mingw-w64-crt/libsrc/ws2_32.c
@@ -2,5 +2,5 @@
 #include <ws2tcpip.h>
 
 /*  IPv6 constants for use in structure assignments (RFC 2553).  */
-const struct in6_addr in6addr_any = {{ IN6ADDR_ANY_INIT }};
-const struct in6_addr in6addr_loopback = {{ IN6ADDR_LOOPBACK_INIT }};
+const struct in6_addr in6addr_any = IN6ADDR_ANY_INIT;
+const struct in6_addr in6addr_loopback = IN6ADDR_LOOPBACK_INIT;
