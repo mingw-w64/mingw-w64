4d463ece0dcf8769e64b9afd1069331c5ddfb9f2
Kai Tietz <ktietz70@googlemail.com>
2014-08-25 21:07:52 +0200

    Add winapifamily check, and additional add Windows 7/8 extensions.

--- mingw-w64-headers/include/raseapif.h
+++ mingw-w64-headers/include/raseapif.h
@@ -1,10 +1,14 @@
 /**
- * This file has no copyright assigned and is placed in the Public Domain.
  * This file is part of the mingw-w64 runtime package.
- * No warranty is given; refer to the file DISCLAIMER.PD within this package.
+ * No warranty is given; refer to the file DISCLAIMER within this package.
  */
+
 #ifndef _RASEAPIF_
 #define _RASEAPIF_
+
+#include <winapifamily.h>
+
+#if WINAPI_FAMILY_PARTITION (WINAPI_PARTITION_DESKTOP)
 
 #ifdef __cplusplus
 extern "C" {
@@ -26,6 +30,8 @@ extern "C" {
 #define RAS_EAP_VALUENAME_STANDALONE_SUPPORTED TEXT("StandaloneSupported")
 #define RAS_EAP_VALUENAME_ROLES_SUPPORTED TEXT("RolesSupported")
 #define RAS_EAP_VALUENAME_PER_POLICY_CONFIG TEXT("PerPolicyConfig")
+#define RAS_EAP_VALUENAME_ISTUNNEL_METHOD TEXT("IsTunnelMethod")
+#define RAS_EAP_VALUENAME_FILTER_INNERMETHODS TEXT("FilterInnerMethods")
 
 #define RAS_EAP_ROLE_AUTHENTICATOR 0x00000001
 #define RAS_EAP_ROLE_AUTHENTICATEE 0x00000002
@@ -35,18 +41,101 @@ extern "C" {
 #define RAS_EAP_ROLE_EXCLUDE_IN_VPN 0x00000010
 
   typedef enum _RAS_AUTH_ATTRIBUTE_TYPE_ {
-    raatMinimum = 0,raatUserName,raatUserPassword,raatMD5CHAPPassword,raatNASIPAddress,raatNASPort,raatServiceType,raatFramedProtocol,
-    raatFramedIPAddress,raatFramedIPNetmask,raatFramedRouting = 10,raatFilterId,raatFramedMTU,raatFramedCompression,raatLoginIPHost,
-    raatLoginService,raatLoginTCPPort,raatUnassigned17,raatReplyMessage,raatCallbackNumber,raatCallbackId =20,raatUnassigned21,
-    raatFramedRoute,raatFramedIPXNetwork,raatState,raatClass,raatVendorSpecific,raatSessionTimeout,raatIdleTimeout,raatTerminationAction,
-    raatCalledStationId = 30,raatCallingStationId,raatNASIdentifier,raatProxyState,raatLoginLATService,raatLoginLATNode,raatLoginLATGroup,
-    raatFramedAppleTalkLink,raatFramedAppleTalkNetwork,raatFramedAppleTalkZone,raatAcctStatusType = 40,raatAcctDelayTime,raatAcctInputOctets,
-    raatAcctOutputOctets,raatAcctSessionId,raatAcctAuthentic,raatAcctSessionTime,raatAcctInputPackets,raatAcctOutputPackets,raatAcctTerminateCause,
-    raatAcctMultiSessionId = 50,raatAcctLinkCount,raatAcctEventTimeStamp = 55,raatMD5CHAPChallenge = 60,raatNASPortType,raatPortLimit,raatLoginLATPort,
-    raatTunnelType,raatTunnelMediumType,raatTunnelClientEndpoint,raatTunnelServerEndpoint,raatARAPPassword = 70,raatARAPFeatures,raatARAPZoneAccess,
-    raatARAPSecurity,raatARAPSecurityData,raatPasswordRetry,raatPrompt,raatConnectInfo,raatConfigurationToken,raatEAPMessage,raatSignature = 80,
-    raatARAPChallengeResponse = 84,raatAcctInterimInterval = 85,raatARAPGuestLogon = 8096,raatCertificateOID,raatEAPConfiguration,
-    raatPEAPEmbeddedEAPTypeId,raatPEAPFastRoamedSession,raatEAPTLV = 8102,raatReserved = 0xFFFFFFFF
+    raatMinimum = 0,
+    raatUserName,
+    raatUserPassword,
+    raatMD5CHAPPassword,
+    raatNASIPAddress,
+    raatNASPort,
+    raatServiceType,
+    raatFramedProtocol,
+    raatFramedIPAddress,
+    raatFramedIPNetmask,
+    raatFramedRouting = 10,
+    raatFilterId,
+    raatFramedMTU,
+    raatFramedCompression,
+    raatLoginIPHost,
+    raatLoginService,
+    raatLoginTCPPort,
+    raatUnassigned17,
+    raatReplyMessage,
+    raatCallbackNumber,
+    raatCallbackId =20,
+    raatUnassigned21,
+    raatFramedRoute,
+    raatFramedIPXNetwork,
+    raatState,
+    raatClass,
+    raatVendorSpecific,
+    raatSessionTimeout,
+    raatIdleTimeout,
+    raatTerminationAction,
+    raatCalledStationId = 30,
+    raatCallingStationId,
+    raatNASIdentifier,
+    raatProxyState,
+    raatLoginLATService,
+    raatLoginLATNode,
+    raatLoginLATGroup,
+    raatFramedAppleTalkLink,
+    raatFramedAppleTalkNetwork,
+    raatFramedAppleTalkZone,
+    raatAcctStatusType = 40,
+    raatAcctDelayTime,
+    raatAcctInputOctets,
+    raatAcctOutputOctets,
+    raatAcctSessionId,
+    raatAcctAuthentic,
+    raatAcctSessionTime,
+    raatAcctInputPackets,
+    raatAcctOutputPackets,
+    raatAcctTerminateCause,
+    raatAcctMultiSessionId = 50,
+    raatAcctLinkCount,
+    raatAcctEventTimeStamp = 55,
+    raatMD5CHAPChallenge = 60,
+    raatNASPortType,
+    raatPortLimit,
+    raatLoginLATPort,
+    raatTunnelType,
+    raatTunnelMediumType,
+    raatTunnelClientEndpoint,
+    raatTunnelServerEndpoint,
+    raatARAPPassword = 70,
+    raatARAPFeatures,
+    raatARAPZoneAccess,
+    raatARAPSecurity,
+    raatARAPSecurityData,
+    raatPasswordRetry,
+    raatPrompt,
+    raatConnectInfo,
+    raatConfigurationToken,
+    raatEAPMessage,
+    raatSignature = 80,
+    raatARAPChallengeResponse = 84,
+    raatAcctInterimInterval = 85,
+    raatNASIPv6Address = 95,
+    raatFramedInterfaceId,
+    raatFramedIPv6Prefix,
+    raatLoginIPv6Host,
+    raatFramedIPv6Route,
+    raatFramedIPv6Pool,
+    raatARAPGuestLogon = 8096,
+    raatCertificateOID,
+    raatEAPConfiguration,
+    raatPEAPEmbeddedEAPTypeId = 8099,
+    raatInnerEAPTypeId = 8099,
+    raatPEAPFastRoamedSession = 8100,
+    raatFastRoamedSession = 8100,
+    raatEAPTLV = 8102,
+    raatCredentialsChanged,
+    raatPeerId = 9000,
+    raatServerId,
+    raatMethodId,
+    raatEMSK,
+    raatSessionId,
+    raatReserved = 0xffffffff
   } RAS_AUTH_ATTRIBUTE_TYPE;
 
 #define raatARAPChallenge 33
@@ -79,6 +168,11 @@ extern "C" {
 #define RAS_EAP_FLAG_RESUME_FROM_HIBERNATE 0x00000200
 #define RAS_EAP_FLAG_PEAP_UPFRONT 0x00000400
 #define RAS_EAP_FLAG_ALTERNATIVE_USER_DB 0x00000800
+#define RAS_EAP_FLAG_PEAP_FORCE_FULL_AUTH 0x00001000
+#define RAS_EAP_FLAG_PRE_LOGON 0x00020000
+#define RAS_EAP_FLAG_CONFG_READONLY 0x00080000
+#define RAS_EAP_FLAG_RESERVED 0x00100000
+#define RAS_EAP_FLAG_SAVE_CREDMAN 0x00200000
 
   typedef struct _PPP_EAP_PACKET {
     BYTE Code;
@@ -112,8 +206,15 @@ extern "C" {
   } PPP_EAP_INPUT,*PPPP_EAP_INPUT;
 
   typedef enum _PPP_EAP_ACTION {
-    EAPACTION_NoAction,EAPACTION_Authenticate,EAPACTION_Done,EAPACTION_SendAndDone,EAPACTION_Send,EAPACTION_SendWithTimeout,
-    EAPACTION_SendWithTimeoutInteractive,EAPACTION_IndicateTLV,EAPACTION_IndicateIdentity
+    EAPACTION_NoAction,
+    EAPACTION_Authenticate,
+    EAPACTION_Done,
+    EAPACTION_SendAndDone,
+    EAPACTION_Send,
+    EAPACTION_SendWithTimeout,
+    EAPACTION_SendWithTimeoutInteractive,
+    EAPACTION_IndicateTLV,
+    EAPACTION_IndicateIdentity
   } PPP_EAP_ACTION;
 
   typedef struct _PPP_EAP_OUTPUT {
@@ -136,18 +237,42 @@ extern "C" {
     DWORD dwSizeInBytes;
     DWORD dwEapTypeId;
     DWORD (WINAPI *RasEapInitialize)(WINBOOL fInitialize);
-    DWORD (WINAPI *RasEapBegin)(VOID **ppWorkBuffer,PPP_EAP_INPUT *pPppEapInput);
+    DWORD (WINAPI *RasEapBegin)(VOID **ppWorkBuffer, PPP_EAP_INPUT *pPppEapInput);
     DWORD (WINAPI *RasEapEnd)(VOID *pWorkBuffer);
-    DWORD (WINAPI *RasEapMakeMessage)(VOID *pWorkBuf,PPP_EAP_PACKET *pReceivePacket,PPP_EAP_PACKET *pSendPacket,DWORD cbSendPacket,PPP_EAP_OUTPUT *pEapOutput,PPP_EAP_INPUT *pEapInput);
+    DWORD (WINAPI *RasEapMakeMessage)(VOID *pWorkBuf, PPP_EAP_PACKET *pReceivePacket, PPP_EAP_PACKET *pSendPacket, DWORD cbSendPacket, PPP_EAP_OUTPUT *pEapOutput, PPP_EAP_INPUT *pEapInput);
   } PPP_EAP_INFO,*PPPP_EAP_INFO;
 
-  DWORD WINAPI RasEapGetInfo(DWORD dwEapTypeId,PPP_EAP_INFO *pEapInfo);
+  typedef struct _LEGACY_IDENTITY_UI_PARAMS {
+    DWORD eapType;
+    DWORD dwFlags;
+    DWORD dwSizeofConnectionData;
+    BYTE *pConnectionData;
+    DWORD dwSizeofUserData;
+    BYTE *pUserData;
+    DWORD dwSizeofUserDataOut;
+    BYTE *pUserDataOut;
+    LPWSTR pwszIdentity;
+    DWORD dwError;
+  } LEGACY_IDENTITY_UI_PARAMS;
+
+  typedef struct _LEGACY_INTERACTIVE_UI_PARAMS {
+    DWORD eapType;
+    DWORD dwSizeofContextData;
+    BYTE *pContextData;
+    DWORD dwSizeofInteractiveUIData;
+    BYTE *pInteractiveUIData;
+    DWORD dwError;
+  } LEGACY_INTERACTIVE_UI_PARAMS;
+
+  DWORD WINAPI RasEapGetInfo(DWORD dwEapTypeId, PPP_EAP_INFO *pEapInfo);
   DWORD WINAPI RasEapFreeMemory(BYTE *pMemory);
-  DWORD WINAPI RasEapInvokeInteractiveUI(DWORD dwEapTypeId,HWND hwndParent,BYTE *pUIContextData,DWORD dwSizeOfUIContextData,BYTE **ppDataFromInteractiveUI,DWORD *pdwSizeOfDataFromInteractiveUI);
-  DWORD WINAPI RasEapInvokeConfigUI(DWORD dwEapTypeId,HWND hwndParent,DWORD dwFlags,BYTE *pConnectionDataIn,DWORD dwSizeOfConnectionDataIn,BYTE **ppConnectionDataOut,DWORD *pdwSizeOfConnectionDataOut);
-  DWORD WINAPI RasEapGetIdentity(DWORD dwEapTypeId,HWND hwndParent,DWORD dwFlags,const WCHAR *pwszPhonebook,const WCHAR *pwszEntry,BYTE *pConnectionDataIn,DWORD dwSizeOfConnectionDataIn,BYTE *pUserDataIn,DWORD dwSizeOfUserDataIn,BYTE **ppUserDataOut,DWORD *pdwSizeOfUserDataOut,WCHAR **ppwszIdentity);
+  DWORD WINAPI RasEapInvokeInteractiveUI(DWORD dwEapTypeId, HWND hwndParent, BYTE *pUIContextData, DWORD dwSizeOfUIContextData, BYTE **ppDataFromInteractiveUI, DWORD *pdwSizeOfDataFromInteractiveUI);
+  DWORD WINAPI RasEapInvokeConfigUI(DWORD dwEapTypeId, HWND hwndParent, DWORD dwFlags, BYTE *pConnectionDataIn, DWORD dwSizeOfConnectionDataIn, BYTE **ppConnectionDataOut, DWORD *pdwSizeOfConnectionDataOut);
+  DWORD WINAPI RasEapGetIdentity(DWORD dwEapTypeId, HWND hwndParent, DWORD dwFlags, const WCHAR *pwszPhonebook, const WCHAR *pwszEntry, BYTE *pConnectionDataIn, DWORD dwSizeOfConnectionDataIn, BYTE *pUserDataIn, DWORD dwSizeOfUserDataIn, BYTE **ppUserDataOut, DWORD *pdwSizeOfUserDataOut, WCHAR **ppwszIdentityOut);
 
 #ifdef __cplusplus
 }
+#endif
+
 #endif
 #endif
