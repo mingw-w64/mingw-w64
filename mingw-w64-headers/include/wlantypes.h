56a42afd9e373e8b6ee1e30e9f69a49c3d531848
Kai Tietz <ktietz70@googlemail.com>
2014-08-27 15:28:55 +0200

    Add winapi-family check, add widl enumation-size specification, and define DOT11_AUTH_ALOGORITHM_... additions

--- mingw-w64-headers/include/wlantypes.h
+++ mingw-w64-headers/include/wlantypes.h
@@ -1,10 +1,14 @@
 /**
- * This file has no copyright assigned and is placed in the Public Domain.
  * This file is part of the mingw-w64 runtime package.
- * No warranty is given; refer to the file DISCLAIMER.PD within this package.
+ * No warranty is given; refer to the file DISCLAIMER within this package.
  */
+
 #ifndef _INC_WLANTYPES
 #define _INC_WLANTYPES
+
+#include <winapifamily.h>
+
+#if WINAPI_FAMILY_PARTITION (WINAPI_PARTITION_DESKTOP)
 
 #ifdef __cplusplus
 extern "C" {
@@ -12,7 +16,19 @@ extern "C" {
 
 #define DOT11_SSID_MAX_LENGTH 32
 
+#define DOT11_AUTH_ALGORITHM_OPEN_SYSTEM DOT11_AUTH_ALGO_80211_OPEN
+#define DOT11_AUTH_ALGORITHM_SHARED_KEY DOT11_AUTH_ALGO_80211_SHARED_KEY
+#define DOT11_AUTH_ALGORITHM_WPA DOT11_AUTH_ALGO_WPA
+#define DOT11_AUTH_ALGORITHM_WPA_PSK DOT11_AUTH_ALGO_WPA_PSK
+#define DOT11_AUTH_ALGORITHM_WPA_NONE DOT11_AUTH_ALGO_WPA_NONE
+#define DOT11_AUTH_ALGORITHM_RSNA DOT11_AUTH_ALGO_RSNA
+#define DOT11_AUTH_ALGORITHM_RSNA_PSK DOT11_AUTH_ALGO_RSNA_PSK
+
+#ifdef __WIDL__
+typedef [v1_enum] enum _DOT11_AUTH_ALGORITHM {
+#else
 typedef enum _DOT11_AUTH_ALGORITHM {
+#endif
   DOT11_AUTH_ALGO_80211_OPEN         = 1,
   DOT11_AUTH_ALGO_80211_SHARED_KEY   = 2,
   DOT11_AUTH_ALGO_WPA                = 3,
@@ -24,7 +40,11 @@ typedef enum _DOT11_AUTH_ALGORITHM {
   DOT11_AUTH_ALGO_IHV_END            = 0xffffffff 
 } DOT11_AUTH_ALGORITHM, *PDOT11_AUTH_ALGORITHM;
 
+#ifdef __WIDL__
+  typedef [v1_enum] enum _DOT11_CIPHER_ALGORITHM {
+#else
 typedef enum _DOT11_CIPHER_ALGORITHM {
+#endif
   DOT11_CIPHER_ALGO_NONE            = 0x00,
   DOT11_CIPHER_ALGO_WEP40           = 0x01,
   DOT11_CIPHER_ALGO_TKIP            = 0x02,
@@ -57,4 +77,5 @@ typedef struct _DOT11_SSID {
 }
 #endif
 
+#endif
 #endif /*_INC_WLANTYPES*/
