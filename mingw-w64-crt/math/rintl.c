4c17da0d8ff3b4e17f8e782ea6c4ad67cda33c50
André Hentschel <nerv@dawncrow.de>
2014-09-20 17:05:23 +0200

    math: Forward rintl to rint on ARM

--- mingw-w64-crt/math/rintl.c
+++ mingw-w64-crt/math/rintl.c
@@ -7,6 +7,10 @@
 
 long double rintl (long double x) {
   long double retval = 0.0L;
+#if defined(_AMD64_) || defined(__x86_64__) || defined(_X86_) || defined(__i386__)
   __asm__ __volatile__ ("frndint;": "=t" (retval) : "0" (x));
+#elif defined(__arm__) || defined(_ARM_)
+    retval = rint(x);
+#endif
   return retval;
 }
