96bb251806da61f79e2485e0899e5dcca116ae80
André Hentschel <nerv@dawncrow.de>
2014-09-20 17:05:21 +0200

    math: Forward truncl to trunc on ARM

--- mingw-w64-crt/math/truncl.c
+++ mingw-w64-crt/math/truncl.c
@@ -9,6 +9,9 @@
 long double
 truncl (long double _x)
 {
+#if defined(_ARM_) || defined(__arm__)
+  return trunc(_x);
+#else
   long double retval = 0.0L;
   unsigned short saved_cw;
   unsigned short tmp_cw;
@@ -19,4 +22,5 @@ truncl (long double _x)
   __asm__ __volatile__ ("frndint;" : "=t" (retval)  : "0" (_x)); /* round towards zero */
   __asm__ __volatile__ ("fldcw %0;" : : "m" (saved_cw) ); /* restore saved control word */
   return retval;
+#endif /* defined(_ARM_) || defined(__arm__) */
 }
