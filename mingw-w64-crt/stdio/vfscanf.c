b57d60931393edebee4f2174391ef3a92f5b37f3
André Hentschel <nerv@dawncrow.de>
2014-10-05 23:54:05 +0200

    stdio: Port vfscanf from ARM to Thumb

--- mingw-w64-crt/stdio/vfscanf.c
+++ mingw-w64-crt/stdio/vfscanf.c
@@ -15,6 +15,39 @@
 #define QUOTE_(x) #x
 #define QUOTE(x) QUOTE_(x)
 
+#if defined(__arm__) || defined(_ARM_)
+int __ms_vfscanf_internal(FILE * __restrict__, const char * __restrict__, va_list);
+asm("\t.text\n"
+    "\t.align 2\n"
+    "\t.thumb_func\n"
+    "\t.globl __ms_vfscanf_internal\n"
+    "__ms_vfscanf_internal:\n\t"
+    "push {r4-r7, lr}\n\t"
+    "mov r4, sp\n\t"
+    "sub r4, r4, #128\n\t"
+
+    "ldr r5, [r2]\n\t"
+    "add r2, r2, #4\n\t"
+    "ldr r6, [r2]\n\t"
+    "add r2, r2, #4\n\t"
+
+    "mov r3, #116\n\t"
+    "1: ldr r7, [r2]\n\t"
+    "add r2, r2, #4\n\t"
+    "str r7, [r4]\n\t"
+    "add r4, r4, #4\n\t"
+    "sub r3, r3, #4\n\t"
+    "cmp r3, #0\n\t"
+    "bne 1b\n\t"
+
+    "sub sp, sp, #128\n\t"
+    "mov r2, r5\n\t"
+    "mov r3, r6\n\t"
+    "bl " QUOTE(__MINGW_USYMBOL(fscanf)) "\n\t"
+    "add sp, sp, #128\n\t"
+    "pop {r4-r7, pc}");
+#endif /* defined(__arm__) || defined(_ARM_) */
+
 int __ms_vfscanf (FILE * __restrict__ stream, const char * __restrict__ format, va_list arg)
 {
   int ret;
@@ -84,35 +117,7 @@ int __ms_vfscanf (FILE * __restrict__ stream, const char * __restrict__ format, 
       "a"(&ret)
     : "ebx", "edi");
 #elif defined(_ARM_) || defined(__arm__)
-  int tmp1, tmp2, tmp3, tmp4, tmp5;
-  __asm__ __volatile__ (
-    "mov %[t1], sp\n\t"
-    "sub %[t1], %[t1], #128\n\t"
-    "mov %[t2], %[arg]\n\t"
-
-    "ldr ip, [%[t2]], #4\n\t"
-    "mov %[t4], ip\n\t"
-    "ldr ip, [%[t2]], #4\n\t"
-    "mov %[t5], ip\n\t"
-
-    "mov %[t3], #120\n\t"
-    "1: ldr ip, [%[t2]], #4\n\t"
-    "str ip, [%[t1]], #4\n\t"
-    "subs %[t3], %[t3], #4\n\t"
-    "bge 1b\n\t"
-
-    "sub sp, sp, #128\n\t"
-    "mov r0, %[s]\n\t"
-    "mov r1, %[format]\n\t"
-    "mov r2, %[t4]\n\t"
-    "mov r3, %[t5]\n\t"
-    "bl " QUOTE(__MINGW_USYMBOL(fscanf)) "\n\t"
-    "add sp, sp, #128\n\t"
-    "mov %[ret], r0\n\t"
-    : [ret] "=r" (ret), [t1] "+r" (tmp1), [t2] "+r" (tmp2), [t3] "+r" (tmp3), [t4] "+r" (tmp4), [t5] "+r" (tmp5)
-    : [s] "r" (stream), [format] "r" (format), [arg] "r" (arg)
-    : "r0", "r1", "r2", "r3", "ip", "cc", "memory"
-    );
+    ret = __ms_vfscanf_internal(stream, format, arg);
 #endif
 
   return ret;
