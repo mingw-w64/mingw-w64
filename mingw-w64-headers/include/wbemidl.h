d20fce35947d7a242700609b7f21d6a04a7bfd15
Kai Tietz <ktietz70@googlemail.com>
2014-11-24 17:01:50 +0100

    Generate headers by IDL. Adjust dispex.h for IID_...variants of guids, which are different handled by wild in comparison to midl.

--- mingw-w64-headers/include/wbemidl.h
+++ mingw-w64-headers/include/wbemidl.h
@@ -6,9 +6,13 @@
 #ifndef __WBEMIDL_H_
 #define __WBEMIDL_H_
 
+#include <winapifamily.h>
+
+#if WINAPI_FAMILY_PARTITION (WINAPI_PARTITION_DESKTOP)
+
 #include <wbemcli.h>
 #include <wbemprov.h>
 #include <wbemtran.h>
 #include <wbemdisp.h>
-
+#endif
 #endif
