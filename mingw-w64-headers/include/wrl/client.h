58b571ec74923c34074bf1ff43d4093c27375312
Jacek Caban <jacek@codeweavers.com>
2015-07-20 18:15:11 +0200

    Added new wrl.h file and a few its dependency headers.

---
+++ mingw-w64-headers/include/wrl/client.h
@@ -0,0 +1,18 @@
+/**
+ * This file has no copyright assigned and is placed in the Public Domain.
+ * This file is part of the mingw-w64 runtime package.
+ * No warranty is given; refer to the file DISCLAIMER.PD within this package.
+ */
+
+#ifndef _WRL_CLIENT_H_
+#define _WRL_CLIENT_H_
+
+#include <stddef.h>
+#include <unknwn.h>
+/* #include <weakreference.h> */
+#include <roapi.h>
+
+/* #include <wrl/def.h> */
+#include <wrl/internal.h>
+
+#endif
---
+++ mingw-w64-headers/include/wrl/wrappers/corewrappers.h
@@ -0,0 +1,18 @@
+/**
+ * This file has no copyright assigned and is placed in the Public Domain.
+ * This file is part of the mingw-w64 runtime package.
+ * No warranty is given; refer to the file DISCLAIMER.PD within this package.
+ */
+
+#ifndef _WRL_COREWRAPPERS_H_
+#define _WRL_COREWRAPPERS_H_
+
+#include <windows.h>
+/* #include <intsafe.h> */
+#include <winstring.h>
+#include <roapi.h>
+
+/* #include <wrl/def.h> */
+#include <wrl/internal.h>
+
+#endif
