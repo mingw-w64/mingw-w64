ace70f13c8bdcc9aa2a52b231e35d590df0a04e0
Jacek Caban <jacek@codeweavers.com>
2015-08-29 17:24:49 +0200

    Report win10 as supported target version.

--- mingw-w64-headers/include/winsdkver.h
+++ mingw-w64-headers/include/winsdkver.h
@@ -10,12 +10,12 @@
 
 #if WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
 
-#define _WIN32_MAXVER         0x0602
-#define _WIN32_WINDOWS_MAXVER 0x0602
-#define NTDDI_MAXVER          0x0602
-#define _WIN32_IE_MAXVER      0x0b00
-#define _WIN32_WINNT_MAXVER   0x0602
-#define WINVER_MAXVER         0x0602
+#define _WIN32_MAXVER         0x0A00
+#define _WIN32_WINDOWS_MAXVER 0x0A00
+#define NTDDI_MAXVER          0x0A00
+#define _WIN32_IE_MAXVER      0x0A00
+#define _WIN32_WINNT_MAXVER   0x0A00
+#define WINVER_MAXVER         0x0A00
 
 #endif
 #endif
