05de869f24eda79bd85fac3ce1fdf3e224f0a73b
Jacek Caban <jacek@codeweavers.com>
2016-08-16 12:43:25 +0200

    Updated imported DirectX headers to current Wine version.

--- mingw-w64-headers/direct-x/include/d3d10.h
+++ mingw-w64-headers/direct-x/include/d3d10.h
@@ -6843,7 +6843,7 @@ void __RPC_STUB ID3D10Multithread_GetMultithreadProtected_Stub(
 #endif  /* __ID3D10Multithread_INTERFACE_DEFINED__ */
 
 #ifndef D3D10_IGNORE_SDK_LAYERS
-/* FIXME: # include <d3d10sdklayers.h> */
+#include "d3d10sdklayers.h"
 #endif
 #include "d3d10misc.h"
 #include "d3d10shader.h"
