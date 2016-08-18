e9e42ce3113b6e544f6a6186e9caee4d28922934
Martin Storsjö <martin@martin.st>
2016-08-18 10:30:37 +0300

    math: Fix rounding in llrint(f) on ARM
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- mingw-w64-crt/math/llrintf.c
+++ mingw-w64-crt/math/llrintf.c
@@ -4,6 +4,7 @@
  * No warranty is given; refer to the file DISCLAIMER.PD within this package.
  */
 #include <math.h>
+#include <fenv.h>
 
 long long llrintf (float x) 
 {
@@ -11,7 +12,15 @@ long long llrintf (float x)
 #if defined(_AMD64_) || defined(__x86_64__) || defined(_X86_) || defined(__i386__)
   __asm__ __volatile__ ("fistpll %0"  : "=m" (retval) : "t" (x) : "st");
 #else
-  retval = (long long)x;
+  int mode = fegetround();
+  if (mode == FE_DOWNWARD)
+    retval = (long long)floorf(x);
+  else if (mode == FE_UPWARD)
+    retval = (long long)ceilf(x);
+  else if (mode == FE_TOWARDZERO)
+    retval = x >= 0 ? (long long)floorf(x) : (long long)ceilf(x);
+  else
+    retval = x >= 0 ? (long long)floorf(x + 0.5) : (long long)ceilf(x - 0.5);
 #endif
   return retval;
 }
