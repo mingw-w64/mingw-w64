eb331bb3678c5821eba279c5118900bed72c5a21
Jacek Caban <jacek@codeweavers.com>
2017-01-16 16:57:03 +0100

    stdlib_s.h: Added _itoa_s overloaded helper.

--- mingw-w64-headers/crt/sec_api/stdlib_s.h
+++ mingw-w64-headers/crt/sec_api/stdlib_s.h
@@ -16,6 +16,7 @@ extern "C" {
 
   _CRTIMP errno_t __cdecl _dupenv_s(char **_PBuffer,size_t *_PBufferSizeInBytes,const char *_VarName);
   _CRTIMP errno_t __cdecl _itoa_s(int _Value,char *_DstBuf,size_t _Size,int _Radix);
+  __DEFINE_CPP_OVERLOAD_SECURE_FUNC_1_1(errno_t, _itoa_s, int, _Value, char, _Dest, int, _Radix)
   _CRTIMP errno_t __cdecl _i64toa_s(__int64 _Val,char *_DstBuf,size_t _Size,int _Radix);
   _CRTIMP errno_t __cdecl _ui64toa_s(unsigned __int64 _Val,char *_DstBuf,size_t _Size,int _Radix);
   _CRTIMP errno_t __cdecl _ltoa_s(long _Val,char *_DstBuf,size_t _Size,int _Radix);
