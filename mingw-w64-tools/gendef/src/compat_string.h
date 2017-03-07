924d4a23ed8fde5f53490c0fd5a07aeaf6a4c5ee
sezero <sezero@users.sourceforge.net>
2017-03-07 12:23:20 +0300

    fix HAVE_STRLWER typo

--- mingw-w64-tools/gendef/src/compat_string.h
+++ mingw-w64-tools/gendef/src/compat_string.h
@@ -18,8 +18,8 @@
 #ifndef __COMPAT_STRING_H
 #define __COMPAT_STRING_H
 
-#ifndef HAVE_STRLWER
-char *strlwr(char *s);
-#endif /* HAVE_STRLWER */
+#ifndef HAVE_STRLWR
+char *strlwr(char *);
+#endif /* HAVE_STRLWR */
 
 #endif /* __COMPAT_STRING_H */
