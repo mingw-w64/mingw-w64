2d36585b035289542975cfba559e193cb2c8d5ec
Liu Hao <lh_mouse@126.com>
2017-03-10 15:31:32 +0800

    winpthreads/src/thread.c: Force aligning ESP on 16-byte boundaries on x86. ESP isn't guaranteed to be aligned on 16-byte boundaries if the thread is created with _beginthreadex(), but modern GCC assumes it is and generates code that requires it. For example, the MOVDQA instruction may be used to load XMMWORDs from the stack when SSE2 is enabled, which results in segment faults if the address isn't correctly aligned.
    Reported-by: Aleksey Vasenev
    Reference: https://sourceforge.net/p/mingw-w64/mailman/message/35294616/
    
    Signed-off-by: Liu Hao <lh_mouse@126.com>

--- mingw-w64-libraries/winpthreads/src/thread.c
+++ mingw-w64-libraries/winpthreads/src/thread.c
@@ -1457,6 +1457,12 @@ pthread_setcanceltype (int type, int *oldtype)
   return 0;
 }
 
+#if defined(__i686__)
+/* Align ESP on 16-byte boundaries. */
+#  if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 2)
+__attribute__((force_align_arg_pointer))
+#  endif
+#endif
 int
 pthread_create_wrapper (void *args)
 {
