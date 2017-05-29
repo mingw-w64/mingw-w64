7e51857a5a486982dfb2f487477e54f4c2fa0e46
Jacek Caban <jacek@codeweavers.com>
2017-05-29 21:27:43 +0200

    sal.h: Moved macros belonging to sa.c from drsiverspecs.h.

--- mingw-w64-headers/include/driverspecs.h
+++ mingw-w64-headers/include/driverspecs.h
@@ -12,22 +12,6 @@
 #define __drv_dispatchType(x)
 #define __drv_dispatchType_other
 
-/*
- * FIXME: These annotations are not driver-only and does not belong here
- */
-#define __in
-#define __in_bcount(Size)
-#define __in_ecount(Size)
-
-#define __out
-#define __out_bcount(Size)
-#define __out_bcount_part(Size, Length)
-#define __out_ecount(Size)
-
-#define __inout
-
-#define __deref_out_ecount(Size)
-
 #define __drv_aliasesMem
 #define __drv_allocatesMem(kind)
 #define __drv_freesMem(kind)
