32ec57d19d9678f0be7c4ecb2b251dba10d860e6
niXman <i.nixman@autistici.org>
2017-08-03 13:01:04 +0300

    _mingw_no_trailing_slash() for _stat64i32() and _wstat64i32()

--- mingw-w64-crt/stdio/_wstat64i32.c
+++ mingw-w64-crt/stdio/_wstat64i32.c
@@ -1,10 +1,74 @@
 #define __CRT__NO_INLINE
 #include <sys/stat.h>
+#include <stdlib.h>
+
+/**
+ * Returns _path without trailing slash if any
+ *
+ * - if _path has no trailing slash, the function returns it
+ * - if _path has a trailing slash, but is of the form C:/, then it returns it
+ * - otherwise, the function creates a new string, which is a copy of _path
+ *   without the trailing slash. It is then the responsibility of the caller
+ *   to free it.
+ */
+
+static wchar_t*
+_mingw_no_trailing_slash (const wchar_t* _path)
+{
+  int len;
+  wchar_t *p;
+  int start;
+
+  p = (wchar_t*)_path;
+
+  if (_path && *_path) {
+    len = wcslen (_path);
+
+    /* Ignore X:\ */
+
+    if (len <= 1 || ((len == 2 || len == 3) && _path[1] == L':'))
+      return p;
+
+    /* Check UNC \\abc\<name>\ */
+    if ((_path[0] == L'\\' || _path[0] == L'/')
+	&& (_path[1] == L'\\' || _path[1] == L'/'))
+      {
+	const wchar_t *r = &_path[2];
+	while (*r != 0 && *r != L'\\' && *r != L'/')
+	  ++r;
+	if (*r != 0)
+	  ++r;
+	if (*r == 0)
+	  return p;
+	while (*r != 0 && *r != L'\\' && *r != L'/')
+	  ++r;
+	if (*r != 0)
+	  ++r;
+	if (*r == 0)
+	  return p;
+      }
+
+    if (_path[len - 1] == L'/' || _path[len - 1] == L'\\')
+      {
+	p = (wchar_t*)malloc (len * sizeof(wchar_t));
+	memcpy (p, _path, (len - 1) * sizeof(wchar_t));
+	p[len - 1] = L'\0';
+      }
+  }
+
+  return p;
+}
 
 int __cdecl _wstat64i32(const wchar_t *_Name,struct _stat64i32 *_Stat)
 {
   struct _stat64 st;
-  int ret=_wstat64(_Name,&st);
+  wchar_t *_path = _mingw_no_trailing_slash(_Name);
+  
+  int ret=_wstat64(_path,&st);
+
+  if (_path != _Name)
+    free(_path);
+
   if (ret == -1) {
     memset(_Stat,0,sizeof(struct _stat64i32));
     return -1;
