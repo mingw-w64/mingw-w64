a4527ef57e4b81ce6fdbe6309908a11427750ef9
niXman <i.nixman@autistici.org>
2017-08-03 13:01:04 +0300

    _mingw_no_trailing_slash() for _stat64i32() and _wstat64i32()
    (cherry picked from commit 32ec57d19d9678f0be7c4ecb2b251dba10d860e6)
    Signed-off-by: Jonathan Yong <10walls@gmail.com>

--- mingw-w64-crt/stdio/_stat64i32.c
+++ mingw-w64-crt/stdio/_stat64i32.c
@@ -1,10 +1,74 @@
 #define __CRT__NO_INLINE
 #include <sys/stat.h>
+#include <stdlib.h>
+
+/**
+ * Returns _path without trailing slash if any
+ *
+ * - if _path has no trailing slash, the function returns it
+ * - if _path has a trailing slash, but is of the form C:/, then it returns it
+ * - otherwise, the function creates a new string, which is a copy of _path
+ *   without the trailing slash. It is then the responsibility of the caller
+ *   to free it.
+ */
+
+static char*
+_mingw_no_trailing_slash (const char* _path)
+{
+  int len;
+  char *p;
+  int start;
+
+  p = (char*)_path;
+
+  if (_path && *_path) {
+    len = strlen (_path);
+
+    /* Ignore X:\ */
+
+    if (len <= 1 || ((len == 2 || len == 3) && _path[1] == ':'))
+      return p;
+
+    /* Check UNC \\abc\<name>\ */
+    if ((_path[0] == '\\' || _path[0] == '/')
+	&& (_path[1] == '\\' || _path[1] == '/'))
+      {
+	const char *r = &_path[2];
+	while (*r != 0 && *r != '\\' && *r != '/')
+	  ++r;
+	if (*r != 0)
+	  ++r;
+	if (*r == 0)
+	  return p;
+	while (*r != 0 && *r != '\\' && *r != '/')
+	  ++r;
+	if (*r != 0)
+	  ++r;
+	if (*r == 0)
+	  return p;
+      }
+
+    if (_path[len - 1] == '/' || _path[len - 1] == '\\')
+      {
+	p = (char*)malloc (len);
+	memcpy (p, _path, len - 1);
+	p[len - 1] = '\0';
+      }
+  }
+
+  return p;
+}
 
 int __cdecl _stat64i32(const char *_Name,struct _stat64i32 *_Stat)
 {
   struct _stat64 st;
-  int ret=_stat64(_Name,&st);
+  char *_path = _mingw_no_trailing_slash(_Name);
+  
+  int ret=_stat64(_path,&st);
+
+  if (_path != _Name)
+    free(_path);
+
   if (ret == -1) {
     memset(_Stat,0,sizeof(struct _stat64i32));
     return -1;
