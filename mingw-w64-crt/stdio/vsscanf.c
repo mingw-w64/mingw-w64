2c043820715ce27fe29559d2d7eef469e4640720
Martin Storsjö <martin@martin.st>
2017-08-03 23:40:57 +0300

    stdio: Share the v*scanf arm assembly wrapper in the scanf.S external asm file
    There's no longer any reason to keep it separate for each function.
    
    This reduces code duplication.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- mingw-w64-crt/stdio/vsscanf.c
+++ mingw-w64-crt/stdio/vsscanf.c
@@ -7,47 +7,12 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-#if defined(_AMD64_) || defined(__x86_64__) || \
-  defined(_X86_) || defined(__i386__)
-
 extern int __ms_vsscanf_internal (
   const char * s,
   const char * format,
   va_list arg,
   int (*func)(const char * __restrict__,  const char * __restrict__, ...))
   asm("__argtos");
-
-#elif defined (__arm__) || defined (_ARM_)
-
-int __ms_vsscanf_internal (const char * __restrict__,
-  const char * __restrict__, va_list,
-  int (*func)(const char * __restrict__,  const char * __restrict__, ...));
-asm("\t.text\n"
-    "\t.align 2\n"
-    "\t.thumb_func\n"
-    "\t.globl __ms_vsscanf_internal\n"
-    "__ms_vsscanf_internal:\n\t"
-    "push {r4-r7, lr}\n\t"
-    "sub sp, sp, #128\n\t"
-    "mov r12, r3\n\t"
-    "mov r4, sp\n\t"
-
-    "ldr r5, [r2], #4\n\t"
-    "ldr r6, [r2], #4\n\t"
-
-    "mov r3, #116\n\t"
-    "1: ldr r7, [r2], #4\n\t"
-    "str r7, [r4], #4\n\t"
-    "subs r3, r3, #4\n\t"
-    "bne 1b\n\t"
-
-    "mov r2, r5\n\t"
-    "mov r3, r6\n\t"
-    "blx r12\n\t"
-    "add sp, sp, #128\n\t"
-    "pop {r4-r7, pc}");
-
-#endif /* defined (__arm__) || defined (_ARM_) */
 
 int __ms_vsscanf (const char * __restrict__ s,
   const char * __restrict__ format, va_list arg)
--- mingw-w64-crt/stdio/vswscanf.c
+++ mingw-w64-crt/stdio/vswscanf.c
@@ -7,47 +7,12 @@
 #include <stdarg.h>
 #include <wchar.h>
 
-#if defined(_AMD64_) || defined(__x86_64__) || \
-  defined(_X86_) || defined(__i386__)
-
 extern int __ms_vswscanf_internal (
   const wchar_t * s,
   const wchar_t * format,
   va_list arg,
   int (*func)(const wchar_t * __restrict__,  const wchar_t * __restrict__, ...))
   asm("__argtos");
-
-#elif defined (__arm__) || defined (_ARM_)
-
-int __ms_vswscanf_internal (const wchar_t * __restrict__,
-  const wchar_t * __restrict__, va_list,
-  int (*func)(const wchar_t * __restrict__,  const wchar_t * __restrict__, ...));
-asm("\t.text\n"
-    "\t.align 2\n"
-    "\t.thumb_func\n"
-    "\t.globl __ms_vswscanf_internal\n"
-    "__ms_vswscanf_internal:\n\t"
-    "push {r4-r7, lr}\n\t"
-    "sub sp, sp, #128\n\t"
-    "mov r12, r3\n\t"
-    "mov r4, sp\n\t"
-
-    "ldr r5, [r2], #4\n\t"
-    "ldr r6, [r2], #4\n\t"
-
-    "mov r3, #116\n\t"
-    "1: ldr r7, [r2], #4\n\t"
-    "str r7, [r4], #4\n\t"
-    "subs r3, r3, #4\n\t"
-    "bne 1b\n\t"
-
-    "mov r2, r5\n\t"
-    "mov r3, r6\n\t"
-    "blx r12\n\t"
-    "add sp, sp, #128\n\t"
-    "pop {r4-r7, pc}");
-
-#endif /* defined (__arm__) || defined (_ARM_) */
 
 int __ms_vswscanf(const wchar_t * __restrict__ s, const wchar_t * __restrict__ format,
   va_list arg)
