868fe281bf7d3dae71e099fe45226e422a4be1cd
Martin Storsjö <martin@martin.st>
2018-07-02 00:25:46 +0300

    crt: Split wassert.c into a separate assert.c
    The _wassert function is overridden in libwinstorecompat. If overriding
    this function within that library, we still want to be able to link
    only the _assert function from libmingwex.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- mingw-w64-crt/misc/wassert.c
+++ mingw-w64-crt/misc/wassert.c
@@ -15,24 +15,6 @@ void __cdecl _wassert (const wchar_t *, const wchar_t *,unsigned);
 void __cdecl _assert (const char *, const char *, unsigned);
 
 void __cdecl
-_assert (const char *_Message, const char *_File, unsigned _Line)
-{
-  wchar_t *m, *f;
-  int i;
-  m = (wchar_t *) malloc ((strlen (_Message) + 1) * sizeof (wchar_t));
-  f = (wchar_t *) malloc ((strlen (_File) + 1) * sizeof (wchar_t));
-  for (i = 0; _Message[i] != 0; i++)
-    m[i] = ((wchar_t) _Message[i]) & 0xff;
-  m[i] = 0;
-  for (i = 0; _File[i] != 0; i++)
-    f[i] = ((wchar_t) _File[i]) & 0xff;
-  f[i] = 0;
-  _wassert (m, f, _Line);
-  free (m);
-  free (f);
-}
-
-void __cdecl
 _wassert (const wchar_t *_Message, const wchar_t *_File, unsigned _Line)
 {
   wchar_t *msgbuf = (wchar_t *) malloc (8192*sizeof(wchar_t));
