5b72d83ec57e2aba171e9fe78a48104c5037f20d
Jacek Caban <jacek@codeweavers.com>
2018-08-30 13:25:48 +0200

    winternl.h: Fix _PEB declaration.
    Signed-off-by: Jacek Caban <jacek@codeweavers.com>

--- mingw-w64-headers/include/winternl.h
+++ mingw-w64-headers/include/winternl.h
@@ -69,11 +69,18 @@ typedef struct _RTL_USER_PROCESS_PARAMETERS {
     PVOID Reserved3[2];
     PPEB_LDR_DATA Ldr;
     PRTL_USER_PROCESS_PARAMETERS ProcessParameters;
-    BYTE Reserved4[104];
-    PVOID Reserved5[52];
+    PVOID Reserved4[3];
+    PVOID AtlThunkSListPtr;
+    PVOID Reserved5;
+    ULONG Reserved6;
+    PVOID Reserved7;
+    ULONG Reserved8;
+    ULONG AtlThunkSListPtr32;
+    PVOID Reserved9[45];
+    BYTE Reserved10[96];
     PPS_POST_PROCESS_INIT_ROUTINE PostProcessInitRoutine;
-    BYTE Reserved6[128];
-    PVOID Reserved7[1];
+    BYTE Reserved11[128];
+    PVOID Reserved12[1];
     ULONG SessionId;
   } PEB,*PPEB;
 
