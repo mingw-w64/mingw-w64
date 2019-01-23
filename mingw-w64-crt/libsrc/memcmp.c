5a74c943c432e4eef14f69d8b8d4b60500baec30
Zebediah Figura <z.figura12@gmail.com>
2019-01-23 12:46:11 -0600

    ntoskrnl: Provide memcmp() on i386.
    It is not exported from the DLL. The implementation was copied and modified from wmemcmp.c in libmingwex.
    
    Signed-off-by: Jacek Caban <jacek@codeweavers.com>

---
+++ mingw-w64-crt/libsrc/memcmp.c
@@ -0,0 +1,26 @@
+/**
+ * This file has no copyright assigned and is placed in the Public Domain.
+ * This file is part of the mingw-w64 runtime package.
+ * No warranty is given; refer to the file DISCLAIMER.PD within this package.
+ */
+
+#define __CRT__NO_INLINE
+#include <stddef.h>
+
+int __cdecl memcmp(const void *_S1, const void *_S2, size_t _N)
+{
+    const unsigned char *s1 = _S1, *s2 = _S2;
+
+    if (_N == 0 || s1 == s2)
+	return 0;	/* even for NULL pointers */
+
+    if ((s1 != NULL) != (s2 != NULL))
+	return s2 == NULL ? 1 : -1;	/* robust */
+
+    for ( ; 0 < _N; ++s1, ++s2, --_N)
+	if (*s1 != *s2)
+	    return (*s1 < *s2 ? -1 : +1);
+
+    return 0;
+}
+
