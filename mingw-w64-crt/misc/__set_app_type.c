494dcbbd982a27ff41491c6ab311559ebb0b2c1c
Martin Storsjö <martin@martin.st>
2019-05-04 15:15:16 +0300

    crt: Provide an empty implementation of __set_app_type for msvcr120_app.dll
    This dll doesn't provide this function, but making it an empty no-op
    function should be fine.
    
    The previous (incorrect) declaration of this function in the def files
    allowed compiling some projects (where linking tests otherwise would fail,
    since the mingw crt startup files has undefined references against this
    function). In practice any real use of that was for building DLLs linked
    against msvcr120_app.dll, so the incorrect __set_app_type import library
    entry was not an issue.
    
    This has the same effect, but without having executables try to import
    a function that doesn't actually exist.
    
    This CRT dll (which is intended only for use with Windows Store apps)
    doesn't actually operate properly when used for standalone exe's though
    (it crashes on exit and e.g. printf function calls doesn't output
    anything), but this still is a small step towards making things less
    incorrect.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

---
+++ mingw-w64-crt/misc/__set_app_type.c
@@ -0,0 +1,12 @@
+/**
+ * This file has no copyright assigned and is placed in the Public Domain.
+ * This file is part of the mingw-w64 runtime package.
+ * No warranty is given; refer to the file DISCLAIMER.PD within this package.
+ */
+
+#include "internal.h"
+
+void __cdecl __set_app_type (int type) {
+}
+
+void (__cdecl *__MINGW_IMP_SYMBOL(__set_app_type))(int) = __set_app_type;
