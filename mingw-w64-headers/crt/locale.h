8a77639cdabe8370e225a4022e9391b9fc20bd12
Christoph Reiter <reiter.christoph@gmail.com>
2020-12-22 21:47:31 +0100

    headers: add missing _W_* lconv struct members
    This seems to have been added with MSVCR100 but is also available in MSVCRT
    on Windows 7 SP1+ (at least), more info here:
    https://sourceforge.net/p/mingw-w64/mailman/message/37183101/
    
    This change is motivated by CPython starting to use these members in v3.8.7
    
    Reference: https://docs.microsoft.com/en-us/cpp/c-runtime-library/reference/localeconv
    Reference: https://sourceforge.net/p/mingw-w64/bugs/870/
    Signed-off-by: Liu Hao <lh_mouse@126.com>

--- mingw-w64-headers/crt/locale.h
+++ mingw-w64-headers/crt/locale.h
@@ -61,6 +61,16 @@ extern "C" {
     char n_sep_by_space;
     char p_sign_posn;
     char n_sign_posn;
+#if __MSVCRT_VERSION__ >= 0xA00 || _WIN32_WINNT >= 0x601
+    wchar_t* _W_decimal_point;
+    wchar_t* _W_thousands_sep;
+    wchar_t* _W_int_curr_symbol;
+    wchar_t* _W_currency_symbol;
+    wchar_t* _W_mon_decimal_point;
+    wchar_t* _W_mon_thousands_sep;
+    wchar_t* _W_positive_sign;
+    wchar_t* _W_negative_sign;
+#endif
   };
 #endif
 
