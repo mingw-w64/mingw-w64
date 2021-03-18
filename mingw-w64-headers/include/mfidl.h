902b16c6af7901d56d1263dc4a1983bbdf01d08b
Liu Hao <lh_mouse@126.com>
2021-03-18 22:29:40 +0800

    headers: Import headers from wine master
    Signed-off-by: Liu Hao <lh_mouse@126.com>

--- mingw-w64-headers/include/mfidl.h
+++ mingw-w64-headers/include/mfidl.h
@@ -34,6 +34,14 @@ interface IMFTopology;
 #endif /* __cplusplus */
 #endif
 
+#ifndef __IMFGetService_FWD_DEFINED__
+#define __IMFGetService_FWD_DEFINED__
+typedef interface IMFGetService IMFGetService;
+#ifdef __cplusplus
+interface IMFGetService;
+#endif /* __cplusplus */
+#endif
+
 #ifndef __IMFClock_FWD_DEFINED__
 #define __IMFClock_FWD_DEFINED__
 typedef interface IMFClock IMFClock;
@@ -1155,6 +1163,87 @@ static FORCEINLINE HRESULT IMFTopology_GetOutputNodeCollection(IMFTopology* This
 
 
 #endif  /* __IMFTopology_INTERFACE_DEFINED__ */
+
+/*****************************************************************************
+ * IMFGetService interface
+ */
+#ifndef __IMFGetService_INTERFACE_DEFINED__
+#define __IMFGetService_INTERFACE_DEFINED__
+
+DEFINE_GUID(IID_IMFGetService, 0xfa993888, 0x4383, 0x415a, 0xa9,0x30, 0xdd,0x47,0x2a,0x8c,0xf6,0xf7);
+#if defined(__cplusplus) && !defined(CINTERFACE)
+MIDL_INTERFACE("fa993888-4383-415a-a930-dd472a8cf6f7")
+IMFGetService : public IUnknown
+{
+    virtual HRESULT STDMETHODCALLTYPE GetService(
+        REFGUID guidService,
+        REFIID riid,
+        LPVOID *ppvObject) = 0;
+
+};
+#ifdef __CRT_UUID_DECL
+__CRT_UUID_DECL(IMFGetService, 0xfa993888, 0x4383, 0x415a, 0xa9,0x30, 0xdd,0x47,0x2a,0x8c,0xf6,0xf7)
+#endif
+#else
+typedef struct IMFGetServiceVtbl {
+    BEGIN_INTERFACE
+
+    /*** IUnknown methods ***/
+    HRESULT (STDMETHODCALLTYPE *QueryInterface)(
+        IMFGetService *This,
+        REFIID riid,
+        void **ppvObject);
+
+    ULONG (STDMETHODCALLTYPE *AddRef)(
+        IMFGetService *This);
+
+    ULONG (STDMETHODCALLTYPE *Release)(
+        IMFGetService *This);
+
+    /*** IMFGetService methods ***/
+    HRESULT (STDMETHODCALLTYPE *GetService)(
+        IMFGetService *This,
+        REFGUID guidService,
+        REFIID riid,
+        LPVOID *ppvObject);
+
+    END_INTERFACE
+} IMFGetServiceVtbl;
+
+interface IMFGetService {
+    CONST_VTBL IMFGetServiceVtbl* lpVtbl;
+};
+
+#ifdef COBJMACROS
+#ifndef WIDL_C_INLINE_WRAPPERS
+/*** IUnknown methods ***/
+#define IMFGetService_QueryInterface(This,riid,ppvObject) (This)->lpVtbl->QueryInterface(This,riid,ppvObject)
+#define IMFGetService_AddRef(This) (This)->lpVtbl->AddRef(This)
+#define IMFGetService_Release(This) (This)->lpVtbl->Release(This)
+/*** IMFGetService methods ***/
+#define IMFGetService_GetService(This,guidService,riid,ppvObject) (This)->lpVtbl->GetService(This,guidService,riid,ppvObject)
+#else
+/*** IUnknown methods ***/
+static FORCEINLINE HRESULT IMFGetService_QueryInterface(IMFGetService* This,REFIID riid,void **ppvObject) {
+    return This->lpVtbl->QueryInterface(This,riid,ppvObject);
+}
+static FORCEINLINE ULONG IMFGetService_AddRef(IMFGetService* This) {
+    return This->lpVtbl->AddRef(This);
+}
+static FORCEINLINE ULONG IMFGetService_Release(IMFGetService* This) {
+    return This->lpVtbl->Release(This);
+}
+/*** IMFGetService methods ***/
+static FORCEINLINE HRESULT IMFGetService_GetService(IMFGetService* This,REFGUID guidService,REFIID riid,LPVOID *ppvObject) {
+    return This->lpVtbl->GetService(This,guidService,riid,ppvObject);
+}
+#endif
+#endif
+
+#endif
+
+
+#endif  /* __IMFGetService_INTERFACE_DEFINED__ */
 
 typedef LONGLONG MFTIME;
 typedef enum _MF_CLOCK_STATE {
