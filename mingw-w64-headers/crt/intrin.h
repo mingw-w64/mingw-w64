c1804804aff14f1b0333d782c9fac1cc904fcd9f
Mateusz Mikuła <mati865@gmail.com>
2021-03-29 19:07:33 +0200

    Enable __MINGW_FORCE_SYS_INTRINS for Clang
    See https://github.com/msys2/CLANG-packages/issues/6 for the details
    
    The intrin.h header tries to define types like e.g. __m64
    if __MMX__ isn't defined. With Clang, mmintrin.h does define such
    a type even if __MMX__ isn't defined (and mmintrin.h is included
    implicitly via x86intrin.h).
    
    This fixes including intrin.h if building with Clang with -march=i686,
    which makes __MMX__ not be defined.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- mingw-w64-headers/crt/intrin.h
+++ mingw-w64-headers/crt/intrin.h
@@ -47,10 +47,10 @@
  * C++ linkage (when GCC headers are explicitly included before intrin.h), but at least their
  * guards will prevent duplicated declarations and avoid conflicts.
  *
- * On GCC 4.9 we may always include those headers. On older GCCs, we may do it only if CPU
+ * On GCC 4.9 and Clang we may always include those headers. On older GCCs, we may do it only if CPU
  * features used by them are enabled, so we need to check macros like __SSE__ or __MMX__ first.
  */
-#if __MINGW_GNUC_PREREQ(4, 9)
+#if __MINGW_GNUC_PREREQ(4, 9) || defined(__clang__)
 #define __MINGW_FORCE_SYS_INTRINS
 #endif
 
