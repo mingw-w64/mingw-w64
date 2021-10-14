54bbe83dfe0a134558bcd85663a4a8b2ff1bc063
Martin Storsjö <martin@martin.st>
2021-10-14 13:17:00 +0300

    crt: Split out the strtold/wcstold frontend aliases to separate object files
    This avoids linker conflicts if object files refer to both 'strtold'
    or 'wcstold' and include them from libucrt*.a before other object
    files pull in the __mingw_* counterparts.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- mingw-w64-crt/gdtoa/strtodnrp.c
+++ mingw-w64-crt/gdtoa/strtodnrp.c
@@ -93,8 +93,4 @@ __mingw_strtod (const char * __restrict__ src, char ** __restrict__ endptr)
 long double __cdecl
 __mingw_strtold (const char * __restrict__ src, char ** __restrict__ endptr)
   __attribute__((alias("__strtod")));
-
-long double __cdecl
-strtold (const char * __restrict__ src, char ** __restrict__ endptr)
-  __attribute__((alias("__strtod")));
 #endif
