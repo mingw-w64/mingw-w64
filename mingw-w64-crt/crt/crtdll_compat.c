609a2498c115ccc355ade1638499d10ed36a940e
Pali Rohár <pali.rohar@gmail.com>
2022-03-07 00:36:12 +0100

    crt: Move dummy stdio _lock() and _unlock() functions into stdio/mingw_dummy__lock.c file
    It would be used also in followup changes.
    
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- mingw-w64-crt/crt/crtdll_compat.c
+++ mingw-w64-crt/crt/crtdll_compat.c
@@ -11,12 +11,6 @@
 #undef __setusermatherr
 void __setusermatherr(__UNUSED_PARAM(int (__cdecl *f)(struct _exception *))) { }
 
-/* Define dummy _lock() and _unlock() functions as crtdll.dll does not provide them */
-void __cdecl _lock(int locknum);
-void __cdecl _unlock(int locknum);
-void __cdecl _lock(__UNUSED_PARAM(int locknum)) { }
-void __cdecl _unlock(__UNUSED_PARAM(int locknum)) { }
-
 /* Define __getmainargs() function via crtdll.dll __GetMainArgs() function */
 extern void __GetMainArgs(int *argc, char ***argv, char ***envp, int expand_wildcards);
 int __cdecl __getmainargs(int *argc, char ***argv, char ***envp, int expand_wildcards, __UNUSED_PARAM(_startupinfo *startup_info))
