7c6112f3ef026b308e9d0dc79475bd0cc85d5794
Pali Rohár <pali.rohar@gmail.com>
2022-11-20 12:23:53 +0100

    crt: Use only __ImageBase symbol in mingw-w64 code
    GNU LD provides __ImageBase symbol since 2.19. This is standard windows
    symbol and should be used instead of custom gnu _image_base__ symbol.
    
    Detect non-existence of __ImageBase symbol at ./configure time and for
    compatibility with older GNU LD versions, define __ImageBase symbol as
    alias to _image_base__ symbol.
    
    This allows to remove all #ifdef hacks for older GNU LD versions as with
    this change __ImageBase works correctly with all GNU LD versions.
    
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- mingw-w64-crt/crt/pseudo-reloc.c
+++ mingw-w64-crt/crt/pseudo-reloc.c
@@ -48,7 +48,7 @@
 
 extern char __RUNTIME_PSEUDO_RELOC_LIST__;
 extern char __RUNTIME_PSEUDO_RELOC_LIST_END__;
-extern IMAGE_DOS_HEADER __MINGW_LSYMBOL(_image_base__);
+extern IMAGE_DOS_HEADER __ImageBase;
 
 void _pei386_runtime_relocator (void);
 
@@ -499,11 +499,7 @@ _pei386_runtime_relocator (void)
 
   do_pseudo_reloc (&__RUNTIME_PSEUDO_RELOC_LIST__,
 		   &__RUNTIME_PSEUDO_RELOC_LIST_END__,
-#ifdef __GNUC__
-		   &__MINGW_LSYMBOL(_image_base__)
-#else
 		   &__ImageBase
-#endif
 		   );
 #ifdef __MINGW64_VERSION_MAJOR
   restore_modified_sections ();
