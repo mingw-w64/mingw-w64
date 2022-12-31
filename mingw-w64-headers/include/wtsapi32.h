931ca20b233e766950e7758cff71c270937b0b64
李通洲 <carter.li@eoitek.com>
2022-12-31 17:10:38 +0800

    wtsapi32: Add more wts API definitions
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- mingw-w64-headers/include/wtsapi32.h
+++ mingw-w64-headers/include/wtsapi32.h
@@ -345,6 +345,48 @@ HANDLE WINAPI WTSVirtualChannelOpenEx(
 
 #endif /*(_WIN32_WINNT >= 0x0600)*/
 
+#if (_WIN32_WINNT >= 0x0601)
+
+typedef struct _WTS_SESSION_INFO_1A {
+  DWORD ExecEnvId;
+  WTS_CONNECTSTATE_CLASS State;
+  DWORD SessionId;
+  LPSTR pSessionName;
+  LPSTR pHostName;
+  LPSTR pUserName;
+  LPSTR pDomainName;
+  LPSTR pFarmName;
+} WTS_SESSION_INFO_1A, *PWTS_SESSION_INFO_1A;
+
+typedef struct _WTS_SESSION_INFO_1W {
+  DWORD ExecEnvId;
+  WTS_CONNECTSTATE_CLASS State;
+  DWORD SessionId;
+  LPWSTR pSessionName;
+  LPWSTR pHostName;
+  LPWSTR pUserName;
+  LPWSTR pDomainName;
+  LPWSTR pFarmName;
+} WTS_SESSION_INFO_1W, * PWTS_SESSION_INFO_1W;
+
+#define WTS_SESSION_INFO_1 __MINGW_NAME_AW(WTS_SESSION_INFO_1)
+#define PWTS_SESSION_INFO_1 __MINGW_NAME_AW(PWTS_SESSION_INFO_1)
+
+WINBOOL WINAPI WTSEnumerateSessionsExA(HANDLE hServer,DWORD* pLevel,DWORD Filter,PWTS_SESSION_INFO_1A* ppSessionInfo,DWORD* pCount);
+WINBOOL WINAPI WTSEnumerateSessionsExW(HANDLE hServer,DWORD* pLevel,DWORD Filter,PWTS_SESSION_INFO_1W* ppSessionInfo,DWORD* pCount);
+#define WTSEnumerateSessionsEx __MINGW_NAME_AW(WTSEnumerateSessionsEx)
+
+typedef enum _WTS_TYPE_CLASS {
+  WTSTypeProcessInfoLevel0,
+  WTSTypeProcessInfoLevel1,
+  WTSTypeSessionInfoLevel1
+} WTS_TYPE_CLASS;
+WINBOOL WTSFreeMemoryExA(WTS_TYPE_CLASS WTSTypeClass,PVOID pMemory,ULONG NumberOfEntries);
+WINBOOL WTSFreeMemoryExW(WTS_TYPE_CLASS WTSTypeClass,PVOID pMemory,ULONG NumberOfEntries);
+#define WTSFreeMemoryEx __MINGW_NAME_AW(WTSFreeMemoryEx)
+
+#endif /*(_WIN32_WINNT >= 0x0601)*/
+
 #ifdef __cplusplus
 }
 #endif
