89ed79fd45043086f390394d34022164fbbf63a5
LIU Hao <lh_mouse@126.com>
2023-06-08 23:15:53 +0800

    headers/winscard: Reformat some function declarations like the other ones
    The contents of this commit should be examined with
    
      git show --color-words=.
    
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- mingw-w64-headers/include/winscard.h
+++ mingw-w64-headers/include/winscard.h
@@ -388,48 +388,15 @@ extern "C" {
   extern WINSCARDAPI LONG WINAPI SCardDlgExtendedError(void);
 
 #if (_WIN32_WINNT >= 0x0600)
-LONG WINAPI SCardGetTransmitCount(
-  SCARDHANDLE hCard,
-  LPDWORD pcTransmitCount
-);
 
-LONG WINAPI SCardReadCacheA(
-  SCARDCONTEXT hContext,
-  UUID *CardIdentifier,
-  DWORD FreshnessCounter,
-  LPSTR LookupName,
-  PBYTE Data,
-  DWORD *DataLen
-);
-
-LONG WINAPI SCardReadCacheW(
-  SCARDCONTEXT hContext,
-  UUID *CardIdentifier,
-  DWORD FreshnessCounter,
-  LPWSTR LookupName,
-  PBYTE Data,
-  DWORD *DataLen
-);
+  extern WINSCARDAPI LONG WINAPI SCardGetTransmitCount(SCARDHANDLE hCard,LPDWORD pcTransmitCount);
+  extern WINSCARDAPI LONG WINAPI SCardReadCacheA(SCARDCONTEXT hContext,UUID *CardIdentifier,DWORD FreshnessCounter,LPSTR LookupName,PBYTE Data,DWORD *DataLen);
+  extern WINSCARDAPI LONG WINAPI SCardReadCacheW(SCARDCONTEXT hContext,UUID *CardIdentifier,DWORD FreshnessCounter,LPWSTR LookupName,PBYTE Data,DWORD *DataLen);
 
 #define SCardReadCache __MINGW_NAME_AW(SCardReadCache)
 
-LONG WINAPI SCardWriteCacheA(
-  SCARDCONTEXT hContext,
-  UUID *CardIdentifier,
-  DWORD FreshnessCounter,
-  LPSTR LookupName,
-  PBYTE Data,
-  DWORD DataLen
-);
-
-LONG WINAPI SCardWriteCacheW(
-  SCARDCONTEXT hContext,
-  UUID *CardIdentifier,
-  DWORD FreshnessCounter,
-  LPWSTR LookupName,
-  PBYTE Data,
-  DWORD DataLen
-);
+  extern WINSCARDAPI LONG WINAPI SCardWriteCacheA(SCARDCONTEXT hContext,UUID *CardIdentifier,DWORD FreshnessCounter,LPSTR LookupName,PBYTE Data,DWORD DataLen);
+  extern WINSCARDAPI LONG WINAPI SCardWriteCacheW(SCARDCONTEXT hContext,UUID *CardIdentifier,DWORD FreshnessCounter,LPWSTR LookupName,PBYTE Data,DWORD DataLen);
 
 #define SCardWriteCache __MINGW_NAME_AW(SCardWriteCache)
 
