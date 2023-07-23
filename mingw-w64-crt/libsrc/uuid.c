01ef3af6e8d469d34d672740581e5579d394e33a
LIU Hao <lh_mouse@126.com>
2023-07-23 19:01:07 +0800

    Revert "crt/libsrc: Include more d2d1 headers in uuid library"
    This reverts commit dc6061eb2e74e0a3c2762f17a42743a726e65e1b.

--- mingw-w64-crt/libsrc/uuid.c
+++ mingw-w64-crt/libsrc/uuid.c
@@ -25,14 +25,7 @@
 #include <oledb.h>
 #include <uiautomation.h>
 #include <urlmon.h>
-#include <d2d1.h>
 #include <d2d1_1.h>
-#include <d2d1_2.h>
-#include <d2d1_3.h>
-#include <d2d1effects.h>
-#include <d2d1effectauthor.h>
-#include <d2d1effects_1.h>
-#include <d2d1effects_2.h>
 #include <d3d11_1.h>
 #include <directmanipulation.h>
 #include <netlistmgr.h>
