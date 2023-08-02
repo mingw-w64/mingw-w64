69846cfb7f7fa33375eae40759d4ff29a4cadf9f
Martin Storsjö <martin@martin.st>
2023-08-02 13:05:46 +0300

    headers: Make __MACHINEI a no-op on non-x86 architectures
    In older MSVC versions that had a __MACHINEI macro, it was defined
    to only expand on Intel 32 and 64 bit x86.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- mingw-w64-headers/crt/intrin.h
+++ mingw-w64-headers/crt/intrin.h
@@ -213,6 +213,11 @@ extern "C" {
 #endif
 
 #if !(defined(_X86_) || defined(__x86_64))
+#undef __MACHINEI
+#define __MACHINEI __MACHINEZ
+#endif
+
+#if !(defined(_X86_) || defined(__x86_64))
 #undef __MACHINEX86X
 #define __MACHINEX86X __MACHINEZ
 #endif
