0ad7f73fdc187a1cd34c7b359bff5e7892b976a7
Pali Rohár <pali.rohar@gmail.com>
2024-06-16 16:40:16 +0200

    crt: Provide _get_output_format() and _set_output_format() functions for all CRT import libraries
    These functions are natively available since msvcr80.dll and also since
    Vista version of msvcrt.dll. They are available also in ARM versions.
    For all older CRT versions include mingw-w64 emulation.
    
    Emulation in misc/output_format.c is slightly updated to optimize code for
    non-msvcrt.dll version which do not provide these functions.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- mingw-w64-crt/misc/output_format.c
+++ mingw-w64-crt/misc/output_format.c
@@ -1,7 +1,6 @@
 #define _get_output_format __dummy__get_output_format
 #define _set_output_format __dummy__set_output_format
 #include <windows.h>
-#include <msvcrt.h>
 
 #undef _get_output_format
 #undef _set_output_format
@@ -10,6 +9,30 @@ static unsigned int last_value = 0;
 typedef unsigned int (*f_get_output_format)(void);
 typedef unsigned int (*f_set_output_format)(unsigned int);
 
+static unsigned int fake_get_output_format(void)
+{
+    return last_value;
+}
+
+static unsigned int fake_set_output_format(unsigned int value)
+{
+    return InterlockedExchange((LONG*)&last_value, value);
+}
+
+
+#ifndef __LIBMSVCRT_OS__
+
+unsigned int __attribute__ ((alias ("fake_set_output_format"))) _set_output_format(unsigned int);
+f_set_output_format __MINGW_IMP_SYMBOL(_set_output_format) = _set_output_format;
+
+
+unsigned int __attribute__ ((alias ("fake_get_output_format"))) _get_output_format(void);
+f_get_output_format __MINGW_IMP_SYMBOL(_get_output_format) = _get_output_format;
+
+#else
+
+#include <msvcrt.h>
+
 static unsigned int init_set_output_format(unsigned int);
 f_set_output_format __MINGW_IMP_SYMBOL(_set_output_format) = init_set_output_format;
 
@@ -17,11 +40,6 @@ unsigned int _set_output_format(unsigned int format);
 unsigned int _set_output_format(unsigned int format)
 {
     return __MINGW_IMP_SYMBOL(_set_output_format)(format);
-}
-
-static unsigned int fake_set_output_format(unsigned int value)
-{
-    return InterlockedExchange((LONG*)&last_value, value);
 }
 
 static unsigned int init_set_output_format(unsigned int format)
@@ -45,11 +63,6 @@ unsigned int _get_output_format(void)
     return __MINGW_IMP_SYMBOL(_get_output_format)();
 }
 
-static unsigned int fake_get_output_format(void)
-{
-    return last_value;
-}
-
 static unsigned int init_get_output_format(void)
 {
   f_get_output_format gof;
@@ -60,3 +73,5 @@ static unsigned int init_get_output_format(void)
 
   return (__MINGW_IMP_SYMBOL(_get_output_format) = gof)();
 }
+
+#endif
