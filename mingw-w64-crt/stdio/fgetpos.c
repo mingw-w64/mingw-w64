2437bc9459b1fc815930940f9aad90cc09c489cd
Pali Rohár <pali.rohar@gmail.com>
2024-06-16 16:40:20 +0200

    crt: Fix fgetpos() and fsetpos() functions for crtdll.dll, msvcrt10.dll and msvcrt20.dll
    Functions fgetpos() and fsetpos() in these libraries have different ABI
    than in all later versions starting with msvcrt40.
    
    Their second argument is pointer to 32-bit signed value, which is not
    compatible with the fpos_t type (64-bit), used by all later versions.
    
    So rename these functions in these import libraries to names with suffix 32
    and provides mingw-w64 fpos_t (64-bit) wrappers around them for ABI
    compatibility.
    
    This change fixes ABI compatibility for those older DLL libraries.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

---
+++ mingw-w64-crt/stdio/fgetpos.c
@@ -0,0 +1,20 @@
+/**
+ * This file has no copyright assigned and is placed in the Public Domain.
+ * This file is part of the mingw-w64 runtime package.
+ * No warranty is given; refer to the file DISCLAIMER.PD within this package.
+ */
+
+#include <stdio.h>
+
+/* Define 64-bit fgetpos() function via 32-bit fgetpos32() function */
+_CRTIMP int __cdecl fgetpos32(FILE *__restrict__ _File, long *__restrict__ _Pos);
+int __cdecl fgetpos(FILE *__restrict__ _File, fpos_t *__restrict__ _Pos)
+{
+  long pos32;
+  int ret;
+
+  ret = fgetpos32(_File, &pos32);
+  *_Pos = pos32;
+  return ret;
+}
+int (__cdecl *__MINGW_IMP_SYMBOL(fgetpos))(FILE *__restrict__ _File, fpos_t *__restrict__ _Pos) = fgetpos;
---
+++ mingw-w64-crt/stdio/fsetpos.c
@@ -0,0 +1,26 @@
+/**
+ * This file has no copyright assigned and is placed in the Public Domain.
+ * This file is part of the mingw-w64 runtime package.
+ * No warranty is given; refer to the file DISCLAIMER.PD within this package.
+ */
+
+#include <stdio.h>
+#include <errno.h>
+#include <limits.h>
+
+/* Define 64-bit fsetpos() function via 32-bit fsetpos32() function */
+_CRTIMP int __cdecl fsetpos32(FILE *__restrict__ _File, long *__restrict__ _Pos);
+int __cdecl fsetpos(FILE *__restrict__ _File, const fpos_t *__restrict__ _Pos)
+{
+  long pos32;
+
+  if (*_Pos > LONG_MAX)
+  {
+    errno = EOVERFLOW;
+    return -1;
+  }
+
+  pos32 = *_Pos;
+  return fsetpos32(_File, &pos32);
+}
+int (__cdecl *__MINGW_IMP_SYMBOL(fsetpos))(FILE *__restrict__ _File, const fpos_t *__restrict__ _Pos) = fsetpos;
