811ff2774b0647174fb294465c87f0759ceeeb27
Pali Rohár <pali.rohar@gmail.com>
2024-09-15 17:40:02 +0200

    crt: Provide __p__winver() function also for ARM msvcrt.dll import library
    ARM32 and ARM64 OS system version of msvcrt.dll do not have neither _winver
    global variable, nor __p__winver() function. But they have _winmajor and
    _winminor global variables.
    
    Provide __p__winver() function emulation for ARM msvcrt import library via
    via values of _winmajor and _winminor global variables.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- mingw-w64-crt/misc/__p__winver.c
+++ mingw-w64-crt/misc/__p__winver.c
@@ -6,6 +6,33 @@
 
 #include <_mingw.h>
 
+#if defined(__arm__) || defined(__aarch64__) || defined(_ARM_) || defined(_ARM64_)
+
+/*
+ * ARM versions of msvcrt.dll do not provide _winver global variable, so emulate
+ * __p__winver() pointer via private variable and fill _winver variable value
+ * from _winmajor and _winminor global variables. All ARM versions provide
+ * _winmajor and _winminor global variables, but do not provide __p__winmajor()
+ * and __p__winminor() functions. To avoid referencing mingw-w64 emulations of
+ * these __p_ functions, access global variables directly.
+ */
+
+extern unsigned int* __MINGW_IMP_SYMBOL(_winmajor);
+extern unsigned int* __MINGW_IMP_SYMBOL(_winminor);
+
+static unsigned int _winver;
+
+unsigned int* __cdecl __p__winver(void);
+unsigned int* __cdecl __p__winver(void)
+{
+    if (!_winver)
+        _winver = (*__MINGW_IMP_SYMBOL(_winmajor) << 8) | *__MINGW_IMP_SYMBOL(_winminor);
+    return &_winver;
+}
+unsigned int* (__cdecl *__MINGW_IMP_SYMBOL(__p__winver))(void) = __p__winver;
+
+#else
+
 extern unsigned int* __MINGW_IMP_SYMBOL(_winver);
 
 unsigned int* __cdecl __p__winver(void);
@@ -14,3 +41,5 @@ unsigned int* __cdecl __p__winver(void)
     return __MINGW_IMP_SYMBOL(_winver);
 }
 unsigned int* (__cdecl *__MINGW_IMP_SYMBOL(__p__winver))(void) = __p__winver;
+
+#endif
