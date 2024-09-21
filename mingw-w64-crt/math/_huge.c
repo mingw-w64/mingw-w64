3f7c8f53ade0b9c13f4a2c054ace562d7567d09f
Pali Rohár <pali.rohar@gmail.com>
2024-09-21 14:24:16 +0200

    crt: Enable HUGE symbol alias
    It is available in msvc, so enable it also in mingw-w64 import libraries
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- mingw-w64-crt/math/_huge.c
+++ mingw-w64-crt/math/_huge.c
@@ -3,3 +3,5 @@
 #undef _HUGE
 static double _HUGE = __builtin_huge_val();
 double * __MINGW_IMP_SYMBOL(_HUGE) = &_HUGE;
+#undef HUGE
+extern double * __attribute__ ((alias (__MINGW64_STRINGIFY(__MINGW_IMP_SYMBOL(_HUGE))))) __MINGW_IMP_SYMBOL(HUGE);
