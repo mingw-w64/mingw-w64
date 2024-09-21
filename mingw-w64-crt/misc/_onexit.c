e36d0410e8844d136128daa8a86ff2bef9b6b222
Pali Rohár <pali.rohar@gmail.com>
2024-09-21 14:29:02 +0200

    crt: Fix _onexit() function for DLL builds
    MS documentation for _onexit function says:
    
      In the case when _onexit is called from within a DLL, routines registered
      with _onexit run when the DLL is unloaded, after DllMain is called with
      DLL_PROCESS_DETACH.
    
    And for this functionality msvc statically links _onexit functionality for
    DLL builds, and do not let applications to use msvcrt.dll's _onexit symbol
    directly.
    
    Fix this issue in mingw-w64 by redefining _onexit function to calls private
    mingw-w64 atexit() function, which already distinguish between EXE and DLL
    builds. Include this _onexit function into every CRT import library and
    disable original _onexit symbol in all def files.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- mingw-w64-crt/misc/_onexit.c
+++ mingw-w64-crt/misc/_onexit.c
@@ -4,14 +4,10 @@
  * No warranty is given; refer to the file DISCLAIMER.PD within this package.
  */
 
-#undef __MSVCRT_VERSION__
-#define _UCRT
-
 #include <stdlib.h>
-#include <corecrt_startup.h>
 
 _onexit_t __cdecl _onexit(_onexit_t func)
 {
-  return _crt_atexit((_PVFV)func) == 0 ? func : NULL;
+  return atexit((void (__cdecl *)(void))func) == 0 ? func : NULL;
 }
 _onexit_t __cdecl (*__MINGW_IMP_SYMBOL(_onexit))(_onexit_t func) = _onexit;
