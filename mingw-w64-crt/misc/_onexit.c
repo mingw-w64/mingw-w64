d2491a9358bddc9573d0ff2fa73989e3175c2009
Pali Rohár <pali.rohar@gmail.com>
2024-09-21 14:29:03 +0200

    crt: Provide onexit symbol alias
    Now when there is working _onexit() function, provide also onexit() aliased
    function, which already has declaration in stdlib.h header file.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- mingw-w64-crt/misc/_onexit.c
+++ mingw-w64-crt/misc/_onexit.c
@@ -11,3 +11,6 @@ _onexit_t __cdecl _onexit(_onexit_t func)
   return atexit((void (__cdecl *)(void))func) == 0 ? func : NULL;
 }
 _onexit_t __cdecl (*__MINGW_IMP_SYMBOL(_onexit))(_onexit_t func) = _onexit;
+
+_onexit_t __attribute__ ((alias ("_onexit"))) __cdecl onexit(_onexit_t);
+extern _onexit_t (__cdecl * __attribute__ ((alias (__MINGW64_STRINGIFY(__MINGW_IMP_SYMBOL(_onexit))))) __MINGW_IMP_SYMBOL(onexit))(_onexit_t);
