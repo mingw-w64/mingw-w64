3e043834e993258833d62ae8d0b11d0e4958770f
Pali Rohár <pali.rohar@gmail.com>
2024-11-27 18:33:33 +0100

    crt: Check for return value from __(w)getmainargs() calls
    Functions __getmainargs() and __wgetmainargs() in system msvcrt.dll library
    since Windows XP and in Visual C++ libraries since msvcr70.dll return
    negative value on error and expect that caller handle it and terminate
    process. Visual C++ 2002+ startup code calls _amsg_exit(_RT_SPACEARG) on
    failure. Do same in mingw-w64 startup code.
    
    Note that mingw-w64 contains compatibility layer around __getmainargs() and
    __wgetmainargs() functions for msvcrt.dll and older VC++ DLL libraries in
    mingw-w64 import libraries to ensure that these functions returns 0 on
    success. This allows to have uniform mingw-w64 crtexe.c startup code, which
    can always expects that __getmainargs() and __wgetmainargs() returns the
    correct value.
    
    Variable argret is used only in the pre_cpp_init() function, so declare it
    on the function stack instead of having it static for the whole file.
    
    With this change, compiled applications linked with msvcrt.dll or new
    library correctly reports runtime failure on allocations errors from
    __getmainargs() and __wgetmainargs() calls.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- mingw-w64-crt/crt/crtexe.c
+++ mingw-w64-crt/crt/crtexe.c
@@ -58,7 +58,6 @@ extern void __main(void);
 static _TCHAR **argv;
 static _TCHAR **envp;
 
-static int argret;
 static int mainret=0;
 static int managedapp;
 static int has_cctor = 0;
@@ -125,6 +124,7 @@ static void __cdecl
 pre_cpp_init (void)
 {
   _startupinfo startinfo;
+  int argret;
 
   startinfo.newmode = _newmode;
 
@@ -133,6 +133,8 @@ pre_cpp_init (void)
 #else
   argret = __getmainargs(&argc,&argv,&envp,_dowildcard,&startinfo);
 #endif
+  if (argret < 0)
+    _amsg_exit(8); /* _RT_SPACEARG */
 }
 
 static int __tmainCRTStartup (void);
