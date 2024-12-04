9af3e9eacff3008e99044b4d51018633c4c8d785
Martin Storsjö <martin@martin.st>
2024-12-04 11:58:01 +0200

    crt: Remove C23 initialization with ={}
    This initialization isn't needed, static variables are guaranteed
    to be zero initialized anyway. This syntax of using an empty brace
    list is only supported since C23 (even though compilers may accept
    it in older language modes too).
    
    This removes one unnecessary change from
    997473bddf5b6b256599f7031064eda9a916c9d6.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- mingw-w64-crt/crt/crt_handler.c
+++ mingw-w64-crt/crt/crt_handler.c
@@ -34,8 +34,8 @@ extern void _fpreset (void);
 EXCEPTION_DISPOSITION __mingw_SEH_error_handler(struct _EXCEPTION_RECORD *, void *, struct _CONTEXT *, void *);
 
 #define MAX_PDATA_ENTRIES 32
-static RUNTIME_FUNCTION emu_pdata[MAX_PDATA_ENTRIES] = {};
-static UNWIND_INFO emu_xdata[MAX_PDATA_ENTRIES] = {};
+static RUNTIME_FUNCTION emu_pdata[MAX_PDATA_ENTRIES];
+static UNWIND_INFO emu_xdata[MAX_PDATA_ENTRIES];
 
 int
 __mingw_init_ehandler (void)
