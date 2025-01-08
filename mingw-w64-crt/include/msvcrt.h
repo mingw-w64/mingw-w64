4b688cfaead3fed7f35dee74d5f6f3b08656e903
Pali Rohár <pali.rohar@gmail.com>
2025-01-08 21:40:08 +0100

    crt: Add include guard for msvcrt.h file
    This allow to include msvcrt.h more times.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- mingw-w64-crt/include/msvcrt.h
+++ mingw-w64-crt/include/msvcrt.h
@@ -1,10 +1,15 @@
-#include <winbase.h>
-
 #ifndef __LIBMSVCRT_OS__
 #error "This file should only be used in libmsvcrt-os.a"
 #endif
+
+#ifndef MSVCRT_H
+#define MSVCRT_H
+
+#include <winbase.h>
 
 static inline HANDLE __mingw_get_msvcrt_handle(void)
 {
     return GetModuleHandleW(L"msvcrt.dll");
 }
+
+#endif
