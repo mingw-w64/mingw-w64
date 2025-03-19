3c8e926c8d855097405ae2a0c546f9a22ac91fb3
Pali Rohár <pali.rohar@gmail.com>
2025-03-19 22:41:11 +0100

    gendef: Remove trailing space in generated def file
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- mingw-w64-tools/gendef/src/gendef.c
+++ mingw-w64-tools/gendef/src/gendef.c
@@ -805,7 +805,7 @@ dump_def (void)
 	}
       else if (seen_ret == 0 && !exp->beData)
         {
-	  fprintf (fp, " ; Check!!! Couldn't determine function argument count. Function doesn't return. ");
+	  fprintf (fp, " ; Check!!! Couldn't determine function argument count. Function doesn't return.");
         }
       fprintf(fp,"\n");
       free (exp);
