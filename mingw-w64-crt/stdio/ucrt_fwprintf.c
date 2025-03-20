9f64d7b27acca5cf0eda29401bc008cff72f6d4d
Martin Storsjö <martin@martin.st>
2025-03-20 13:38:40 +0200

    crt: Remove workarounds for defining non-inline versions of inline functions
    We stopped providing these functions as inline in
    30c0b623dbaf9dcc218c9068625f5e8dddd57aab.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- mingw-w64-crt/stdio/ucrt_fwprintf.c
+++ mingw-w64-crt/stdio/ucrt_fwprintf.c
@@ -4,24 +4,11 @@
  * No warranty is given; refer to the file DISCLAIMER.PD within this package.
  */
 
-// For ucrt, this function normally is an inline function in stdio.h.
-// libmingwex doesn't use the ucrt version of headers, and wassert.c can
-// end up requiring a concrete version of it.
-
-#ifdef __GNUC__
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Winline"
-#endif
-
 #undef __MSVCRT_VERSION__
 #define _UCRT
 
-#define fwprintf real_fwprintf
-
 #include <stdarg.h>
 #include <stdio.h>
-
-#undef fwprintf
 
 int __cdecl fwprintf(FILE *ptr, const wchar_t *fmt, ...);
 
@@ -34,6 +21,3 @@ int __cdecl fwprintf(FILE *ptr, const wchar_t *fmt, ...)
   va_end(ap);
   return ret;
 }
-#ifdef __GNUC__
-#pragma GCC diagnostic pop
-#endif
