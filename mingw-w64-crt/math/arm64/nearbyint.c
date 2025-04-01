0ca516e448c9ede382876fcf622b6440d3068a44
Jacek Caban <jacek@codeweavers.com>
2025-04-01 21:24:26 +0200

    crt: Move ARM64 nearbyint implementation to C files.
    In preparation for using naked functions.
    
    Signed-off-by: Jacek Caban <jacek@codeweavers.com>

---
+++ mingw-w64-crt/math/arm64/nearbyint.c
@@ -0,0 +1,14 @@
+/**
+ * This file has no copyright assigned and is placed in the Public Domain.
+ * This file is part of the mingw-w64 runtime package.
+ * No warranty is given; refer to the file DISCLAIMER.PD within this package.
+ */
+
+#include <math.h>
+#include <internal.h>
+
+__ASM_NAKED_FUNC(nearbyint,
+                 "mrs x1, fpcr\n\t"
+                 "frintx d0, d0\n\t"
+                 "msr fpcr, x1\n\t"
+                 "ret")
---
+++ mingw-w64-crt/math/arm64/nearbyintf.c
@@ -0,0 +1,14 @@
+/**
+ * This file has no copyright assigned and is placed in the Public Domain.
+ * This file is part of the mingw-w64 runtime package.
+ * No warranty is given; refer to the file DISCLAIMER.PD within this package.
+ */
+
+#include <math.h>
+#include <internal.h>
+
+__ASM_NAKED_FUNC(nearbyintf,
+                 "mrs x1, fpcr\n\t"
+                 "frintx s0, s0\n\t"
+                 "msr fpcr, x1\n\t"
+                 "ret")
