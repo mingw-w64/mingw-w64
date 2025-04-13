72d71a53a893b9f579be806054a69f4fd5d8431c
Pali Rohár <pali.rohar@gmail.com>
2025-04-13 18:34:34 +0200

    crt: Provide emulation of _fstat32i64, _stat32i64 and _wstat32i64 functions
    Functions _fstat32i64 (alias of _fstati64), _stat32i64 (alias of _stati64)
    and _wstat32i64 (alias of _wstati64) are available since msvcrt40.dll.
    These functions returns 64-bit st_size and 32-bit file timestamps.
    
    For pre-msvcrt40 CRT import libraries provides mingw-w64 emulation of those
    functions via the _fstat64, _stat64 and _stat64 functions, which returns
    both st_size and file timestamps in 64-bit precision.
    
    When timestamp does not fit into the 32-bit field then sets field to invalid
    value -1. This is behavior of msvcrt functions with 32-bit time_t fields.
    
    Note that _fstat64, _stat64 and _stat64 functions are available in all CRT
    import libraries, in pre-msvcr70 CRT import libraries they are emulated.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

---
+++ mingw-w64-crt/stdio/_wstat32i64.c
@@ -0,0 +1,42 @@
+/**
+ * This file has no copyright assigned and is placed in the Public Domain.
+ * This file is part of the mingw-w64 runtime package.
+ * No warranty is given; refer to the file DISCLAIMER.PD within this package.
+ */
+
+#include <sys/stat.h>
+#include <stdint.h>
+
+/* When the file time does not fit into the st_Xtime field:
+ *           crtdll-msvcr71   msvcr80+
+ * st_Xtime       -1             -1
+ * errno       no change       EINVAL
+ * returns         0              0
+ *
+ * This file is used only for pre-msvcrt40 builds,
+ * So use the pre-msvcrt40 behavior - fills -1 without errno change.
+ */
+int __cdecl _wstat32i64(const wchar_t *_Name,struct _stat32i64 *_Stat)
+{
+  struct _stat64 st;
+  int ret=_wstat64(_Name,&st);
+  if (ret != 0)
+    return ret;
+  _Stat->st_dev=st.st_dev;
+  _Stat->st_ino=st.st_ino;
+  _Stat->st_mode=st.st_mode;
+  _Stat->st_nlink=st.st_nlink;
+  _Stat->st_uid=st.st_uid;
+  _Stat->st_gid=st.st_gid;
+  _Stat->st_rdev=st.st_rdev;
+  _Stat->st_size=st.st_size;
+  _Stat->st_atime=((st.st_atime > INT32_MAX) ? -1 : st.st_atime);
+  _Stat->st_mtime=((st.st_mtime > INT32_MAX) ? -1 : st.st_mtime);
+  _Stat->st_ctime=((st.st_ctime > INT32_MAX) ? -1 : st.st_ctime);
+  return 0;
+}
+int (__cdecl *__MINGW_IMP_SYMBOL(_wstat32i64))(const wchar_t *, struct _stat32i64 *) = _wstat32i64;
+
+#undef _wstati64
+int __attribute__ ((alias ("_wstat32i64"))) __cdecl _wstati64(const wchar_t *, struct _stat32i64 *);
+extern int __attribute__ ((alias (__MINGW64_STRINGIFY(__MINGW_IMP_SYMBOL(_wstat32i64))))) (__cdecl *__MINGW_IMP_SYMBOL(_wstati64))(const wchar_t *, struct _stat32i64 *);
