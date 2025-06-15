3716b01c8fbaa88de5f37e098f99890edf13007d
Pali Rohár <pali.rohar@gmail.com>
2025-06-15 15:30:14 +0200

    crt: Define lseek64 as alias in crt-aliases.def.in
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- mingw-w64-crt/stdio/_lseeki64.c
+++ mingw-w64-crt/stdio/_lseeki64.c
@@ -20,3 +20,6 @@ __int64 __cdecl _lseeki64(int fd, __int64 offset, int whence)
   return _lseek(fd, offset, whence);
 }
 __int64 (__cdecl *__MINGW_IMP_SYMBOL(_lseeki64))(int, __int64, int) = _lseeki64;
+
+__int64 __attribute__ ((alias ("_lseeki64"))) __cdecl lseek64(int, __int64, int);
+extern __int64 (__cdecl * __attribute__((alias (__MINGW64_STRINGIFY(__MINGW_IMP_SYMBOL(_lseeki64))))) __MINGW_IMP_SYMBOL(lseek64))(int, __int64, int);
