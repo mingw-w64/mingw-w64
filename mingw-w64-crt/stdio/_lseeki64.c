2423e8a8e28b56b844788554847b63eef5e7a0c7
Pali Rohár <pali.rohar@gmail.com>
2025-06-15 15:30:13 +0200

    crt: Provide _lseeki64() function for pre-msvcrt40 builds
    Function _lseeki64() is available since msvcrt40.dll. For older CRT
    libraries provides emulation via lseek().
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

---
+++ mingw-w64-crt/stdio/_lseeki64.c
@@ -0,0 +1,22 @@
+/**
+ * This file has no copyright assigned and is placed in the Public Domain.
+ * This file is part of the mingw-w64 runtime package.
+ * No warranty is given; refer to the file DISCLAIMER.PD within this package.
+ */
+
+#include <io.h>
+#include <errno.h>
+#include <limits.h>
+
+/* Define 64-bit _lseeki64() function via 32-bit _lseek() function */
+__int64 __cdecl _lseeki64(int fd, __int64 offset, int whence)
+{
+  if (offset > LONG_MAX)
+  {
+    errno = EOVERFLOW;
+    return -1;
+  }
+
+  return _lseek(fd, offset, whence);
+}
+__int64 (__cdecl *__MINGW_IMP_SYMBOL(_lseeki64))(int, __int64, int) = _lseeki64;
