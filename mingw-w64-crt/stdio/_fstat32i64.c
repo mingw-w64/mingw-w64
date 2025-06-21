6303d2f2c78984446f25e739a7462e3293cf71d4
Pali Rohár <pali.rohar@gmail.com>
2025-06-21 13:37:53 +0200

    crt: Provide emulation of _fstat32i64, _stat32i64 and _wstat32i64 functions for 64-bit msvcrt builds
    All 64-bit CRT libraries except the msvcrt.dll provides native _fstat32i64,
    _stat32i64 and _wstat32i64 functions. 64-bit system os msvcrt.dll is an
    exception and does not contain these function.
    
    mingw-w64 already contains emulation of these functions for pre-msvcrt40
    32-bit builds. Extend this emulation for 64-bit msvcrt.dll builds.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>
    (cherry picked from commit 69963970ab35d1744115152639667f10e2bb24e4)
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- mingw-w64-crt/stdio/_fstat32i64.c
+++ mingw-w64-crt/stdio/_fstat32i64.c
@@ -6,6 +6,7 @@
 
 #include <sys/stat.h>
 #include <stdint.h>
+#include <errno.h>
 
 /* When the file time does not fit into the st_Xtime field:
  *           crtdll-msvcr71   msvcr80+
@@ -13,8 +14,9 @@
  * errno       no change       EINVAL
  * returns         0              0
  *
- * This file is used only for pre-msvcrt40 builds,
- * So use the pre-msvcrt40 behavior - fills -1 without errno change.
+ * This file is used for 32-bit pre-msvcrt40 builds and 64-bit msvcrt.dll builds.
+ * So for 32-bit use the pre-msvcrt40 behavior - fills -1 without errno change.
+ * And for 64-bit use the msvcr80+ behavior - fills -1 with setting errno to EINVAL.
  */
 int __cdecl _fstat32i64(int _FileDes,struct _stat32i64 *_Stat)
 {
@@ -33,10 +35,16 @@ int __cdecl _fstat32i64(int _FileDes,struct _stat32i64 *_Stat)
   _Stat->st_atime=((st.st_atime < 0 || st.st_atime > INT32_MAX) ? -1 : st.st_atime);
   _Stat->st_mtime=((st.st_mtime < 0 || st.st_mtime > INT32_MAX) ? -1 : st.st_mtime);
   _Stat->st_ctime=((st.st_ctime < 0 || st.st_ctime > INT32_MAX) ? -1 : st.st_ctime);
+#ifdef _WIN64
+  if (_Stat->st_atime == -1 || _Stat->st_mtime == -1 || _Stat->st_ctime == -1)
+    errno = EINVAL;
+#endif
   return 0;
 }
 int (__cdecl *__MINGW_IMP_SYMBOL(_fstat32i64))(int, struct _stat32i64 *) = _fstat32i64;
 
+#ifndef _WIN64
 #undef _fstati64
 int __attribute__ ((alias ("_fstat32i64"))) __cdecl _fstati64(int, struct _stat32i64 *);
 extern int __attribute__ ((alias (__MINGW64_STRINGIFY(__MINGW_IMP_SYMBOL(_fstat32i64))))) (__cdecl *__MINGW_IMP_SYMBOL(_fstati64))(int, struct _stat32i64 *);
+#endif
