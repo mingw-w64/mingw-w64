mingw-w64/mingw-w64

commit 53a3686f0920f1dc2fba34691d8eb8094851d82e

crt: stat: Fail if the pathname ends with a slash and isn't a directory
POSIX requires stat("foo/", &st) to fail if "foo" isn't a directory or
a symbolic link to a directory. See ENOTDIR in [1].

The statXX functions, that are used with MSVCRT, already have code
to remove a trailing slash. Extend it to reject non-directories with
ENOTDIR if a trailing slash was removed.

These functions aren't used with UCRT because UCRT handles trailing
slashes better than MSVCRT. One minor issue with UCRT is that "foo/"
results in ENOENT instead of ENOTDIR. This commit doesn't fix it.

Preserve errno when calling free().

[1] https://pubs.opengroup.org/onlinepubs/9799919799/functions/fstatat.html

Signed-off-by: Martin Storsjö <martin@martin.st>

mingw-w64-crt/stdio/msvcr110pre_stat32.c
@@ -23,8 +23,7 @@ int __cdecl stat32(const char *_Filename, struct _stat32 *_Stat)
   if (_path == NULL && _Filename != NULL)
     return -1;
   int ret = _stat32i64(_path, &st);
-  if (_path != _Filename)
-    free(_path);
+  ret = __mingw_fix_stat_finish(ret, _Filename, _path, _Stat->st_mode);
   if (ret != 0)
     return ret;
   if (st.st_size > UINT32_MAX) {

mingw-w64-crt/stdio/msvcr110pre_stat64i32.c
@@ -23,8 +23,7 @@ int __cdecl stat64i32(const char *_Filename, struct _stat64i32 *_Stat)
   if (_path == NULL && _Filename != NULL)
     return -1;
   int ret = _stat64(_path, &st);
-  if (_path != _Filename)
-    free(_path);
+  ret = __mingw_fix_stat_finish(ret, _Filename, _path, _Stat->st_mode);
   if (ret != 0)
     return ret;
   if (st.st_size > UINT32_MAX) {

mingw-w64-crt/stdio/msvcr110pre_wstat32.c
@@ -23,8 +23,7 @@ int __cdecl wstat32(const wchar_t *_Filename, struct _stat32 *_Stat)
   if (_path == NULL && _Filename != NULL)
     return -1;
   int ret = _wstat32i64(_path, &st);
-  if (_path != _Filename)
-    free(_path);
+  ret = __mingw_fix_stat_finish(ret, _Filename, _path, _Stat->st_mode);
   if (ret != 0)
     return ret;
   if (st.st_size > UINT32_MAX) {

mingw-w64-crt/stdio/msvcr110pre_wstat64i32.c
@@ -23,8 +23,7 @@ int __cdecl wstat64i32(const wchar_t *_Filename, struct _stat64i32 *_Stat)
   if (_path == NULL && _Filename != NULL)
     return -1;
   int ret = _wstat64(_path, &st);
-  if (_path != _Filename)
-    free(_path);
+  ret = __mingw_fix_stat_finish(ret, _Filename, _path, _Stat->st_mode);
   if (ret != 0)
     return ret;
   if (st.st_size > UINT32_MAX) {

mingw-w64-crt/Makefile.am
@@ -1112,7 +1112,7 @@ src_libmingwex=\
   stdio/asprintf.c \
   stdio/fopen64.c          stdio/fseeko32.c          stdio/fseeko64.c                                   stdio/ftello.c          \
   stdio/ftello64.c         stdio/ftruncate64.c       stdio/lltoa.c            stdio/lltow.c             stdio/lseek64.c         \
-  stdio/__mingw_fix_stat.h \
+  stdio/__mingw_fix_stat.h stdio/__mingw_fix_stat_finish.c \
   stdio/__mingw_fix_stat_path.c stdio/__mingw_fix_wstat_path.c \
   \
   stdio/mingw_pformat.h    mingw_sformat.h           mingw_swformat.h \

mingw-w64-crt/stdio/__mingw_fix_stat.h
@@ -9,5 +9,7 @@
 
 char* __mingw_fix_stat_path (const char* _path);
 wchar_t* __mingw_fix_wstat_path (const wchar_t* _path);
+int __mingw_fix_stat_finish(int ret, const void *orig_path, void *used_path,
+                            unsigned short mode);
 
 #endif

mingw-w64-crt/stdio/__mingw_fix_stat_finish.c
@@ -1,0 +1,36 @@
+/**
+ * This file has no copyright assigned and is placed in the Public Domain.
+ * This file is part of the mingw-w64 runtime package.
+ * No warranty is given; refer to the file DISCLAIMER.PD within this package.
+ */
+
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <errno.h>
+#include "__mingw_fix_stat.h"
+
+int __mingw_fix_stat_finish(int ret, const void *orig_path, void *used_path,
+                            unsigned short mode)
+{
+  /*
+   * If the original pathname and used pathname differ, it means that
+   * __mingw_fix_stat_path or __mingw_fix_wstat_path had to allocate
+   * a temporary buffer and remove a trailing directory separator.
+   * In this case the temporary allocation has to be freed, and the
+   * stat function succeeds only if the pathname was a directory.
+   */
+  if (orig_path != used_path) {
+    /* Save errno because we call free. */
+    int saved_errno = errno;
+    free(used_path);
+
+    if (ret == 0 && !S_ISDIR(mode)) {
+      ret = -1;
+      saved_errno = ENOTDIR;
+    }
+
+    errno = saved_errno;
+  }
+
+  return ret;
+}

mingw-w64-crt/stdio/msvcr110plus_stat32.c
@@ -15,9 +15,7 @@ int __cdecl stat32(const char *_Filename, struct _stat32 *_Stat)
   if (_path == NULL && _Filename != NULL)
     return -1;
   int ret = _stat32(_path, _Stat);
-  if (_path != _Filename)
-    free(_path);
-  return ret;
+  return __mingw_fix_stat_finish(ret, _Filename, _path, _Stat->st_mode);
 }
 int (__cdecl *__MINGW_IMP_SYMBOL(stat32))(const char *, struct _stat32 *) = stat32;
 

commit 4b0e02a52f8271a74ceb20f66529fb1e4ac1e67f

crt: Read the right st_mode field for __mingw_fix_stat_finish
This fixes a regression from
53a3686f0920f1dc2fba34691d8eb8094851d82e; at this point,
_Stat->st_mode is uninitialized in these functions.

Signed-off-by: Martin Storsjö <martin@martin.st>

mingw-w64-crt/stdio/msvcr110pre_stat32.c
@@ -23,7 +23,7 @@ int __cdecl stat32(const char *_Filename, struct _stat32 *_Stat)
   if (_path == NULL && _Filename != NULL)
     return -1;
   int ret = _stat32i64(_path, &st);
-  ret = __mingw_fix_stat_finish(ret, _Filename, _path, _Stat->st_mode);
+  ret = __mingw_fix_stat_finish(ret, _Filename, _path, st.st_mode);
   if (ret != 0)
     return ret;
   if (st.st_size > UINT32_MAX) {

mingw-w64-crt/stdio/msvcr110pre_stat64i32.c
@@ -23,7 +23,7 @@ int __cdecl stat64i32(const char *_Filename, struct _stat64i32 *_Stat)
   if (_path == NULL && _Filename != NULL)
     return -1;
   int ret = _stat64(_path, &st);
-  ret = __mingw_fix_stat_finish(ret, _Filename, _path, _Stat->st_mode);
+  ret = __mingw_fix_stat_finish(ret, _Filename, _path, st.st_mode);
   if (ret != 0)
     return ret;
   if (st.st_size > UINT32_MAX) {

mingw-w64-crt/stdio/msvcr110pre_wstat32.c
@@ -23,7 +23,7 @@ int __cdecl wstat32(const wchar_t *_Filename, struct _stat32 *_Stat)
   if (_path == NULL && _Filename != NULL)
     return -1;
   int ret = _wstat32i64(_path, &st);
-  ret = __mingw_fix_stat_finish(ret, _Filename, _path, _Stat->st_mode);
+  ret = __mingw_fix_stat_finish(ret, _Filename, _path, st.st_mode);
   if (ret != 0)
     return ret;
   if (st.st_size > UINT32_MAX) {

mingw-w64-crt/stdio/msvcr110pre_wstat64i32.c
@@ -23,7 +23,7 @@ int __cdecl wstat64i32(const wchar_t *_Filename, struct _stat64i32 *_Stat)
   if (_path == NULL && _Filename != NULL)
     return -1;
   int ret = _wstat64(_path, &st);
-  ret = __mingw_fix_stat_finish(ret, _Filename, _path, _Stat->st_mode);
+  ret = __mingw_fix_stat_finish(ret, _Filename, _path, st.st_mode);
   if (ret != 0)
     return ret;
   if (st.st_size > UINT32_MAX) {