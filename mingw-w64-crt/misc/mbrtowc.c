13850af6b1afa67d74b2a6705b4c83a47a3e9866
Kirill Makurin <maiddaisuki@outlook.com>
2025-07-09 17:04:34 +0900

    crt: new implementation for C95 conversion functions
    New implementation writes to output buffer only if successful
    conversion has occured. CRT's versions and old implementation may
    write to out buffer even if conversion has failed.
    
    POSIX specifies that C95 conversion functions *may* fail with errno
    set to EINVAL if conversion state described in `mbstate_t` is invalid.
    This error condition is now possible with mbrlen, mbrtowc, mbsrtowcs,
    wcrtomb and wcsrtombs functions.
    
    Signed-off-by: Kirill Makurin <maiddaisuki@outlook.com>

--- mingw-w64-crt/misc/mbrtowc.c
+++ mingw-w64-crt/misc/mbrtowc.c
@@ -12,148 +12,227 @@
 #include <errno.h>
 #include <windows.h>
 
-static int __MINGW_ATTRIB_NONNULL(1) __MINGW_ATTRIB_NONNULL(4)
-__mbrtowc_cp (wchar_t * __restrict__ pwc, const char * __restrict__ s,
-	      size_t n, mbstate_t* __restrict__ ps,
-	      const unsigned int cp, const unsigned int mb_max) 
-{
-  union {
-    mbstate_t val;
-    char mbcs[4];
-  } shift_state;
+/**
+ * Private `mbstate_t` to use if caller did not supply one.
+ */
+static mbstate_t state_mbrlen = {0};
+static mbstate_t state_mbrtowc = {0};
+static mbstate_t state_mbsrtowcs = {0};
 
-  /* Do the prelim checks */
-  if (s == NULL)
+static size_t mbrtowc_cp (
+  wchar_t *__restrict__ wc,
+  const char *__restrict__ mbs,
+  size_t count,
+  mbstate_t *__restrict__ state,
+  unsigned cp,
+  int mb_cur_max
+) {
+  /* Set `state` to initial state */
+  if (mbs == NULL) {
+    *state = 0;
     return 0;
+  }
 
-  if (n == 0)
-    /* The standard doesn't mention this case explicitly. Tell
-       caller that the conversion from a non-null s is incomplete. */
-    return -2;
+  /* Detect invalid conversion state */
+  if ((unsigned) *state > 0xFF) {
+    goto einval;
+  }
 
-  /* Save the current shift state, in case we need it in DBCS case.  */
-  shift_state.val = *ps;
-  *ps = 0;
+  /* Both ISO C and POSIX do not mention this case */
+  if (count == 0) {
+    return (size_t) -2;
+  }
 
-  if (!*s)
-    {
-      *pwc = 0;
+  /* Treat `state` as an array of bytes */
+  union {
+    mbstate_t state;
+    char bytes[4];
+  } conversion_state = {.state = *state};
+
+  /* For SBCS code pages `state` must always be in initial state */
+  if (mb_cur_max == 1 && conversion_state.bytes[0]) {
+    goto einval;
+  }
+
+  /* Handle "C" locale */
+  if (cp == 0) {
+    if (wc != NULL) {
+      *wc = (unsigned char) mbs[0];
+    }
+    return !!mbs[0];
+  }
+
+  /* Length of potential multibyte character */
+  int length = 1;
+
+  if (conversion_state.bytes[0]) {
+    conversion_state.bytes[1] = mbs[0];
+    length = 2;
+  } else if (mb_cur_max == 2 && IsDBCSLeadByteEx (cp, (BYTE) mbs[0])) {
+    conversion_state.bytes[0] = mbs[0];
+
+    /* We need to examine mbs[1] */
+    if (count < 2) {
+      *state = conversion_state.state;
+      return (size_t) -2;
+    }
+
+    conversion_state.bytes[1] = mbs[1];
+    length = 2;
+  } else {
+    conversion_state.bytes[0] = mbs[0];
+  }
+
+  /* Store terminating '\0' */
+  if (conversion_state.bytes[0] == '\0') {
+    if (wc != NULL) {
+      *wc = L'\0';
+      *state = 0;
+    }
+    return 0;
+  }
+
+  /* Truncated multibyte character */
+  if (length == 2 && conversion_state.bytes[1] == '\0') {
+    goto eilseq;
+  }
+
+  /* Converted wide character */
+  wchar_t wcOut = WEOF;
+
+  int ret = MultiByteToWideChar (
+    cp, MB_ERR_INVALID_CHARS, conversion_state.bytes, length, &wcOut, 1
+  );
+
+  if (ret != 1) {
+    goto eilseq;
+  }
+
+  if (wc != NULL) {
+    *wc = wcOut;
+    *state = 0;
+  }
+
+  return length;
+
+eilseq:
+  _set_errno (EILSEQ);
+  return (size_t) -1;
+
+einval:
+  _set_errno (EINVAL);
+  return (size_t) -1;
+}
+
+size_t mbrlen (
+  const char *__restrict__ mbs,
+  size_t count,
+  mbstate_t *__restrict__ state
+) {
+  /* Use private `mbstate_t` if caller did not supply one */
+  if (state == NULL) {
+    state = &state_mbrlen;
+  }
+  wchar_t wc = WEOF;
+  return mbrtowc (&wc, mbs, count, state);
+}
+
+size_t mbrtowc (
+  wchar_t *__restrict__ wc,
+  const char *__restrict__ mbs,
+  size_t count,
+  mbstate_t *__restrict__ state
+) {
+  /* Use private `mbstate_t` if caller did not supply one */
+  if (state == NULL) {
+    state = &state_mbrtowc;
+  }
+
+  /* Code page used by current locale */
+  unsigned cp = ___lc_codepage_func ();
+  /* Maximum character length in `cp` */
+  int mb_cur_max = MB_CUR_MAX;
+
+  return mbrtowc_cp (wc, mbs, count, state, cp, mb_cur_max);
+}
+
+size_t mbsrtowcs (
+  wchar_t *wcs,
+  const char **__restrict__ mbs,
+  size_t count,
+  mbstate_t *__restrict__ state
+) {
+  /* Use private `mbstate_t` if caller did not supply one */
+  if (state == NULL) {
+    state = &state_mbsrtowcs;
+  }
+
+  /* Code page used by current locale */
+  unsigned cp = ___lc_codepage_func ();
+  /* Maximum character length in `cp` */
+  int mb_cur_max = MB_CUR_MAX;
+
+  /* Treat `state` as array of bytes */
+  union {
+    mbstate_t state;
+    char bytes[4];
+  } conversion_state = {.state = *state};
+
+  /* Total number of wide character written to `wcs` */
+  size_t  wcConverted = 0;
+  /* Converted wide character */
+  wchar_t wc = 0;
+
+  /* Next multibyte character to convert */
+  const char *mbc = *mbs;
+
+  while (1) {
+    const size_t length = mbrtowc_cp (
+      &wc, mbc, mb_cur_max, &conversion_state.state, cp, mb_cur_max
+    );
+
+    /* Conversion failed */
+    if (length == (size_t) -1) {
+      if (wcs != NULL) {
+        *mbs = mbc;
+        *state = conversion_state.state;
+      }
+      return (size_t) -1;
+    }
+
+    /* POSIX and ISO C are silent about this */
+    if (wcs != NULL && count == 0) {
       return 0;
     }
 
-  if (mb_max > 1)
-    {
-      if (shift_state.mbcs[0] != 0)
-	{
-	  /* Complete the mb char with the trailing byte.  */
-	  shift_state.mbcs[1] = *s;  /* the second byte */
-	  if (MultiByteToWideChar(cp, MB_ERR_INVALID_CHARS,
-				  shift_state.mbcs, 2, pwc, 1)
-		 == 0)
-	    {
-	      /* An invalid trailing byte */
-	      errno = EILSEQ;
-	      return -1;
-	    }
-	  return 2;
-	}
-      else if (IsDBCSLeadByteEx (cp, *s))
-	{
-	  /* If told to translate one byte, just save the leadbyte
-	     in *ps.  */
-	  if (n < 2)
-	    {
-	      ((char*) ps)[0] = *s;
-	      return -2;
-	    }
-	  /* Else translate the first two bytes  */  
-	  else if (MultiByteToWideChar (cp, MB_ERR_INVALID_CHARS,
-					s, 2, pwc, 1)
-		    == 0)
-	    {
-	      errno = EILSEQ;
-	      return -1;
-	    }
-	  return 2;
-	}
+    /* Store terminating L'\0' and stop */
+    if (length == 0) {
+      if (wcs != NULL) {
+        *wcs = L'\0';
+        *mbs = NULL;
+      }
+      break;
     }
 
-  /* Fall through to single byte char  */
-  if (cp == 0)
-      *pwc = (wchar_t)(unsigned char)*s;
-
-  else if (MultiByteToWideChar (cp, MB_ERR_INVALID_CHARS, s, 1, pwc, 1)
-	    == 0)
-    {
-      errno = EILSEQ;
-      return  -1;
+    /* Store converted `wc` in `wcs` */
+    if (wcs != NULL) {
+      *wcs = wc;
+      wcs += 1;
     }
 
-  return 1;
-}
+    wcConverted += 1;
+    mbc += length;
 
-size_t
-__cdecl
-mbrtowc (wchar_t * __restrict__ pwc, const char * __restrict__ s,
-	 size_t n, mbstate_t* __restrict__ ps)
-{
-  static mbstate_t internal_mbstate = 0;
-  wchar_t  byte_bucket = 0;
-  wchar_t* dst = pwc ? pwc : &byte_bucket;
-
-  return (size_t) __mbrtowc_cp (dst, s, n, ps ? ps : &internal_mbstate,
-				___lc_codepage_func(), MB_CUR_MAX);
-}
-
-
-size_t
-__cdecl
-mbsrtowcs (wchar_t* __restrict__ dst,  const char ** __restrict__ src,
-	   size_t len, mbstate_t* __restrict__ ps)
-{
-  int ret =0 ;
-  size_t n = 0;
-  static mbstate_t internal_mbstate = 0;
-  mbstate_t* internal_ps = ps ? ps : &internal_mbstate;
-  const unsigned int cp = ___lc_codepage_func();
-  const unsigned int mb_max = MB_CUR_MAX;
-
-  if (src == NULL || *src == NULL)	/* undefined behavior */
-    return 0;
-
-  if (dst != NULL)
-    {
-      while (n < len
-	     && (ret = __mbrtowc_cp(dst, *src, len - n,
-				    internal_ps, cp, mb_max))
-		  > 0)
-	{
-	  ++dst;
-	  *src += ret;
-	  n += ret;
-	}
-
-      if (n < len && ret == 0)
-	*src = (char *)NULL;
+    /* `count` wide characters have been stored in `wcs`, stop */
+    if (wcs != NULL && wcConverted == count) {
+      *mbs = mbc;
+      break;
     }
-  else
-    {
-      wchar_t byte_bucket = 0;
-      while ((ret = __mbrtowc_cp (&byte_bucket, *src + n, mb_max,
-				     internal_ps, cp, mb_max))
-		  > 0)
-	n += ret;
-    }
-  return n;
-}
+  }
 
-size_t
-__cdecl
-mbrlen (const char * __restrict__ s, size_t n,
-	mbstate_t * __restrict__ ps)
-{
-  static mbstate_t s_mbstate = 0;
-  wchar_t byte_bucket = 0;
-  return __mbrtowc_cp (&byte_bucket, s, n, (ps) ? ps : &s_mbstate,
-		       ___lc_codepage_func(), MB_CUR_MAX);
+  if (wcs != NULL) {
+    *state = conversion_state.state;
+  }
+
+  return wcConverted;
 }
--- mingw-w64-crt/misc/wcrtomb.c
+++ mingw-w64-crt/misc/wcrtomb.c
@@ -13,91 +13,144 @@
 #include <limits.h>
 #include <windows.h>
 
-__attribute__((noinline))
-static int __MINGW_ATTRIB_NONNULL(1)
- __wcrtomb_cp (char *dst, wchar_t wc, const unsigned int cp,
-	       const unsigned int mb_max)
-{
-  if (cp == 0)
-    {
-      if (wc > 255)
-	{
-	  errno = EILSEQ;
-	  return -1;
-	}
-      *dst = (char) wc;
-      return 1;
+static size_t wcrtomb_cp (
+  char *__restrict__ mbc,
+  wchar_t wc,
+  mbstate_t *__restrict__ state,
+  unsigned cp,
+  int mb_cur_max
+) {
+  /* Set `state` to initial state */
+  if (mbc == NULL) {
+    if (state != NULL) {
+      *state = 0;
     }
-  else
-    {
-      int invalid_char = 0;
+    return 1;
+  }
 
-      int size = WideCharToMultiByte (cp, 0 /* Is this correct flag? */,
-				      &wc, 1, dst, mb_max,
-				      NULL, &invalid_char);
-      if (size == 0 || invalid_char)  
-	{
-	  errno = EILSEQ;
-	  return -1;
-	}
-      return size;
+  /* Detect invalid conversion state */
+  if (state != NULL && *state) {
+    _set_errno (EINVAL);
+    return (size_t) -1;
+  }
+
+  /* Store terminating L'\0' */
+  if (wc == L'\0') {
+    if (mbc != NULL) {
+      mbc[0] = '\0';
     }
+    return 1;
+  }
+
+  /* Handle "C" locale */
+  if (cp == 0) {
+    if (wc > 0xFF) {
+      goto eilseq;
+    }
+    if (mbc != NULL) {
+      mbc[0] = (char) wc;
+    }
+    return 1;
+  }
+
+  BOOL defaultCharacterUsed = FALSE;
+  char buffer[2] = {0, 0};
+
+  /* For consistency with CRT, we do not use WC_NO_BEST_FIT_CHARS */
+  int ret = WideCharToMultiByte (
+    cp, 0, &wc, 1, buffer, mb_cur_max, NULL, &defaultCharacterUsed
+  );
+
+  if (ret == 0 || ret > mb_cur_max || defaultCharacterUsed) {
+    goto eilseq;
+  }
+
+  memcpy (mbc, buffer, ret);
+  return ret;
+
+eilseq:
+  _set_errno (EILSEQ);
+  return (size_t) -1;
 }
 
-size_t
-__cdecl
-wcrtomb (char *dst, wchar_t wc, mbstate_t * __UNUSED_PARAM (ps))
-{
-  char byte_bucket [MB_LEN_MAX];
-  char* tmp_dst = dst ? dst : &byte_bucket[0];
-  return (size_t)__wcrtomb_cp (tmp_dst, wc, ___lc_codepage_func(),
-			       MB_CUR_MAX);
+size_t wcrtomb (
+  char *__restrict__ mbc,
+  wchar_t wc,
+  mbstate_t *__restrict__ state
+) {
+  /* Code page used by current locale */
+  unsigned cp = ___lc_codepage_func ();
+  /* Maximum character length in `cp` */
+  int mb_cur_max = MB_CUR_MAX;
+
+  return wcrtomb_cp (mbc, wc, state, cp, mb_cur_max);
 }
 
-size_t
-__cdecl
-wcsrtombs (char *dst, const wchar_t **src, size_t len,
-		  mbstate_t * __UNUSED_PARAM (ps))
-{
-  int ret = 0;
-  size_t n = 0;
-  const unsigned int cp = ___lc_codepage_func();
-  const unsigned int mb_max = MB_CUR_MAX;
-  const wchar_t *pwc = *src;
+size_t wcsrtombs (
+  char *__restrict__ mbs,
+  const wchar_t **__restrict__ wcs,
+  size_t count,
+  mbstate_t *__restrict__ state
+) {
+  /* Code page used by current locale */
+  unsigned cp = ___lc_codepage_func ();
+  /* Maximum character length in `cp` */
+  int mb_cur_max = MB_CUR_MAX;
 
-  if (src == NULL || *src == NULL) /* undefined behavior */
-    return 0;
+  /* Buffer to store single converted character */
+  char mbc[2];
+  /* Total number of bytes stored in `mbs` */
+  size_t mbcConverted = 0;
 
-  if (dst != NULL)
-    {
-      while (n < len)
-	{
-	  if ((ret = __wcrtomb_cp (dst, *pwc, cp, mb_max)) <= 0)
-	    return (size_t) -1;
-	  n += ret;
-	  dst += ret;
-	  if (*(dst - 1) == '\0')
-	    {
-	      *src = (wchar_t *) NULL;
-	      return (n  - 1);
-	    }
-	  pwc++;
-	}
-      *src = pwc;
-    }
-  else
-    {
-      char byte_bucket [MB_LEN_MAX];
-      while (1)
-	{
-	  if ((ret = __wcrtomb_cp (&byte_bucket[0], *pwc, cp, mb_max)) <= 0)
-	    return (size_t) -1;
-	  n += ret;
-	  if (byte_bucket [ret - 1] == '\0')
-	    return (n - 1);
-	  pwc++;
-	}
+  /* Next wide character to convert */
+  const wchar_t *wc = *wcs;
+
+  while (1) {
+    const size_t length = wcrtomb_cp (mbc, *wc, state, cp, mb_cur_max);
+
+    /* Conversion failed */
+    if (length == (size_t) -1) {
+      if (mbs != NULL) {
+        *wcs = wc;
+      }
+      return (size_t) -1;
     }
 
-  return n;
+    /* POSIX and ISO C are silent about this */
+    if (mbs != NULL && count == 0) {
+      return 0;
+    }
+
+    /* Terminating '\0' has been converted, stop */
+    if (mbc[0] == '\0') {
+      if (mbs != NULL) {
+        *mbs = '\0';
+        *wcs = NULL;
+      }
+      break;
+    }
+
+    /* Storing `mbc` in `mbs` would exceed `count` */
+    if (mbs != NULL && mbcConverted + length > count) {
+      *wcs = wc;
+      break;
+    }
+
+    /* Write `mbc` to `mbs` */
+    if (mbs != NULL) {
+      memcpy (mbs, mbc, length);
+      mbs += length;
+    }
+
+    mbcConverted += length;
+    wc += 1;
+
+    /* `count` bytes have been written to `mbs`, stop */
+    if (mbs != NULL && mbcConverted == count) {
+      *wcs = wc;
+      break;
+    }
+  }
+
+  return mbcConverted;
 }
