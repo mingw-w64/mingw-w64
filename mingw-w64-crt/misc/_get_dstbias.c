56ff5aaeacf93f0ea861658bb444a6433204aa0f
Pali Rohár <pali.rohar@gmail.com>
2025-07-13 12:56:19 +0200

    crt: Add emulation of _get_dstbias() function for pre-msvcr80 import libs
    Function _get_dstbias() is available in msvcr80+ and UCRT.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

---
+++ mingw-w64-crt/misc/_get_dstbias.c
@@ -0,0 +1,20 @@
+/**
+ * This file has no copyright assigned and is placed in the Public Domain.
+ * This file is part of the mingw-w64 runtime package.
+ * No warranty is given; refer to the file DISCLAIMER.PD within this package.
+ */
+#include <time.h>
+#include <errno.h>
+
+errno_t __cdecl _get_dstbias(long *value)
+{
+  if (!value)
+  {
+    errno = EINVAL;
+    return EINVAL;
+  }
+
+  *value = _dstbias;
+  return 0;
+}
+errno_t (__cdecl *__MINGW_IMP_SYMBOL(_get_dstbias))(long *) = _get_dstbias;
