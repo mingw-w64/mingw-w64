e4fcd8fd7556ff73f51c2770b1c67d70bcc13ce1
Pali Rohár <pali.rohar@gmail.com>
2025-08-15 23:28:50 +0200

    crt: Split mbrtowc.c into 3 files mbrlen.c mbrtowc.c and mbsrtowcs.c
    Put every function into its own file, so at the link time only used
    function are statically linked and not all of them.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

---
+++ mingw-w64-crt/misc/mbrlen.c
@@ -0,0 +1,20 @@
+/**
+ * This file has no copyright assigned and is placed in the Public Domain.
+ * This file is part of the mingw-w64 runtime package.
+ * No warranty is given; refer to the file DISCLAIMER.PD within this package.
+ */
+#include <wchar.h>
+
+size_t mbrlen (
+  const char *__restrict__ mbs,
+  size_t count,
+  mbstate_t *__restrict__ state
+) {
+  /* Use private `mbstate_t` if caller did not supply one */
+  if (state == NULL) {
+    static mbstate_t state_mbrlen = {0};
+    state = &state_mbrlen;
+  }
+  wchar_t wc = WEOF;
+  return mbrtowc (&wc, mbs, count, state);
+}
