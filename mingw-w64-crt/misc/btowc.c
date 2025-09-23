1b237405e72120b850d20177d9d498f0367454d3
Pali Rohár <pali.rohar@gmail.com>
2025-09-23 20:35:30 +0200

    crt: Remove duplicate files ucrt_btowc.c/ucrt_wctob.c and move btowc.c/wctob.c back to libmingwex
    Implementation of btowc.c and wctob.c is same for all CRTs except the
    mbstate_t structure. Define __LARGE_MBSTATE_T for btowc.c and wctob.c in
    the same way as it is used in mingw_pformat.c, mingw_sformat.c and
    mingw_swformat.c files to make code compatible with all CRT libs.
    
    This change allows to move btowc.c and wctob.c files back to libmingwex and
    compile them only once, like it was before commit 9d00799905c3 ("crt:
    always use replacements for btowc and wctob").
    
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- mingw-w64-crt/misc/btowc.c
+++ mingw-w64-crt/misc/btowc.c
@@ -3,6 +3,9 @@
  * This file is part of the mingw-w64 runtime package.
  * No warranty is given; refer to the file DISCLAIMER.PD within this package.
  */
+
+#define __LARGE_MBSTATE_T
+
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
 #endif
