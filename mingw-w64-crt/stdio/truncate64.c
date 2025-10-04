236027be4f57bb329dd7d87fcc87f1aaa1a06ac7
Pali Rohár <pali.rohar@gmail.com>
2025-10-05 01:26:30 +0200

    crt: Split truncate64 implementation into its own file
    With this change linker does not statically link 32-bit truncate() function
    when only 64-bit truncate64() is used.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- mingw-w64-crt/stdio/truncate64.c
+++ mingw-w64-crt/stdio/truncate64.c
@@ -2,11 +2,11 @@
 #include <fcntl.h>
 #include <errno.h>
 
-int truncate(const char *pathname, _off_t len){
+int truncate64(const char *pathname, _off64_t len){
   int ret, err;
   int fd = _open(pathname,_O_BINARY|_O_RDWR);
   if (fd == -1) return fd;
-  ret = ftruncate(fd,len);
+  ret = ftruncate64(fd,len);
   err = errno;
   _close(fd);
   errno = err;
