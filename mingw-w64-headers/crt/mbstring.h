fe72d8bb48eb0d1de6eaf479aa67989c2df5b7c9
Martin Storsjö <martin@martin.st>
2025-10-21 14:24:33 +0300

    headers: Make all definitions of "struct _iobuf" equivalent
    This applies the change from fe226dd9028f3f64bef4b4384edb1ef2c95c15f4
    to the copy of this struct definition in wchar.h and mbstring.h.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- mingw-w64-headers/crt/mbstring.h
+++ mingw-w64-headers/crt/mbstring.h
@@ -16,6 +16,9 @@ extern "C" {
 
 #ifndef _FILE_DEFINED
   struct _iobuf {
+#ifdef _UCRT
+    void *_Placeholder;
+#else
     char *_ptr;
     int _cnt;
     char *_base;
@@ -24,6 +27,7 @@ extern "C" {
     int _charbuf;
     int _bufsiz;
     char *_tmpfname;
+#endif
   };
   typedef struct _iobuf FILE;
 #define _FILE_DEFINED
