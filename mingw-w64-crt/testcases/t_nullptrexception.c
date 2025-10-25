eb5680dd25681b368211c72e9e0e1b10b0de4909
Pali Rohár <pali.rohar@gmail.com>
2025-10-25 17:45:37 +0200

    crt: Disable the program crash dialog for t_nullptrexception test
    Test t_nullptrexception is expected to crash.
    
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- mingw-w64-crt/testcases/t_nullptrexception.c
+++ mingw-w64-crt/testcases/t_nullptrexception.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <windows.h>
 
 char *p = NULL;
 
 int main()
 {
+  SetErrorMode(SetErrorMode(0) | SEM_NOGPFAULTERRORBOX);
   printf ("Raise uncaught NULL pointer exception...\n");
   *p = 0;
   return 0;
