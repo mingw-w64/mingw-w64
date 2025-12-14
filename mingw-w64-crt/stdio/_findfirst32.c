491de403da97d4dd2c0c29a43326e1b30e9b7e70
Pali Rohár <pali.rohar@gmail.com>
2025-12-14 11:50:57 +0100

    crt: Provide emulation of _(w)findfirst32 and _(w)findnext32 functions
    These 32-bit wide functions are missing only in 64-bit system OS msvcrt.dll
    libraries. Provide mingw-w64 emulation as a simple wrapper around 64-bit
    wide functions.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

---
+++ mingw-w64-crt/stdio/_findfirst32.c
@@ -0,0 +1,19 @@
+#include <io.h>
+#include <string.h>
+
+intptr_t __cdecl _findfirst32(const char *_Filename, struct _finddata32_t *_FindData)
+{
+  struct __finddata64_t fd;
+  intptr_t ret = _findfirst64(_Filename, &fd);
+  if (ret == -1)
+    return -1;
+  _FindData->attrib = fd.attrib;
+  _FindData->time_create = fd.time_create;
+  _FindData->time_access = fd.time_access;
+  _FindData->time_write = fd.time_write;
+  _FindData->size = (_fsize_t)fd.size;
+  _Static_assert(sizeof(_FindData->name) == sizeof(fd.name), "mismatch size of _FindData->name and fd.name");
+  memcpy(_FindData->name, fd.name, sizeof(_FindData->name));
+  return ret;
+}
+intptr_t (__cdecl *__MINGW_IMP_SYMBOL(_findfirst32))(const char *, struct _finddata32_t *) = _findfirst32;
---
+++ mingw-w64-crt/stdio/_findnext32.c
@@ -0,0 +1,19 @@
+#include <io.h>
+#include <string.h>
+
+int __cdecl _findnext32(intptr_t _FindHandle, struct _finddata32_t *_FindData)
+{
+  struct __finddata64_t fd;
+  int ret = _findnext64(_FindHandle, &fd);
+  if (ret != 0)
+    return ret;
+  _FindData->attrib = fd.attrib;
+  _FindData->time_create = fd.time_create;
+  _FindData->time_access = fd.time_access;
+  _FindData->time_write = fd.time_write;
+  _FindData->size = (_fsize_t)fd.size;
+  _Static_assert(sizeof(_FindData->name) == sizeof(fd.name), "mismatch size of _FindData->name and fd.name");
+  memcpy(_FindData->name, fd.name, sizeof(_FindData->name));
+  return 0;
+}
+int (__cdecl *__MINGW_IMP_SYMBOL(_findnext32))(intptr_t, struct _finddata32_t *) = _findnext32;
