de461cf488b86f6c91c35a5d0fe1976bc611462f
Pali Rohár <pali.rohar@gmail.com>
2025-12-14 12:18:46 +0100

    crt: test: t_fseeko64: Fix compile warning: pointer targets in passing argument 3 of ‘SetFilePointer’ differ in signedness
    Use the correct structure for passing 64-bit offset: LARGE_INTEGER.
    
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- mingw-w64-crt/testcases/t_fseeko64.c
+++ mingw-w64-crt/testcases/t_fseeko64.c
@@ -8,14 +8,11 @@ static const char *writebuf = "TESTVECTORSTRING";
 static char szPath[MAX_PATH];
 
 static int writefile(const char *path){
-  OVERLAPPED ov;
+  LARGE_INTEGER li = { .QuadPart = 0x100000000ull };
   DWORD dwResult;
-  memset(&ov,0,sizeof(OVERLAPPED));
-  ov.Offset = 0x0;
-  ov.OffsetHigh = 0x1;
   HANDLE fd = CreateFileA(path,GENERIC_READ | GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
   if (fd == INVALID_HANDLE_VALUE) return 1;
-  if (SetFilePointer(fd, ov.Offset, (PLONG) &ov.OffsetHigh, FILE_BEGIN) == INVALID_SET_FILE_POINTER && GetLastError() != NO_ERROR) {
+  if (SetFilePointer(fd, li.LowPart, &li.HighPart, FILE_BEGIN) == INVALID_SET_FILE_POINTER && GetLastError() != NO_ERROR) {
     printf("writefile: seek failed: winerror=%lu\n", GetLastError());
     CloseHandle(fd);
     return 1;
