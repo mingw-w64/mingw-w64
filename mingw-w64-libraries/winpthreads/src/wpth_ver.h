ffc3df43940c264dbb52d98d2e99ae18fd4a709d
Pali Rohár <pali.rohar@gmail.com>
2025-12-20 17:19:54 +0100

    winpthreads: Remove extra duplicate trailing nul bytes from strings in the PE DLL resource section
    Duplicate trailing nul bytes are not needed.
    
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- mingw-w64-libraries/winpthreads/src/wpth_ver.h
+++ mingw-w64-libraries/winpthreads/src/wpth_ver.h
@@ -24,7 +24,7 @@
 #define __WPTHREADS_VERSION__
 
 #define WPTH_VERSION 1,0,0,0
-#define WPTH_VERSION_STRING "1, 0, 0, 0\0"
+#define WPTH_VERSION_STRING "1, 0, 0, 0"
 #define WPTH_VERSION_MAJOR_STRING "1"
 
 #endif
