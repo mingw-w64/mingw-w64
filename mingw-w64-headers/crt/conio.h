9a3390922549c585be7ca17eb9a02a0ec4684232
Kirill Makurin <maiddaisuki@outlook.com>
2025-12-20 09:34:31 +0900

    headers: conio.h: reorder declarations
    Signed-off-by: Kirill Makurin <maiddaisuki@outlook.com>
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- mingw-w64-headers/crt/conio.h
+++ mingw-w64-headers/crt/conio.h
@@ -15,11 +15,33 @@
 extern "C" {
 #endif
 
+/**
+ * Functions to read/write strings from/to console.
+ */
+
   _CRTIMP char *__cdecl _cgets(char *_Buffer) __MINGW_ATTRIB_DEPRECATED_SEC_WARN;
   _CRTIMP int __cdecl _cputs(const char *_Str);
+
+/**
+ * Functions to read/write single characters from/to console.
+ */
+
+  _CRTIMP int __cdecl _kbhit(void);
   _CRTIMP int __cdecl _getch(void);
   _CRTIMP int __cdecl _getche(void);
-  _CRTIMP int __cdecl _kbhit(void);
+  _CRTIMP int __cdecl _putch(int _Ch);
+  _CRTIMP int __cdecl _ungetch(int _Ch);
+
+#if __MSVCRT_VERSION__ >= 0x800
+  _CRTIMP int __cdecl _getch_nolock(void);
+  _CRTIMP int __cdecl _getche_nolock(void);
+  _CRTIMP int __cdecl _putch_nolock(int _Ch);
+  _CRTIMP int __cdecl _ungetch_nolock(int _Ch);
+#endif
+
+/**
+ * Formatted console I/O functions.
+ */
 
 #ifdef _UCRT
   int __cdecl __conio_common_vcprintf(unsigned __int64 _Options, const char *_Format, _locale_t _Locale, va_list _ArgList);
@@ -60,7 +82,6 @@ extern "C" {
     __builtin_va_end(_ArgList);
     return _Ret;
   }
-
   __mingw_ovr int __cdecl _vcprintf_p(const char * __restrict__ _Format,va_list _ArgList)
   {
     return __conio_common_vcprintf_p(0, _Format, NULL, _ArgList);
@@ -100,19 +121,25 @@ extern "C" {
     __builtin_va_end(_ArgList);
     return _Ret;
   }
-#else
+#else /* !_UCRT */
   _CRTIMP int __cdecl _cprintf(const char * __restrict__ _Format,...);
+  _CRTIMP int __cdecl _cprintf_l(const char * __restrict__ _Format,_locale_t _Locale,...);
+  _CRTIMP int __cdecl _cprintf_p(const char * __restrict__ _Format,...);
+  _CRTIMP int __cdecl _cprintf_p_l(const char * __restrict__ _Format,_locale_t _Locale,...);
+  _CRTIMP int __cdecl _vcprintf(const char * __restrict__ _Format,va_list _ArgList);
+  _CRTIMP int __cdecl _vcprintf_l(const char * __restrict__ _Format,_locale_t _Locale,va_list _ArgList);
+  _CRTIMP int __cdecl _vcprintf_p(const char * __restrict__ _Format,va_list _ArgList);
+  _CRTIMP int __cdecl _vcprintf_p_l(const char * __restrict__ _Format,_locale_t _Locale,va_list _ArgList);
   _CRTIMP int __cdecl _cscanf(const char * __restrict__ _Format,...) __MINGW_ATTRIB_DEPRECATED_SEC_WARN;
   _CRTIMP int __cdecl _cscanf_l(const char * __restrict__ _Format,_locale_t _Locale,...) __MINGW_ATTRIB_DEPRECATED_SEC_WARN;
+#endif /* _UCRT */
 
-  _CRTIMP int __cdecl _vcprintf(const char * __restrict__ _Format,va_list _ArgList);
-  _CRTIMP int __cdecl _cprintf_p(const char * __restrict__ _Format,...);
-  _CRTIMP int __cdecl _vcprintf_p(const char * __restrict__ _Format,va_list _ArgList);
-  _CRTIMP int __cdecl _cprintf_l(const char * __restrict__ _Format,_locale_t _Locale,...);
-  _CRTIMP int __cdecl _vcprintf_l(const char * __restrict__ _Format,_locale_t _Locale,va_list _ArgList);
-  _CRTIMP int __cdecl _cprintf_p_l(const char * __restrict__ _Format,_locale_t _Locale,...);
-  _CRTIMP int __cdecl _vcprintf_p_l(const char * __restrict__ _Format,_locale_t _Locale,va_list _ArgList);
-#endif
+/**
+ * Port I/O functions.
+ *
+ * These functions were removed from CRT in msvcr80.dll.
+ * They are still available in i386 system versions of msvcrt.dll.
+ */
 
 #if defined(_X86_) && !defined(__x86_64)
   int __cdecl _inp(unsigned short);
@@ -121,15 +148,6 @@ extern "C" {
   int __cdecl _outp(unsigned short,int);
   unsigned short __cdecl _outpw(unsigned short,unsigned short);
   unsigned long __cdecl _outpd(unsigned short,unsigned long);
-#endif
-
-  _CRTIMP int __cdecl _putch(int _Ch);
-  _CRTIMP int __cdecl _ungetch(int _Ch);
-#if __MSVCRT_VERSION__ >= 0x800
-  _CRTIMP int __cdecl _getch_nolock(void);
-  _CRTIMP int __cdecl _getche_nolock(void);
-  _CRTIMP int __cdecl _putch_nolock(int _Ch);
-  _CRTIMP int __cdecl _ungetch_nolock(int _Ch);
 #endif
 
 #ifndef	NO_OLDNAMES
