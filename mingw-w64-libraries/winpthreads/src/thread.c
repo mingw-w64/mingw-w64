mingw-w64/mingw-w64

commit cb7f42e05b94cbb1d94edcebdbfb47b652a45484

winpthreads: Make winpthreads work on Win98
Signed-off-by: LIU Hao <lh_mouse@126.com>

mingw-w64-libraries/winpthreads/src/thread.c
@@ -74,6 +74,19 @@ SetThreadName_VEH (PEXCEPTION_POINTERS ExceptionInfo)
 
   return EXCEPTION_CONTINUE_SEARCH;
 }
+
+static PVOID (*AddVectoredExceptionHandlerFuncPtr) (ULONG, PVECTORED_EXCEPTION_HANDLER);
+static ULONG (*RemoveVectoredExceptionHandlerFuncPtr) (PVOID);
+
+static void __attribute__((constructor))
+ctor (void)
+{
+  HMODULE module = LoadLibrary("kernel32.dll");
+  if (module == NULL) return;
+
+  AddVectoredExceptionHandlerFuncPtr = (__typeof__(AddVectoredExceptionHandlerFuncPtr)) GetProcAddress(module, "AddVectoredExceptionHandler");
+  RemoveVectoredExceptionHandlerFuncPtr = (__typeof__(RemoveVectoredExceptionHandlerFuncPtr)) GetProcAddress(module, "RemoveVectoredExceptionHandler");
+}
 #endif
 
 typedef struct _THREADNAME_INFO
@@ -434,7 +447,8 @@ __dyn_tls_pthread (HANDLE hDllHandle, DWORD dwReason, LPVOID lpreserved)
 #if defined(USE_VEH_FOR_MSC_SETTHREADNAME)
       if (lpreserved == NULL && SetThreadName_VEH_handle != NULL)
         {
-          RemoveVectoredExceptionHandler (SetThreadName_VEH_handle);
+          if (RemoveVectoredExceptionHandlerFuncPtr != NULL)
+            RemoveVectoredExceptionHandlerFuncPtr (SetThreadName_VEH_handle);
           SetThreadName_VEH_handle = NULL;
         }
 #endif
@@ -443,7 +457,10 @@ __dyn_tls_pthread (HANDLE hDllHandle, DWORD dwReason, LPVOID lpreserved)
   else if (dwReason == DLL_PROCESS_ATTACH)
     {
 #if defined(USE_VEH_FOR_MSC_SETTHREADNAME)
-      SetThreadName_VEH_handle = AddVectoredExceptionHandler (1, &SetThreadName_VEH);
+      if (AddVectoredExceptionHandlerFuncPtr != NULL)
+        SetThreadName_VEH_handle = AddVectoredExceptionHandlerFuncPtr (1, &SetThreadName_VEH);
+      else
+        SetThreadName_VEH_handle = NULL;
       /* Can't do anything on error anyway, check for NULL later */
 #endif
     }

mingw-w64-libraries/winpthreads/src/misc.c
@@ -24,6 +24,16 @@
 #include "windows.h"
 #include "misc.h"
 
+static ULONGLONG (*GetTickCount64FuncPtr) (VOID);
+
+static void __attribute__((constructor)) ctor (void)
+{
+  HMODULE module = LoadLibrary("kernel32.dll");
+  if (module == NULL) return;
+
+  GetTickCount64FuncPtr = (__typeof__(GetTickCount64FuncPtr)) GetProcAddress(module, "GetTickCount64");
+}
+
 unsigned long long _pthread_time_in_ms(void)
 {
     FILETIME ft;
@@ -55,10 +65,9 @@ unsigned long long _pthread_rel_time_in_ms(const struct timespec *ts)
 static unsigned long long
 _pthread_get_tick_count (long long *frequency)
 {
-#if defined (_WIN32_WINNT) && (_WIN32_WINNT >= _WIN32_WINNT_VISTA)
-  (void) frequency; /* unused */
-  return GetTickCount64 ();
-#else
+  if (GetTickCount64FuncPtr != NULL)
+    return GetTickCount64FuncPtr ();
+
   LARGE_INTEGER freq, timestamp;
 
   if (*frequency == 0)
@@ -74,7 +83,6 @@ _pthread_get_tick_count (long long *frequency)
 
   /* Fallback */
   return GetTickCount ();
-#endif
 }
 
 /* A wrapper around WaitForSingleObject() that ensures that

commit 221f37239a50d9afc1dfffa6d8f325c6a75a0070

winpthreads: Fix usage of VectoredExceptionHandler
Currently AddVectoredExceptionHandlerFuncPtr and
RemoveVectoredExceptionHandlerFuncPtr are resolved by function named "ctor"
which is called only for GCC builds via __attribute__((constructor)).

Each function marked by this attribute is called from the DllMain()
function. DllMain() is called by DLL entry point function, and it is called
after executing all NT TLS callbacks.

Currenly the AddVectoredExceptionHandlerFuncPtr is used from the NT TLS
callback named __dyn_tls_pthread, hence it is always NULL and therefore
VectoredExceptionHandler is never used. This issue was introduced in the
commit cb7f42e05b94 ("winpthreads: Make winpthreads work on Win98").

Fix this problem and resolve AddVectoredExceptionHandler and
RemoveVectoredExceptionHandler pointers directly in the NT TLS callback
__dyn_tls_pthread, so they are available since beginning, and not just
after executing DllMain.

Signed-off-by: LIU Hao <lh_mouse@126.com>

mingw-w64-libraries/winpthreads/src/thread.c
@@ -94,19 +94,6 @@ SetThreadName_VEH (PEXCEPTION_POINTERS ExceptionInfo)
 
   return EXCEPTION_CONTINUE_SEARCH;
 }
-
-static PVOID (WINAPI *AddVectoredExceptionHandlerFuncPtr) (ULONG, PVECTORED_EXCEPTION_HANDLER);
-static ULONG (WINAPI *RemoveVectoredExceptionHandlerFuncPtr) (PVOID);
-
-static void __attribute__((constructor))
-ctor (void)
-{
-  HMODULE module = GetModuleHandleA("kernel32.dll");
-  if (module) {
-    AddVectoredExceptionHandlerFuncPtr = (__typeof__(AddVectoredExceptionHandlerFuncPtr)) GetProcAddress(module, "AddVectoredExceptionHandler");
-    RemoveVectoredExceptionHandlerFuncPtr = (__typeof__(RemoveVectoredExceptionHandlerFuncPtr)) GetProcAddress(module, "RemoveVectoredExceptionHandler");
-  }
-}
 #endif
 
 typedef struct _THREADNAME_INFO
@@ -447,16 +434,20 @@ replace_spin_keys (pthread_spinlock_t *old, pthread_spinlock_t new)
 static void WINAPI
 __dyn_tls_pthread (HANDLE hDllHandle, DWORD dwReason, LPVOID lpreserved)
 {
+#if defined(USE_VEH_FOR_MSC_SETTHREADNAME)
+  static PVOID (WINAPI *AddVectoredExceptionHandlerFuncPtr) (ULONG, PVOID) = NULL;
+  static ULONG (WINAPI *RemoveVectoredExceptionHandlerFuncPtr) (PVOID) = NULL;
+  static BOOL haveVectoredExceptionHandlerFuncs = FALSE;
+#endif
   _pthread_v *t = NULL;
   pthread_spinlock_t new_spin_keys = PTHREAD_SPINLOCK_INITIALIZER;
 
   if (dwReason == DLL_PROCESS_DETACH)
     {
 #if defined(USE_VEH_FOR_MSC_SETTHREADNAME)
-      if (lpreserved == NULL && SetThreadName_VEH_handle != NULL)
+      if (lpreserved == NULL && SetThreadName_VEH_handle != NULL && haveVectoredExceptionHandlerFuncs)
         {
-          if (RemoveVectoredExceptionHandlerFuncPtr != NULL)
-            RemoveVectoredExceptionHandlerFuncPtr (SetThreadName_VEH_handle);
+          RemoveVectoredExceptionHandlerFuncPtr (SetThreadName_VEH_handle);
           SetThreadName_VEH_handle = NULL;
         }
 #endif
@@ -465,10 +456,18 @@ __dyn_tls_pthread (HANDLE hDllHandle, DWORD dwReason, LPVOID lpreserved)
   else if (dwReason == DLL_PROCESS_ATTACH)
     {
 #if defined(USE_VEH_FOR_MSC_SETTHREADNAME)
-      if (AddVectoredExceptionHandlerFuncPtr != NULL)
+      if (!haveVectoredExceptionHandlerFuncs)
+        {
+          HMODULE kernel32 = GetModuleHandleA("kernel32.dll");
+          if (kernel32)
+            {
+              AddVectoredExceptionHandlerFuncPtr = (PVOID (WINAPI *)(ULONG, PVOID))(void(*)(void)) GetProcAddress(kernel32, "AddVectoredExceptionHandler");
+              RemoveVectoredExceptionHandlerFuncPtr = (ULONG (WINAPI *)(PVOID))(void(*)(void)) GetProcAddress(kernel32, "RemoveVectoredExceptionHandler");
+              haveVectoredExceptionHandlerFuncs = AddVectoredExceptionHandlerFuncPtr != NULL && RemoveVectoredExceptionHandlerFuncPtr != NULL;
+            }
+        }
+      if (haveVectoredExceptionHandlerFuncs)
         SetThreadName_VEH_handle = AddVectoredExceptionHandlerFuncPtr (1, &SetThreadName_VEH);
-      else
-        SetThreadName_VEH_handle = NULL;
       /* Can't do anything on error anyway, check for NULL later */
 #endif
     }