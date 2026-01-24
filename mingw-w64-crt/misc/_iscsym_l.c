160c80f1ec74d40494178fab0cdcbd321b2f2dc4
Kirill Makurin <maiddaisuki@outlook.com>
2026-01-24 18:39:58 +0900

    crt: provide _iscsym[f]_l functions as externals
    _iscsym_l and _iscsymf_l are only available as macros in Microsoft headers.
    
    These macros are not exposed under the following conditions:
    
     - when _CTYPE_DISABLE_MACROS is defined; this is what Microsoft headers do
     - for C++ compilations; this avoids possible clashes with class member names
    
    This will result in compilation error with C++ or if _CTYPE_DISABLE_MACROS
    macro is defined. To avoid this, provide both functions as externals.
    
    Signed-off-by: Kirill Makurin <maiddaisuki@outlook.com>
    Signed-off-by: LIU Hao <lh_mouse@126.com>

---
+++ mingw-w64-crt/misc/_iscsym_l.c
@@ -0,0 +1,22 @@
+/**
+ * This file has no copyright assigned and is placed in the Public Domain.
+ * This file is part of the mingw-w64 runtime package.
+ * No warranty is given; refer to the file DISCLAIMER.PD within this package.
+ */
+
+#undef __MSVCRT_VERSION__
+#define __MSVCRT_VERSION__ 0x0800
+
+#define _CTYPE_DISABLE_MACROS
+#include <ctype.h>
+
+/**
+ * See ctype.h for rationale.
+ *
+ * Note that import symbol __MINGW_IMP_SYMBOL(_iscsym_l) is not provided on
+ * purpose.
+ */
+
+int __cdecl _iscsym_l (wint_t _C, _locale_t _Locale) {
+  return (_isalnum_l (_C, _Locale) || _C == '_');
+}
--- mingw-w64-headers/crt/ctype.h
+++ mingw-w64-headers/crt/ctype.h
@@ -152,14 +152,29 @@ _CRTIMP int __cdecl iscsymf(int _C) __MINGW_ATTRIB_DEPRECATED_MSVC2005;
  * They are available since msvcr80.dll.
  */
 
-/* These are also available in msvcrt.dll since Windows Vista. */
-#if __MSVCRT_VERSION__ >= 0x0800 || (__MSVCRT_VERSION__ == 0x0600 && _WIN32_WINNT >= 0x0600)
-_CRTIMP int __cdecl _isctype_l(int _C,int _Type,_locale_t _Locale);
+/**
+ * _iscsym_l and _iscsymf_l are only available as macros in Microsoft headers.
+ *
+ * We do not expose those macros under following conditions:
+ *
+ * - when _CTYPE_DISABLE_MACROS is defined; this is what Microsoft headers do
+ * - for C++ compilations; this avoids possible clashes with class member names
+ *
+ * To make them available under above conditions, we provide them as externals.
+ */
+#if __MSVCRT_VERSION__ >= 0x0800
+int __cdecl _iscsym_l(wint_t _C, _locale_t _Locale);
+int __cdecl _iscsymf_l(wint_t _C, _locale_t _Locale);
 
 #if !defined(_CTYPE_DISABLE_MACROS) && !defined(__cplusplus)
 #define _iscsym_l(_c,_p)  (_isalnum_l(_c,_p) || ((_c)=='_'))
 #define _iscsymf_l(_c,_p) (_isalpha_l(_c,_p) || ((_c)=='_'))
 #endif /* !_CTYPE_DISABLE_MACROS && !__cplusplus */
+#endif
+
+/* These are also available in msvcrt.dll since Windows Vista. */
+#if __MSVCRT_VERSION__ >= 0x0800 || (__MSVCRT_VERSION__ == 0x0600 && _WIN32_WINNT >= 0x0600)
+_CRTIMP int __cdecl _isctype_l(int _C,int _Type,_locale_t _Locale);
 #endif
 
 #ifdef __cplusplus
