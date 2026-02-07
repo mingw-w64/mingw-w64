05c73542b50cd6f9a137e4ac6ee1455fff427190
Kirill Makurin <maiddaisuki@outlook.com>
2026-02-07 11:05:10 +0900

    crt: isblank.c: document _isctype ('\t', _BLANK) behavior among CRTs
    Signed-off-by: Kirill Makurin <maiddaisuki@outlook.com>
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- mingw-w64-crt/misc/isblank.c
+++ mingw-w64-crt/misc/isblank.c
@@ -1,6 +1,13 @@
 #define _CTYPE_DISABLE_MACROS
 #include <ctype.h>
 
+/**
+ * Calls to `_isctype ('\t', _BLANK)` return inconsistent results
+ * depending on CRT and active locale.
+ *
+ * In all CRTs it returns zero in "C" locale and non-zero otherwise.
+ */
+
 int __cdecl isblank (int _C)
 {
   return (_isctype(_C, _BLANK) || _C == '\t');
