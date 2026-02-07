4c2e9e65e1e78af6f79f2cc8325d1fb0060a25a2
Kirill Makurin <maiddaisuki@outlook.com>
2026-02-07 11:03:27 +0900

    crt: tidy-up wctype.c
    Fix inconsistent use of tabs and spaces in the comments.
    Remove trailing whitespace.
    
    Signed-off-by: Kirill Makurin <maiddaisuki@outlook.com>
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- mingw-w64-crt/misc/wctype.c
+++ mingw-w64-crt/misc/wctype.c
@@ -8,10 +8,10 @@
   7.25.2.2.2 The wctype function
 
   Contributed by: Danny Smith  <dannysmith@usesr.sourcefoge.net>
-		  2005-02-24
-   
+  2005-02-24
+
   This source code is placed in the PUBLIC DOMAIN. It is modified
-  from the Q8 package created by Doug Gwyn <gwyn@arl.mil>  
+  from the Q8 package created by Doug Gwyn <gwyn@arl.mil>
 
   The wctype function constructs a value with type wctype_t that
   describes a class of wide characters identified by the string
@@ -19,7 +19,7 @@
 
   In particular, we map the property strings so that:
 
-  iswctype(wc, wctype("alnum")) == iswalnum(wc) 
+  iswctype(wc, wctype("alnum")) == iswalnum(wc)
   iswctype(wc, wctype("alpha")) == iswalpha(wc)
   iswctype(wc, wctype("cntrl")) == iswcntrl(wc)
   iswctype(wc, wctype("digit")) == iswdigit(wc)
@@ -30,35 +30,35 @@
   iswctype(wc, wctype("space")) == iswspace(wc)
   iswctype(wc, wctype("upper")) == iswupper(wc)
   iswctype(wc, wctype("xdigit")) == iswxdigit(wc)
-
 */
 
-#include	<string.h>
-#include	<wctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include <wctype.h>
 
 /* Using the bit-OR'd ctype character classification flags as return
    values achieves compatibility with MS iswctype().  */
 static const struct {
   const char *name;
-  wctype_t flags;} cmap[] = {
-    {"alnum", _ALPHA|_DIGIT},
-    {"alpha", _ALPHA},
-    {"cntrl", _CONTROL},
-    {"digit", _DIGIT},
-    {"graph", _PUNCT|_ALPHA|_DIGIT},
-    {"lower", _LOWER},
-    {"print", _BLANK|_PUNCT|_ALPHA|_DIGIT},
-    {"punct", _PUNCT},
-    {"space", _SPACE},
-    {"upper", _UPPER},
-    {"xdigit", _HEX}
-  };
+  wctype_t flags;
+} cmap[] = {
+  {"alnum",  _ALPHA|_DIGIT},
+  {"alpha",  _ALPHA},
+  {"cntrl",  _CONTROL},
+  {"digit",  _DIGIT},
+  {"graph",  _ALPHA|_DIGIT|_PUNCT},
+  {"lower",  _LOWER},
+  {"print",  _ALPHA|_BLANK|_DIGIT|_PUNCT},
+  {"punct",  _PUNCT},
+  {"space",  _SPACE},
+  {"upper",  _UPPER},
+  {"xdigit", _HEX}
+};
 
-#define NCMAP	(sizeof cmap / sizeof cmap[0])
 wctype_t wctype (const char *property)
 {
   int i;
-  for (i = 0; i < (int) NCMAP; ++i)
+  for (i = 0; i < (int) _countof (cmap); ++i)
     if (strcmp (property, cmap[i].name) == 0)
       return cmap[i].flags;
   return 0;
