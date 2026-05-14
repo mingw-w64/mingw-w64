ef1e01e46574c23f07bbc5963ecbe9f8bacb1a70
Pali Rohár <pali.rohar@gmail.com>
2026-05-14 21:00:12 +0200

    crt: Fix __mingw_has_sse() for pre-Win2k systems
    Function __mingw_has_sse() currently can return true on systems without
    SSE support. Executing SSE instructions then cause process crash.
    
    Problem happens when CPU itself has SSE support, but CR4.OSFXSR is not
    enabled by system. In this cases SSE is disabled and cannot be used.
    
    This can be easily triggered in emulators (e.g. with -cpu qemu32) on
    Windows 9x and Windows NT 3.x/4.x systems which either do not support SSE
    at all or contain whitelist of CPUs for which they enable CR4.OSFXSR.
    
    Improve __mingw_has_sse() function to check that the SSE instruction can be
    executed.
    
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- mingw-w64-crt/misc/__mingw_has_sse.c
+++ mingw-w64-crt/misc/__mingw_has_sse.c
@@ -5,6 +5,36 @@
  */
 
 #if defined(__i386__)
+#include <windows.h>
+
+struct illegal_instruction_establisher_frame {
+  EXCEPTION_REGISTRATION_RECORD exception_record;
+  BOOL illegal_instruction_occured;
+};
+
+#if defined(__i386__)
+/* We need to make sure that we align the stack to 16 bytes for the sake of SSE */
+__attribute__((force_align_arg_pointer))
+#endif
+static EXCEPTION_DISPOSITION __cdecl
+illegal_instruction_handler(EXCEPTION_RECORD *ExceptionRecord,
+                            PVOID EstablisherFrame,
+                            CONTEXT *ContextRecord,
+                            PVOID DispatcherContext __attribute__ ((unused)))
+{
+
+  if (!(ExceptionRecord->ExceptionFlags & EXCEPTION_UNWINDING) &&
+      !(ExceptionRecord->ExceptionFlags & EXCEPTION_NONCONTINUABLE) &&
+      ExceptionRecord->ExceptionCode == STATUS_ILLEGAL_INSTRUCTION)
+    {
+      ((struct illegal_instruction_establisher_frame *)EstablisherFrame)->illegal_instruction_occured = TRUE;
+      ContextRecord->Eip += 3; /* skip "movaps %%xmm0, %%xmm0" */
+      return ExceptionContinueExecution;
+    }
+
+  return ExceptionContinueSearch;
+}
+
 int __mingw_has_sse (void);
 static int __has_sse = -1;
 
@@ -31,7 +61,38 @@ int __mingw_has_sse(void)
     : "=a" (cpuInfo[0]), "=b" (cpuInfo[1]), "=c" (cpuInfo[2]),
     "=d" (cpuInfo[3])
     : "a" (infoType));
-  __has_sse = (cpuInfo[3] & 0x2000000);
-  return __has_sse;
+  if (!(cpuInfo[3] & 0x2000000))
+    {
+      __has_sse = 0;
+      return 0;
+    }
+
+  /* Pre-Win2k systems do not have to enable CR4.OSFXSR and then
+   * SSE instructions cannot be used even when CPU has SSE support.
+   * So these systems has to be marked as without SSE support.
+   * Try to execute SSE nop "movaps %%xmm0, %%xmm0" and check if system
+   * throws SEH exception STATUS_ILLEGAL_INSTRUCTION or not.
+   * Note that IsProcessorFeaturePresent(PF_XMMI_INSTRUCTIONS_AVAILABLE)
+   * cannot be used as PF_XMMI_INSTRUCTIONS_AVAILABLE flag is not returned
+   * on pre-Win2k systems, even when system properly enabled CR4.OSFXSR.
+   */
+  struct illegal_instruction_establisher_frame establisher_frame = {
+    .exception_record = {
+      .Next = (EXCEPTION_REGISTRATION_RECORD *)__readfsdword (0),
+      .Handler = (PEXCEPTION_ROUTINE)(INT_PTR)illegal_instruction_handler,
+    },
+    .illegal_instruction_occured = FALSE,
+  };
+  __writefsdword (0, (DWORD)&establisher_frame);
+  __asm__ volatile ("movaps %%xmm0, %%xmm0" :);
+  __writefsdword (0, (DWORD)establisher_frame.exception_record.Next);
+  if (establisher_frame.illegal_instruction_occured)
+    {
+      __has_sse = 0;
+      return 0;
+    }
+
+  __has_sse = 1;
+  return 1;
 }
 #endif  /* __i386__ */
