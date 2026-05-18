8924640741acd05d0580a6addf6c1c4b6e24f812
Pali Rohár <pali.rohar@gmail.com>
2026-05-18 19:15:56 +0200

    crt: test: Run t_excpt_int_overflow also from new thread spawned by _beginthreadex()
    Handling of exceptions for main thread is different than for threads
    spawned by CRT _beginthreadex() function. So this new test verifies that
    behavior is same for main thread and non-main CRT threads.
    
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- mingw-w64-crt/testcases/t_excpt_int_overflow.c
+++ mingw-w64-crt/testcases/t_excpt_int_overflow.c
@@ -1,20 +1,45 @@
 #include <stdio.h>
 #include <limits.h>
+#include <process.h>
 #include <windows.h>
+
+/* This test is run as XFAIL, so it has to return non-zero on success (as the crash is expected) and zero on error */
+#define FAILURE 0
+
+#if defined(__i386__)
+/* We need to make sure that we align the stack to 16 bytes for the sake of SSE */
+__attribute__((force_align_arg_pointer))
+#endif
+static unsigned __stdcall test(void *arg __attribute__((unused)))
+{
+  puts("execute: INT_MIN / -1");
+  volatile int a = INT_MIN;
+  volatile int b = -1;
+  volatile int c = a / b;
+  printf("result: FAILED, program continued, result is: %d\n", c);
+  return FAILURE;
+}
 
 int main(void)
 {
 #if defined(__arm__) || defined(__aarch64__) || defined(__arm64ec__)
   puts("Division overflow is ignored on ARM");
   return 77;
-#else
+#endif
   SetErrorMode(SetErrorMode(0) | SEM_NOGPFAULTERRORBOX);
-  puts("execute: INT_MIN / -1");
-  volatile int a = INT_MIN;
-  volatile int b = -1;
-  volatile int c = a / b;
-  (void)c;
-  puts("result: FAILED, program continued");
-  return 0;
+#ifdef RUN_TEST_IN_THREAD
+  HANDLE thread = (HANDLE)_beginthreadex(NULL, 0, test, NULL, 0, &(unsigned){0} /*out: ThreadId*/);
+  if (!thread)
+  {
+    puts("failed to spawn thread");
+    return FAILURE;
+  }
+  DWORD ret;
+  WaitForSingleObject(thread, INFINITE);
+  GetExitCodeThread(thread, &ret);
+  CloseHandle(thread);
+  return ret;
+#else
+  return test(NULL);
 #endif
 }
