00002c87f7c1bc14ef92c2e66684d36a9238f6c6
Pali Rohár <pali.rohar@gmail.com>
2026-05-18 19:55:09 +0200

    headers: Remove definition of struct threadlocaleinfostruct and threadlocinfo
    Current definition matches only ABI of msvcr80, msvcr90 and msvcr100 DLLs.
    It is not compatible with older or new msvcrt version and probably is also
    not compatible with system os version of msvcrt. So usage of threadlocinfo
    and struct threadlocaleinfostruct in most cases cause unpredictable results.
    
    mingw-w64 does not dereference struct threadlocaleinfostruct or threadlocinfo
    anymore, so definition can be safely removed from header files. Last usage
    was removed in commit e705546e5ca0 ("crt: ctype.h: remove macro versions of
    locale-specific functions").
    
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- mingw-w64-headers/crt/corecrt.h
+++ mingw-w64-headers/crt/corecrt.h
@@ -454,41 +454,6 @@ typedef struct tagLC_ID {
 } LC_ID,*LPLC_ID;
 #endif /* _TAGLC_ID_DEFINED */
 
-#ifndef _THREADLOCALEINFO
-#define _THREADLOCALEINFO
-typedef struct threadlocaleinfostruct {
-#ifdef _UCRT
-  const unsigned short *_locale_pctype;
-  int _locale_mb_cur_max;
-  unsigned int _locale_lc_codepage;
-#else
-  int refcount;
-  unsigned int lc_codepage;
-  unsigned int lc_collate_cp;
-  unsigned long lc_handle[6];
-  LC_ID lc_id[6];
-  struct {
-    char *locale;
-    wchar_t *wlocale;
-    int *refcount;
-    int *wrefcount;
-  } lc_category[6];
-  int lc_clike;
-  int mb_cur_max;
-  int *lconv_intl_refcount;
-  int *lconv_num_refcount;
-  int *lconv_mon_refcount;
-  struct lconv *lconv;
-  int *ctype1_refcount;
-  unsigned short *ctype1;
-  const unsigned short *pctype;
-  const unsigned char *pclmap;
-  const unsigned char *pcumap;
-  struct __lc_time_data *lc_time_curr;
-#endif
-} threadlocinfo;
-#endif /* _THREADLOCALEINFO */
-
 #ifndef __crt_typefix
 #define __crt_typefix(ctype)
 #endif
