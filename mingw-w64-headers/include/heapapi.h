734e01d475c1caec5019d8b8eecd528524ff962a
Kirill Makurin <maiddaisuki@outlook.com>
2026-05-21 12:53:49 +0900

    headers: heapapi.h: move declarations of HEAP_SUMMARY and HeapSummary under WINAPI_PARTITION_APP
    Recent Microsoft headers guard declarations of `HeapSummary` function and
    `HeapSummary` structure under `WINAPI_PARTITION_APP`.
    
    Signed-off-by: Kirill Makurin <maiddaisuki@outlook.com>
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- mingw-w64-headers/include/heapapi.h
+++ mingw-w64-headers/include/heapapi.h
@@ -15,18 +15,7 @@ extern "C" {
 #endif
 
 #if WINAPI_FAMILY_PARTITION (WINAPI_PARTITION_DESKTOP)
-  typedef struct _HEAP_SUMMARY {
-    DWORD cb;
-    SIZE_T cbAllocated;
-    SIZE_T cbCommitted;
-    SIZE_T cbReserved;
-    SIZE_T cbMaxReserve;
-  } HEAP_SUMMARY,*PHEAP_SUMMARY;
-
-  typedef PHEAP_SUMMARY LPHEAP_SUMMARY;
-
   WINBASEAPI WINBOOL WINAPI HeapValidate (HANDLE hHeap, DWORD dwFlags, LPCVOID lpMem);
-  WINBOOL WINAPI HeapSummary (HANDLE hHeap, DWORD dwFlags, LPHEAP_SUMMARY lpSummary);
 #endif
 
 #if WINAPI_FAMILY_PARTITION (WINAPI_PARTITION_DESKTOP) || NTDDI_VERSION >= NTDDI_WIN10_19H1
@@ -41,6 +30,17 @@ extern "C" {
 #endif
 
 #if WINAPI_FAMILY_PARTITION (WINAPI_PARTITION_APP)
+  typedef struct _HEAP_SUMMARY {
+    DWORD cb;
+    SIZE_T cbAllocated;
+    SIZE_T cbCommitted;
+    SIZE_T cbReserved;
+    SIZE_T cbMaxReserve;
+  } HEAP_SUMMARY,*PHEAP_SUMMARY;
+
+  typedef PHEAP_SUMMARY LPHEAP_SUMMARY;
+
+  WINBOOL WINAPI HeapSummary (HANDLE hHeap, DWORD dwFlags, LPHEAP_SUMMARY lpSummary);
   WINBASEAPI HANDLE WINAPI HeapCreate (DWORD flOptions, SIZE_T dwInitialSize, SIZE_T dwMaximumSize);
   WINBASEAPI SIZE_T WINAPI HeapCompact (HANDLE hHeap, DWORD dwFlags);
   WINBASEAPI WINBOOL WINAPI HeapDestroy (HANDLE hHeap);
