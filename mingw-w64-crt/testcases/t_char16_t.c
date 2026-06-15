77e0c246cb18fb82735118b14e3013e9cc609186
Kirill Makurin <maiddaisuki@outlook.com>
2026-06-16 08:45:20 +0900

    crt: tests: update t_char16_t
    Use runtime check rather than compilation time check; the latter, in case of
    compilation error, would prevent the rest of tests from running.
    
    Add t_char16_t to testcases/Makefile.am.
    
    Signed-off-by: Kirill Makurin <maiddaisuki@outlook.com>
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- mingw-w64-crt/testcases/t_char16_t.c
+++ mingw-w64-crt/testcases/t_char16_t.c
@@ -1,18 +1,11 @@
-#define COMPILETIME_CHECK
-
-#if !defined(COMPILETIME_CHECK)
 #include <stdlib.h>
-#endif
 #include <uchar.h>
 
 int main (void)
 {
-/* wchar_t should compatible to char16_t on Windows */
-#if defined(COMPILETIME_CHECK)
-typedef int dummy[2 * (sizeof(wchar_t) == sizeof(char16_t)) - 1];
-#else /*if defined(RUNTIME_CHECK)*/
+	/* wchar_t should be compatible with char16_t on Windows */
 	if (sizeof(wchar_t) != sizeof(char16_t))
 		abort ();
-#endif
+
 	return 0;
 }
