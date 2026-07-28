4ee8c51ce2b834f2868ec9f3e90f5f88872a56b2
Pali Rohár <pali.rohar@gmail.com>
2026-07-28 21:41:31 +0200

    crt: tls: Add workaround for GNU LD --gc-sections bug which erase .tls section
    Currently all mingw-w64-crt/crt files which are using TLS callbacks are
    referencing the _tls_used variable which is used by linker for .tls section
    via following code pattern in global scope:
    
      extern const IMAGE_TLS_DIRECTORY _tls_used;
      static __attribute__((used)) const IMAGE_TLS_DIRECTORY *const _include_tls_used = &_tls_used;
    
    This is working fine except when the GNU LD --gc-sections is used. Due to
    GNU LD bug https://sourceware.org/pipermail/binutils/2026-July/150496.html
    the _tls_used variable is removed and its pointer replaced by some bogus
    value.
    
    To workaround this bug, it is needed to reference the _tls_used variable
    inside functions which will not be garbage-collected. Here we use static
    __attribute__((destructor)) functions which meet this requirement. The new
    code pattern in global scope looks like:
    
      extern const IMAGE_TLS_DIRECTORY _tls_used;
      static __attribute__((destructor)) void _include_tls_used(void) { asm volatile ("" :: "r" (&_tls_used)); }
    
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- mingw-w64-crt/crt/tlsthrd_register.c
+++ mingw-w64-crt/crt/tlsthrd_register.c
@@ -16,4 +16,4 @@ static _CRTALLOC(".CRT$XLD") PIMAGE_TLS_CALLBACK __mingw_TLScallback_ptr = __min
 
 /* Force tlssup.c (_tls_used symbol for .tls linker section) to be linked.  */
 extern const IMAGE_TLS_DIRECTORY _tls_used;
-static __attribute__((used)) const IMAGE_TLS_DIRECTORY *const _include_tls_used = &_tls_used;
+static __attribute__((destructor)) void _include_tls_used(void) { asm volatile ("" :: "r" (&_tls_used)); }
