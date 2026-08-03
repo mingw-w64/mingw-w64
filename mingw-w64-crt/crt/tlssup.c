cebb62aee3be70491b1cd48835230c00206cab34
LIU Hao <lh_mouse@126.com>
2026-08-03 13:13:52 +0800

    crt/tlssup: Retain `_tls_used` for code that uses `_tls_index`
    This is achieved by allocating it in `.CRT$XLZZ` which is marked `KEEP` in
    builtin linker scripts of LD.
    
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- mingw-w64-crt/crt/tlssup.c
+++ mingw-w64-crt/crt/tlssup.c
@@ -15,7 +15,7 @@
 __attribute__((used))
 ULONG _tls_index = -1; /* -1 as uninitialized value */
 
-/* TLS raw template data start and end. 
+/* TLS raw template data start and end.
    We use here pointer-types for start/end so that tls-data remains
    aligned on pointer-size-width.  This seems to be required for
    pe-loader. */
@@ -25,8 +25,10 @@ _CRTALLOC(".tls$ZZZ") char *_tls_end = NULL;
 _CRTALLOC(".CRT$XLA") PIMAGE_TLS_CALLBACK __xl_a = 0;
 _CRTALLOC(".CRT$XLZ") PIMAGE_TLS_CALLBACK __xl_z = 0;
 
-__attribute__((used))
-const IMAGE_TLS_DIRECTORY _tls_used = {
+/* This is placed next to `__xl_z` so it will not be garbage-collected by the
+   linker. The linker should have retained `_tls_used` but it's only fixed in
+   Binutils 2.48.  */
+_CRTALLOC(".CRT$XLZZ") const IMAGE_TLS_DIRECTORY _tls_used = {
   (ULONG_PTR) &_tls_start, (ULONG_PTR) &_tls_end,
   (ULONG_PTR) &_tls_index, (ULONG_PTR) (&__xl_a+1),
   (ULONG) 0, (ULONG) 0
