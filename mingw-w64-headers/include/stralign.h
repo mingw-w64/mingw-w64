65eb97ee87d9a90da685f4e80ace89e0e5e0c23f
Zhongteng Gui <dragon-archer@outlook.com>
2026-08-27 15:34:27 +0800

    headers: merge stralign_s.h into stralign.h
    stralign_s.h is only used by stralign.h, and it's actually not part of
    CRT headers. Plus, stralign_s.h is not provided by MSVC UCRT, so it can
    be dropped safely.
    
    Note that our stralign.h differs from MSVC UCRT's stralign.h a lot,
    which may need further adjustments.
    
    Also updated configure{,.ac} to remove stralign_s.h.
    
    Signed-off-by: Zhongteng Gui <dragon-archer@outlook.com>
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- mingw-w64-headers/include/stralign.h
+++ mingw-w64-headers/include/stralign.h
@@ -198,9 +198,17 @@ extern "C" {
 #define ua_tcscpy strcpy
 #endif
 
+#if !defined(_X86_) && defined(_WSTRING_S_DEFINED)
+#if defined(__cplusplus) && defined(_WConst_Return)
+  static __inline PUWSTR ua_wcscpy_s(PUWSTR Destination,size_t DestinationSize,PCUWSTR Source) {
+    if(WSTR_ALIGNED(Source) && WSTR_ALIGNED(Destination)) return (wcscpy_s((PWSTR)Destination,DestinationSize,(PCWSTR)Source)==0 ? Destination : NULL);
+    return uaw_wcscpy((PCUWSTR)String,Character);
+  }
+#endif
+#endif
+
 #ifdef __cplusplus
 }
 #endif
 
-#include <sec_api/stralign_s.h>
 #endif
