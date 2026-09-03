772388c59556d3f347714d7ff100bfa775570388
Zhongteng Gui <dragon-archer@outlook.com>
2026-09-03 22:09:48 +0800

    headers: crt: Merge swprintf.inl into corecrt_wstdio.h
    It's not referenced elsewhere, and it doesn't contain any inline
    definitions now.
    
    Accoredingly updated configure.ac and Makefile.am.
    
    Signed-off-by: Zhongteng Gui <dragon-archer@outlook.com>
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- mingw-w64-headers/crt/corecrt_wstdio.h
+++ mingw-w64-headers/crt/corecrt_wstdio.h
@@ -600,7 +600,26 @@ int vsnwprintf (wchar_t *__stream, size_t __n, const wchar_t *__format, __builti
 #endif /* !_UCRT */
 
 #ifndef RC_INVOKED
-#include <swprintf.inl>
+
+#ifdef __cplusplus
+
+extern "C++" {
+
+/* __attribute__((__format__ (gnu_wprintf, 2, 0))) */ __MINGW_ATTRIB_NONNULL(2)
+int vswprintf (wchar_t *__stream, const wchar_t *__format, __builtin_va_list __local_argv) __MINGW_ASM_CALL(_vswprintf);
+
+/* __attribute__((__format__ (gnu_wprintf, 2, 3))) */ __MINGW_ATTRIB_NONNULL(2)
+int swprintf (wchar_t *__stream, const wchar_t *__format, ...) __MINGW_ASM_CALL(_swprintf);
+
+}
+
+#elif defined(_CRT_NON_CONFORMING_SWPRINTFS)
+
+#define swprintf _swprintf
+#define vswprintf _vswprintf
+
+#endif /* __cplusplus */
+
 #endif
 
 #ifdef _CRT_NON_CONFORMING_SWPRINTFS
