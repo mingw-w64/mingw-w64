4f820ac4c1ec18b9e5a0c69df92604679c236ef5
Zhongteng Gui <dragon-archer@outlook.com>
2026-09-04 22:25:14 +0800

    crt: Fix __mingw_get_crt_info and remove broken revstamp.h
    revstamp.h isn't autogenrated and is broken (and likely have been broken
    since migrating from SVN to Git), so remove it.
    
    Use __DATE__ and __TIME__ instead of __MINGW_W64_REV_STAMP in
    __mingw_get_crt_info.
    
    Accordingly changes Makefile.am.
    
    Signed-off-by: Zhongteng Gui <dragon-archer@outlook.com>
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- mingw-w64-crt/crt/mingw_custom.c
+++ mingw-w64-crt/crt/mingw_custom.c
@@ -5,12 +5,10 @@
  */
 
 #include <_mingw.h>
-#include "../revstamp.h"
 
 const char *__mingw_get_crt_info (void)
 {
-  return "MinGW-W64 Runtime " __MINGW64_VERSION_STR " ("
-         __MINGW64_VERSION_STATE " - "
-	 "rev. " __MINGW_W64_REV ") " __MINGW_W64_REV_STAMP;
+  return "MinGW-w64 Runtime " __MINGW64_VERSION_STR " ("
+         __MINGW64_VERSION_STATE ") " __DATE__ " " __TIME__;
 }
 
