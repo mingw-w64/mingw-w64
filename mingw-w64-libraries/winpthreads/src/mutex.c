00e208efaf34af1bbf5c6ec27d1adff7dc9f552e
Kirill Makurin <maiddaisuki@outlook.com>
2026-09-11 17:22:23 +0900

    Revert "Add code to tolerate sloppy code of libgomp."
    This reverts commit 628fdbff240760b0de3167cf1d197d448284606c.
    
    Signed-off-by: Kirill Makurin <maiddaisuki@outlook.com>
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- mingw-w64-libraries/winpthreads/src/mutex.c
+++ mingw-w64-libraries/winpthreads/src/mutex.c
@@ -65,13 +65,8 @@ typedef struct {
    a mutex_impl_t). */
 static BOOL
 is_static_initializer(pthread_mutex_t m)
-{
-  /* Treat 0 as a static initializer as well (for normal mutexes),
-     to tolerate sloppy code in libgomp. (We should rather fix that code!) */
-  intptr_t v = (intptr_t)m;
-  return v >= -3 && v <= 0;
-/* Should be simple:
-  return (uintptr_t)m >= (uintptr_t)-3; */
+{ 
+  return (uintptr_t)m >= (uintptr_t)-3;
 }
 
 /* Create and return the implementation part of a mutex from a static
