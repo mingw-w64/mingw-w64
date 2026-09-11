e20528ccdef5ecfe1954f542471124562e6b808c
Kirill Makurin <maiddaisuki@outlook.com>
2026-09-11 17:22:24 +0900

    winpthreads: tests: update pthread_mutex/mutex5.c
    Signed-off-by: Kirill Makurin <maiddaisuki@outlook.com>
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- mingw-w64-libraries/winpthreads/tests/pthread_mutex/mutex5.c
+++ mingw-w64-libraries/winpthreads/tests/pthread_mutex/mutex5.c
@@ -1,14 +1,10 @@
 /*
- * mutex5.c
- *
- *
- * --------------------------------------------------------------------------
- *
  *      Pthreads-win32 - POSIX Threads Library for Win32
  *      Copyright(C) 1998 John E. Bossom
  *      Copyright(C) 1999,2005 Pthreads-win32 contributors
+ *      Copyright(C) 2026 mingw-w64 project
  *
- *      Contact Email: rpj@callisto.canberra.edu.au
+ *      Contact Email: mingw-w64-public@lists.sourceforge.net
  *
  *      The current list of contributors is contained
  *      in the file CONTRIBUTORS included with the source
@@ -30,37 +26,34 @@
  *      License along with this library in the file COPYING.LIB;
  *      if not, write to the Free Software Foundation, Inc.,
  *      59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
- *
- * --------------------------------------------------------------------------
- *
- * Confirm the equality/inequality of the various mutex types,
- * and the default not-set value.
  */
 
 #include "test.h"
 
-static pthread_mutexattr_t mxAttr;
-
-/* Prevent optimiser from removing dead or obvious asserts. */
-int _optimiseFoil;
-#define FOIL(x) (_optimiseFoil = x)
+/**
+ * Test Summary:
+ *
+ * Confirm the equality/inequality of `PTHREAD_MUTEX_*` mutex type constants.
+ */
 
 int main(void)
 {
-  int mxType = -1;
+  /**
+   * Mutex types defined by POSIX.
+   */
+  assert(PTHREAD_MUTEX_DEFAULT == PTHREAD_MUTEX_NORMAL);
+  assert(PTHREAD_MUTEX_DEFAULT != PTHREAD_MUTEX_ERRORCHECK);
+  assert(PTHREAD_MUTEX_DEFAULT != PTHREAD_MUTEX_RECURSIVE);
+  assert(PTHREAD_MUTEX_RECURSIVE != PTHREAD_MUTEX_ERRORCHECK);
 
-  assert(FOIL(PTHREAD_MUTEX_DEFAULT) == PTHREAD_MUTEX_NORMAL);
-  assert(FOIL(PTHREAD_MUTEX_DEFAULT) != PTHREAD_MUTEX_ERRORCHECK);
-  assert(FOIL(PTHREAD_MUTEX_DEFAULT) != PTHREAD_MUTEX_RECURSIVE);
-  assert(FOIL(PTHREAD_MUTEX_RECURSIVE) != PTHREAD_MUTEX_ERRORCHECK);
-
-  assert(FOIL(PTHREAD_MUTEX_NORMAL) == PTHREAD_MUTEX_FAST_NP);
-  assert(FOIL(PTHREAD_MUTEX_RECURSIVE) == PTHREAD_MUTEX_RECURSIVE_NP);
-  assert(FOIL(PTHREAD_MUTEX_ERRORCHECK) == PTHREAD_MUTEX_ERRORCHECK_NP);
-
-  assert(pthread_mutexattr_init(&mxAttr) == 0);
-  assert(pthread_mutexattr_gettype(&mxAttr, &mxType) == 0);
-  assert(mxType == PTHREAD_MUTEX_NORMAL);
+  /**
+   * Implementation-defined mutex types.
+   */
+  assert(PTHREAD_MUTEX_FAST_NP == PTHREAD_MUTEX_NORMAL);
+  assert(PTHREAD_MUTEX_TIMED_NP == PTHREAD_MUTEX_NORMAL);
+  assert(PTHREAD_MUTEX_ADAPTIVE_NP == PTHREAD_MUTEX_NORMAL);
+  assert(PTHREAD_MUTEX_RECURSIVE_NP == PTHREAD_MUTEX_RECURSIVE);
+  assert(PTHREAD_MUTEX_ERRORCHECK_NP == PTHREAD_MUTEX_ERRORCHECK);
 
   return 0;
 }
