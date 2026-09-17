mingw-w64/mingw-w64

commit 8c4baed92d4dee4fe1874af860a61aa5c945b6f5

headers: crt: define mbstate_t in corecrt.h
This follows UCRT headers.

Signed-off-by: Zhongteng Gui <dragon-archer@outlook.com>
Signed-off-by: LIU Hao <lh_mouse@126.com>

mingw-w64-headers/crt/corecrt.h
@@ -141,6 +141,16 @@ typedef __time64_t time_t;
 #endif
 #endif /* _TIME_T_DEFINED */
 
+#if defined(_UCRT) || defined(__LARGE_MBSTATE_T)
+  typedef struct _Mbstatet {
+    unsigned long _Wchar;
+    unsigned short _Byte, _State;
+  } _Mbstatet;
+  typedef _Mbstatet mbstate_t;
+#else
+  typedef int mbstate_t;
+#endif
+
 #ifndef _CRT_SECURE_CPP_NOTHROW
 #define _CRT_SECURE_CPP_NOTHROW throw()
 #endif

mingw-w64-headers/crt/wchar.h
@@ -26,15 +26,6 @@ _CRT_BEGIN_C_HEADER
 #define WCHAR_MAX 0xffffU
 #endif
 
-#if defined(_UCRT) || defined(__LARGE_MBSTATE_T)
-  typedef struct _Mbstatet {
-    unsigned long _Wchar;
-    unsigned short _Byte, _State;
-  } _Mbstatet;
-  typedef _Mbstatet mbstate_t;
-#else
-  typedef int mbstate_t;
-#endif
   typedef wchar_t _Wint_t;
 
   _CRTIMP wint_t __cdecl btowc(int);

mingw-w64-headers/crt/yvals.h
@@ -255,10 +255,6 @@ _STD_END
 _C_STD_BEGIN
 _CRTIMP void __cdecl _Atexit(void (__cdecl *)(void));
 
-#if !defined(_UCRT) && !defined(__LARGE_MBSTATE_T)
-typedef int _Mbstatet;
-#endif
-
 #define _ATEXIT_T void
 #define _Mbstinit(x) mbstate_t x = {0}
 _C_STD_END

commit b19001a4b38b633b48f9d709fe3fecd5e9905fd4

headers: corecrt.h: Don't define mbstate_t for Cygwin.
This fixes a regression that was caused by 8c4baed92d4dee4fe1874af860a61aa5c945b6f5.
Cygwin has its own `mbstate_t` which is different from the Windows one.

Signed-off-by: Zhongteng Gui <dragon-archer@outlook.com>
Reference: https://sourceforge.net/p/mingw-w64/mailman/message/59391723/
Signed-off-by: LIU Hao <lh_mouse@126.com>

mingw-w64-headers/crt/corecrt.h
@@ -141,6 +141,7 @@ typedef __time64_t time_t;
 #endif
 #endif /* _TIME_T_DEFINED */
 
+#ifndef __CYGWIN__ /* Cygwin defines mbstate_t themselves */
 #if defined(_UCRT) || defined(__LARGE_MBSTATE_T)
   typedef struct _Mbstatet {
     unsigned long _Wchar;
@@ -149,6 +150,7 @@ typedef __time64_t time_t;
   typedef _Mbstatet mbstate_t;
 #else
   typedef int mbstate_t;
+#endif
 #endif
 
 #ifndef _CRT_SECURE_CPP_NOTHROW